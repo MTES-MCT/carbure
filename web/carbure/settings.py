1855511182bd60801f6ef5c2d53467332085f205
Martin Planes <martin.planes@gmail.com>
2020-01-20 12:42:04 +0100

    [django] install apps

--- web/carbure/settings.py
+++ web/carbure/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'producers',
+    'operators',
+    'administrators'
 ]
 
 MIDDLEWARE = [
