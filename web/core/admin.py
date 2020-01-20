487b0cae41adf3a0278de4e34419bc535b4c24de
Martin Planes <martin.planes@gmail.com>
2020-01-20 15:58:04 +0100

    [models]

--- web/core/admin.py
+++ web/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import *
+
+class EntityAdmin(admin.ModelAdmin):
+    list_display = ('entity_type', 'name', 'parent_entity')
+    search_fields = ('entity_type', 'name')
+    list_filter = ('entity_type',)
+admin.site.register(Entity, EntityAdmin)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user', 'entity')
+    search_fields = ('user', 'entity')
+admin.site.register(PlatformUser, UserAdmin)
+
+
+
