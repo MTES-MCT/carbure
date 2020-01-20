3754b37a639c92b156de3610da25cdf27eafc3cc
Martin Planes <martin.planes@gmail.com>
2020-01-20 10:52:34 +0100

    [django] add static folder

--- web/carbure/settings.py
+++ web/carbure/settings.py
@@ -121,5 +121,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
