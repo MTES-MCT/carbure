5d3ae8af74f927a215dda6dbb8fe3affbf527589
Martin Planes <martin.planes@gmail.com>
2020-01-20 15:58:04 +0100

    [models]

--- web/core/models.py
+++ web/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Entity(models.Model):
+  ENTITY_TYPES = (('Producteur', 'Producteur'), ('Opérateur', 'Opérateur'), ('Administration', 'Administration'), ('Unknown', 'Unknown'))
+
+  name = models.CharField(max_length=64)
+  date_added = models.DateTimeField(auto_now_add=True)
+  entity_type = models.CharField(max_length=64, choices=ENTITY_TYPES, default='Unknown')
+  parent_entity = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    db_table = 'entities'
+    verbose_name = 'Entity'
+    verbose_name_plural = 'Entities'
+
+class PlatformUser(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  date_added = models.DateTimeField(auto_now_add=True)
+  entity = models.ForeignKey(Entity, on_delete=models.CASCADE)
+      
+  def __str__(self):
+    return self.user.email
+
+  class Meta:
+    db_table = 'users'
+    verbose_name = 'User'
+    verbose_name_plural = 'Users'
