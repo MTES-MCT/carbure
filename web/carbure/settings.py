bcf8808c078a3573c63c2ab67d671bebfc96f36b
Martin Planes <martin.planes@gmail.com>
2020-01-20 12:36:53 +0100

    [django] homepage using etalab template

--- web/carbure/settings.py
+++ web/carbure/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'carbure.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.environ['CARBURE_HOME'] + '/web/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
