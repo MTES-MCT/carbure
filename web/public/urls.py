8f28527b6dfe28300d7111b5e70b610a5f3bfcad
Martin Planes <martin.planes@gmail.com>
2020-01-23 12:44:01 +0100

    [web] change public index url

--- web/public/urls.py
+++ web/public/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('htmlreference', views.htmlreference, name='htmlreference'),
 ]
