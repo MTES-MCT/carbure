ddb9f059cebe2274892f4d0ef3534dc85fcf0ad4
Martin Planes <martin.planes@gmail.com>
2020-01-27 11:48:31 +0100

    [web/producers] initial urls and templates

--- web/producers/views.py
+++ web/producers/views.py
@@ -4,5 +4,24 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def producers_index(request):
   context = {}
-  return render(request, 'producers/index.html', context)
+  context['current_url_name'] = 'producers-index'
+  return render(request, 'producers/attestations.html', context)
+
+@login_required
+def producers_inbox(request):
+  context = {}
+  context['current_url_name'] = 'producers-inbox'
+  return render(request, 'producers/inbox.html', context)
+
+@login_required
+def producers_settings(request):
+  context = {}
+  context['current_url_name'] = 'producers-settings'
+  return render(request, 'producers/settings.html', context)
+
+@login_required
+def producers_attestation(request):
+  context = {}
+  context['current_url_name'] = 'producers-attestation'
+  return render(request, 'producers/attestation.html', context)
 
