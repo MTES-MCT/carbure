9202574604f32cac3663949bab5e154063071248
Martin Planes <martin.planes@gmail.com>
2020-01-27 13:30:00 +0100

    [core] decorators

---
+++ web/core/decorators.py
@@ -0,0 +1,17 @@
+from django.core.exceptions import PermissionDenied
+from core.models import Entity, UserDetails, UserRights
+
+def enrich_with_user_details(function):
+    def wrap(request, *args, **kwargs):
+        user_details = UserDetails.objects.get(user=request.user)
+        user_rights = UserRights.objects.filter(user=user_details)
+        context = {}
+        context['user_name'] = user_details.user.name
+        context['nb_entities'] = len(user_rights)
+        context['entities'] = [u.entity for u in user_rights]
+        context['user_entity'] = user_rights[0].entity.name
+        kwargs['context'] = context
+        return function(request, *args, **kwargs)
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
+    return wrap
--- web/producers/views.py
+++ web/producers/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from core.decorators import enrich_with_user_details
+
 
 @login_required
-def producers_index(request):
-  context = {}
+@enrich_with_user_details
+def producers_index(request, *args, **kwargs):
+  context = kwargs['context']
   context['current_url_name'] = 'producers-index'
   return render(request, 'producers/attestations.html', context)
 
 @login_required
+@enrich_with_user_details
 def producers_inbox(request):
   context = {}
   context['current_url_name'] = 'producers-inbox'
   return render(request, 'producers/inbox.html', context)
 
 @login_required
+@enrich_with_user_details
 def producers_settings(request):
   context = {}
   context['current_url_name'] = 'producers-settings'
   return render(request, 'producers/settings.html', context)
 
 @login_required
+@enrich_with_user_details
 def producers_attestation(request):
   context = {}
   context['current_url_name'] = 'producers-attestation'
