2416316fc1bb841419b95dd654cea28ba4ab0375
Martin Planes <martin.planes@gmail.com>
2020-01-27 11:48:31 +0100

    [web/producers] initial urls and templates

--- web/producers/urls.py
+++ web/producers/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.producers_index, name='producers-index'),
+    path('attestation', views.producers_attestation, name='producers-attestation'),
+    path('inbox', views.producers_inbox, name='producers-inbox'),
+    path('settings', views.producers_settings, name='producers-settings'),
 ]
