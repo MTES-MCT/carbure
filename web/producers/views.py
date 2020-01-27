dfb6baadea05089cd2ea1d4203c372abda9a134d
Martin Planes <martin.planes@gmail.com>
2020-01-27 11:21:53 +0100

    [producers] add home page

--- web/producers/views.py
+++ web/producers/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def producers_index(request):
   context = {}
   return render(request, 'producers/index.html', context)
