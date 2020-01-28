de5281c096269e870bf55533e34ded9604e148cc
Martin Planes <martin.planes@gmail.com>
2020-01-28 12:23:37 +0100

    [flake8] fix a few warnings

--- web/core/models.py
+++ web/core/models.py
@@ -3,45 +3,45 @@ from django.conf import settings
 
 
 class Entity(models.Model):
-  ENTITY_TYPES = (('Producteur', 'Producteur'), ('Opérateur', 'Opérateur'), ('Administration', 'Administration'), ('Unknown', 'Unknown'))
+    ENTITY_TYPES = (('Producteur', 'Producteur'), ('Opérateur', 'Opérateur'), 
+                    ('Administration', 'Administration'), ('Unknown', 'Unknown'))
 
-  name = models.CharField(max_length=64)
-  date_added = models.DateTimeField(auto_now_add=True)
-  entity_type = models.CharField(max_length=64, choices=ENTITY_TYPES, default='Unknown')
-  parent_entity = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    date_added = models.DateTimeField(auto_now_add=True)
+    entity_type = models.CharField(max_length=64, choices=ENTITY_TYPES, default='Unknown')
+    parent_entity = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
-  class Meta:
-    db_table = 'entities'
-    verbose_name = 'Entity'
-    verbose_name_plural = 'Entities'
+    class Meta:
+        db_table = 'entities'
+        verbose_name = 'Entity'
+        verbose_name_plural = 'Entities'
 
 class UserDetails(models.Model):
-  USER_TYPES = (('Producteur', 'Producteur'), ('Opérateur', 'Opérateur'), ('Administrateur', 'Administrateur'))
-  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-  date_added = models.DateTimeField(auto_now_add=True)
-  user_type = models.CharField(max_length=64, choices=USER_TYPES, default='Producteur')
+    USER_TYPES = (('Producteur', 'Producteur'), ('Opérateur', 'Opérateur'), ('Administrateur', 'Administrateur'))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
+    user_type = models.CharField(max_length=64, choices=USER_TYPES, default='Producteur')
 
-  def __str__(self):
-    return self.user.email
+    def __str__(self):
+        return self.user.email
 
-  class Meta:
-    db_table = 'users'
-    verbose_name = 'User'
-    verbose_name_plural = 'Users'
-
+    class Meta:
+        db_table = 'users'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
 
 class UserRights(models.Model):
-  entity = models.ForeignKey(Entity, on_delete=models.CASCADE)
-  user = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
-  date_added = models.DateTimeField(auto_now_add=True)
+    entity = models.ForeignKey(Entity, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserDetails, on_delete=models.CASCADE)
+    date_added = models.DateTimeField(auto_now_add=True)
 
-  def __str__(self):
-    return '%s - %s' % (self.user.user.email, self.entity.name)
+    def __str__(self):
+        return '%s - %s' % (self.user.user.email, self.entity.name)
 
-  class Meta:
-    db_table = 'users_rights'
-    verbose_name = 'User Right'
-    verbose_name_plural = 'Users Rights'
+    class Meta:
+        db_table = 'users_rights'
+        verbose_name = 'User Right'
+        verbose_name_plural = 'Users Rights'
