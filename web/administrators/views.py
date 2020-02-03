21a83f351b7697f742faf259cf53cea77ff01d86
Martin Planes <martin.planes@gmail.com>
2020-02-03 19:17:23 +0100

    [templates] ajout template home page administrateur

--- web/administrators/views.py
+++ web/administrators/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from core.decorators import enrich_with_user_details
 from django.shortcuts import render
 
-def administrators_index(request):
-  context = {}
-  return render(request, 'administrators/index.html', context)
-
+@login_required
+@enrich_with_user_details
+def administrators_index(request, *args, **kwargs):
+  context = kwargs['context']
+  context['current_url_name'] = 'administrators_index-index'
+  return render(request, 'administrators/attestations_admin.html', context)
