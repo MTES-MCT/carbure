ff04a672731b174c559079262675c4e9f25d13ed
Martin Planes <martin.planes@gmail.com>
2020-02-10 13:37:26 +0100

    [operators] draft

--- web/operators/urls.py
+++ web/operators/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.operators_index, name='operators-index'),
+    path('new', views.operators_new_lots, name='operators-new-lots'),
+    path('pending', views.operators_pending_lots, name='operators-pending-lots'),
+    path('settings', views.operators_settings, name='operators-settings'),
 ]
