49b4248c0c0c79f7c5a9e82516de7ac3086edd2d
Martin Planes <martin.planes@gmail.com>
2020-02-10 13:37:26 +0100

    [operators] draft

--- web/operators/views.py
+++ web/operators/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from core.decorators import enrich_with_user_details
 
-def operators_index(request):
-  context = {}
-  return render(request, 'operators/index.html', context)
+@login_required
+@enrich_with_user_details
+def operators_index(request, *args, **kwargs):
+  context = kwargs['context']
+  context['current_url_name'] = 'operators-index'
+  return render(request, 'operators/lots.html', context)
 
+@login_required
+@enrich_with_user_details
+def operators_new_lots(request, *args, **kwargs):
+  context = kwargs['context']
+  context['current_url_name'] = 'operators-new-lots'
+  return render(request, 'operators/new_lots.html', context)
+
+@login_required
+@enrich_with_user_details
+def operators_pending_lots(request, *args, **kwargs):
+  context = kwargs['context']
+  context['current_url_name'] = 'operators-pending-lots'
+  return render(request, 'operators/pending_lots.html', context)
+
+@login_required
+@enrich_with_user_details
+def operators_settings(request, *args, **kwargs):
+  context = kwargs['context']
+  context['current_url_name'] = 'operators-settings'
+  return render(request, 'operators/settings.html', context)
