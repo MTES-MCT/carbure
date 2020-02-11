bf189705d5578675fbeafb4197609fa922f682e6
Martin Planes <martin.planes@gmail.com>
2020-02-11 09:01:08 +0100

    [administrators]

--- web/administrators/urls.py
+++ web/administrators/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.administrators_index, name='administrators-index'),
+    path('export', views.administrators_export, name='administrators-export'),
+    path('controles', views.administrators_controles, name='administrators-controles'),
+    path('settings', views.administrators_settings, name='administrators-settings'),
 ]
--- web/administrators/views.py
+++ web/administrators/views.py
@@ -6,5 +6,28 @@ from django.shortcuts import render
 @enrich_with_user_details
 def administrators_index(request, *args, **kwargs):
   context = kwargs['context']
-  context['current_url_name'] = 'administrators_index-index'
-  return render(request, 'administrators/attestations_admin.html', context)
+  context['current_url_name'] = 'administrators-index'
+  return render(request, 'administrators/lots.html', context)
+
+
+@login_required
+@enrich_with_user_details
+def administrators_export(request, *args, **kwargs):
+  context = kwargs['context']
+  context['current_url_name'] = 'administrators-export'
+  return render(request, 'administrators/export.html', context)
+
+@login_required
+@enrich_with_user_details
+def administrators_controles(request, *args, **kwargs):
+  context = kwargs['context']
+  context['current_url_name'] = 'administrators-controles'
+  return render(request, 'administrators/controles.html', context)
+
+
+@login_required
+@enrich_with_user_details
+def administrators_settings(request, *args, **kwargs):
+  context = kwargs['context']
+  context['current_url_name'] = 'administrators-settings'
+  return render(request, 'administrators/settings.html', context)
