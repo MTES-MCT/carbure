483244f7bb3d6849bee1f77b8a7037694aa5cff6
Martin Planes <martin.planes@gmail.com>
2020-02-11 11:04:42 +0100

    [administrators] ajout page notifications

--- web/administrators/urls.py
+++ web/administrators/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.administrators_index, name='administrators-index'),
     path('export', views.administrators_export, name='administrators-export'),
     path('controles', views.administrators_controles, name='administrators-controles'),
+    path('notifications', views.administrators_notifications, name='administrators-notifications'),
     path('settings', views.administrators_settings, name='administrators-settings'),
 ]
