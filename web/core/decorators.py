199c4bc12c3c0edc832504eb0d25759479c06a82
Martin Planes <martin.planes@gmail.com>
2020-02-17 12:49:33 +0100

    [auth] restrict apps access

--- web/core/decorators.py
+++ web/core/decorators.py
@@ -1,5 +1,5 @@
 from core.models import UserRights
-
+from django.core.exceptions import PermissionDenied
 
 def enrich_with_user_details(function):
     def wrap(request, *args, **kwargs):
@@ -8,9 +8,44 @@ def enrich_with_user_details(function):
         context['user_name'] = request.user.name
         context['nb_entities'] = len(user_rights)
         context['entities'] = [u.entity for u in user_rights]
-        context['user_entity'] = user_rights[0].entity.name
+        if len(user_rights) == 0:
+            raise PermissionDenied
+        context['user_entity'] = user_rights[0].entity
+        context['user_entity_name'] = user_rights[0].entity.name
         kwargs['context'] = context
         return function(request, *args, **kwargs)
     wrap.__doc__ = function.__doc__
     wrap.__name__ = function.__name__
     return wrap
+
+def restrict_to_producers(function):
+    def wrap(request, *args, **kwargs):
+        context = kwargs['context']
+        if context['user_entity'].entity_type != 'Producteur':
+            raise PermissionDenied
+        return function(request, *args, **kwargs)
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
+    return wrap
+
+
+def restrict_to_operators(function):
+    def wrap(request, *args, **kwargs):
+        context = kwargs['context']
+        if context['user_entity'].entity_type != 'Opérateur':
+            raise PermissionDenied
+        return function(request, *args, **kwargs)
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
+    return wrap
+
+
+def restrict_to_administrators(function):
+    def wrap(request, *args, **kwargs):
+        context = kwargs['context']
+        if context['user_entity'].entity_type != 'Administration':
+            raise PermissionDenied
+        return function(request, *args, **kwargs)
+    wrap.__doc__ = function.__doc__
+    wrap.__name__ = function.__name__
+    return wrap        
