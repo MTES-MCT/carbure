31c5c94ac1529df6f7be69314f43532052154d96
Martin Planes <martin.planes@gmail.com>
2020-02-23 14:58:58 +0100

    [docker]

---
+++ web/migration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# database model migration
+python3 /app/web/manage.py migrate --noinput
+
+# 'static' fixtures (countries, filiere de production, types de biocarburants...)
+python3 /app/web/manage.py loaddata /app/web/fixtures/countries.json
+
+
+# test data: only load in dev environment
+if [ "$IMAGE_TAG" = "dev" ] ;
+then
+    python3 /app/web/manage.py loaddata /app/web/fixtures/authtools_user.json
+    python3 /app/web/manage.py loaddata /app/web/fixtures/entities.json
+    python3 /app/web/manage.py loaddata /app/web/fixtures/userrights.json
+    python3 /app/web/manage.py loaddata /app/web/fixtures/userpreferences.json
+fi
