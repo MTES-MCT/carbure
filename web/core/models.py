d9b85e9ba9f857d6edbb84f5912f48f5b1f0cd5b
Martin Planes <martin.planes@gmail.com>
2020-02-23 12:31:33 +0100

    [models]

--- web/core/models.py
+++ web/core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from django.utils import timezone
 
 class Entity(models.Model):
     ENTITY_TYPES = (('Producteur', 'Producteur'), ('Opérateur', 'Opérateur'),
@@ -32,3 +32,70 @@ class UserRights(models.Model):
         db_table = 'users_rights'
         verbose_name = 'User Right'
         verbose_name_plural = 'Users Rights'
+
+class TypeBioCarburant(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    date_added = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'type_biocarburants'
+        verbose_name = 'Type de Biocarburant'
+        verbose_name_plural = 'Types de Biocarburants'
+
+class FiliereProduction(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    date_added = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'filieres_production'
+        verbose_name = 'Filiere de Production'
+        verbose_name_plural = 'Filieres de Production'
+
+class Pays(models.Model):
+    code_pays = models.CharField(max_length=64)
+    name = models.CharField(max_length=128)
+    date_added = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'pays'
+        verbose_name = 'Pays'
+        verbose_name_plural = 'Pays'
+
+class Lot(models.Model):
+    LOT_STATUS = (('Draft', 'Draft'), ('Validated', 'Validated'), ('Declared', 'Declared'))
+
+    depot = models.CharField(max_length=64, blank=True)
+    num_dae = models.CharField(max_length=64, blank=True)
+    date_entree = models.DateField(blank=True)
+    volume = models.IntegerField(default=0)
+    type_biocarburant = models.ForeignKey(TypeBioCarburant, null=True), 
+    filiere_production = models.ForeignKey(FiliereBiocarburant, null=True)
+    categorie = models.CharField(max_length=64)
+    systeme_fournisseur = models.CharField(max_length=64)
+    pays_origine = models.ForeignKey(Pays, null=True)
+    respect_crit_durabilite = models.BooleanField(default=False)
+    ges_transport = models.FloatField(blank=True)
+    ges_distribution = models.FloatField(blank=True)
+    ges_fossile = models.FloatField(blank=True)
+    pays_production = models.ForeignKey(PaysOrigine)
+    date_mise_en_service = models.DateField(blank=True)
+    status = models.CharField(max_length=64, choices=LOT_STATUS, default='Draft')
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        db_table = 'lots'
+        verbose_name = 'Lot'
+        verbose_name_plural = 'Lots'
