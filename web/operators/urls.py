db3c8dcae2472a8aa227b304011f01aa38504381
Martin Planes <martin.planes@gmail.com>
2020-02-26 10:02:06 +0100

    [operators]

--- web/operators/urls.py
+++ web/operators/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.operators_index, name='operators-index'),
     path('declaration', views.operators_declaration, name='operators-declaration'),
+    path('lot', views.operators_lot, name='operators-lot'),
     path('export', views.operators_export, name='operators-export'),
     path('annuaire', views.operators_annuaire, name='operators-annuaire'),
     path('new', views.operators_new_lots, name='operators-new-lots'),
