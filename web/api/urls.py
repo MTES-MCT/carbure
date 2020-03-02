94663f643749faf6ed3937cbb04decc56f6016f5
Martin Planes <martin.planes@gmail.com>
2020-03-02 15:35:13 +0100

    [producers] loading lots from api

--- web/api/urls.py
+++ web/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('lot/validate', views.lot_validate, name='api-lot-validate'),
     path('lot/save', views.lot_save, name='api-lot-save'),
+    path('producers/sample-lots', views.producers_sample_lots, name='api-producers-sample-lots'),
 ]
