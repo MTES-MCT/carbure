d14569a8f73a2d3e3392bb2beb8d89cb22f1e3e5
Martin Planes <martin.planes@gmail.com>
2020-03-02 11:04:57 +0100

    [emails]

--- web/carbure/settings.py
+++ web/carbure/settings.py
@@ -137,3 +137,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'index'
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
