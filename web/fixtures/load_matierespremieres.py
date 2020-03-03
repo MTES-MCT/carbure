9e8b98c4e02da3b094757125e1c76f50045c4559
Martin Planes <martin.planes@gmail.com>
2020-03-03 13:17:17 +0100

    [models&fixtures]

--- web/fixtures/load_matierespremieres.py
+++ web/fixtures/load_matierespremieres.py
@@ -5,13 +5,13 @@ import csv
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carbure.settings")
 django.setup()
 
-from core.models import TypeBiocarburant 
+from core.models import MatierePremiere
 
-filename = '%s/web/fixtures/biocarburants.csv' % (os.environ['CARBURE_HOME'])
+filename = '%s/web/fixtures/matierespremieres.csv' % (os.environ['CARBURE_HOME'])
 
 with open(filename) as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
     for row in reader:
         name = row[0]
-        obj, created = TypeBiocarburant.objects.update_or_create(name=name, defaults={'description':''})
+        obj, created = MatierePremiere.objects.update_or_create(name=name, defaults={'description':''})
 
