3c038ea47daea37a44245bf2a7a6068131ebcaa6
Martin Planes <martin.planes@gmail.com>
2020-03-03 13:27:01 +0100

    [models] add production site

--- web/producers/models.py
+++ web/producers/models.py
@@ -14,3 +14,17 @@ class AttestationProducer(models.Model):
         db_table = 'producer_attestations'
         verbose_name = 'Attestation de Durabilité'
         verbose_name_plural = 'Attestations de Durabilité'	
+
+class ProductionSite(models.Model):
+    producer = models.ForeignKey(Entity, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64, blank=False, null=False)
+    identifiant = models.CharField(max_length=64, blank=False, null=False)
+    num_accise = models.CharField(max_length=64, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'producer_sites'
+        verbose_name = 'Site de Production'
+        verbose_name_plural = 'Sites de Production'	
