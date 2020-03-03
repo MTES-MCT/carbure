99d66f41dc104483872000f5f41a22ae3f3d5459
Martin Planes <martin.planes@gmail.com>
2020-03-03 17:54:12 +0100

    [api] autocomplete public data

--- web/api/urls.py
+++ web/api/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	# public, autocomplete api
+	path('type-biocarburant-autocomplete/', views.type_biocarburant_autocomplete, name='type-biocarburant-autocomplete'),
+	path('matiere-premiere-autocomplete/', views.matiere_premiere_autocomplete, name='matiere-premiere-autocomplete'),
+	path('pays-autocomplete/', views.pays_autocomplete, name='pays-autocomplete'),
     path('lot/validate', views.lot_validate, name='api-lot-validate'),
     path('lot/save', views.lot_save, name='api-lot-save'),
     path('producers/sample-lots', views.producers_sample_lots, name='api-producers-sample-lots'),
--- web/api/views.py
+++ web/api/views.py
@@ -1,6 +1,32 @@
 from django.contrib.auth.decorators import login_required
 from core.decorators import enrich_with_user_details, restrict_to_producers
 from django.http import JsonResponse
+import json
+from core.models import TypeBiocarburant, MatierePremiere
+
+def type_biocarburant_autocomplete(request):
+  q = request.GET.get('q', '')
+  types = TypeBiocarburant.objects.filter(name__icontains=q)
+  results = [{'name':i.name, 'description':i.description} for i in types]
+  data = json.dumps(results)
+  mimetype = 'application/json'
+  return HttpResponse(data, mimetype)
+
+def matiere_premiere_autocomplete(request):
+  q = request.GET.get('q', '')
+  mps = MatierePremiere.objects.filter(name__icontains=q)
+  results = [{'name':i.name, 'description':i.description} for i in mps]
+  data = json.dumps(results)
+  mimetype = 'application/json'
+  return HttpResponse(data, mimetype)
+
+def pays_autocomplete(request):
+  q = request.GET.get('q', '')
+  countries = Pays.objects.filter(name__icontains=q)
+  results = [{'name':i.name, 'code_pays':i.code_pays} for i in countries]
+  data = json.dumps(results)
+  mimetype = 'application/json'
+  return HttpResponse(data, mimetype)
 
 @login_required
 @enrich_with_user_details
