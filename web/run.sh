a6325815ea7fb749680ade5e181e1c764fad8093
Martin Planes <martin.planes@gmail.com>
2020-03-03 13:18:59 +0100

    [fixtures]

--- web/run.sh
+++ web/run.sh
@@ -6,6 +6,8 @@ python3 /app/web/manage.py migrate --noinput
 
 # 'static' fixtures (countries, filiere de production, types de biocarburants...)
 python3 /app/web/manage.py loaddata /app/web/fixtures/countries.json
+python3 /app/web/manage.py loaddata /app/web/fixtures/biocarburants.json
+python3 /app/web/manage.py loaddata /app/web/fixtures/matierespremieres.json
 
 # test data: only load in dev environment
 if [ "$IMAGE_TAG" = "dev" ] ;
