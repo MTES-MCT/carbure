92b261b01bd2630fb707210c8baf9a48d6d67f62
Martin Planes <martin.planes@gmail.com>
2020-03-03 13:27:01 +0100

    [models] add production site

--- web/producers/admin.py
+++ web/producers/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from producers.models import AttestationProducer
+from producers.models import AttestationProducer, ProductionSite
 
 
 class AttestationProducerAdmin(admin.ModelAdmin):
@@ -9,3 +9,11 @@ class AttestationProducerAdmin(admin.ModelAdmin):
     list_filter = ('producer',)
 
 admin.site.register(AttestationProducer, AttestationProducerAdmin)
+
+
+class ProductionSiteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'identifiant', 'num_accise')
+    search_fields = ('name', 'identifiant', 'num_accise', 'producer')
+    list_filter = ('producer',)
+
+admin.site.register(ProductionSite, ProductionSiteAdmin)
