b518f4b2378317effa36fc769347fae350ec8764
Martin Planes <martin.planes@gmail.com>
2020-03-12 11:01:19 +0100

    [core] lot model update

--- web/api/views.py
+++ web/api/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from core.decorators import enrich_with_user_details, restrict_to_producers
 from django.http import JsonResponse, HttpResponse
 import json
-from core.models import TypeBiocarburant, MatierePremiere, Pays
+from core.models import TypeBiocarburant, MatierePremiere, Pays, Lot
 
 def type_biocarburant_autocomplete(request):
   q = request.GET.get('q', '')
@@ -45,43 +45,5 @@ def lot_validate(request, *args, **kwargs):
 @restrict_to_producers
 def producers_sample_lots(request, *args, **kwargs):
   context = kwargs['context']
-  sample_lots = [
-    {'num_dae':"19FRG0432000482724020", 'site': "GRANDPUITS", "date_entree": "1/10/2019", 'volume':	"28,687",
-	 'type':"EMHV", 'matiere_premiere': "Colza", 'systeme_fournisseur':	"SV-2BS010010", 'pays_origine': "FR",
-	 'ges_transport_distribution':"1.00", 'ges_total':"34.12", 'ges_fossile':"83.80", 'ges_reductions':"59.28%",
-	 'pays_implantation': "FR", 'date_mise_en_service':"1/7/2007", 'statut': "Validé", 'client':"OperateurGamma",
-	 'checkbox': """<input type="checkbox" />"""
-    },
-     {'num_dae':"19FRG0432000482724020", 'site': "GRANDPUITS", "date_entree": "1/10/2019", 'volume':	"28,687",
-	 'type':"EMHV", 'matiere_premiere': "Blé", 'systeme_fournisseur':	"SV-2BS010010", 'pays_origine': "FR",
-	 'ges_transport_distribution':"1.00", 'ges_total':"34.12", 'ges_fossile':"83.80", 'ges_reductions':"59.28%",
-	 'pays_implantation': "FR", 'date_mise_en_service':"1/7/2007", 'statut': "Validé", 'client':"OperateurGamma",
-	 'checkbox': """<input type="checkbox" />"""
-    },
-     {'num_dae':"19FRG0432000482724020", 'site': "GRANDPUITS", "date_entree": "1/10/2019", 'volume':	"2,687",
-	 'type':"EMHV", 'matiere_premiere': "Blé", 'systeme_fournisseur':	"SV-2BS010010", 'pays_origine': "MY",
-	 'ges_transport_distribution':"1.00", 'ges_total':"34.12", 'ges_fossile':"83.80", 'ges_reductions':"59.28%",
-	 'pays_implantation': "FR", 'date_mise_en_service':"1/7/2007", 'statut': "Brouillon", 'client':"OperateurGamma",
-	 'checkbox': """<input type="checkbox" />"""
-    },
-     {'num_dae':"19FRG0432000482724020", 'site': "GRANDPUITS", "date_entree": "1/10/2019", 'volume':	"128,687",
-	 'type':"EMHV", 'matiere_premiere': "Colza", 'systeme_fournisseur':	"SV-2BS010010", 'pays_origine': "FR",
-	 'ges_transport_distribution':"1.00", 'ges_total':"34.12", 'ges_fossile':"83.80", 'ges_reductions':"59.28%",
-	 'pays_implantation': "FR", 'date_mise_en_service':"1/7/2007", 'statut': "Brouillon", 'client':"OperateurGamma",
-	 'checkbox': """<input type="checkbox" />"""
-    },
-     {'num_dae':"19FRG0432000482724020", 'site': "GRANDPUITS", "date_entree": "1/10/2019", 'volume':	"28,687",
-	 'type':"EMHV", 'matiere_premiere': "Colza", 'systeme_fournisseur':	"SV-2BS010010", 'pays_origine': "FR",
-	 'ges_transport_distribution':"1.00", 'ges_total':"34.12", 'ges_fossile':"83.80", 'ges_reductions':"59.28%",
-	 'pays_implantation': "FR", 'date_mise_en_service':"1/7/2007", 'statut': "Validé", 'client':"OperateurGamma",
-	 'checkbox': """<input type="checkbox" />"""
-    },
-     {'num_dae':"19FRG0432000482724020", 'site': "GRANDPUITS", "date_entree": "1/10/2019", 'volume':	"28,687",
-	 'type':"EMHV", 'matiere_premiere': "Colza", 'systeme_fournisseur':	"SV-2BS010010", 'pays_origine': "FR",
-	 'ges_transport_distribution':"1.00", 'ges_total':"34.12", 'ges_fossile':"83.80", 'ges_reductions':"59.28%",
-	 'pays_implantation': "FR", 'date_mise_en_service':"1/7/2007", 'statut': "Validé", 'client':"OperateurGamma",
-	 'checkbox': """<input type="checkbox" />"""
-    },
-   ]
-
+  sample_lots = Lot.objects.all()
   return JsonResponse({'data':sample_lots})
