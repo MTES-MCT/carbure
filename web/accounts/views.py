9f7d2e770921f897b553ace5401cc0f795f040a0
Martin Planes <martin.planes@gmail.com>
2020-03-13 16:53:16 +0100

    [accounts]

--- web/accounts/views.py
+++ web/accounts/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from core.decorators import enrich_with_user_details
 
 @login_required
-def profile(request):
-    return render(request, "accounts/profile.html")
+@enrich_with_user_details
+def profile(request, *args, **kwargs):
+    context = kwargs['context']
+    return render(request, "accounts/profile.html", context)
 
