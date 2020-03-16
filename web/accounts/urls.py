59e6e7908ea3b3be093db731a1f4dd9e621265a2
Martin Planes <martin.planes@gmail.com>
2020-03-16 11:00:39 +0100

    [accounts] password change

--- web/accounts/urls.py
+++ web/accounts/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile', views.profile, name='profile'),    
+    path('profile', views.profile, name='profile'),
+    path('password_reset', views.custom_password_reset, name='custom_password_reset'),
+    path('password_reset_success', views.custom_password_reset_success, name='custom_password_reset_success'),            
 ]
--- web/accounts/views.py
+++ web/accounts/views.py
@@ -10,3 +10,19 @@ def profile(request, *args, **kwargs):
     context = kwargs['context']
     return render(request, "accounts/profile.html", context)
 
+@login_required
+@enrich_with_user_details
+def custom_password_reset(request, *args, **kwargs):
+    context = kwargs['context']
+    if request.method == 'POST':
+        request.user.set_password(request.POST['new_password1'])
+        request.user.save()
+        redirect('custom_password_reset_success')
+    return render(request, "accounts/password_reset.html", context)
+
+@login_required
+@enrich_with_user_details
+def custom_password_reset_success(request, *args, **kwargs):
+    context = kwargs['context']
+    return render(request, "accounts/password_reset_done.html", context)
+
