059b1b2178c04d86d5ca5cfceb7300c7f0594a80
Martin Planes <martin.planes@gmail.com>
2020-03-16 16:41:45 +0100

    [administrators] gestion utilisateurs

--- web/administrators/urls.py
+++ web/administrators/urls.py
@@ -14,4 +14,9 @@ urlpatterns = [
     # api-style urls
     path('suivi-certificats/validate/<int:id>', views.administrators_validate_certificate, name='administrators-validate-certificate'),
 
+
+    path('users/add-entity', views.administrators_add_entity, name='administrators-add-entity'),
+    path('users/add-user', views.administrators_add_user, name='administrators-add-user'),
+    path('users/add-right', views.administrators_add_right, name='administrators-add-right'),
 ]
+
