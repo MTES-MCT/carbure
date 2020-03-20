0a1daef04044a0f8f08c0f776a0eefddda0b292b
Martin Planes <martin.planes@gmail.com>
2020-03-20 16:13:20 +0100

    [fixtures] remove useless fixtures

--- web/run.sh
+++ web/run.sh
@@ -9,18 +9,4 @@ python3 /app/web/manage.py loaddata /app/web/fixtures/countries.json
 python3 /app/web/manage.py loaddata /app/web/fixtures/biocarburants.json
 python3 /app/web/manage.py loaddata /app/web/fixtures/matierespremieres.json
 
-# test data: only load in dev environment
-if [ "$IMAGE_TAG" = "dev" ] ;
-then
-    python3 /app/web/manage.py loaddata /app/web/fixtures/authtools_user.json
-    python3 /app/web/manage.py loaddata /app/web/fixtures/entities.json
-    python3 /app/web/manage.py loaddata /app/web/fixtures/userrights.json
-    python3 /app/web/manage.py loaddata /app/web/fixtures/userpreferences.json
-
-    python3 /app/web/manage.py loaddata /app/web/fixtures/productionsites.json
-    python3 /app/web/manage.py loaddata /app/web/fixtures/productionsiteinputs.json
-    python3 /app/web/manage.py loaddata /app/web/fixtures/productionsiteoutputs.json
-    python3 /app/web/manage.py loaddata /app/web/fixtures/producercertificates.json
-fi
-
 uwsgi --ini /app/web/carbure_uwsgi.ini --touch-reload=/app/web/carbure_uwsgi.ini
