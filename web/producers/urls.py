c34cffb8871f3d27ed47d868b53f942446de6b9b
Martin Planes <martin.planes@gmail.com>
2020-03-20 14:18:32 +0100

    [producers]

--- web/producers/urls.py
+++ web/producers/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('export', views.producers_export, name='producers-export'),
     path('attestation/<int:attestation_id>', views.producers_attestation, name='producers-attestation'),
     path('attestation/<int:attestation_id>/lot/new', views.producers_new_lot, name='producers-attestation-new-lot'),
-    path('attestation/<int:attestation_id>/lot/save', views.producers_save_lot, name='producers-attestation-save-lot'),
     #path('attestation/<int:attestation_id>/lot/<int:lot_id>/edit', views.producers_edit_lot, name='producers-attestation-edit-lot'),
 
 
@@ -16,8 +15,4 @@ urlpatterns = [
 
     # settings
     path('settings', views.producers_settings, name='producers-settings'),
-    path('settings/add-site', views.producers_settings_add_site, name='producers-settings-add-site'),
-    path('settings/add-certif', views.producers_settings_add_certif, name='producers-settings-add-certif'),
-    path('settings/add-mp', views.producers_settings_add_mp, name='producers-settings-add-mp'),
-    path('settings/add-biocarburant', views.producers_settings_add_biocarburant, name='producers-settings-add-biocarburant'),
 ]
