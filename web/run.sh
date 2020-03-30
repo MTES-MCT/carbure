9acc774e0477160d5f729c4bb1b702f646421d2e
Martin Planes <martin.planes@gmail.com>
2020-03-30 16:04:03 +0200

    [fixtures]

--- web/run.sh
+++ web/run.sh
@@ -5,9 +5,10 @@
 python3 /app/web/manage.py migrate --noinput
 
 # 'static' fixtures (countries, filiere de production, types de biocarburants...)
-python3 /app/web/manage.py loaddata /app/web/fixtures/countries.json
-python3 /app/web/manage.py loaddata /app/web/fixtures/biocarburants.json
-python3 /app/web/manage.py loaddata /app/web/fixtures/matierespremieres.json
-python3 /app/web/manage.py loaddata /app/web/fixtures/ghgvalues.json
+python3 /app/web/fixtures/load_biocarburants.py
+python3 /app/web/fixtures/load_countries.py
+python3 /app/web/fixtures/load_matierespremieres.py
+python3 /app/web/fixtures/load_ghg_values.py
+
 
 uwsgi --ini /app/web/carbure_uwsgi.ini --touch-reload=/app/web/carbure_uwsgi.ini
