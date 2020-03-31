d1fe5560aab211b7c812101c75d046a4e512c8f0
Martin Planes <martin.planes@gmail.com>
2020-03-31 13:18:08 +0200

    [backup]

--- scripts/backup/backup_prod_db.sh
+++ scripts/backup/backup_prod_db.sh
@@ -4,9 +4,9 @@
 mysqldump -h $DJANGO_DB_HOST -u $DJANGO_DB_USER -p"$DJANGO_DB_PASSWORD" $DJANGO_DATABASE > /tmp/backup-$(date +\%F).sql
 
 # upload db
-python /app/scripts/backup/s3backup.py -b carbure.database -f /tmp/backup-$(date +\%F).sql
+python3 /app/scripts/backup/s3backup.py -b carbure.database -f /tmp/backup-$(date +\%F).sql
 
 # cleanup s3 bucket
-python /app/scripts/backup/s3cleanup.py -b carbure.database
+python3 /app/scripts/backup/s3cleanup.py -b carbure.database
 
 echo "OK"
