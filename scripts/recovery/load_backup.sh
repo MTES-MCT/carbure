f784f60f719619840b30abeab0b810a751a7ba0e
Martin Planes <martin.planes@gmail.com>
2020-03-31 16:01:51 +0200

    [recovery]

--- scripts/recovery/load_backup.sh
+++ scripts/recovery/load_backup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ "$ENV" = "dev" ] || [ "$ENV" = "staging" ]
+then
+    mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /tmp/backup.sql
+else
+    echo "This script can only run in dev or staging environments"
+fi
 
-mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /tmp/backup.sql
+
