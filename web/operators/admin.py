aef9a14aa3887013470228d7b50263e7fa64b99e
Martin Planes <martin.planes@gmail.com>
2020-04-01 11:23:57 +0200

    [operators] model

--- web/operators/admin.py
+++ web/operators/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from operators.models import OperatorDeclaration
+
+
+class OperatorDeclarationAdmin(admin.ModelAdmin):
+    list_display = ('period', 'operator', 'deadline')
+    search_fields = ('period', 'operator')
+    list_filter = ('operator',)
+
+admin.site.register(OperatorDeclaration, OperatorDeclarationAdmin)
+
--- web/operators/models.py
+++ web/operators/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from core.models import Entity
+
+class OperatorDeclaration(models.Model):
+    operator = models.ForeignKey(Entity, on_delete=models.CASCADE)
+    period = models.CharField(max_length=7, blank=False, null=False)
+    deadline = models.DateField(blank=False, null=False)
+
+    def __str__(self):
+        return '%s - %s' % (self.period, self.operator.name)
+
+    class Meta:
+        db_table = 'operator_declaration'
+        verbose_name = 'Déclaration de Durabilité'
+        verbose_name_plural = 'Déclarations de Durabilité'
