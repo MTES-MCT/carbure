771d1f7261bdcd919a97badd570674dafceb1aca
Martin Planes <martin.planes@gmail.com>
2020-04-01 15:14:49 +0200

    [operators] remove unused page

--- web/operators/urls.py
+++ web/operators/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.operators_index, name='operators-index'),
     path('declaration/<int:declaration_id>', views.operators_declaration, name='operators-declaration'),
-    path('lot', views.operators_lot, name='operators-lot'),
     path('affiliations', views.operators_affiliations, name='operators-affiliations'),
     path('controles', views.operators_controles, name='operators-controles'),
     path('settings', views.operators_settings, name='operators-settings'),
