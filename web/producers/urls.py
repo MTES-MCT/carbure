0d0f875732fb8a7dbcbc2ccb91e49bf968e7131f
Martin Planes <martin.planes@gmail.com>
2020-04-15 10:58:03 +0200

    [operators] add entity in url

--- web/producers/urls.py
+++ web/producers/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('<slug:producer_name>/attestation/<slug:attestation_period>', views.producers_attestation, name='producers-attestation'),
     path('<slug:producer_name>/corrections', views.producers_corrections, name='producers-corrections'),
     path('<slug:producer_name>/controles', views.producers_controles, name='producers-controles'),
-    # settings
     path('<slug:producer_name>/settings', views.producers_settings, name='producers-settings'),
 ]
