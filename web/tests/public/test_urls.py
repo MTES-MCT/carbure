a80c89c7a47a3217c8e5c8518c0b55e06400ca4d
Martin Planes <martin.planes@gmail.com>
2020-04-17 12:00:49 +0200

    [tests]

---
+++ web/tests/public/test_urls.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+from producers.urls import urlpatterns
+from django.contrib.auth import get_user_model
+
+from core.models import Entity, UserRights
+from producers.models import *
+
+import datetime
+
+class PublicUrlsTest(TestCase):
+    def test_index(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
