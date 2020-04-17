20457fd6a648326bfe8f04f135c341139ae74c27
Martin Planes <martin.planes@gmail.com>
2020-04-17 11:41:54 +0200

    [tests]

--- web/tests/producers/test_urls.py
+++ web/tests/producers/test_urls.py
@@ -4,19 +4,37 @@ from producers.urls import urlpatterns
 from django.contrib.auth import get_user_model
 
 from core.models import Entity, UserRights
+from producers.models import *
 
-class ProducersTest(TestCase):
+import datetime
+
+class ProducersUrlsTest(TestCase):
     def setUp(self):
         user_model = get_user_model()
         self.producer = user_model.objects.create_user(email='testproducer@almalexia.org', name='MP TEST', password='totopouet42')
         self.entity, created = Entity.objects.update_or_create(name='BioRaf1', entity_type='Producteur')
         right, created = UserRights.objects.update_or_create(user=self.producer, entity=self.entity)
-
-    def test_login(self):
         self.client.login(username='testproducer@almalexia.org', password='totopouet42')
 
-        for url in urlpatterns:
-            print(url)
-            response = self.client.get(reverse(url.name, kwargs={'producer_name':self.entity.url_friendly_name()}))
-            self.assertEqual(response.status_code, 200)
+    def test_index(self):
+        response = self.client.get(reverse('producers-index', kwargs={'producer_name':self.entity.url_friendly_name()}))
+        self.assertEqual(response.status_code, 200)
+
+        # then fetch the latest attestation
+        today = datetime.date.today()
+        period = today.strftime('%Y-%m')
+        response = self.client.get(reverse('producers-attestation', kwargs={'producer_name':self.entity.url_friendly_name(), 'attestation_period':period}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_settings(self):
+        response = self.client.get(reverse('producers-settings', kwargs={'producer_name':self.entity.url_friendly_name()}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_corrections(self):
+        response = self.client.get(reverse('producers-corrections', kwargs={'producer_name':self.entity.url_friendly_name()}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_controles(self):
+        response = self.client.get(reverse('producers-controles', kwargs={'producer_name':self.entity.url_friendly_name()}))
+        self.assertEqual(response.status_code, 200)
 
