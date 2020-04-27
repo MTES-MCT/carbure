d3336c250e0f0d93040dd0f2cf34dd5cbb6f1fcc
Martin Planes <martin.planes@gmail.com>
2020-04-27 15:52:47 +0200

    [docker]

--- docker/run_nginx.sh
+++ docker/run_nginx.sh
@@ -4,5 +4,5 @@
 envsubst '$$NGINX_HOSTS $$NGINX_SSL_FOLDER' < /etc/nginx/conf.d/web.template > /etc/nginx/conf.d/default.conf
 
 while :;
-do sleep 6h & wait $${!}; nginx -s reload;
+do sleep 6h & wait ${!}; nginx -s reload;
 done & nginx -g "daemon off;"
