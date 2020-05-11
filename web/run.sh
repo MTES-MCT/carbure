e1e2ff7f1894f4b3ace725094e6f34ef5df4f111
Martin Planes <martin.planes@gmail.com>
2020-05-11 10:42:32 +0200

    [env]

--- web/run.sh
+++ web/run.sh
@@ -10,7 +10,7 @@ python3 /app/web/fixtures/load_countries.py
 python3 /app/web/fixtures/load_matierespremieres.py
 python3 /app/web/fixtures/load_ghg_values.py
 
-if [ "$IMAGE_TAG" = "dev" ] ; then
+if [ "$IMAGE_TAG" = "local" ] ; then
     python3 /app/web/manage.py runserver 0.0.0.0:8001
 else
     uwsgi --ini /app/web/carbure_uwsgi.ini --touch-reload=/app/web/carbure_uwsgi.ini
