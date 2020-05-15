b25fab094512ef6eedce03bc2ee8458c32eabff3
Martin Planes <martin.planes@gmail.com>
2020-05-15 13:55:47 +0200

    [api] split api over multiple files

--- web/api/urls.py
+++ web/api/urls.py
@@ -1,66 +1,63 @@
 from django.urls import path
 
-from . import views
+from . import public_api
+from . import producers_api
+from . import operators_api
+from . import administrators_api
 
 urlpatterns = [
-	# public, autocomplete api
-	path('biocarburant-autocomplete/', views.biocarburant_autocomplete, name='api-biocarburant-autocomplete'),
-	path('matiere-premiere-autocomplete/', views.matiere_premiere_autocomplete, name='api-matiere-premiere-autocomplete'),
-    path('country-autocomplete/', views.country_autocomplete, name='api-country-autocomplete'),
-	path('operators-autocomplete/', views.operators_autocomplete, name='api-operators-autocomplete'),
-
-    # public csv
-    path('biocarburant-csv/', views.biocarburant_csv, name='api-biocarburant-csv'),
-    path('matiere-premiere-csv/', views.matiere_premiere_csv, name='api-matiere-premiere-csv'),
-    path('country-csv/', views.country_csv, name='api-country-csv'),
-    path('operators-csv/', views.operators_csv, name='api-operators-csv'),
+    # public, autocomplete api
+    path('biocarburant-autocomplete/', public_api.biocarburant_autocomplete, name='api-biocarburant-autocomplete'),
+    path('matiere-premiere-autocomplete/', public_api.matiere_premiere_autocomplete, name='api-matiere-premiere-autocomplete'),
+    path('country-autocomplete/', public_api.country_autocomplete, name='api-country-autocomplete'),
+    path('operators-autocomplete/', public_api.operators_autocomplete, name='api-operators-autocomplete'),
+    path('biocarburant-csv/', public_api.biocarburant_csv, name='api-biocarburant-csv'),
+    path('matiere-premiere-csv/', public_api.matiere_premiere_csv, name='api-matiere-premiere-csv'),
+    path('country-csv/', public_api.country_csv, name='api-country-csv'),
+    path('operators-csv/', public_api.operators_csv, name='api-operators-csv'),
 
     # private template
-    path('producers-template-csv/', views.producers_import_csv_template, name='api-producers-template-csv'),
-
-    # private, producers
-    path('producers/lots/drafts', views.producers_lots_drafts, name='api-producers-lots-drafts'),
-    path('producers/lots/corrections', views.producers_lots_corrections, name='api-producers-lots-corrections'),
-    path('producers/lots/valid', views.producers_lots_valid, name='api-producers-lots-valid'),
-    path('producers/lots/all', views.producers_lots_all, name='api-producers-lots-all'),
-    path('producers/lot/save', views.producers_save_lot, name='producers-api-attestation-save-lot'),
-    path('producers/lot/duplicate', views.producers_duplicate_lot, name='producers-api-duplicate-lot'),
-    path('producers/lot/delete', views.producers_delete_lots, name='producers-api-delete-lots'),
-    path('producers/lot/validate', views.producers_validate_lots, name='producers-api-validate-lots'),
-    path('producers/lot/comments', views.producers_lot_comments, name='producers-api-lot-comments'),
-    path('producers/lot/save-comment', views.producers_lot_save_comment, name='producers-api-save-comment'),
-    path('producers/lot/errors', views.producers_lot_errors, name='producers-api-lot-errors'),
-    path('producers/settings/add-site', views.producers_settings_add_site, name='producers-api-settings-add-site'),
-    path('producers/settings/add-certif', views.producers_settings_add_certif, name='producers-api-settings-add-certif'),
-    path('producers/settings/delete-certif', views.producers_settings_delete_certif, name='producers-api-settings-delete-certif'),
-    path('producers/settings/add-mp', views.producers_settings_add_mp, name='producers-api-settings-add-mp'),
-    path('producers/settings/add-biocarburant', views.producers_settings_add_biocarburant, name='producers-api-settings-add-biocarburant'),
-    path('producers/settings/delete-mp', views.producers_settings_delete_mp, name='producers-api-settings-delete-mp'),
-    path('producers/settings/delete-biocarburant', views.producers_settings_delete_biocarburant, name='producers-api-settings-delete-bc'),
-    path('producers/export/drafts', views.producers_attestation_export_drafts, name='producers-api-attestation-export-drafts'),
-    path('producers/export/valid', views.producers_attestation_export_valid, name='producers-api-attestation-export-valid'),
-    path('producers/corrections', views.producers_corrections, name='api-producers-corrections'),
-    path('producers/production-sites-autocomplete/', views.producers_prod_site_autocomplete, name='producers-api-production-sites-autocomplete'),
-    path('producers/biocarburant-autocomplete/', views.producers_biocarburant_autocomplete, name='producers-api-biocarburants-autocomplete'),
-    path('producers/mp-autocomplete/', views.producers_mp_autocomplete, name='producers-api-mps-autocomplete'),
-    path('producers/ges/', views.producers_ges, name='producers-api-ges'),
+    path('producers/csv-template', producers_api.producers_import_csv_template, name='api-producers-template-csv'),
+    path('producers/lots/drafts', producers_api.producers_lots_drafts, name='api-producers-lots-drafts'),
+    path('producers/lots/corrections', producers_api.producers_lots_corrections, name='api-producers-lots-corrections'),
+    path('producers/lots/valid', producers_api.producers_lots_valid, name='api-producers-lots-valid'),
+    path('producers/lots/all', producers_api.producers_lots_all, name='api-producers-lots-all'),
+    path('producers/lot/save', producers_api.producers_save_lot, name='producers-api-attestation-save-lot'),
+    path('producers/lot/duplicate', producers_api.producers_duplicate_lot, name='producers-api-duplicate-lot'),
+    path('producers/lot/delete', producers_api.producers_delete_lots, name='producers-api-delete-lots'),
+    path('producers/lot/validate', producers_api.producers_validate_lots, name='producers-api-validate-lots'),
+    path('producers/lot/comments', producers_api.producers_lot_comments, name='producers-api-lot-comments'),
+    path('producers/lot/save-comment', producers_api.producers_lot_save_comment, name='producers-api-save-comment'),
+    path('producers/lot/errors', producers_api.producers_lot_errors, name='producers-api-lot-errors'),
+    path('producers/settings/add-site', producers_api.producers_settings_add_site, name='producers-api-settings-add-site'),
+    path('producers/settings/add-certif', producers_api.producers_settings_add_certif, name='producers-api-settings-add-certif'),
+    path('producers/settings/delete-certif', producers_api.producers_settings_delete_certif, name='producers-api-settings-delete-certif'),
+    path('producers/settings/add-mp', producers_api.producers_settings_add_mp, name='producers-api-settings-add-mp'),
+    path('producers/settings/add-biocarburant', producers_api.producers_settings_add_biocarburant, name='producers-api-settings-add-biocarburant'),
+    path('producers/settings/delete-mp', producers_api.producers_settings_delete_mp, name='producers-api-settings-delete-mp'),
+    path('producers/settings/delete-biocarburant', producers_api.producers_settings_delete_biocarburant, name='producers-api-settings-delete-bc'),
+    path('producers/export/drafts', producers_api.producers_attestation_export_drafts, name='producers-api-attestation-export-drafts'),
+    path('producers/export/valid', producers_api.producers_attestation_export_valid, name='producers-api-attestation-export-valid'),
+    path('producers/corrections', producers_api.producers_corrections, name='api-producers-corrections'),
+    path('producers/production-sites-autocomplete/', producers_api.producers_prod_site_autocomplete, name='producers-api-production-sites-autocomplete'),
+    path('producers/biocarburant-autocomplete/', producers_api.producers_biocarburant_autocomplete, name='producers-api-biocarburants-autocomplete'),
+    path('producers/mp-autocomplete/', producers_api.producers_mp_autocomplete, name='producers-api-mps-autocomplete'),
+    path('producers/ges/', producers_api.producers_ges, name='producers-api-ges'),
 
     # private, operators
-    path('operators/lots/declared', views.operators_lots, name='operators-api-declared-lots'),
-    path('operators/lots/affiliated', views.operators_lots_affilies, name='operators-api-affiliated-lots'),
-    path('operators/lots/accept', views.operators_lot_accept, name='operators-api-accept-lots'),
-    path('operators/lots/accept-correction', views.operators_lot_accept_correction, name='operators-api-accept-lot-correction'),
-    path('operators/lots/accept-with-comment', views.operators_lot_accept_with_comment, name='operators-api-accept-lot-with-comment'),
-    path('operators/lots/reject', views.operators_lot_reject, name='operators-api-reject-lots'),
-    path('operators/lots/comments', views.operators_lot_comments, name='operators-api-lot-comments'),
-    path('operators/lots/export', views.operators_export_lots, name='operators-api-declaration-export'),
-
-
+    path('operators/lots/declared', operators_api.operators_lots, name='operators-api-declared-lots'),
+    path('operators/lots/affiliated', operators_api.operators_lots_affilies, name='operators-api-affiliated-lots'),
+    path('operators/lots/accept', operators_api.operators_lot_accept, name='operators-api-accept-lots'),
+    path('operators/lots/accept-correction', operators_api.operators_lot_accept_correction, name='operators-api-accept-lot-correction'),
+    path('operators/lots/accept-with-comment', operators_api.operators_lot_accept_with_comment, name='operators-api-accept-lot-with-comment'),
+    path('operators/lots/reject', operators_api.operators_lot_reject, name='operators-api-reject-lots'),
+    path('operators/lots/comments', operators_api.operators_lot_comments, name='operators-api-lot-comments'),
+    path('operators/lots/export', operators_api.operators_export_lots, name='operators-api-declaration-export'),
 
     # private, administrators
-    path('administrators/users-autocomplete/', views.admin_users_autocomplete, name='admin-api-users-autocomplete'),
-    path('administrators/entities-autocomplete/', views.admin_entities_autocomplete, name='admin-api-entities-autocomplete'),
-    path('administrators/lots', views.admin_lots, name='admin-api-lots'),
-    path('administrators/export', views.admin_lots_export, name='admin-api-lots-export'),
+    path('administrators/users-autocomplete/', administrators_api.admin_users_autocomplete, name='admin-api-users-autocomplete'),
+    path('administrators/entities-autocomplete/', administrators_api.admin_entities_autocomplete, name='admin-api-entities-autocomplete'),
+    path('administrators/lots', administrators_api.admin_lots, name='admin-api-lots'),
+    path('administrators/export', administrators_api.admin_lots_export, name='admin-api-lots-export'),
 
 ]
