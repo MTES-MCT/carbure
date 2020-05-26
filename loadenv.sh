3b5e6eee1a251b29aa3fed31596d7187b3a112fa
Martin Planes <martin.planes@gmail.com>
2020-05-26 17:53:01 +0200

    [env]

---
+++ loadenv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+function loaddotenv() {
+    set -o allexport; source .env; set +o allexport
+}
+
+loaddotenv
+export MYSQL_HOST="$(docker inspect --format '{{ .NetworkSettings.Networks.carbure.IPAddress }}' carbure_mariadb)"
+export MYSQL_PORT=3306
+export PYTHONPATH="$CARBURE_HOME/web:$CARBURE_HOME"
+export ALLOWED_HOSTS="carbure.local"
+source venv/bin/activate
