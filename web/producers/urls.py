d204dc3f8401b477daa8c2e8c6ff625c985d298f
Martin Planes <martin.planes@gmail.com>
2020-06-04 14:01:22 +0200

    [new api]

--- web/producers/urls.py
+++ web/producers/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('<slug:producer_name>/', views.producers_index, name='producers-index'),
     path('<slug:producer_name>/controles', views.producers_controles, name='producers-controles'),
     path('<slug:producer_name>/settings', views.producers_settings, name='producers-settings'),
+
+
+
+    path('v2/<slug:producer_name>/', views.producers_index_v2, name='producers-index-v2'),
+
 ]
