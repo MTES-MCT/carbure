9d0cc12bf62ffa310303ca9cf90843fba9ac7499
Martin Planes <martin.planes@gmail.com>
2020-06-05 17:31:46 +0200

    [fixtures] ignore header

--- web/fixtures/load_biocarburants.py
+++ web/fixtures/load_biocarburants.py
@@ -5,7 +5,7 @@ import csv
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carbure.settings")
 django.setup()
 
-from core.models import Biocarburant 
+from core.models import Biocarburant
 
 filename = '%s/web/fixtures/csv/biocarburants.csv' % (os.environ['CARBURE_HOME'])
 
@@ -14,4 +14,6 @@ with open(filename) as csvfile:
     for row in reader:
         code = row[0]
         name = row[1]
+        if code == "code":
+            continue
         obj, created = Biocarburant.objects.update_or_create(code=code, defaults={'name': name, 'description':''})
