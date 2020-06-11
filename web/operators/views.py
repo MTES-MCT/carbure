aaa56f4e180a376850fe49ed380f2d0780bc9c0c
Martin Planes <martin.planes@gmail.com>
2020-06-11 12:33:41 +0200

    [operators] v2

--- web/operators/views.py
+++ web/operators/views.py
@@ -2,22 +2,20 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from core.decorators import enrich_with_user_details, restrict_to_operators
 
-import datetime
-import calendar
 
 @login_required
 @enrich_with_user_details
 @restrict_to_operators
 def operators_index(request, *args, **kwargs):
-  context = kwargs['context']
-  context['current_url_name'] = 'operators-index'
-  return render(request, 'operators/declarations.html', context)
+    context = kwargs['context']
+    context['current_url_name'] = 'operators-index'
+    return render(request, 'operators/declarations_v2.html', context)
+
 
 @login_required
 @enrich_with_user_details
 @restrict_to_operators
 def operators_controles(request, *args, **kwargs):
-  context = kwargs['context']
-  context['current_url_name'] = 'operators-controles'
-  return render(request, 'operators/controles.html', context)
-
+    context = kwargs['context']
+    context['current_url_name'] = 'operators-controles'
+    return render(request, 'operators/controles.html', context)
