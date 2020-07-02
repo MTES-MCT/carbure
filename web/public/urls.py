4cef0554e20f68c93b85bd8ed50ead2ef32a0184
Martin Planes <martin.planes@gmail.com>
2020-07-02 18:13:33 +0200

    [stats] remove internal page and link to metabase instead

--- web/public/urls.py
+++ web/public/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('home', views.home, name='home'), # LOGIN_REDIRECT_URL: will dispatch to either /producers/, /operators/ or /administrators/ depending on account type
     path('annuaire', views.annuaire, name='annuaire'),
     path('htmlreference', views.htmlreference, name='htmlreference'),
-    path('stats', views.stats, name='public-stats'),
 ]
