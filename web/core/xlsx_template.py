7fdd36cf92691add2fa953d60c762b2c6f17e9a6
Martin Planes <martin.planes@gmail.com>
2020-07-03 17:56:21 +0200

    [traders] fix xlsx template download

--- web/core/xlsx_template.py
+++ web/core/xlsx_template.py
@@ -220,6 +220,68 @@ def make_lots_sheet_v2_simple(workbook, entity):
             colid += 1
 
 
+def make_traders_lots_sheet(workbook, entity):
+    worksheet_lots = workbook.add_worksheet("lots")
+    mps = MatierePremiere.objects.all()
+    bcs = Biocarburant.objects.all()
+    delivery_sites = Depot.objects.all()
+    countries = Pays.objects.all()
+
+    # 3/10 chances of having an imported lot
+    unknown_producers = [{'name': 'ITANOL', 'country': 'IT', 'production_site': 'BERGAMO', 'ref': 'ISCC-IT-100001010', 'date':'2017-12-01', 'dc':'IT_001_2020'},
+                         {'name': 'ITANOL', 'country': 'IT', 'production_site': 'FIRENZE', 'ref': 'ISCC-IT-100001011', 'date':'2014-03-01', 'dc':''},
+                         {'name': 'PORTUGASOIL', 'country': 'PT', 'production_site': 'LISBOA', 'ref': 'ISCC-PT-100001110', 'date':'2011-10-01', 'dc':''},
+                         {'name': 'PORTUGASOIL', 'country': 'PT', 'production_site': 'PORTO', 'ref': 'ISCC-PT-100001080', 'date':'2013-07-01', 'dc':''},
+                         {'name': 'BIOCATALAN', 'country': 'ES', 'production_site': 'EL MASNOU', 'ref': 'ISCC-ES-100002010', 'date':'2016-02-01', 'dc':'ES_012_2016'},
+                         {'name': 'BIOCATALAN', 'country': 'ES', 'production_site': 'TARRAGONA', 'ref': 'ISCC-ES-100005010', 'date':'2019-12-01', 'dc':''},
+                         {'name': 'BIOBAO', 'country': 'ES', 'production_site': 'HONDARRIBIA', 'ref': 'ISCC-ES-100004010', 'date':'2007-11-01', 'dc':'ES_011_2018'},
+                         {'name': 'BONDUELLE', 'country': 'FR', 'production_site': 'TOURS', 'ref': 'ISCC-FR-100001011', 'date':'2001-01-01', 'dc':''},
+                         {'name': 'BONDUELLE', 'country': 'FR', 'production_site': 'NUEIL LES AUBIERS', 'ref': 'ISCC-FR-100001012', 'date':'2004-06-01', 'dc':'FR_042_2016'},
+                         {'name': 'GEANTVERT', 'country': 'FR', 'production_site': 'BRUZAC', 'ref': 'ISCC-FR-100001013', 'date':'2005-04-01', 'dc':''},
+                         {'name': 'GEANTVERT', 'country': 'FR', 'production_site': 'NIMES', 'ref': 'ISCC-FR-100001014', 'date':'1997-07-01', 'dc':'FR_002_2017'},
+                         {'name': '', 'country': '', 'production_site': '', 'ref': 'ISCC-ES-100002010', 'date':'2016-02-01', 'dc':'ES_012_2016'},
+                         {'name': '', 'country': '', 'production_site': '', 'ref': 'ISCC-ES-100005010', 'date':'2019-12-01', 'dc':''},
+                         {'name': '', 'country': '', 'production_site': '', 'ref': 'ISCC-ES-100004010', 'date':'2007-11-01', 'dc':'ES_011_2018'},
+                         {'name': '', 'country': '', 'production_site': '', 'ref': 'ISCC-FR-100001011', 'date':'2001-01-01', 'dc':''},
+                         {'name': '', 'country': '', 'production_site': '', 'ref': 'ISCC-FR-100001012', 'date':'2004-06-01', 'dc':'FR_042_2016'},
+                         {'name': '', 'country': '', 'production_site': '', 'ref': 'ISCC-FR-100001013', 'date':'2005-04-01', 'dc':''},
+                         {'name': '', 'country': '', 'production_site': '', 'ref': 'ISCC-FR-100001014', 'date':'1997-07-01', 'dc':'FR_002_2017'},
+                         ]
+
+    vendors = ['LUR BERRI', 'ITANOL', 'FINNHUB', 'DEUTSCHE BIOFUELS GMBH', 'SUCRERIE HARIBO', '', '', '', '', '']
+
+    # header
+    bold = workbook.add_format({'bold': True})
+    columns = ['producer', 'production_site', 'production_site_country', 'production_site_reference',
+               'production_site_commissioning_date', 'double_counting_registration',
+               'vendor', 'volume', 'biocarburant_code', 'matiere_premiere_code', 'pays_origine_code',
+               'eec', 'el', 'ep', 'etd', 'eu', 'esca', 'eccs', 'eccr', 'eee', 'e',
+               'dae', 'champ_libre', 'delivery_date', 'delivery_site', 'delivery_site_country']
+    for i, c in enumerate(columns):
+        worksheet_lots.write(0, i, c, bold)
+
+    volumes = [1200, 2800, 8000, 4500, 13000, 35000, 34960, 27854, 18000]
+    clientid = 'import_batch_%s' % (datetime.date.today().strftime('%Y%m%d'))
+    today = datetime.date.today().strftime('%Y-%m-%d')
+    for i in range(10):
+        mp = random.choice(mps)
+        vendor = random.choice(vendors)
+        bc = random.choice(bcs)
+        country = random.choice(countries)
+        site = random.choice(delivery_sites)
+        volume = random.choice(volumes)
+        row = []
+        p = random.choice(unknown_producers)
+        row += [p['name'], p['production_site'], p['country'], p['ref'], p['date'], p['dc']]
+        row += [vendor, volume, bc.code, mp.code, country.code_pays, 12, 4, 2, 0, 3.3, 0, 0, 0, 0, 0, 'FR000000123', clientid]
+        row += [today, site.depot_id, 'FR']
+
+        colid = 0
+        for elem in row:
+            worksheet_lots.write(i+1, colid, elem)
+            colid += 1
+
+
 def make_operators_lots_sheet(workbook, entity):
     worksheet_lots = workbook.add_worksheet("lots")
     mps = MatierePremiere.objects.all()
@@ -248,7 +310,7 @@ def make_operators_lots_sheet(workbook, entity):
                          {'name': '', 'country': '', 'production_site': '', 'ref': 'ISCC-FR-100001014', 'date':'1997-07-01', 'dc':'FR_002_2017'},
                          ]
 
-    vendors = ['LUR BERRI', 'ITANOL', 'FINNHUB', 'DEUTSCHE BIOFUELS GMBH', 'SUCRERIE HARIBO']
+    vendors = ['LUR BERRI', 'ITANOL', 'FINNHUB', 'DEUTSCHE BIOFUELS GMBH', 'SUCRERIE HARIBO', '', '', '', '', '']
 
     # header
     bold = workbook.add_format({'bold': True})
@@ -401,7 +463,7 @@ def create_template_xlsx_v2_advanced(entity):
 
 def create_template_xlsx_v2_operators(entity):
     # Create an new Excel file and add a worksheet.
-    location = '/tmp/carbure_template_traders.xlsx'
+    location = '/tmp/carbure_template_operators.xlsx'
     workbook = xlsxwriter.Workbook(location)
     make_operators_lots_sheet(workbook, entity)
     make_mps_sheet(workbook)
@@ -415,7 +477,7 @@ def create_template_xlsx_v2_traders(entity):
     # Create an new Excel file and add a worksheet.
     location = '/tmp/carbure_template_traders.xlsx'
     workbook = xlsxwriter.Workbook(location)
-    make_operators_lots_sheet(workbook, entity)
+    make_traders_lots_sheet(workbook, entity)
     make_mps_sheet(workbook)
     make_biofuels_sheet(workbook)
     make_countries_sheet(workbook)
