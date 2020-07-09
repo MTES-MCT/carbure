b7d6add4999f460fc918fa8b6ba4c2d273a50953
Martin Planes <martin.planes@gmail.com>
2020-07-09 10:27:04 +0200

    [templates] add new design

--- web/producers/urls.py
+++ web/producers/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('v2/<slug:producer_name>/mass-balance', v2.producers_mass_balance, name='producers-mb'),
     path('v2/<slug:producer_name>/archives', v2.producers_histo, name='producers-histo'),
     path('v2/import-documentation', v2.producers_import_doc, name='producers-import-documentation'),
+
+
+    path('v2/new_design', v2.new_design, name='producers-new-design'),
+
 ]
