027f75b2ad1b0c265b4dd08ef8a6f7c2a5b1b7fa
Martin Planes <martin.planes@gmail.com>
2020-07-21 16:31:19 +0200

    [producers] ajout de la page de stats privees.

--- web/producers/urls.py
+++ web/producers/urls.py
@@ -10,8 +10,7 @@ urlpatterns = [
     path('v2/<slug:producer_name>/mass-balance', v2.producers_mass_balance, name='producers-mb'),
     path('v2/<slug:producer_name>/archives', v2.producers_histo, name='producers-histo'),
     path('v2/import-documentation', v2.producers_import_doc, name='producers-import-documentation'),
-
-
+    path('v2/<slug:producer_name>/stats', v2.stats, name='producers-stats'),
     path('v2/new_design', v2.new_design, name='producers-new-design'),
 
 ]
