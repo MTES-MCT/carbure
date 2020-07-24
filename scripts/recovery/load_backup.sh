c89d693c036e5f7889b4623a348905419ce9196d
Martin Planes <martin.planes@gmail.com>
2020-07-24 16:18:13 +0200

    [scripts] allow local env to load prod db backup

--- scripts/recovery/load_backup.sh
+++ scripts/recovery/load_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$ENV" = "dev" ] || [ "$ENV" = "staging" ]
+if [ "$ENV" = "local" ] || [ "$ENV" = "dev" ] || [ "$ENV" = "staging" ]
 then
     mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /tmp/backup.sql
 else
