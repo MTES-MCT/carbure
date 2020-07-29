c64db65056d24cdf60e9c8a9287b1bd5f3fd36fc
Martin Planes <martin.planes@gmail.com>
2020-07-29 12:15:06 +0200

    [producers] corrections: load lots with data_origin_entity = self

--- web/api/v2/producers/producers_get.py
+++ web/api/v2/producers/producers_get.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
+from itertools import chain
 
 from core.decorators import enrich_with_user_details, restrict_to_producers
 
@@ -58,8 +59,17 @@ def get_mb(request, *args, **kwargs):
 @restrict_to_producers
 def get_corrections(request, *args, **kwargs):
     context = kwargs['context']
-    transactions = LotTransaction.objects.filter(carbure_vendor=context['user_entity'], delivery_status__in=['R', 'AC', 'AA'], lot__status="Validated")
-    comments = TransactionComment.objects.filter(tx__in=transactions)
+    # corrections de type "Durabilite" ou "Les deux" pour mes lots
+    tx_added = LotTransaction.objects.filter(lot__data_origin_entity=context['user_entity'], delivery_status__in=['R', 'AC', 'AA'], lot__status="Validated")
+    comments_tx_added = TransactionComment.objects.filter(tx__in=tx_added, topic__in=['SUSTAINABILITY', 'BOTH'])
+
+    # corrections de type "Transaction" ou "les deux" pour les lots vendus
+    tx_sold = LotTransaction.objects.filter(carbure_vendor=context['user_entity'], delivery_status__in=['R', 'AC', 'AA'], lot__status="Validated")
+    comments_tx_sold = TransactionComment.objects.filter(tx__in=tx_sold, topic__in=['TX', 'BOTH'])
+
+    # union de tout ça
+    transactions = set(list(chain(tx_added, tx_sold)))
+    comments = set(list(chain(comments_tx_added, comments_tx_sold)))
     txsez = serializers.serialize('json', transactions, use_natural_foreign_keys=True)
     commentssez = serializers.serialize('json', comments, use_natural_foreign_keys=True)
     return JsonResponse({'transactions': txsez, 'comments': commentssez})
