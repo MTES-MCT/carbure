ece52fd1eaa277039e8df4f589a4f879b5c7d85a
Martin Planes <martin.planes@gmail.com>
2020-08-04 10:37:07 +0200

    [admin] server side pagination fixed

--- web/api/v2/administrators/admin_get.py
+++ web/api/v2/administrators/admin_get.py
@@ -17,22 +17,24 @@ def get_out(request, *args, **kwargs):
     search = request.GET.get('search[value]', None)
 
     if search != '':
-        transactions = LotTransaction.objects.filter(lot__status='Validated').filter(Q(lot__matiere_premiere__name__icontains=search) |
-                                                                                 Q(lot__biocarburant__name__icontains=search) |
-                                                                                 Q(lot__carbure_producer__name__icontains=search) |
-                                                                                 Q(lot__unknown_producer__icontains=search) |
-                                                                                 Q(lot__carbure_id__icontains=search) |
-                                                                                 Q(lot__pays_origine__name__icontains=search) |
-                                                                                 Q(carbure_client__name__icontains=search) |
-                                                                                 Q(unknown_client__icontains=search) |
-                                                                                 Q(carbure_delivery_site__name__icontains=search) |
-                                                                                 Q(unknown_delivery_site__icontains=search)
-                                                                                 )
-        page = transactions[start:start+length]
+        transactions = LotTransaction.objects.filter(lot__status='Validated')
+        filtered = transactions.filter(Q(lot__matiere_premiere__name__icontains=search) |
+                                       Q(lot__biocarburant__name__icontains=search) |
+                                       Q(lot__carbure_producer__name__icontains=search) |
+                                       Q(lot__unknown_producer__icontains=search) |
+                                       Q(lot__carbure_id__icontains=search) |
+                                       Q(lot__pays_origine__name__icontains=search) |
+                                       Q(carbure_client__name__icontains=search) |
+                                       Q(unknown_client__icontains=search) |
+                                       Q(carbure_delivery_site__name__icontains=search) |
+                                       Q(unknown_delivery_site__icontains=search)
+                                       )
+        page = filtered[start:start+length]
     else:
         transactions = LotTransaction.objects.filter(lot__status='Validated')
+        filtered = transactions
         page = transactions[start:start+length]
     comments = TransactionComment.objects.filter(tx__in=[t for t in page])
     txsez = serializers.serialize('json', page, use_natural_foreign_keys=True)
     commentssez = serializers.serialize('json', comments, use_natural_foreign_keys=True)
-    return JsonResponse({'transactions': txsez, 'comments': commentssez, 'recordsFiltered': len(page), 'recordsTotal': len(transactions)})
+    return JsonResponse({'transactions': txsez, 'comments': commentssez, 'recordsFiltered': len(filtered), 'recordsTotal': len(transactions)})
