42b301db28a2454f1704d3a3d44736c862e2e48f
Martin Planes <martin.planes@gmail.com>
2020-09-02 10:52:03 +0200

    [mac] add is_mac flag to LotTransaction and load it from excel

--- web/core/models.py
+++ web/core/models.py
@@ -253,6 +253,8 @@ class LotTransaction(models.Model):
 
     # other
     champ_libre = models.CharField(max_length=64, blank=True, null=True, default='')
+    # mise a consommation?
+    is_mac = models.BooleanField(default=False)
 
     def __str__(self):
         return str(self.id)
