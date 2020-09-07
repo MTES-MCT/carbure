086a0ea50718931ed83893abb2441c1835b38875
Martin Planes <martin.planes@gmail.com>
2020-09-07 14:12:14 +0200

    [sanity checks] csv loader updated

--- web/fixtures/load_checkrules.py
+++ web/fixtures/load_checkrules.py
@@ -12,15 +12,19 @@ filename = '%s/web/fixtures/csv/rules.csv' % (os.environ['CARBURE_HOME'])
 with open(filename) as csvfile:
     reader = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in reader:
+        if len(row) < 10:
+            print('Incomplete row:')
+            print(row)
+            continue
         cond_col = row[0]
         cond = row[1]
         cond_value = row[2]
         check_col = row[3]
         check = row[4]
         check_value = row[5]
-        warning_to_user = row[6]
-        warning_to_admin = row[7]
-        block_validation = row[8]
+        warning_to_user = bool(row[6])
+        warning_to_admin = bool(row[7])
+        block_validation = bool(row[8])
         message = row[9]
 
         if cond_col == 'condition_col':
