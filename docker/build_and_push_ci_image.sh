5de3462752d19a4d81f3c3a94c6c3ce1af7e3f76
Martin Planes <martin.planes@gmail.com>
2020-09-15 13:05:08 +0200

    [docker] remove unused additional dockerfile

--- docker/build_and_push_ci_image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t registry.gitlab.com/la-fabrique-numerique/biocarburants/gitlab_runner:latest .  -f docker/Dockerfile.ci
-docker push registry.gitlab.com/la-fabrique-numerique/biocarburants/gitlab_runner:latest
-
