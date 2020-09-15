aa9028c3cf45e9ad30fd542e7a11459c306c01cf
Martin Planes <martin.planes@gmail.com>
2020-09-15 13:56:27 +0200

    [fixtures] load 2019 data script: add transactions

--- web/fixtures/load_2019_data.py
+++ web/fixtures/load_2019_data.py
@@ -27,6 +27,7 @@ today = datetime.date.today()
 now = datetime.datetime.now()
 mtes = Entity.objects.get(name='MTES - DGEC')
 robot = usermodel.objects.get(name='MTES Robot')
+france = Pays.objects.get(code_pays='FR')
 
 r = LotV2.objects.filter(added_by_user=robot).delete()
 print(r)
@@ -54,6 +55,11 @@ for i, row in enumerate(lots_sheet):
 
 for i, lot in enumerate(lots):
     # {'Opérateur': 'UDP', 'producer': None, 'production_site': None, 'production_site_country': 'NL', 'production_site_reference': 'EU-ISCC-Cert-DE100-17452019', 'production_site_commissioning_date': datetime.datetime(2008, 11, 29, 0, 0), 'double_counting_registration': None, 'vendor': None, 'volume': 2242.0, 'biocarburant_code': 'EMHV', 'matiere_premiere_code': 'COLZA', 'pays_origine_code': 'UA', 'eec': None, 'el': None, 'ep': 38.9, 'etd': 1.0, 'eu': None, 'esca': None, 'eccs': None, 'eccr': None, 'eee': None, 'dae': '19BEK4AP7G7V00EGNBHL4', 'champ_libre': None, 'delivery_date': datetime.datetime(2019, 12, 18, 0, 0), 'delivery_site': None, 'Total des émissions de GES': 39.9}
+    if lot['dae'] is None:
+        print('No DAE')
+        print(i, lot)
+        continue
+    
     lop = lot['Opérateur']
     if lop not in operators:
         print('Could not find operator %s in operators' % (lop))
@@ -146,4 +152,21 @@ for i, lot in enumerate(lots):
     obj.matiere_premiere = mp
     obj.volume = vol
     obj.save()
+
+    tx = LotTransaction()
+    tx.lot = obj
+    tx.vendor_is_in_carbure = False
+    tx.carbure_vendor = None
+    tx.unknown_vendor = ''
+    tx.dae = lot['dae']
+    tx.client_is_in_carbure = True
+    tx.carbure_client = op
+    tx.unknown_client = ''
+    tx.delivery_date = lot['delivery_date']
+    tx.delivery_site_is_in_carbure = False
+    tx.carbure_delivery_site = None
+    tx.unknown_delivery_site = ''
+    tx.unknown_delivery_site_country = france
+    tx.delivery_status = 'A'
+    tx.save()
     print('created lot %d' % (i), end='\r')
