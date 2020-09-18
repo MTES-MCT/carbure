426eb0a28244d53cf897cebf2bfe3e72a89baa43
Martin Planes <martin.planes@gmail.com>
2020-09-18 12:39:57 +0200

    [api] v3 public -> common

--- web/api/v3/urls.py
+++ web/api/v3/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('public/', include('api.v3.public.urls')),
+    path('common/', include('api.v3.common.urls')),
     path('lots/', include('api.v3.lots.urls')),
     path('settings/', include('api.v3.settings.urls')),
     path('admin/', include('api.v3.admin.urls')),
