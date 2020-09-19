6c472d327d71517212e22a3aa7c959b5bbfab231
jfalxa <jfalxa.sta@gmail.com>
2020-09-19 13:16:22 +0200

    [front] create custom select component

---
+++ front/src/components/icons.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import cl from "clsx"
+
+// icons were adapted from https://github.com/tabler/tabler-icons
+
+type IconProps = {
+  size?: number
+  color?: string
+  stroke?: number
+  className?: string
+  [k: string]: string | number | void
+}
+
+export const ChevronDown = ({
+  size = 24,
+  color = "currentColor",
+  stroke = 2,
+  className,
+  ...props
+}: IconProps) => (
+  <svg
+    className={cl("icon", "icon-chevron-down", className)}
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    strokeWidth={stroke}
+    stroke={color}
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <polyline points="6 9 12 15 18 9" />
+  </svg>
+)
