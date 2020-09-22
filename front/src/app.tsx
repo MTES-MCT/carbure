24698e66f080fabdd63f4715764fc2d1a929ff81
jfalxa <jfalxa.sta@gmail.com>
2020-09-22 17:37:09 +0200

    [front] load settings when starting app

--- front/src/app.tsx
+++ front/src/app.tsx
@@ -1,14 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import { BrowserRouter, Route } from "react-router-dom"
+
+import useAPI from "./hooks/use-api"
+import { getSettings } from "./services/settings"
 
 import Topbar from "./components/top-bar"
 import Logout from "./routes/logout"
 import Transactions from "./routes/transactions"
+import Exit from "./components/exit"
 
 const App = () => {
+  const [entity, setEntity] = useState(-1)
+
+  const settings = useAPI(getSettings)
+  settings.useResolve()
+
+  // select the default entity if not already done
+  if (entity === -1 && settings.data) {
+    setEntity(settings.data.rights[0].entity.id)
+  }
+
+  if (settings.loading) {
+    return <h1>Loading...</h1>
+  }
+
+  if (settings.error) {
+    return <Exit to="/" />
+  }
+
   return (
     <BrowserRouter basename="/v2">
-      <Topbar />
+      <Topbar settings={settings} entity={entity} setEntity={setEntity} />
 
       <Route path="/transactions">
         <Transactions />
