36b4291eedda5bb916bb8db9d40310049d41511d
jfalxa <jfalxa.sta@gmail.com>
2020-09-23 18:53:19 +0200

    [front] do not send empty params to api

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -10,15 +10,30 @@ export type ApiResponse<T> =
   | Promise<{ status: "forbidden"; message: string }>
   | Promise<{ status: "success"; data: T }>
 
+function readyParams(params: Params) {
+  const okParams: Params = {}
+
+  for (const key in params) {
+    if (params[key] || params[key] === 0) {
+      okParams[key] = params[key]
+    }
+  }
+
+  return okParams
+}
+
 function get<T>(
   endpoint: string,
   params?: Params,
   options?: Options
 ): ApiResponse<T> {
-  return fetch(
-    API_ROOT + endpoint + "?" + stringify(params),
-    options
-  ).then((res) => res.json())
+  let url = API_ROOT + endpoint
+
+  if (params) {
+    url += "?" + stringify(readyParams(params))
+  }
+
+  return fetch(url, options).then((res) => res.json())
 }
 
 function post<T>(
@@ -26,11 +41,13 @@ function post<T>(
   body?: Params,
   options?: Options
 ): ApiResponse<T> {
-  return fetch(API_ROOT + endpoint, {
-    ...options,
-    method: "POST",
-    body: JSON.stringify(body),
-  }).then((res) => res.json())
+  const fetchOptions: Params = { ...options, method: "POST" }
+
+  if (body) {
+    fetchOptions.body = JSON.stringify(readyParams(body))
+  }
+
+  return fetch(API_ROOT + endpoint, fetchOptions).then((res) => res.json())
 }
 
 export default { get, post }
