11e1ae0eabb6fdd1a022c312a563c78cff36aaea
jfalxa <jfalxa.sta@gmail.com>
2020-09-28 15:02:20 +0200

    [front] create modal component

---
+++ front/src/components/system/modal.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react"
+import { createPortal } from "react-dom"
+import { Route, RouteProps, useHistory } from "react-router-dom"
+import cl from "clsx"
+
+import styles from "./modal.module.css"
+import { Cross } from "../icons"
+
+const root = document.getElementById("modal")!
+
+export function useModal() {
+  const [isOpen, setOpened] = useState(false)
+
+  return {
+    isOpen,
+    open: () => setOpened(true),
+    close: () => setOpened(false),
+  }
+}
+
+type ModalProps = {
+  className?: string
+  children: React.ReactNode
+  onClose?: (event: React.MouseEvent) => void
+  [k: string]: any
+}
+
+const Modal = ({ className, onClose, children, ...props }: ModalProps) => {
+  return createPortal(
+    <div className={styles.modalWrapper}>
+      <div className={styles.overlay} onClick={onClose} />
+      <div {...props} className={cl(styles.modal, className)}>
+        <Cross className={styles.closeModal} onClick={onClose} />
+        {children}
+      </div>
+    </div>,
+    root
+  )
+}
+
+export const ModalRoute = ({ children, ...props }: RouteProps) => {
+  const history = useHistory()
+
+  return (
+    <Route {...props}>
+      <Modal onClose={() => history.goBack()}>{children}</Modal>
+    </Route>
+  )
+}
+
+export default Modal
