5b47a408f7ba0a8334b3279303ae0a5f9bf4197c
jfalxa <jfalxa.sta@gmail.com>
2020-09-30 17:40:25 +0200

    [front] custom hook for TransactionAdd

---
+++ front/src/hooks/use-transaction-add.ts
@@ -0,0 +1,25 @@
+import { EntitySelection } from "./use-app"
+
+import useTransactionForm from "../hooks/helpers/use-transaction-form"
+
+import useClose from "./helpers/use-close"
+import { addLots } from "../services/lots"
+
+export default function useTransactionAdd(entity: EntitySelection) {
+  const close = useClose("/transactions")
+  const { form, request, change, resolve } = useTransactionForm()
+
+  function submit() {
+    if (entity.selected && form) {
+      resolve(addLots(entity.selected.id, form))
+    }
+  }
+
+  return {
+    form,
+    request,
+    change,
+    submit,
+    close,
+  }
+}
