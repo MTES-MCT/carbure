1be14bcb88a6a90505b58e944d0e557a865e6ef7
jfalxa <jfalxa.sta@gmail.com>
2020-09-30 17:40:25 +0200

    [front] custom hook for TransactionAdd

--- front/src/routes/transaction-add.tsx
+++ front/src/routes/transaction-add.tsx
@@ -1,45 +1,22 @@
 import React from "react"
-import { Redirect, useHistory } from "react-router-dom"
-
-import useTransactionForm, {
-  toTransactionPostData,
-} from "../hooks/helpers/use-transaction-form"
-import useAPI from "../hooks/helpers/use-api"
+import { Redirect } from "react-router-dom"
 
 import { AsyncButton, Button, Title } from "../components/system"
 import Modal from "../components/system/modal"
 import TransactionForm from "../components/transaction-form"
 import { Save, Cross } from "../components/system/icons"
 import { EntitySelection } from "../hooks/use-app"
-import { addLots } from "../services/lots"
+import useTransactionAdd from "../hooks/use-transaction-add"
 
 type TransactionAddProps = {
   entity: EntitySelection
 }
 
 const TransactionAdd = ({ entity }: TransactionAddProps) => {
-  const history = useHistory()
-  const [addedLot, resolve] = useAPI()
-
-  const [form, change] = useTransactionForm()
+  const { form, request, change, submit, close } = useTransactionAdd(entity)
 
   if (entity.selected === null) {
     return <Redirect to="/transactions" />
-  }
-
-  function close() {
-    history.push("/transactions")
-  }
-
-  function submit() {
-    if (entity.selected && form) {
-      const params = {
-        entity_id: entity.selected.id,
-        ...toTransactionPostData(form),
-      }
-
-      resolve(addLots(params))
-    }
   }
 
   return (
@@ -51,7 +28,7 @@ const TransactionAdd = ({ entity }: TransactionAddProps) => {
           submit
           kind="primary"
           icon={Save}
-          loading={addedLot.loading}
+          loading={request.loading}
         >
           Créer lot
         </AsyncButton>
