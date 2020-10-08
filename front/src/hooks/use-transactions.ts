4fe0bc3571a64e1c9314e26b7022073f54e9ccba
jfalxa <jfalxa.sta@gmail.com>
2020-10-08 15:45:11 +0200

    [front] avoid unecessary set state when sorting

--- front/src/hooks/use-transactions.ts
+++ front/src/hooks/use-transactions.ts
@@ -103,9 +103,9 @@ function useSortingSelection(): SortingSelection {
   function sortBy(column: string) {
     if (current.column !== column) {
       return setCurrent({ column, order: "asc" })
-    } else if (current.order === "asc") {
+    } else if (current.column && current.order === "asc") {
       return setCurrent({ column, order: "desc" })
-    } else if (current.order === "desc") {
+    } else if (current.column && current.order === "desc") {
       return setCurrent({ column: "", order: "asc" })
     }
   }
