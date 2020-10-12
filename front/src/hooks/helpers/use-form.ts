96c14301d7fc1458fd4d48547e8155acc6abb742
jfalxa <jfalxa.sta@gmail.com>
2020-10-12 16:54:57 +0200

    [front] make useForm initial state mandatory

--- front/src/hooks/helpers/use-form.ts
+++ front/src/hooks/helpers/use-form.ts
@@ -17,13 +17,13 @@ function parseValue(element: FormFields) {
 }
 
 export type FormHook<T> = [
-  T | null,
+  T,
   <T extends FormFields>(e: React.ChangeEvent<T>) => void,
-  React.Dispatch<React.SetStateAction<T | null>>
+  React.Dispatch<React.SetStateAction<T>>
 ]
 
 export default function useForm<T>(initialState: T): FormHook<T> {
-  const [form, setForm] = useState<T | null>(initialState)
+  const [form, setForm] = useState<T>(initialState)
 
   function change<T extends FormFields>(e: React.ChangeEvent<T>) {
     if (form) {
