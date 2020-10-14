cf6656fbb2b02f800145760ee009aa1b285f8df3
Martin Planes <martin.planes@gmail.com>
2020-10-14 13:55:57 +0200

    [front] transactions-out-summary

--- front/src/components/transaction-out-summary-form.tsx
+++ front/src/components/transaction-out-summary-form.tsx
@@ -8,19 +8,45 @@ import { Box, LabelCheckbox, LabelInput, LabelTextArea } from "./system"
 import { FormFields } from "../hooks/helpers/use-form"
 
 type TransactionOutSummaryFormProps = {
-  readOnly?: boolean
   data: TransactionOutSummaryFormState
-  onChange: <T extends FormFields>(e: React.ChangeEvent<T>) => void
+  onChange: <T extends FormFields>(e: React.ChangeEvent<T>) => void,
+  loading: boolean
 }
 
 const TransactionOutSummaryForm = ({
-  readOnly = false,
+  data,
   onChange,
+  loading,
 }: TransactionOutSummaryFormProps) => {
   return (
       <Box row>
-        <Box>
-        </Box>
+        {loading && <p>Chargement</p> }
+        {loading || 
+          <table>
+            <thead>
+              <tr>
+                <th>Client</th>
+                <th>Site de Livraison</th>
+                <th>Biocarburant</th>
+                <th>Volume</th>
+                <th>Réduction de GES</th>
+              </tr>
+            </thead>
+            <tbody>
+            {Object.entries(data).map(([client, sites]) => (
+              Object.entries(sites).map(([delivery_site, biocarburants]) => (
+                Object.entries(biocarburants).map(([biocarburant, values]) => (
+                  <tr>
+                    <td>{client}</td>
+                    <td>{delivery_site}</td>
+                    <td>{biocarburant}</td>
+                    <td>{values.volume}L</td>
+                    <td>{values.avg_ghg_reduction}%</td>
+                  </tr> 
+                ))))))}       
+            </tbody>
+          </table>
+        }
       </Box>
   )
 }
