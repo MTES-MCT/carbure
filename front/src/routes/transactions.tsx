72da425e66d76c5561d90879a40aec16b37a530c
Martin Planes <martin.planes@gmail.com>
2020-10-19 09:30:04 +0200

    [front] page Stocks (WIP)

--- front/src/routes/transactions.tsx
+++ front/src/routes/transactions.tsx
@@ -4,13 +4,13 @@ import useTransactions from "../hooks/use-transactions"
 
 import { Main } from "../components/system"
 import { Route, Switch } from "../components/relative-route"
-import TransactionSnapshot from "../components/transaction-snapshot"
+import { TransactionSnapshot, StockSnapshot } from "../components/transaction-snapshot"
 import TransactionList from "../components/transaction-list"
 import TransactionDetails from "./transaction-details"
 import TransactionAdd from "./transaction-add"
 import TransactionOutSummary from "./transaction-out-summary"
 
-const Transactions = () => {
+export const Transactions = () => {
   const {
     entity,
     status,
@@ -73,6 +73,55 @@ const Transactions = () => {
           />
         </Route>
       </Switch>
+    </Main>
+  )
+}
+
+export const Stocks = () => {
+   const {
+    entity,
+    status,
+    filters,
+    year,
+    pagination,
+    snapshot,
+    transactions,
+    selection,
+    search,
+    sorting,
+    deleter,
+    duplicator,
+    validator,
+    uploader,
+    refresh,
+  } = useTransactions()
+
+  if (entity === null) {
+    return null
+  }
+
+  return (
+    <Main>
+      <StockSnapshot
+        snapshot={snapshot}
+        status={status}
+        filters={filters}
+        year={year}
+        search={search}
+      />
+
+      <TransactionList
+        transactions={transactions}
+        status={status}
+        sorting={sorting}
+        selection={selection}
+        pagination={pagination}
+        uploader={uploader}
+        deleter={deleter}
+        validator={validator}
+        onDuplicate={duplicator.resolve}
+        onExportAll={transactions.exportAll}
+      />
     </Main>
   )
 }
