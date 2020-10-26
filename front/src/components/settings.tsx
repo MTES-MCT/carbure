d82fab8d06633d8cef73fc8900b4d8a65c590d7b
Martin Planes <martin.planes@gmail.com>
2020-10-26 13:32:26 +0100

    [front] settings

--- front/src/components/settings.tsx
+++ front/src/components/settings.tsx
@@ -1,57 +1,65 @@
 import React from "react"
+import { EntitySelection } from "../hooks/helpers/use-entity"
+import { UserRight } from "../services/types"
 
 import styles from "./settings.module.css"
+import { ApiState } from "../hooks/helpers/use-api"
+import { Settings } from "../services/types"
 
-import { Title, Box } from "./system"
+import { Box } from "./system"
 
-type AnnuaireSettingsProps = {
-    data: number
+type GeneralSettingsProps = {
+  entity: EntitySelection,
+  settings: ApiState<Settings>,
 }
   
-export const AnnuaireSettings = ({
-    data,
-}: AnnuaireSettingsProps) => (
-    <Box>
-        <p>Annuaire</p>
-    </Box>
-)
+export const GeneralSettings = ({
+  entity,
+  settings,
+}: GeneralSettingsProps) => {
+  const currentEntitySettings: UserRight | undefined = settings.data?.rights.find(element => element.entity.id === entity)
 
-type MACSettingsProps = {
-    data: number
-}
-  
-export const MACSettings = ({
-    data,
-}: MACSettingsProps) => (
-    <Box>
-        <p>MAC</p>
-    </Box>
-)
+  if (currentEntitySettings === undefined) {
+    return null
+  }
 
+  return (
+    <Box>
+      <fieldset>
+        <h3>Mises à Consommation</h3>
+        <input type="checkbox" checked={currentEntitySettings.entity.has_mac} name="checkbox" id="checkbox-mac" />
+        <label>Ma société effectue des Mises à Consommation</label>
+        <h3>Trading</h3>
+        <input type="checkbox" checked={currentEntitySettings.entity.has_trading} name="checkbox" id="checkbox-trading" />
+        <label>Ma société a une activité de négoce</label>
+      </fieldset>
+    </Box>  
+  )
+} 
   
 type ProductionSitesSettingsProps = {
-    data: number
+  data: number
 }
   
 export const ProductionSitesSettings = ({
-    data,
+  data,
 }: ProductionSitesSettingsProps) => (
-    <Box>
-        <p>Annuaire</p>
-    </Box>
+  <Box>
+    <p>Sites de Production</p>
+  </Box>
 )
 
   
 type DeliverySitesSettingsProps = {
-    data: number
+  data: number
 }
   
 export const DeliverySitesSettings = ({
-    data,
+  data,
 }: DeliverySitesSettingsProps) => (
-    <Box>
-        <p>DeliverySites</p>
-    </Box>
+  <Box>
+    <p>Dépôts</p>
+  </Box>
 )
 
   
