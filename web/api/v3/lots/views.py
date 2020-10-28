5eb206e6ed6a38e0d1c7900361599353e27f1531
Martin Planes <martin.planes@gmail.com>
2020-10-28 16:15:07 +0100

    [api] v3 update reject and reject-all

--- web/api/v3/lots/views.py
+++ web/api/v3/lots/views.py
@@ -501,8 +501,12 @@ def accept_lot(request):
 
 def reject_lot(request):
     tx_ids = request.POST.get('tx_ids', None)
+    tx_comment = request.POST.get('comment', None)
     if not tx_ids:
-        return JsonResponse({'status': 'forbidden', 'message': "Missing tx_ids"}, status=403)
+        return JsonResponse({'status': 'error', 'message': "Missing tx_ids"}, status=400)
+    if not tx_comment:
+        return JsonResponse({'status': 'error', 'message': "Missing comment"}, status=400)
+
     for tx_id in tx_ids:
         if tx_id is None:
             return JsonResponse({'status': 'error', 'message': "Missing TX ID from POST data"}, status=400)
@@ -517,6 +521,11 @@ def reject_lot(request):
             return JsonResponse({'status': 'forbidden', 'message': "User not allowed"}, status=403)
         tx.delivery_status = 'R'
         tx.save()
+        txerr = TransactionComment()
+        txerr.entity = tx.carbure_client
+        txerr.tx = tx
+        txerr.comment = tx_comment
+        txerr.save()
     return JsonResponse({'status': 'success'})
 
 
@@ -608,6 +617,46 @@ def accept_all(request):
         except Exception:
             return JsonResponse({'status': 'error', 'message': 'Incorrect format for year. Expected YYYY'}, status=400)
     lots.filter(delivery_date__gte=date_from).filter(delivery_date__lte=date_until).update(delivery_status='A')
+    return JsonResponse({'status': 'success'})
+
+
+def reject_all(request):
+    entity_id = request.POST.get('entity_id', False)
+    if not entity_id:
+        return JsonResponse({'status': 'error', 'message': "Missing entity_id"}, status=400)
+    tx_comment = request.POST.get('comment', None)
+    if not tx_comment:
+        return JsonResponse({'status': 'error', 'message': "Missing comment"}, status=400)
+    try:
+        entity = Entity.objects.get(id=entity_id)
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': "Unknown entity %s" % (entity_id), 'extra': str(e)},
+                            status=400)
+
+    rights = [r.entity for r in UserRights.objects.filter(user=request.user)]
+    if entity not in rights:
+        return JsonResponse({'status': 'forbidden', 'message': "User not allowed"}, status=403)
+
+    txs = LotTransaction.objects.filter(carbure_client=entity, delivery_status__in=[
+        'N', 'AC', 'AA'])
+    year = request.POST.get('year', False)
+    date_from = datetime.date.today().replace(month=1, day=1)
+    date_until = datetime.date.today().replace(month=12, day=31)
+    if year:
+        try:
+            year = int(year)
+            date_from = datetime.date(year=year, month=1, day=1)
+            date_until = datetime.date(year=year, month=12, day=31)
+        except Exception:
+            return JsonResponse({'status': 'error', 'message': 'Incorrect format for year. Expected YYYY'}, status=400)
+    txs = txs.filter(delivery_date__gte=date_from).filter(delivery_date__lte=date_until)
+    txs.update(delivery_status='R')
+    for tx in txs:
+        txerr = TransactionComment()
+        txerr.entity = tx.carbure_client
+        txerr.tx = tx
+        txerr.comment = tx_comment
+        txerr.save()
     return JsonResponse({'status': 'success'})
 
 
