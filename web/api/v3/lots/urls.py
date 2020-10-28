2452496abefb8f914b36d15e583644b1ba3531ed
Martin Planes <martin.planes@gmail.com>
2020-10-28 16:15:07 +0100

    [api] v3 update reject and reject-all

--- web/api/v3/lots/urls.py
+++ web/api/v3/lots/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('delete-all-drafts', views.delete_all_drafts, name='api-v3-delete-all-drafts'),
     path('validate-all-drafts', views.validate_all_drafts, name='api-v3-validate-all-drafts'),
     path('accept-all', views.accept_all, name='api-v3-accept-all'),
+    path('reject-all', views.reject_all, name='api-v3-reject-all'),
 
     # IMPORT/FILES
     path('upload', views.upload, name='api-v3-upload'),
