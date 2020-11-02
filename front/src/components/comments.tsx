d2d3c50c29e47f268960f83d5c0b76eb47e147af
jfalxa <jfalxa.sta@gmail.com>
2020-11-02 18:24:47 +0100

    [front] create prompts for comments in dialogs

--- front/src/components/comments.tsx
+++ front/src/components/comments.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react"
 import { Comment } from "../services/types"
 import styles from "./comments.module.css"
-import { AsyncButton, Box, Input } from "./system"
+import { AsyncButton, Box, Button, Input, LabelInput } from "./system"
 import { Collapsible } from "./system/alert"
+import { PromptFormProps } from "./system/dialog"
+import RadioGroup from "./system/radio-group"
 
 type CommentsProps = {
   loading: boolean
@@ -49,6 +51,96 @@ const Comments = ({ loading, comments, onComment }: CommentsProps) => {
         </AsyncButton>
       </Box>
     </Collapsible>
+  )
+}
+
+export const CommentPrompt = ({
+  onConfirm,
+  onCancel,
+}: PromptFormProps<string>) => {
+  const [comment, setComment] = useState("")
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
+    onConfirm(comment)
+  }
+
+  return (
+    <Box as="form" onSubmit={onSubmit}>
+      <LabelInput
+        label="Commentaire (obligatoire)"
+        value={comment}
+        className={styles.promptInput}
+        onChange={(e) => setComment(e.target.value)}
+      />
+
+      <Box row className={styles.dialogButtons}>
+        <Button
+          level="primary"
+          disabled={!comment}
+          onClick={() => onConfirm(comment)}
+        >
+          OK
+        </Button>
+        <Button onClick={onCancel}>Annuler</Button>
+      </Box>
+    </Box>
+  )
+}
+
+interface CommentWithType {
+  comment: string
+  topic: string
+}
+
+// prettier-ignore
+const TOPICS = [
+  { value: "sustainability", label: "Il y a un problème relatif à la durabilité ou aux caractéristiques du lot (provenance, volume, gaz à effet de serre)" },
+  { value: "tx", label: "Il y a un problème sur la transaction (numéro douanier, site de livraison, date)" },
+  { value: "both", label: "Les deux" },
+]
+
+export const CommentWithTypePrompt = ({
+  onConfirm,
+  onCancel,
+}: PromptFormProps<CommentWithType>) => {
+  const [comment, setComment] = useState("")
+  const [topic, setTopic] = useState("")
+
+  function onChangeTopic(e: React.ChangeEvent<HTMLInputElement>) {
+    setTopic(e.target.value)
+  }
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault()
+
+    if (comment && topic) {
+      onConfirm({ comment, topic })
+    }
+  }
+
+  return (
+    <Box as="form" onSubmit={onSubmit}>
+      <RadioGroup value={topic} options={TOPICS} onChange={onChangeTopic} />
+
+      <LabelInput
+        label="Commentaire (obligatoire)"
+        value={comment}
+        className={styles.promptInput}
+        onChange={(e) => setComment(e.target.value)}
+      />
+
+      <Box row className={styles.dialogButtons}>
+        <Button
+          level="primary"
+          disabled={!comment || !topic}
+          onClick={() => onConfirm({ comment, topic })}
+        >
+          Accepter et demander une correction
+        </Button>
+        <Button onClick={onCancel}>Annuler</Button>
+      </Box>
+    </Box>
   )
 }
 
