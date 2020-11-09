b71c991a437881d2b6544cfdb647e686bc429ef1
Martin Planes <martin.planes@gmail.com>
2020-11-09 15:53:13 +0100

    [cron]

--- cron/download_iscc_certificates.sh
+++ cron/download_iscc_certificates.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker cp carbure_app:/app/web/fixtures/fetch_iscc_certificates.py /tmp
-python3 /tmp/fetch_iscc_certificates.py
-docker cp /tmp/Certificates_$(date +%Y_%m_%d).csv carbure_app:/tmp
-docker exec carbure_app python3 /app/web/fixtures/load_iscc_certificates.py
+python3 /app/web/fixtures/fetch_iscc_certificates.py
+python3 /app/web/fixtures/load_iscc_certificates.py
