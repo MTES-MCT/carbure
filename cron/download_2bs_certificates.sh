bd1bd682f0c1f7ec36e45f1c4f057c9ad4504e4f
Martin Planes <martin.planes@gmail.com>
2020-11-09 15:53:13 +0100

    [cron]

--- cron/download_2bs_certificates.sh
+++ cron/download_2bs_certificates.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-docker cp carbure_app:/app/web/fixtures/fetch_2bs_certificates.py /tmp
-python3 /tmp/fetch_2bs_certificates.py
-docker cp /tmp/Certificates_2bs_$(date +%Y_%m_%d).csv carbure_app:/tmp
-docker exec carbure_app python3 /app/web/fixtures/load_2bs_certificates.py
+python3 /app/web/fixtures/fetch_2bs_certificates.py
+python3 /app/web/fixtures/load_2bs_certificates.py
