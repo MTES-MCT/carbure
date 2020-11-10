b61d9629435a058b1c652a4ca30f52282c3c5878
Martin Planes <martin.planes@gmail.com>
2020-11-10 12:29:09 +0100

    [back] remove unused views

--- web/public/views.py
+++ web/public/views.py
@@ -13,34 +13,5 @@ def index(request):
     return render(request, 'public/index.html', context)
 
 
-@login_required
-@enrich_with_user_details
-def home(request, *args, **kwargs):
-    context = kwargs['context']
-    if context['user_entity'].entity_type == 'Administration':
-        return redirect('administrators-index')
-    elif context['user_entity'].entity_type == 'Producteur':
-        return redirect('producers-index', producer_name=context['url_friendly_name'])
-    elif context['user_entity'].entity_type == 'Opérateur':
-        return redirect('operators-index', operator_name=context['url_friendly_name'])
-    elif context['user_entity'].entity_type == 'Trader':
-        return redirect('traders-index', trader_name=context['url_friendly_name'])
-    else:
-        raise Http404("Unknown User Type")
-
-
-def htmlreference(request):
-    context = {}
-    return render(request, 'common/reference.html', context)
-
-
-@login_required
-@enrich_with_user_details
-def annuaire(request, *args, **kwargs):
-    context = kwargs['context']
-    context['current_url_name'] = 'annuaire'
-    return render(request, 'common/annuaire.html', context)
-
-
 def stats(request):
     return redirect('https://metabase.carbure.beta.gouv.fr/public/dashboard/a9c045a5-c2fb-481a-ab85-f55bce8ae3c0')
