343756bfe579437664ee2833b6744117fa2efae9
Martin Planes <martin.planes@gmail.com>
2020-11-11 14:43:51 +0100

    [urls]

--- web/public/urls.py
+++ web/public/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home'), # LOGIN_REDIRECT_URL: will dispatch to either /producers/, /operators/ or /administrators/ depending on account type
-    path('annuaire', views.annuaire, name='annuaire'),
-    path('htmlreference', views.htmlreference, name='htmlreference'),
     path('stats', views.stats, name='stats'),
 ]
