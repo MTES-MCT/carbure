fd762a7510a83d24dedd54534dd328996f3ae1fe
Martin Planes <martin.planes@gmail.com>
2020-11-11 13:23:58 +0100

    [cron] only send email in prod

--- cron/download_2bs_certificates.sh
+++ cron/download_2bs_certificates.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 python3 /app/web/fixtures/fetch_2bs_certificates.py
-python3 /app/web/fixtures/load_2bs_certificates.py --email
+
+if [ "$IMAGE_TAG" = "prod" ]; then
+    python3 /app/web/fixtures/load_2bs_certificates.py --email
+else
+    python3 /app/web/fixtures/load_2bs_certificates.py
+fi    
+
+
