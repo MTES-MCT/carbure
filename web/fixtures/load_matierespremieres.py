04e3d1f2bff243ea3d0376885f3fcbcec33e7b1f
Martin Planes <martin.planes@gmail.com>
2020-11-11 11:45:23 +0100

    [fixtures] add flag huile_vegetale on matieres premieres

--- web/fixtures/load_matierespremieres.py
+++ web/fixtures/load_matierespremieres.py
@@ -13,7 +13,13 @@ with open(filename) as csvfile:
     reader = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in reader:
         code = row[0]
-        name = row[1]
+        if code == 'code':
+            # header
+            continue
+       
+        name = row[1]        
         compat_alcool = row[2]
         compat_graisse = row[3]
-        obj, created = MatierePremiere.objects.update_or_create(code=code, defaults={'name':name, 'description':'', 'compatible_alcool': compat_alcool, 'compatible_graisse': compat_graisse})
+        is_double_compte = row[4]
+        is_huile_vegetale = row[5]
+        obj, created = MatierePremiere.objects.update_or_create(code=code, defaults={'name':name, 'description':'', 'compatible_alcool': compat_alcool, 'compatible_graisse': compat_graisse, 'is_double_compte': is_double_compte, 'is_huile_vegetale': is_huile_vegetale})
