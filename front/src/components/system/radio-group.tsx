0e254ba7b628e03d41482ade669387f83fdfdad1
jfalxa <jfalxa.sta@gmail.com>
2020-11-12 18:03:42 +0100

    [front] add radio group for depot type

--- front/src/components/system/radio-group.tsx
+++ front/src/components/system/radio-group.tsx
@@ -5,18 +5,27 @@ import styles from "./radio-group.module.css"
 
 type RadioGroupProps = {
   value: string
+  name?: string
+  row?: boolean
   options: { value: string; label: string }[]
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const RadioGroup = ({ value, options, onChange }: RadioGroupProps) => (
-  <Box className={styles.radioGroup}>
+const RadioGroup = ({
+  row,
+  value,
+  name,
+  options,
+  onChange,
+}: RadioGroupProps) => (
+  <Box row={row} className={styles.radioGroup}>
     {options.map((option) => (
       <Box row as="label" key={option.value} className={styles.radioGroupLabel}>
         <input
           type="radio"
           checked={option.value === value}
           value={option.value}
+          name={name}
           className={styles.radioGroupButton}
           onChange={onChange}
         />
