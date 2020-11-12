7fe0b16ce27039119522504ab898b34472fcbc3a
Martin Planes <martin.planes@gmail.com>
2020-11-12 17:30:40 +0100

    [api] sanity_checks not in background anymore

--- cron/download_iscc_certificates.sh
+++ cron/download_iscc_certificates.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 python3 /app/web/fixtures/fetch_iscc_certificates.py
-python3 /app/web/fixtures/load_iscc_certificates.py
+
+if [ "$IMAGE_TAG" = "prod" ]; then
+    python3 /app/web/fixtures/load_iscc_certificates.py --email
+else
+    python3 /app/web/fixtures/load_iscc_certificates.py
+fi    
+
+
