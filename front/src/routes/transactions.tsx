349366ad42dcd4ea5f902dbfbbf77df41eba9986
jfalxa <jfalxa.sta@gmail.com>
2020-11-18 16:54:06 +0100

    [front] try to stay as much as possible on the same page when switching entity

--- front/src/routes/transactions.tsx
+++ front/src/routes/transactions.tsx
@@ -5,7 +5,7 @@ import { EntitySelection } from "../hooks/helpers/use-entity"
 import useTransactions from "../hooks/use-transactions"
 
 import { Main } from "../components/system"
-import { Route, Switch } from "../components/relative-route"
+import { Redirect, Route, Switch } from "../components/relative-route"
 import { TransactionSnapshot } from "../components/transaction/transaction-snapshot"
 import { TransactionList } from "../components/transaction/transaction-list"
 import TransactionFilters from "../components/transaction/transaction-filters"
@@ -14,6 +14,21 @@ import TransactionDetails from "./transaction/transaction-details"
 import TransactionAdd from "./transaction/transaction-add"
 import TransactionOutSummary from "./transaction/transaction-out-summary"
 import TransactionInSummary from "./transaction/transaction-in-summary"
+import { EntityType, LotStatus } from "../services/types"
+
+// prettier-ignore
+const OPERATOR_STATUSES = [
+  LotStatus.Draft,
+  LotStatus.Inbox,
+  LotStatus.Accepted,
+]
+
+const PRODUCTOR_TRADER_STATUSES = [
+  LotStatus.Draft,
+  LotStatus.Validated,
+  LotStatus.ToFix,
+  LotStatus.Accepted,
+]
 
 export const Transactions = ({ entity }: { entity: EntitySelection }) => {
   const {
@@ -38,6 +53,21 @@ export const Transactions = ({ entity }: { entity: EntitySelection }) => {
 
   if (entity === null) {
     return null
+  }
+
+  const isTrader = entity.entity_type === EntityType.Trader
+  const isOperator = entity.entity_type === EntityType.Operator
+  const isProducer = entity.entity_type === EntityType.Producer
+
+  if (isOperator && !OPERATOR_STATUSES.includes(status.active)) {
+    return <Redirect relative to=".." />
+  }
+
+  if (
+    (isProducer || isTrader) &&
+    !PRODUCTOR_TRADER_STATUSES.includes(status.active)
+  ) {
+    return <Redirect relative to=".." />
   }
 
   return (
