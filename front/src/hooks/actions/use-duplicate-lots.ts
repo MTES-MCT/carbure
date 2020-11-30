c378c733ced5cc157e8c9b39c2eeefad485a5c43
jfalxa <jfalxa.sta@gmail.com>
2020-11-30 15:04:03 +0100

    [front] add notification to duplicate lot

--- front/src/hooks/actions/use-duplicate-lots.ts
+++ front/src/hooks/actions/use-duplicate-lots.ts
@@ -4,6 +4,7 @@ import * as api from "../../services/lots"
 import useAPI from "../helpers/use-api"
 
 import { confirm } from "../../components/system/dialog"
+import { useNotificationContext } from "../../components/system/notifications"
 
 export interface LotDuplicator {
   loading: boolean
@@ -14,6 +15,7 @@ export default function useDuplicateLot(
   entity: EntitySelection,
   refresh: () => void
 ): LotDuplicator {
+  const notifications = useNotificationContext()
   const [request, resolveDuplicate] = useAPI(api.duplicateLot)
 
   async function duplicateLot(lotID: number) {
@@ -23,7 +25,14 @@ export default function useDuplicateLot(
     )
 
     if (entity !== null && shouldDuplicate) {
-      await resolveDuplicate(entity.id, lotID).then(refresh)
+      await resolveDuplicate(entity.id, lotID)
+
+      refresh()
+
+      notifications.push({
+        level: "success",
+        text: "Le lot a bien été dupliqué !",
+      })
     }
 
     return shouldDuplicate
