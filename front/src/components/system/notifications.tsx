6be89a098005843561ece0bb11aee4d6e0176af9
jfalxa <jfalxa.sta@gmail.com>
2020-11-30 14:15:22 +0100

    [front] add different levels of notifications

--- front/src/components/system/notifications.tsx
+++ front/src/components/system/notifications.tsx
@@ -1,9 +1,11 @@
-import React, { useCallback, useState } from "react"
+import React, { useCallback, useContext, useState } from "react"
+import cl from "clsx"
 import ReactDOM from "react-dom"
-import { Box } from "."
-import { Cross } from "./icons"
 
 import styles from "./notifications.module.css"
+
+import { Box } from "."
+import { Cross } from "./icons"
 
 const DEFAULT_TIMEOUT = 5000
 
@@ -11,21 +13,26 @@ const portal = document.getElementById("notifications")!
 
 interface Notification {
   key: string
+  level: "default" | "success" | "error" | "warning"
   text: string
   timeout: NodeJS.Timeout
 }
 
 interface NotificationsHook {
   list: Notification[]
-  push: (n: string, t?: number) => void
+  push: (n: {
+    text: string
+    duration?: number
+    level?: Notification["level"]
+  }) => void
   dispose: (k: string) => void
 }
 
 function useNotifications(): NotificationsHook {
   const [notifications, setNotifications] = useState<Notification[]>([])
 
-  const push = useCallback(
-    (text: string, duration: number = DEFAULT_TIMEOUT) => {
+  const push: NotificationsHook["push"] = useCallback(
+    ({ text, duration = DEFAULT_TIMEOUT, level = "default" }) => {
       const key = `${text}-${Date.now()}`
 
       // set auto close with timeout
@@ -33,7 +40,7 @@ function useNotifications(): NotificationsHook {
         setNotifications((list) => list.filter((n) => n.key !== key))
       }, duration)
 
-      const notification = { key, text, timeout }
+      const notification = { key, text, timeout, level }
       setNotifications((list) => [...list, notification])
 
       return notification
@@ -68,12 +75,22 @@ const Notifications = ({ notifications }: NotificationsProps) => {
 
   return ReactDOM.createPortal(
     <Box as="ul" className={styles.notificationsWrapper}>
-      {notifications.list.map((notification, i) => (
-        <Box as="li" row key={i} className={styles.notification}>
-          {notification.text}
+      {notifications.list.map((n, i) => (
+        <Box
+          as="li"
+          row
+          key={i}
+          className={cl(
+            styles.notification,
+            n.level === "success" && styles.success,
+            n.level === "warning" && styles.warning,
+            n.level === "error" && styles.error
+          )}
+        >
+          {n.text}
           <Cross
             title="Fermer la notification"
-            onClick={() => notifications.dispose(notification.key)}
+            onClick={() => notifications.dispose(n.key)}
           />
         </Box>
       ))}
@@ -82,11 +99,15 @@ const Notifications = ({ notifications }: NotificationsProps) => {
   )
 }
 
-export const NotificationsContext = React.createContext<NotificationsHook>({
+const NotificationsContext = React.createContext<NotificationsHook>({
   list: [],
   push: () => {},
   dispose: () => {},
 })
+
+export function useNotificationContext() {
+  return useContext(NotificationsContext)
+}
 
 type NotificationsProviderProps = {
   children: React.ReactNode
