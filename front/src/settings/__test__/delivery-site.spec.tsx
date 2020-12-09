91371f7deac3cea5afd09a889af3f3c0a1597ef7
jfalxa <jfalxa.sta@gmail.com>
2020-12-09 17:17:08 +0100

    [front] test: delete delivery site

--- front/src/settings/__test__/delivery-site.spec.tsx
+++ front/src/settings/__test__/delivery-site.spec.tsx
@@ -2,11 +2,11 @@ import { render, waitFor, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 
 import { Entity } from "common/types"
-import { producer } from "common/__test__/data"
+import { deliverySite, producer } from "common/__test__/data"
 import { useGetSettings } from "settings/hooks/use-get-settings"
 import Settings from "../index"
 
-import server from "./api"
+import server, { setDeliverySites } from "./api"
 
 const SettingsWithHooks = ({ entity }: { entity: Entity }) => {
   const settings = useGetSettings()
@@ -14,14 +14,24 @@ const SettingsWithHooks = ({ entity }: { entity: Entity }) => {
 }
 
 beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
+
+afterEach(() => {
+  server.resetHandlers()
+  setDeliverySites([])
+})
+
 afterAll(() => server.close())
 
 test("check the delivery site section of the settings", async () => {
   render(<SettingsWithHooks entity={producer} />)
 
   screen.getByText("Dépôts")
+  screen.getByText("Ajouter un dépôt")
   screen.getByText("Aucun dépôt trouvé")
+})
+
+test("add a delivery site in settings", async () => {
+  render(<SettingsWithHooks entity={producer} />)
 
   const button = screen.getByText("Ajouter un dépôt")
 
@@ -45,6 +55,34 @@ test("check the delivery site section of the settings", async () => {
   userEvent.click(screen.getByText("Ajouter"))
 
   await waitFor(() => {
+    screen.getByText("10")
     screen.getByText("Test Delivery Site")
+    screen.getByText("Autre")
+    screen.getByText("Test City, France")
+  })
+})
+
+test("remove a delivery site section in settings", async () => {
+  setDeliverySites([deliverySite])
+
+  render(<SettingsWithHooks entity={producer} />)
+
+  screen.getByText("Dépôts")
+
+  const deleteButton = await waitFor(() => {
+    screen.getByText("10")
+    screen.getByText("Test Delivery Site")
+    screen.getByText("Autre")
+    screen.getByText("Test City, France")
+
+    return screen.getByTitle("Supprimer le dépôt").closest("svg")!
+  })
+
+  // click on the delete button and then confirm the action on the popup
+  userEvent.click(deleteButton)
+  userEvent.click(screen.getByText("OK"))
+
+  await waitFor(() => {
+    screen.getByText("Aucun dépôt trouvé")
   })
 })
