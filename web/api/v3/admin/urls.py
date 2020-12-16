4f7fde67f4721ca54eb472bdb6955db251804346
Martin Planes <martin.planes@gmail.com>
2020-12-16 12:04:11 +0100

    [tests] add test suite for admin api

--- web/api/v3/admin/urls.py
+++ web/api/v3/admin/urls.py
@@ -6,13 +6,12 @@ urlpatterns = [
     path('entities', views.get_entities, name='api-v3-admin-get-entities'),
     path('rights', views.get_rights, name='api-v3-admin-get-rights'),
 
-    # TODO
     path('rights/add', views.add_rights, name='api-v3-admin-add-rights'),
     path('rights/delete', views.delete_rights, name='api-v3-admin-delete-rights'),
     path('entities/add', views.add_entity, name='api-v3-admin-add-entity'),
-    path('entities/del', views.delete_entity, name='api-v3-admin-del-entity'),
+    path('entities/del', views.delete_entity, name='api-v3-admin-delete-entity'),
     path('users/add', views.add_user, name='api-v3-admin-add-user'),
     path('users/reset-password', views.reset_user_password, name='api-v3-admin-reset-user-password'),
-    path('users/del', views.delete_user, name='api-v3-admin-del-user'),
+    path('users/del', views.delete_user, name='api-v3-admin-delete-user'),
 
 ]
