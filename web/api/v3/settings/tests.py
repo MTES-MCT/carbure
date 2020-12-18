0d1f133930f5dcae5724ef13ffa1bb7c8f11979a
Martin Planes <martin.planes@gmail.com>
2020-12-18 14:00:05 +0100

    [tests] api/v3/settings almost done

--- web/api/v3/settings/tests.py
+++ web/api/v3/settings/tests.py
@@ -1,8 +1,10 @@
+import datetime
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
 from core.models import Entity, UserRights, Pays, MatierePremiere, Biocarburant, Depot, EntityDepot
+from core.models import ISCCCertificate, DBSCertificate
 from producers.models import ProductionSite, ProductionSiteInput, ProductionSiteOutput
 from api.v3.admin.urls import urlpatterns
 
@@ -235,4 +237,166 @@ class SettingsAPITest(TestCase):
         self.assertEqual(len(data), 0)
 
     def test_iscc_certificates(self):
+        url_get = 'api-v3-settings-get-iscc-certificates'
+        url_add = 'api-v3-settings-add-iscc-certificate'
+        url_delete = 'api-v3-settings-delete-iscc-certificate'
+        # get 0
+        response = self.client.get(reverse(url_get), {'entity_id': self.entity1.id})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()['data']
+        self.assertEqual(len(data), 0)        
+        # add 1
+        certid = 'ISCC-TEST-01'
+        today = datetime.date.today()
+        crtdata = {'certificate_holder': 'MTES Robot', 'addons':'', 
+        'valid_from':today, 'valid_until':today, 'issuing_cb':'veritas', 
+        'location':'paris', 'download_link':'http://localhost/test.pdf'}
+        crt, _ = ISCCCertificate.objects.update_or_create(certificate_id=certid, defaults=crtdata)
+        postdata = {'entity_id': self.entity1.id, 'certificate_id': certid}
+        response = self.client.post(reverse(url_add), postdata)
+        self.assertEqual(response.status_code, 200)        
+        # get 1
+        response = self.client.get(reverse(url_get), {'entity_id': self.entity1.id})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()['data']
+        self.assertEqual(len(data), 1)            
+        # delete 1
+        postdata = {'entity_id': self.entity1.id, 'certificate_id': certid}
+        response = self.client.post(reverse(url_delete), postdata)
+        self.assertEqual(response.status_code, 200)
+        # get 0
+        response = self.client.get(reverse(url_get), {'entity_id': self.entity1.id})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()['data']
+        self.assertEqual(len(data), 0)
+
+    def test_2bs_certificates(self):
+        url_get = 'api-v3-settings-get-2bs-certificates'
+        url_add = 'api-v3-settings-add-2bs-certificate'
+        url_delete = 'api-v3-settings-delete-2bs-certificate'
+        # get 0
+        response = self.client.get(reverse(url_get), {'entity_id': self.entity1.id})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()['data']
+        self.assertEqual(len(data), 0)        
+        # add 1
+        certid = '2BS-TEST-01'
+        today = datetime.date.today()
+        crtdata = {'certificate_holder': 'MTES Robot', 
+        'valid_from':today, 'valid_until':today, 'holder_address':'paris france', 
+        'certification_type':'TST TST', 'download_link':'http://localhost/test.pdf'}
+        crt, _ = DBSCertificate.objects.update_or_create(certificate_id=certid, defaults=crtdata)
+        postdata = {'entity_id': self.entity1.id, 'certificate_id': certid}
+        response = self.client.post(reverse(url_add), postdata)
+        self.assertEqual(response.status_code, 200)        
+        # get 1
+        response = self.client.get(reverse(url_get), {'entity_id': self.entity1.id})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()['data']
+        self.assertEqual(len(data), 1)            
+        # delete 1
+        postdata = {'entity_id': self.entity1.id, 'certificate_id': certid}
+        response = self.client.post(reverse(url_delete), postdata)
+        self.assertEqual(response.status_code, 200)
+        # get 0
+        response = self.client.get(reverse(url_get), {'entity_id': self.entity1.id})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()['data']
+        self.assertEqual(len(data), 0)           
+
+    def test_advanced_certificates_behaviour(self):
+        # get my certificates 0
+        url_get_crts = 'api-v3-settings-get-my-certificates'
+        response = self.client.get(reverse(url_get_crts), {'entity_id': self.entity1.id})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()['data']
+        self.assertEqual(len(data), 0)           
+        # add 1 iscc and 1 2bs certificate
+        ## start by creating iscc cert
+        certid1 = 'ISCC-TEST-01'
+        today = datetime.date.today()
+        crtdata = {'certificate_holder': 'MTES Robot', 'addons':'', 
+        'valid_from':today, 'valid_until':today, 'issuing_cb':'veritas', 
+        'location':'paris', 'download_link':'http://localhost/test.pdf'}
+        crt, _ = ISCCCertificate.objects.update_or_create(certificate_id=certid1, defaults=crtdata)
+        ## then 2bs
+        certid2 = '2BS-TEST-01'
+        crtdata = {'certificate_holder': 'MTES Robot', 
+        'valid_from':today, 'valid_until':today, 'holder_address':'paris france', 
+        'certification_type':'TST TST', 'download_link':'http://localhost/test.pdf'}
+        crt, _ = DBSCertificate.objects.update_or_create(certificate_id=certid2, defaults=crtdata)        
+        ## add those certificates to my company
+        postdata = {'entity_id': self.entity1.id, 'certificate_id': certid1}
+        response = self.client.post(reverse('api-v3-settings-add-iscc-certificate'), postdata)
+        self.assertEqual(response.status_code, 200)     
+        postdata = {'entity_id': self.entity1.id, 'certificate_id': certid2}
+        response = self.client.post(reverse('api-v3-settings-add-2bs-certificate'), postdata)
+        self.assertEqual(response.status_code, 200)             
+        ## and a production site
+        france, _ = Pays.objects.update_or_create(code_pays='FR', name='France')
+        psite = {'country_code':'FR', 'name':'Site prod 1', 
+                 'date_mise_en_service': '2020-12-01', 'ges_option': 'ACTUAL', 
+                 'entity_id': self.entity1.id, 'eligible_dc': 'true', 
+                 'dc_reference': 'DC-FR-12-493', 'site_id': 'FR0001', 
+                 'city':'Seynod', 'postal_code':'74600', 
+                 'manager_name':'Gaston Gasoil', 'manager_phone': '0145247000', 
+                 'manager_email':'g.gasoil@superpetrole.com'}
+        ## add the production site to my company
+        response = self.client.post(reverse('api-v3-settings-add-production-site'), psite)
+        self.assertEqual(response.status_code, 200)        
+        site = ProductionSite.objects.get(site_id='FR0001')
+        # set iscc certificate and 2bs certificate to production site
+        postdata = {'entity_id': self.entity1.id, 'production_site_id': site.id, 'certificate_ids': [certid1, certid2]}
+        response = self.client.post(reverse('api-v3-settings-set-production-site-certificates'), postdata)
+        self.assertEqual(response.status_code, 200)        
+        # get my certificates 2
+        response = self.client.get(reverse(url_get_crts), {'entity_id': self.entity1.id})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()['data']
+        self.assertEqual(len(data), 2)
+        # get-production sites, check if they contain the certificates
+        response = self.client.get(reverse('api-v3-settings-get-production-sites'), {'entity_id': self.entity1.id})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()['data']
+        self.assertEqual(len(data), 1)
+        psite = data[0]
+        self.assertEqual(len(psite['certificates']), 2)
+        # update-iscc certificate
+        ## start by creating a new certificate
+        certid1bis = 'ISCC-TEST-02'
+        crtdata = {'certificate_holder': 'MTES Robot', 'addons':'', 
+        'valid_from':today, 'valid_until':today, 'issuing_cb':'veritas', 
+        'location':'paris', 'download_link':'http://localhost/test.pdf'}
+        crt, _ = ISCCCertificate.objects.update_or_create(certificate_id=certid1bis, defaults=crtdata)        
+        ## then call update method
+        postdata = {'entity_id': self.entity1.id, 'old_certificate_id': certid1, 'new_certificate_id': certid1bis}
+        response = self.client.post(reverse('api-v3-settings-update-iscc-certificate'), postdata)
+        self.assertEqual(response.status_code, 200)    
+
+        # update 2bs certificate
+        ## start by creating a new certificate
+        certid2bis = '2BS-TEST-02'
+        crtdata = {'certificate_holder': 'MTES Robot', 
+        'valid_from':today, 'valid_until':today, 'holder_address':'paris france', 
+        'certification_type':'TST TST', 'download_link':'http://localhost/test.pdf'}
+        crt, _ = DBSCertificate.objects.update_or_create(certificate_id=certid2bis, defaults=crtdata)          
+        ## then call update method
+        postdata = {'entity_id': self.entity1.id, 'old_certificate_id': certid2, 'new_certificate_id': certid2bis}
+        response = self.client.post(reverse('api-v3-settings-update-2bs-certificate'), postdata)
+        self.assertEqual(response.status_code, 200)    
+        # get-production sites, check if they contain the updated certificates
+        response = self.client.get(reverse('api-v3-settings-get-production-sites'), {'entity_id': self.entity1.id})
+        self.assertEqual(response.status_code, 200)
+        data = response.json()['data']
+        self.assertEqual(len(data), 1)
+        psite = data[0]
+        self.assertEqual(len(psite['certificates']), 2)
+        for c in psite['certificates']:
+            if c['type'] == 'ISCC':
+                self.assertEqual(c['certificate_id'], certid1bis)
+            if c['type'] == '2BS':
+                self.assertEqual(c['certificate_id'], certid2bis)
+
+
+    def test_entity_access_request(self):
         pass
