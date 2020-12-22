93f2129a966849f8fc96e59eee6cbef2f25fcba9
Martin Planes <martin.planes@gmail.com>
2020-12-22 15:25:49 +0100

    [tests] accounts

--- web/accounts/tests.py
+++ web/accounts/tests.py
@@ -1,3 +1,4 @@
+import time
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
@@ -6,6 +7,7 @@ from core.models import Entity, UserRights
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from accounts.tokens import account_activation_token
+from django_otp.plugins.otp_email.models import EmailDevice
 
 class AccountTest(TestCase):
     def setUp(self):
@@ -13,10 +15,7 @@ class AccountTest(TestCase):
         self.user_name = 'Jean Test'
         self.user_email = 'foobar@example.com'
         self.user_password = 'totopouet'
-
         self.user1 = user_model.objects.create_user(email='testuser1@toto.com', name='Le Super Testeur 1', password='totopouet')
-
-
 
 
     def test_register(self):
@@ -63,6 +62,7 @@ class AccountTest(TestCase):
         user = usermodel.objects.get(email=self.user_email)
         self.assertEqual(user.is_active, True)        
 
+
     def test_ro_pages(self):
         loggedin = self.client.login(username='testuser1@toto.com', password='totopouet')
         self.assertTrue(loggedin)        
@@ -77,12 +77,43 @@ class AccountTest(TestCase):
 
 
     def test_login(self):
-        # try to login with non existant user
+        user_model = get_user_model()
+        user_model.objects.create_user(email=self.user_email, name='Le Super Testeur 1', password=self.user_password)
+
+        # try to login with non existent user
+        response = self.client.post(reverse('login'), {'username': 'bademail', 'password': 'badpasword'})
+        self.assertEqual(response.status_code, 200)
         # try to login with existing user but bad password
+        response = self.client.post(reverse('login'), {'username': self.user_email, 'password': 'badpasword'})
+        self.assertEqual(response.status_code, 200)
         # login
-        # be redirected to otp_verify page
+        response = self.client.post(reverse('login'), {'username': self.user_email, 'password': self.user_password})
+        self.assertEqual(response.status_code, 302) # be redirected to otp_verify page
+        self.assertEqual(response.url, reverse('otp-verify'))
+        # load otp-verify page to generate token
+        response = self.client.get(reverse('otp-verify'))
+        self.assertEqual(response.status_code, 200)              
         # try to access another page -> redirect to otp_verify
+        response = self.client.get(reverse('api-v3-settings-get'))
+        self.assertEqual(response.status_code, 302)
+        # ensure an otp token has been generated
+        usermodel = get_user_model()
+        user = usermodel.objects.get(email=self.user_email)
+        device = EmailDevice.objects.get(user=user)
+        self.assertIsNotNone(device.token)
         # submit wrong otp -> try to access another page -> redirect to otp_verify
+        response = self.client.post(reverse('otp-verify'), {'otp_token': '111111'})
+        self.assertEqual(response.status_code, 200)        
+        response = self.client.get(reverse('api-v3-settings-get'))
+        self.assertEqual(response.status_code, 302)        
         # submit good otp
+        # wait (otp wait time 1,2,4,8 seconds etc..)
+        time.sleep(1.5)
+        response = self.client.post(reverse('otp-verify'), {'otp_token': device.token})
+        self.assertEqual(response.status_code, 302) #  redirected to home page
         # check that we are logged in
+        response = self.client.get(reverse('api-v3-settings-get'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_password_change(self):
         pass
