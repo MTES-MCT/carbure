d6654465fbd779557b4261f8c1f8a76447e0459b
jfalxa <jfalxa.sta@gmail.com>
2021-01-06 18:25:59 +0100

    [front] fix transactions prod/trader tests

--- front/src/common/__test__/helpers.ts
+++ front/src/common/__test__/helpers.ts
@@ -1,10 +1,13 @@
 import { screen, waitForElementToBeRemoved } from "@testing-library/react"
 
-export function clone(data: any) {
-  return JSON.parse(JSON.stringify(data))
-}
+const timeout = 2000
+const interval = 25
 
 export async function waitWhileLoading() {
-  const loaders = await screen.findAllByTitle("Chargement...")
-  await waitForElementToBeRemoved(loaders)
+  const loaders = await screen.findAllByTitle("Chargement...", {}, { timeout, interval }) // prettier-ignore
+  await waitForElementToBeRemoved(loaders, { timeout, interval })
+}
+
+export function clone(data: any) {
+  return JSON.parse(JSON.stringify(data))
 }
