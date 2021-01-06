8b08a9b32f395b587690ab29de2716cf409e33e4
jfalxa <jfalxa.sta@gmail.com>
2021-01-06 16:13:52 +0100

    [front] fix test for production site settings

--- front/src/settings/__test__/iscc-certificates.spec.tsx
+++ front/src/settings/__test__/iscc-certificates.spec.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor, screen } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 
 import { Entity } from "common/types"
