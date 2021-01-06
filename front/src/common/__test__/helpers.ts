1064d9e47004562dfab1f73f007cc41ca25d92e6
jfalxa <jfalxa.sta@gmail.com>
2021-01-06 15:53:35 +0100

    [front] fix company settings tests

--- front/src/common/__test__/helpers.ts
+++ front/src/common/__test__/helpers.ts
@@ -1,5 +1,9 @@
 import { screen, waitForElementToBeRemoved } from "@testing-library/react"
 
+export function clone(data: any) {
+  return JSON.parse(JSON.stringify(data))
+}
+
 export async function waitWhileLoading() {
   const loaders = await screen.findAllByTitle("Chargement...")
   await waitForElementToBeRemoved(loaders)
