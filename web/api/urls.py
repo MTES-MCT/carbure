27f712b6790df2a87502fefb204caaa300d50caa
Martin Planes <martin.planes@gmail.com>
2021-01-22 16:35:42 +0100

    [cleanup]

--- web/api/urls.py
+++ web/api/urls.py
@@ -2,6 +2,4 @@ from django.urls import path, include
 
 urlpatterns = [
     path('v3/', include('api.v3.urls')),
-    path('v2/', include('api.v2.urls')),
-    path('v1/', include('api.v1.urls')),
 ]
