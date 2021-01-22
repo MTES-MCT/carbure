d156420ed4e9171506df64cb9c3cf7c2ba7f81cb
Martin Planes <martin.planes@gmail.com>
2021-01-22 16:16:05 +0100

    [cleanup] remove legacy django apps

--- web/operators/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OperatorsConfig(AppConfig):
-    name = 'operators'
--- web/producers/urls.py
+++ web/producers/urls.py
@@ -1,17 +1,3 @@
-from django.urls import path
-
-from . import views
-from . import v2
-
 urlpatterns = [
-    path('<slug:producer_name>/controles', views.producers_controles, name='producers-controles'),
-    path('<slug:producer_name>/settings', views.producers_settings, name='producers-settings'),
-    path('v2/<slug:producer_name>/', v2.producers_index_v2, name='producers-index'),
-    path('v2/<slug:producer_name>/mass-balance', v2.producers_mass_balance, name='producers-mb'),
-    path('v2/<slug:producer_name>/archives', v2.producers_histo, name='producers-histo'),
-    path('v2/import-documentation', v2.producers_import_doc, name='producers-import-documentation'),
-    path('v2/<slug:producer_name>/stats', v2.stats, name='producers-stats'),
-    path('v2/<slug:producer_name>/mac', v2.mac, name='producers-mac'),
-    path('v2/new_design', v2.new_design, name='producers-new-design'),
 
 ]
