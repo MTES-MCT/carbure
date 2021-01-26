6e6b9ac697b3e1cf1f81fe21986c5a02340db50b
jfalxa <jfalxa.sta@gmail.com>
2021-01-26 18:17:27 +0100

    [front] add alert to filter entities with pending access requests

--- front/src/entities/index.tsx
+++ front/src/entities/index.tsx
@@ -1,6 +1,6 @@
-import { Main, Title } from "common/components"
-import { Alert } from "common/components/alert"
-import { AlertTriangle } from "common/components/icons"
+import { LoaderOverlay, Main, Title } from "common/components"
+import { Alert, AlertFilter } from "common/components/alert"
+import { AlertCircle, AlertTriangle } from "common/components/icons"
 import { LabelInput } from "common/components/input"
 import { useRelativePush } from "common/components/relative-route"
 import Table, { Column } from "common/components/table"
@@ -46,7 +46,9 @@ const COLUMNS: Column<api.EntityDetails>[] = [
 
 const Entities = () => {
   const push = useRelativePush()
+
   const [query, setQuery] = useState("")
+  const [requestOnly, setRequestOnly] = useState(false)
   const [entities, getEntities] = useAPI(api.getEntities)
 
   useEffect(() => {
@@ -57,6 +59,8 @@ const Entities = () => {
     value: e,
     onClick: () => push(`${e.entity.id}`),
   }))
+
+  const requestCount = rows.filter((e) => e.value.requests > 0).length
 
   return (
     <Main>
@@ -76,7 +80,31 @@ const Entities = () => {
             Aucune société trouvée pour ces paramètres.
           </Alert>
         )}
+
+        {requestCount > 0 && (
+          <AlertFilter
+            level="warning"
+            icon={AlertCircle}
+            active={requestOnly}
+            onActivate={() => setRequestOnly(true)}
+            onDispose={() => setRequestOnly(false)}
+          >
+            {requestCount === 1 ? (
+              <span>
+                <b>1 société</b> a des utilisateurs en attente d'autorisation.
+              </span>
+            ) : (
+              <span>
+                <b>{requestCount} sociétés</b> ont des utilisateurs en attente
+                d'autorisation.
+              </span>
+            )}
+          </AlertFilter>
+        )}
+
         {rows.length > 0 && <Table columns={COLUMNS} rows={rows} />}
+
+        {entities.loading && <LoaderOverlay />}
       </SettingsBody>
     </Main>
   )
