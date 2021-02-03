8046de3181793d58ad87d0a702a8ef3c0e9cdae4
jfalxa <jfalxa.sta@gmail.com>
2021-02-03 18:21:51 +0100

    [front] remove the annoying full dom dump on test errors

--- front/src/setupTests.ts
+++ front/src/setupTests.ts
@@ -1,5 +1,15 @@
 import "@testing-library/jest-dom/extend-expect"
 import { render as baseRender } from "@testing-library/react"
+import { configure } from "@testing-library/dom"
+
+configure({
+  testIdAttribute: "data-my-test-id",
+  getElementError(message, container) {
+    const error = new Error(message)
+    error.name = "TestingLibraryElementError"
+    return error
+  },
+})
 
 const modal = document.createElement("div")
 modal.setAttribute("id", "modal")
