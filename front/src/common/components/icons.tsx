ac0caadb3140ad5cec8a02ebcc7a06b46e93fc1d
jfalxa <jfalxa.sta@gmail.com>
2021-02-18 13:26:46 +0100

    [front] add ability to check/uncheck and send declaration reminders

--- front/src/common/components/icons.tsx
+++ front/src/common/components/icons.tsx
@@ -237,3 +237,18 @@ export const Alarm = ({ className, ...props }: IconProps) => (
   </Icon>
 )
 
+export const CheckCircle = ({ className, ...props }: IconProps) => (
+  <Icon {...props} className={cl("icon-check-circle", className)}>
+    <polyline points="9 11 12 14 20 6" />
+    <path d="M20 12v6a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h9" />
+  </Icon>
+)
+
+export const Bell = ({ className, ...props }: IconProps) => (
+  <Icon {...props} className={cl("icon-bell", className)}>
+    <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
+    <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
+    <path d="M21 6.727a11.05 11.05 0 0 0 -2.794 -3.727" />
+    <path d="M3 6.727a11.05 11.05 0 0 1 2.792 -3.727" />
+  </Icon>
+)
--- front/src/dashboard/components/declarations.tsx
+++ front/src/dashboard/components/declarations.tsx
@@ -1,13 +1,17 @@
+import { useEffect } from "react"
 import cl from "clsx"
 import { DeclarationsByMonth } from "../api"
 import { Declaration, Entity } from "common/types"
-import { LoaderOverlay, Title } from "common/components"
+import { Box, LoaderOverlay, Title } from "common/components"
 import Table, { Row } from "common/components/table"
 import { Section, SectionHeader } from "common/components/section"
 import { padding } from "transactions/components/list-columns"
 import styles from "./declarations.module.css"
 import { useRelativePush } from "common/components/relative-route"
-import { ApiState } from "common/hooks/use-api"
+import useAPI from "common/hooks/use-api"
+import { Bell, Check, Cross } from "common/components/icons"
+import * as api from "../api"
+import { confirm } from "common/components/dialog"
 
 type RowData = { entity: Entity; declarations: DeclarationsByMonth }
 
@@ -15,33 +19,23 @@ const entityColumn = {
   render: (v: RowData) => v.entity.name,
 }
 
-enum Evaluation {
-  Checked = 0,
-  Declared = 1,
-  InProgress = 2,
-  Idle = 3,
-}
-
-function evaluateMonth(
-  declaration: Declaration | null
-): Evaluation | undefined {
-  if (!declaration) return
-
-  if (declaration.checked) {
-    return Evaluation.Checked
-  } else if (declaration.declared) {
-    return Evaluation.Declared
-  } else if (Object.values(declaration.lots).some((v) => v > 0)) {
-    return Evaluation.InProgress
-  } else {
-    return Evaluation.Idle
-  }
-}
-
-function renderMonthSummary(month: string) {
+function renderMonthSummary(
+  month: string,
+  askCheck: (d: Declaration, t: boolean) => Promise<any>,
+  sendReminder: (d: Declaration) => Promise<any>
+) {
   return (v: RowData) => {
     const relativePush = useRelativePush()
-    const ev = evaluateMonth(v.declarations[month])
+    const decl = v.declarations[month]
+
+    // has some lots in transit but nothing declared or approved by admin
+    const isInProgress =
+      Object.values(decl.lots).some((v) => v > 0) &&
+      !decl.checked &&
+      !decl.declared
+
+    // no lots at all for the given period
+    const isIdle = !Object.values(decl.lots).some((v) => v === 0)
 
     const pushToTransactions = () =>
       relativePush(`../transactions/declaration`, {
@@ -50,38 +44,119 @@ function renderMonthSummary(month: string) {
       })
 
     return (
-      <ul
+      <Box
+        row
         onClick={pushToTransactions}
         className={cl(
           styles.declarationCell,
-          ev === Evaluation.Checked && styles.declarationChecked,
-          ev === Evaluation.Declared && styles.declarationDeclared,
-          ev === Evaluation.InProgress && styles.declarationInProgress,
-          ev === Evaluation.Idle && styles.declarationIdle
+          decl.checked && styles.declarationChecked,
+          !decl.checked && decl.declared && styles.declarationDeclared,
+          isInProgress && styles.declarationInProgress,
+          isIdle && styles.declarationIdle
         )}
       >
-        <li>{v.declarations[month]?.lots.drafts ?? 0} brouillons</li>
-        <li>{v.declarations[month]?.lots.validated ?? 0} envoyés</li>
-        <li>{v.declarations[month]?.lots.received ?? 0} reçus</li>
-        <li>{v.declarations[month]?.lots.corrections ?? 0} corrections</li>
-      </ul>
+        <ul className={styles.declarationSummary}>
+          <li>{v.declarations[month]?.lots.drafts ?? 0} brouillons</li>
+          <li>{v.declarations[month]?.lots.validated ?? 0} envoyés</li>
+          <li>{v.declarations[month]?.lots.received ?? 0} reçus</li>
+          <li>{v.declarations[month]?.lots.corrections ?? 0} corrections</li>
+        </ul>
+
+        {!decl.declared && (
+          <Bell
+            className={styles.declarationValidation}
+            title="Relancer l'utilisateur"
+            onClick={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              sendReminder(decl)
+            }}
+          />
+        )}
+
+        {!decl.checked && decl.declared && (
+          <Check
+            className={styles.declarationValidation}
+            title="Valider la déclaration"
+            onClick={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              askCheck(decl, true)
+            }}
+          />
+        )}
+
+        {decl.checked && (
+          <Cross
+            className={styles.declarationValidation}
+            title="Annuler la déclaration"
+            onClick={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              askCheck(decl, false)
+            }}
+          />
+        )}
+      </Box>
     )
   }
 }
 
-type DeclarationsProps = {
-  declarations: ApiState<
-    [Entity[], string[], Record<number, Record<string, Declaration>>]
-  >
-}
+const Declarations = () => {
+  const [declarations, getDeclarations] = useAPI(api.getDeclarations)
+  const [, checkDeclaration] = useAPI(api.checkDeclaration)
+  const [, uncheckDeclaration] = useAPI(api.uncheckDeclaration)
+  const [, sendReminder] = useAPI(api.sendDeclarationReminder)
 
-const Declarations = ({ declarations }: DeclarationsProps) => {
+  async function askDeclarationCheck(
+    declaration: Declaration,
+    toggle: boolean
+  ) {
+    const ok = await confirm(
+      toggle ? "Validation déclaration" : "Annulation déclaration",
+      toggle
+        ? "Voulez-vous valider cette déclaration ?"
+        : "Voulez-vous annuler la validation de cette déclaration ?"
+    )
+
+    if (ok) {
+      if (toggle) {
+        await checkDeclaration(declaration.id)
+      } else {
+        await uncheckDeclaration(declaration.id)
+      }
+
+      await getDeclarations()
+    }
+  }
+
+  async function askSendReminder(declaration: Declaration) {
+    const period = `${declaration.year}/${("0" + declaration.month).slice(-2)}`
+
+    const ok = await confirm(
+      "Relance déclaration",
+      `Voulez-vous relancer ${declaration.entity.name} pour la période du ${period} ?`
+    )
+
+    if (ok) {
+      await sendReminder(
+        declaration.entity.id,
+        declaration.month,
+        declaration.year
+      )
+    }
+  }
+
+  useEffect(() => {
+    getDeclarations()
+  }, [getDeclarations])
+
   const [entities = [], months = [], declarationsByEntites = {}] =
     declarations.data ?? []
 
   const columns = months?.map((month) => ({
     header: month,
-    render: renderMonthSummary(month),
+    render: renderMonthSummary(month, askDeclarationCheck, askSendReminder),
   }))
 
   const rows: Row<RowData>[] = entities?.map((e) => ({
