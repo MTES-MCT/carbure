4c2af5c5faa2807b1c7dfe4f9cde7da489cb32e5
Martin Planes <martin.planes@gmail.com>
2021-03-15 15:35:33 +0100

    [scripts] additional backup to backblaze

--- scripts/backup/backup_prod_db.sh
+++ scripts/backup/backup_prod_db.sh
@@ -4,7 +4,8 @@
 mysqldump -h $DJANGO_DB_HOST -u $DJANGO_DB_USER -p"$DJANGO_DB_PASSWORD" $DJANGO_DATABASE > /tmp/backup-$(date +\%F).sql
 
 # upload db
-python3 /app/scripts/backup/s3backup.py -b carbure.database -f /tmp/backup-$(date +\%F).sql
+python3 /app/scripts/backup/s3backup.py -b carbure.database -f /tmp/backup-$(date +\%F).sql # scaleway
+python3 /app/scripts/backup/s3backblaze.py -f /tmp/backup-$(date +\%F).sql # backblaze
 
 # cleanup s3 bucket
 python3 /app/scripts/backup/s3cleanup.py -b carbure.database
