3e2b0f1f28b10ff005b7cde6049eb8a6efb6d1f0
Martin Planes <martin.planes@gmail.com>
2021-03-26 17:03:08 +0100

    [redcert] docker-downloader WIP

---
+++ web/fixtures/redcert/index.js
@@ -0,0 +1,55 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs/promises");
+
+(async () => {
+  const REDCERT_URL = "https://redcert.eu/ZertifikateDatenAnzeige.aspx";
+
+  console.log("Start loading REDCERT website");
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  page.setDefaultNavigationTimeout(60000);
+
+  // load redcert site
+  await page.goto(REDCERT_URL, { waitUntil: "load" });
+
+  console.log("Page loaded");
+
+  // click on button to switch to english
+  await page.click("#ctl00_languageEnglishLinkButton");
+
+  // prepare file download
+  await page._client.send("Page.setDownloadBehavior", {
+    behavior: "allow",
+    downloadPath: "./downloads",
+  });
+
+  // click on file export button
+  await page.click(
+    "#ctl00_mainContentPlaceHolder_exportPublicZertifikatListeButton"
+  );
+
+  console.log("Start downloading REDCERT certificates");
+
+  // reset download dir
+  await fs.rm("./downloads", { recursive: true, force: true });
+  await fs.mkdir("./downloads");
+
+  let file = null;
+  while (file === null) {
+    const dir = await fs.readdir("./downloads");
+    c;
+    // check if the file is there yet
+    if (dir.length > 0) {
+      file = dir[0];
+    }
+
+    // wait 5s
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+  }
+
+  console.log("Certificates downloaded:", file);
+
+  await browser.close();
+})();
---
+++ web/fixtures/redcert/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it redcert_downloader index.js
