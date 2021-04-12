d0664e33dc97e413f593f44659398187b0dfdd81
jfalxa <jfalxa.sta@gmail.com>
2021-04-13 00:54:58 +0200

    [front] change text for declaration validation button

--- front/src/common/components/icons.tsx
+++ front/src/common/components/icons.tsx
@@ -263,10 +263,21 @@ export const Flask = ({ className, ...props }: IconProps) => (
 
 export const Forward = ({ className, ...props }: IconProps) => (
   <Icon {...props} className={cl("icon-forward", className)}>
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <path d="M12 18h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v7.5" />
     <path d="M3 6l9 6l9 -6" />
     <path d="M15 18h6" />
     <path d="M18 15l3 3l-3 3" />
+  </Icon>
+)
+
+export const Certificate = ({ className, ...props }: IconProps) => (
+  <Icon {...props} className={cl("icon-certificate", className)}>
+    <circle cx="15" cy="15" r="3" />
+    <path d="M13 17.5v4.5l2 -1.5l2 1.5v-4.5" />
+    <path d="M10 19h-5a2 2 0 0 1 -2 -2v-10c0 -1.1 .9 -2 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -1 1.73" />
+    <line x1="6" y1="9" x2="18" y2="9" />
+    <line x1="6" y1="12" x2="9" y2="12" />
+    <line x1="6" y1="15" x2="8" y2="15" />
   </Icon>
 )
