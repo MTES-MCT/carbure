b48adfdc1c23ba41a528dd5121eb86dff931760f
Martin Planes <martin.planes@gmail.com>
2021-04-16 11:23:19 +0200

    [account] add link to resend otp token

--- web/accounts/urls.py
+++ web/accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('resend-activation-link', views.resend_activation_link, name='resend-activation-link'),
     path('activate/<str:uidb64>/<str:token>', views.activate, name='activate'),
     path('otp_verify', views.otp_verify, name='otp-verify'),
+    path('resend_otp_code', views.resend_otp_code, name='resend-otp-code'),
     path('profile', views.profile, name='profile'),
     path('password-change', views.custom_password_change, name='custom-password-change'),
     path('password-change-success', views.custom_password_change_success, name='custom-password-change-success'),
