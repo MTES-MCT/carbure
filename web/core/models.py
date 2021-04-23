6e70e216dfbfe261ac5cf523a1521d9c804db543
Martin Planes <martin.planes@gmail.com>
2021-04-23 15:03:51 +0200

    [model] add remaining_volume field

--- web/core/models.py
+++ web/core/models.py
@@ -263,6 +263,7 @@ class LotV2(models.Model):
 
     # lot details
     volume = models.FloatField(default=0.0)
+    remaining_volume = models.FloatField(default=0.0)
     matiere_premiere = models.ForeignKey(MatierePremiere, null=True, on_delete=models.SET_NULL)
     biocarburant = models.ForeignKey(Biocarburant, null=True, on_delete=models.SET_NULL)
     pays_origine = models.ForeignKey(Pays, null=True, on_delete=models.SET_NULL)
@@ -314,7 +315,7 @@ class LotV2(models.Model):
         'unknown_producer': self.unknown_producer, 'production_site_is_in_carbure': self.production_site_is_in_carbure, 'carbure_production_site': self.carbure_production_site.natural_key() if self.carbure_production_site else None,
         'unknown_production_site': self.unknown_production_site, 'unknown_production_country': self.unknown_production_country.natural_key() if self.unknown_production_country else None,
         'unknown_production_site_com_date': self.unknown_production_site_com_date, 'unknown_production_site_reference': self.unknown_production_site_reference,
-        'unknown_production_site_dbl_counting': self.unknown_production_site_dbl_counting, 'volume': round(self.volume, 2), 'matiere_premiere': self.matiere_premiere.natural_key() if self.matiere_premiere else None,
+        'unknown_production_site_dbl_counting': self.unknown_production_site_dbl_counting, 'volume': round(self.volume, 2), 'remaining_volume': round(self.remaining_volume, 2), 'matiere_premiere': self.matiere_premiere.natural_key() if self.matiere_premiere else None,
         'biocarburant': self.biocarburant.natural_key() if self.biocarburant else None, 'pays_origine': self.pays_origine.natural_key() if self.pays_origine else None,
         'eec': self.eec, 'el': self.el, 'ep': self.ep, 'etd': self.etd, 'eu': self.eu, 'esca': self.esca, 'eccs': self.eccs, 'eccr': self.eccr, 'eee': self.eee,
         'ghg_total': self.ghg_total, 'ghg_reference': self.ghg_reference, 'ghg_reduction': self.ghg_reduction, 'status': self.status, 'source': self.source,
