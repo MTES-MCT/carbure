06f1f42418e3ae8f5040b73664480b887f869aa2
jfalxa <jfalxa.sta@gmail.com>
2021-04-26 12:35:07 +0200

    [front] use position: sticky instead of calculating it with JS

--- front/src/common/components/sticky.tsx
+++ front/src/common/components/sticky.tsx
@@ -1,49 +1,9 @@
 import cl from "clsx"
-import React, { useEffect, useRef, useState } from "react"
+import { SystemProps } from "."
 import styles from "./sticky.module.css"
 
-function getPosition(element: HTMLElement): number {
-  const prev = element.previousElementSibling
-  const rect = prev?.getBoundingClientRect()
-  return rect ? rect.bottom : 0
-}
-
-const Sticky = ({ children }: { children: React.ReactNode }) => {
-  const [sticky, setSticky] = useState(false)
-  const div = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const container = document.getElementById("root")!
-
-    function onScroll(e: Event) {
-      if (!div.current) return
-
-      const scrollY = container.scrollTop
-      const posY = getPosition(div.current) + scrollY
-
-      if (scrollY >= posY) {
-        setSticky(true)
-      } else if (sticky && scrollY <= posY) {
-        setSticky(false)
-      }
-    }
-
-    container.addEventListener("scroll", onScroll)
-    return () => container.removeEventListener("scroll", onScroll)
-  })
-
-  return (
-    <React.Fragment>
-      {/* keep the previous space occupied when getting fixed position  */}
-      {sticky && <div style={{ height: 56 }} />}
-      <div
-        ref={div}
-        className={cl(styles.sticky, sticky && styles.stickyFixed)}
-      >
-        {children}
-      </div>
-    </React.Fragment>
-  )
+const Sticky = ({ className, children }: SystemProps) => {
+  return <div className={cl(styles.sticky, className)}>{children}</div>
 }
 
 export default Sticky
