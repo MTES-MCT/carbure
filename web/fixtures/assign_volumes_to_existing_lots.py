29c7eca7efa22fa1cec19d92027cdd90ae23b9f7
jfalxa <jfalxa.sta@gmail.com>
2021-04-29 18:13:20 +0200

    [script] fix settings remaining_volume on stocks

--- web/fixtures/assign_volumes_to_existing_lots.py
+++ web/fixtures/assign_volumes_to_existing_lots.py
@@ -1,14 +1,5 @@
 import sys, os
 import django
-import csv
-import calendar
-import datetime
-import re
-import argparse
-import openpyxl
-import pandas as pd
-from typing import TYPE_CHECKING, Dict, List, Optional
-from pandas._typing import FilePathOrBuffer, Scalar
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carbure.settings")
 django.setup()
@@ -17,14 +8,15 @@ from core.models import LotV2, LotTransaction, Entity
 
 def try_assign_volumes():
     entities = Entity.objects.filter(entity_type__in=['Producteur', 'Trader'])
-    stock = LotTransaction.objects.filter(carbure_client__in=entities, lot__status='Validated', delivery_status='A')
+    stock = LotTransaction.objects.filter(carbure_client__in=entities, lot__status='Validated')
 
     for s in stock:
         # do we have children?
         children = LotTransaction.objects.filter(lot__parent_lot=s.lot)
+        print('Found lot from %s' % (s.lot.added_by.name))
         if children.count() == 0:
             print('Setting remaining_volume back on %d' % (s.id))
-            s.lot.remaining_volune = s.lot.volume
+            s.lot.remaining_volume = s.lot.volume
             s.lot.save()
         else:
             print('Found tx with children %d' % (s.id))
