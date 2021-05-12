5f0e94fca642aba33173dff5c026cf9d8a78813e
Martin Planes <martin.planes@gmail.com>
2021-05-12 10:40:57 +0200

    [api] fix get-lots (operators)

--- web/api/v3/lots/helpers.py
+++ web/api/v3/lots/helpers.py
@@ -75,7 +75,7 @@ def get_entity_lots_by_status(entity, status):
         if status == 'draft':
             txs = txs.filter(Q(lot__added_by=entity, lot__status='Draft'))
         elif status == 'in':
-            txs = txs.filter(Q(carbure_client=entity) | Q(lot__added_by=entity, is_mac=True))
+            txs = txs.filter(Q(carbure_client=entity))
             txs = txs.filter(delivery_status__in=['N', 'AC', 'AA'], lot__status="Validated", is_mac=False)
         elif status == 'accepted':
             txs = txs.filter(Q(carbure_client=entity) | Q(lot__added_by=entity, is_mac=True))
