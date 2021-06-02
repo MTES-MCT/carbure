6a7d72d88cbc456e15010fae4c350d1bf0b29823
Martin Planes <martin.planes@gmail.com>
2021-06-02 16:35:30 +0200

    [superadmin] display tx period

--- web/core/admin.py
+++ web/core/admin.py
@@ -170,12 +170,17 @@ class UnknownDeliverySiteListFilter(admin.SimpleListFilter):
             return queryset.filter(carbure_delivery_site__isnull=False)
 
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('get_lot_mp', 'get_lot_bc', 'get_lot_volume', 'get_lot_supplier', 'carbure_vendor', 'get_production_site', 'carbure_client', 'dae', 'carbure_delivery_site', 'delivery_date', 'delivery_status', 'unknown_client', 'carbure_vendor_certificate', 'get_lot_unknown_vendor_certificate')
+    list_display = ('get_lot_period', 'get_lot_mp', 'get_lot_bc', 'get_lot_volume', 'get_lot_supplier', 'carbure_vendor', 'get_production_site', 'carbure_client', 'dae', 'carbure_delivery_site', 'delivery_date', 'delivery_status', 'unknown_client', 'carbure_vendor_certificate', 'get_lot_unknown_vendor_certificate')
     search_fields = ('lot__id', 'dae', 'champ_libre', 'lot__carbure_id', 'lot__volume')
     list_filter = ('lot__status', ('lot__biocarburant', NameSortedRelatedOnlyDropdownFilter), ('lot__matiere_premiere', NameSortedRelatedOnlyDropdownFilter), 'delivery_status', ('lot__period', DropdownFilter), 'client_is_in_carbure', ('carbure_vendor', NameSortedRelatedOnlyDropdownFilter), ('carbure_client', NameSortedRelatedOnlyDropdownFilter),  
                    'is_mac', 'is_batch', 'delivery_site_is_in_carbure', ('carbure_delivery_site', NameSortedRelatedOnlyDropdownFilter), ('lot__carbure_production_site', NameSortedRelatedOnlyDropdownFilter), TxPartOfForwardListFilter, UnknownDeliverySiteListFilter, UnknownClientListFilter)
     raw_id_fields = ('lot', 'parent_tx')
     actions = ['rerun_sanity_checks', 'delete_ghosts', 'change_transaction_delivery_site', 'change_transaction_client', 'assign_transaction_certificate', 'change_transaction_delivery_status']
+
+    def get_lot_period(self, obj):
+        return obj.lot.period
+    get_lot_period.admin_order_field  = 'lot__period'
+    get_lot_period.short_description = 'Period'
 
 
     def get_lot_mp(self, obj):
