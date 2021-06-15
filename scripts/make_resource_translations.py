4e605a93ef86087a1a0d603aa129f193bbfa19e1
jfalxa <jfalxa.sta@gmail.com>
2021-06-15 15:38:15 +0200

    [front] translate countries to english

--- scripts/make_resource_translations.py
+++ scripts/make_resource_translations.py
@@ -18,7 +18,7 @@ from core.models import MatierePremiere, Biocarburant, Pays
 queries = {
     'feedstocks.json': MatierePremiere.objects.all().annotate(value=F('code'), label=F('name')).values('value', 'label').order_by('value'),
     'biofuels.json': Biocarburant.objects.all().annotate(value=F('code'), label=F('name')).values('value', 'label').order_by('value'),
-    'countries.json': Pays.objects.all().annotate(value=F('code_pays'), label=F('name')).values('value', 'label').order_by('value')
+    'countries.json': Pays.objects.all().annotate(value=F('code_pays'), label=F('name'), label_en=F('name_en')).values('value', 'label', 'label_en').order_by('value')
 }
 
 
@@ -26,7 +26,7 @@ def make_translation_file(filename, query, lang):
     translation = {}
 
     for result in query:
-        translation[result['value']] = result['label']
+        translation[result['value']] = result['label_en'] if lang == 'en' and 'label_en' in result else result['label']
 
     file_path = os.path.join(env('CARBURE_HOME'), 'front/public/locales', lang, filename)
     file = open(file_path, "w+")
