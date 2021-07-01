474b181bdf221f13ee9aec12ce07b75b58308ff3
jfalxa <jfalxa.sta@gmail.com>
2021-07-01 18:29:47 +0200

    [front] allow access to public pages to non connected users

--- front/src/carbure/index.tsx
+++ front/src/carbure/index.tsx
@@ -5,8 +5,6 @@ import { EntityType, LotStatus } from "common/types"
 import useEntity from "./hooks/use-entity"
 import { UserRightProvider } from "./hooks/use-rights"
 
-import { Alert } from "common/components/alert"
-import { AlertTriangle } from "common/components/icons"
 import { Redirect, Route, Switch } from "common/components/relative-route"
 
 import Topbar from "./components/top-bar"
@@ -43,6 +41,10 @@ const DevBanner = () => (
 // has to be nested in a route so we can get data from useParams()
 const Org = ({ app }: { app: AppHook }) => {
   const { entity, pending } = useEntity(app)
+
+  if (app.settings.error === "User not verified") {
+    return <Exit to="/accounts/login" />
+  }
 
   // a user with entities tries to access the pending or another entity's page
   if (app.hasEntities() && !entity) {
@@ -138,37 +140,25 @@ const Org = ({ app }: { app: AppHook }) => {
 
 const Carbure = () => {
   const app = useApp()
-  const { settings, getDefaultEntity } = app
-
-  if (settings.error === "User not verified") {
-    return <Exit to="/accounts/login" />
-  }
+  const { getDefaultEntity } = app
 
   return (
     <div id="app">
-      {settings.error && (
-        <Alert level="error" icon={AlertTriangle}>
-          {settings.error}
-        </Alert>
-      )}
+      <Switch>
+        <Route path="/org/:entity">
+          <Org app={app} />
+        </Route>
 
-      {!settings.error && settings.data && (
-        <Switch>
-          <Route path="/org/:entity">
-            <Org app={app} />
-          </Route>
+        <Route path="/logout">
+          <Exit to="/accounts/logout" />
+        </Route>
 
-          <Route path="/logout">
-            <Exit to="/accounts/logout" />
-          </Route>
+        <Route path="/public_stats">
+          <PublicStats />
+        </Route>
 
-          <Route path="/public_stats">
-            <PublicStats />
-          </Route>
-
-          <Redirect to={`/org/${getDefaultEntity()}`} />
-        </Switch>
-      )}
+        <Redirect to={`/org/${getDefaultEntity()}`} />
+      </Switch>
     </div>
   )
 }
