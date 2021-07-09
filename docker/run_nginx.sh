cd17a6b40bc5000765e0f84c6ac9c35e10cb9897
Martin Planes <martin.planes@gmail.com>
2021-07-09 13:00:48 +0200

    [docker] remove metabase from dev and staging servers

--- docker/run_nginx.sh
+++ docker/run_nginx.sh
@@ -5,10 +5,14 @@ echo $IMAGE_TAG
 if [ "$IMAGE_TAG" = "local" ]; then
    echo "Running locally - dev mode"
    envsubst '$$NGINX_HOSTS' < /etc/nginx/conf.d/web.dev.template > /etc/nginx/conf.d/default.conf
+elif [ "$IMAGE_TAG" = "prod" ]; then
+   echo "Running in prod"
+   envsubst '$$NGINX_HOSTS $$NGINX_SSL_FOLDER $$METABASE_HOST $$METABASE_SSL_FOLDER' < /etc/nginx/conf.d/web.prod.template > /etc/nginx/conf.d/default.conf
 else
-   echo "Running in a server - $IMAGE_TAG"
-   envsubst '$$NGINX_HOSTS $$NGINX_SSL_FOLDER $$METABASE_HOST $$METABASE_SSL_FOLDER' < /etc/nginx/conf.d/web.template > /etc/nginx/conf.d/default.conf
-fi    
+   echo "Running in dev or staging"
+   envsubst '$$NGINX_HOSTS $$NGINX_SSL_FOLDER $$METABASE_HOST $$METABASE_SSL_FOLDER' < /etc/nginx/conf.d/web.devstaging.template > /etc/nginx/conf.d/default.conf
+fi
+
 
 while :;
 do sleep 6h & wait ${!}; nginx -s reload;
