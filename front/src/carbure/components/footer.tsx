b35bea14e7f0fd3b75fac5fa540b9c4742fc683c
jfalxa <jfalxa.sta@gmail.com>
2021-09-06 16:04:05 +0200

    [front] make important links in footer more visible

--- front/src/carbure/components/footer.tsx
+++ front/src/carbure/components/footer.tsx
@@ -6,57 +6,12 @@ import logoMTES from "../assets/images/MTE.svg"
 import logoFabNum from "../assets/images/logo-fabriquenumerique.svg"
 import logoBetaGouv from "../assets/images/betagouvfr.svg"
 import { Link } from "common/components/relative-route"
-import { LinkedIn, Slack } from "common/components/icons"
+import { ExternalLink, LinkedIn, Slack } from "common/components/icons"
 
 const Footer = () => (
   <footer className={styles.footer}>
     <div className={styles.footerUpper}>
-      <div>
-        <img src={logoMTES} alt="Logo MTES" className={cl(styles.footerMTES)} />
-
-        <img
-          src={logoFabNum}
-          alt="Logo Fabrique Numerique"
-          className={styles.footerFanum}
-        />
-      </div>
-
-      <div>
-        <p className={styles.footerDescription}>
-          <Trans>
-            CarbuRe est un service numérique de l’État incubé à la Fabrique
-            numérique du Ministère de la Transition Écologique, membre du réseau
-            d’incubateurs <a href="http://beta.gouv.fr">beta.gouv.fr</a>
-          </Trans>
-        </p>
-        <img
-          src={logoBetaGouv}
-          alt="Logo Fabrique Numerique"
-          className={styles.footerBetagouv}
-        />
-      </div>
-    </div>
-
-    <div className={styles.footerLinks}>
       <ul className={styles.footerCarbureLinks}>
-        <li>
-          <a
-            target="_blank"
-            href="https://carbure-1.gitbook.io/faq/mentions-legales-et-cgu/mentions-legales-et-conditions-generales-dutilisation#cookies"
-            rel="noreferrer"
-          >
-            <Trans>Cookies</Trans>
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://carbure-1.gitbook.io/faq/mentions-legales-et-cgu/mentions-legales-et-conditions-generales-dutilisation"
-            rel="noreferrer"
-          >
-            <Trans>Conditions générales d'utilisation</Trans>
-          </a>
-        </li>
         <li>
           <a
             href="https://carbure-1.gitbook.io/faq/"
@@ -64,11 +19,13 @@ const Footer = () => (
             rel="noreferrer"
           >
             <Trans>Guide d'utilisation de CarbuRe</Trans>
+            <ExternalLink />
           </a>
         </li>
         <li>
-          <Link to="/public_stats">
-            <Trans>Statistiques</Trans>
+          <Link to="/public_stats" target="_blank" rel="noreferrer">
+            <Trans>Statistiques publiques</Trans>
+            <ExternalLink />
           </Link>
         </li>
       </ul>
@@ -92,6 +49,77 @@ const Footer = () => (
           >
             <LinkedIn />
             <Trans>LinkedIn</Trans>
+          </a>
+        </li>
+      </ul>
+    </div>
+
+    <div className={styles.footerMiddle}>
+      <div className={styles.footerLogos}>
+        <img src={logoMTES} alt="Logo MTES" className={cl(styles.footerMTES)} />
+
+        <img
+          src={logoFabNum}
+          alt="Logo Fabrique Numerique"
+          className={styles.footerFanum}
+        />
+
+        <img
+          src={logoBetaGouv}
+          alt="Logo Fabrique Numerique"
+          className={styles.footerBetagouv}
+        />
+      </div>
+
+      <div className={styles.footerTexts}>
+        <p className={styles.footerDescription}>
+          <Trans>
+            CarbuRe est un service numérique de l’État incubé à la Fabrique
+            numérique du Ministère de la Transition Écologique, membre du réseau
+            d’incubateurs <a href="http://beta.gouv.fr">beta.gouv.fr</a>
+          </Trans>
+        </p>
+
+        <ul className={styles.footerGouvLinks}>
+          <li>
+            <a href="https://legifrance.gouv.fr">legifrance.gouv.fr</a>
+          </li>
+          <li>
+            <a href="https://gouvernement.fr">gouvernement.fr</a>
+          </li>
+          <li>
+            <a href="https://service-public.fr">service-public.fr</a>
+          </li>
+          <li>
+            <a href="https://data.gouv.fr">data.gouv.fr</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div className={styles.footerLower}>
+      <ul className={styles.footerLegalLinks}>
+        <li>
+          <a
+            target="_blank"
+            href="https://carbure-1.gitbook.io/faq/mentions-legales-et-cgu/mentions-legales-et-conditions-generales-dutilisation"
+            rel="noreferrer"
+          >
+            <Trans>Mentions légales</Trans>
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://carbure-1.gitbook.io/faq/mentions-legales-et-cgu/mentions-legales-et-conditions-generales-dutilisation#cookies"
+            rel="noreferrer"
+          >
+            <Trans>Données personnelles et cookies</Trans>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <Trans>Accessibilité : non conforme</Trans>
           </a>
         </li>
       </ul>
--- front/src/common/components/icons.tsx
+++ front/src/common/components/icons.tsx
@@ -353,3 +353,11 @@ export const LinkedIn = ({ className, ...props }: IconProps) => (
     <path d="M16 16v-3a2 2 0 0 0 -4 0" />
   </Icon>
 )
+
+export const ExternalLink = ({ className, ...props }: IconProps) => (
+  <Icon {...props} className={cl("icon-external-link", className)}>
+    <path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+    <line x1="10" y1="14" x2="20" y2="4" />
+    <polyline points="15 4 20 4 20 9" />
+  </Icon>
+)
