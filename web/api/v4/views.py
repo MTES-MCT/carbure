1257092388f510ef07c290c6cfa93aa275df5f13
Martin Planes <martin.planes@gmail.com>
2021-11-08 11:38:04 +0100

    [api] snapshot

--- web/api/v4/views.py
+++ web/api/v4/views.py
@@ -3,9 +3,41 @@ import traceback
 from django.http.response import JsonResponse
 from core.decorators import check_user_rights
 from api.v4.helpers import get_entity_lots_by_status, get_lots_with_metadata
+from core.models import CarbureLot, CarbureStock
 
-def get_snapshot(request):
-    pass
+@check_user_rights
+def get_snapshot(request, *args, **kwargs):
+    context = kwargs['context']
+    entity = context['entity']
+    year = request.GET.get('year', False)
+    if year:
+        try:
+            year = int(year)
+        except Exception:
+            return JsonResponse({'status': 'error', 'message': 'Incorrect format for year. Expected YYYY'}, status=400)
+
+    lots = CarbureLot.objects.filter(year=year)
+    data = {}
+    drafts = lots.filter(added_by=entity, lot_status=CarbureLot.DRAFT).count()
+    lots_in = lots.filter(carbure_client=entity, lot_status__in=[CarbureLot.PENDING, CarbureLot.ACCEPTED, CarbureLot.FROZEN])
+    lots_in_tofix = lots_in.exclude(correction_status=CarbureLot.NO_PROBLEMO).count()
+    stock = CarbureStock.objects.filter(carbure_client=entity, remaining_volume__gt=0).count()
+    lots_out = lots.filter(carbure_supplier=entity, lot_status__in=[CarbureLot.PENDING, CarbureLot.ACCEPTED, CarbureLot.FROZEN])
+    lots_out_tofix = lots_out.exclude(correction_status=CarbureLot.NO_PROBLEMO).count()
+    data['lots'] = {'draft': drafts, 'in': lots_in.count(), 'in_tofix': lots_in_tofix, 'stock': stock, 'out': lots_out.count(), 'out_tofix': lots_out_tofix}
+    base_filters = [
+        'periods',
+        'biocarburants',
+        'matieres_premieres',
+        'countries_of_origin',
+        'production_sites',
+        'delivery_sites',
+        'suppliers',
+        'clients'
+    ]
+    data['filters'] = base_filters
+    return JsonResponse({'status': 'success', 'data': data})
+
 
 @check_user_rights
 def get_lots(request, *args, **kwargs):
