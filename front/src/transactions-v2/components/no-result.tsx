797b9fd7dcb33da2915b1689d67ebe6bf7828453
jfalxa <jfalxa.sta@gmail.com>
2021-11-11 19:45:00 +0100

    [front] various adjustments

--- front/src/transactions-v2/components/no-result.tsx
+++ front/src/transactions-v2/components/no-result.tsx
@@ -3,28 +3,33 @@ import Button from "common-v2/components/button"
 import { AlertCircle } from "common-v2/components/icons"
 import { useTranslation } from "react-i18next"
 
-interface NoResultProps {
+interface NoResultProps extends ResetButtonProps {
   loading: boolean
-  filterCount: number
-  onReset: () => void
 }
 
-export const NoResult = ({ loading, filterCount, onReset }: NoResultProps) => {
+export const NoResult = ({ loading, count, onReset }: NoResultProps) => {
   const { t } = useTranslation()
 
   return (
     <Alert loading={loading} variant="warning" icon={AlertCircle}>
-      <p>{t("Aucun résultat trouvé pour cette recherche")}</p>
-
-      <Button
-        asideX
-        variant="link"
-        label={t("Réinitialiser les filtres ({{amount}})", {
-          amount: filterCount,
-        })}
-        action={onReset}
-      />
+      <p>{t("Aucune transaction trouvée pour cette recherche")}</p>
+      <ResetButton count={count} onReset={onReset} />
     </Alert>
+  )
+}
+
+interface ResetButtonProps {
+  count: number
+  onReset: () => void
+}
+
+export const ResetButton = ({ count, onReset }: ResetButtonProps) => {
+  const { t } = useTranslation()
+  return (
+    <Button asideX variant="link" action={onReset}>
+      {t("Réinitialiser les filtres")}
+      <span>({count})</span>
+    </Button>
   )
 }
 
