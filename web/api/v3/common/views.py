9680f620bab6263ce34a2e667c8cc55d17e45a72
Martin Planes <martin.planes@gmail.com>
2021-11-17 16:05:48 +0100

    [api] production-site search latency improvement

--- web/api/v3/common/views.py
+++ web/api/v3/common/views.py
@@ -99,7 +99,7 @@ def get_delivery_sites(request):
 def get_production_sites(request):
     q = request.GET.get('query', False)
     pid = request.GET.get('producer_id', False)
-    psites = ProductionSite.objects.all().order_by('name')
+    psites = ProductionSite.objects.select_related('country', 'producer').prefetch_related('productionsiteinput_set', 'productionsiteoutput_set').all().order_by('name')
     if q:
         psites = psites.filter(name__icontains=q)
     if pid:
@@ -110,11 +110,11 @@ def get_production_sites(request):
         v.inputs = []
         v.outputs = []
 
-    inputs = ProductionSiteInput.objects.filter(production_site__in=psites)
+    inputs = ProductionSiteInput.objects.select_related('matiere_premiere').filter(production_site__in=psites)
     for i in inputs:
         psitesbyid[i.production_site.id].inputs.append(i.matiere_premiere.natural_key())
 
-    outputs = ProductionSiteOutput.objects.filter(production_site__in=psites)
+    outputs = ProductionSiteOutput.objects.select_related('biocarburant').filter(production_site__in=psites)
     for o in outputs:
         psitesbyid[o.production_site.id].outputs.append(o.biocarburant.natural_key())
     sez = [{'name': p.name, 'id': p.id, 'country': p.country.natural_key(),
