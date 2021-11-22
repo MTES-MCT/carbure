e620b6abba79b645f4bf5bfe93ad3f934c419604
Martin Planes <martin.planes@gmail.com>
2021-11-22 16:52:33 +0100

    [api v4] sanity checks

--- web/api/v4/helpers.py
+++ web/api/v4/helpers.py
@@ -10,10 +10,10 @@ from django.db.models.query_utils import Q
 from django.http.response import HttpResponse, JsonResponse
 from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
-from certificates.models import DoubleCountingRegistration, EntityISCCTradingCertificate
+from certificates.models import DoubleCountingRegistration
 
 from core.ign_distance import get_distance
-from core.models import Biocarburant, CarbureLot, CarbureLotComment, CarbureLotEvent, CarbureStock, CarbureStockEvent, CarbureStockTransformation, Depot, Entity, EntityDepot, MatierePremiere, Pays, TransactionDistance, UserRights
+from core.models import Biocarburant, CarbureLot, CarbureLotComment, CarbureLotEvent, CarbureStock, CarbureStockEvent, CarbureStockTransformation, Depot, Entity, EntityCertificate, EntityDepot, GenericCertificate, MatierePremiere, Pays, TransactionDistance, UserRights
 from core.models import GenericError
 from core.serializers import CarbureLotCommentSerializer, CarbureLotEventSerializer, CarbureLotPublicSerializer, CarbureStockEventSerializer, CarbureStockPublicSerializer, GenericErrorSerializer
 from core.xlsx_v3 import export_carbure_lots, export_carbure_stock
@@ -733,7 +733,7 @@ def handle_eth_to_etbe_transformation(user, stock, transformation):
 def get_prefetched_data(entity=None):
     lastyear = datetime.date.today() - datetime.timedelta(days=365)
     d = {}
-    d['producers'] = {p.name: p for p in Entity.objects.filter(entity_type='Producteur')}
+    d['producers'] = {p.name: p for p in Entity.objects.filter(entity_type=Entity.PRODUCER)}
     d['countries'] = {p.code_pays: p for p in Pays.objects.all()}
     d['biocarburants'] = {b.code: b for b in Biocarburant.objects.all()}
     d['matieres_premieres'] = {m.code: m for m in MatierePremiere.objects.all()}
@@ -741,7 +741,7 @@ def get_prefetched_data(entity=None):
         # get only my production sites
         d['production_sites'] = {ps.name: ps for ps in ProductionSite.objects.prefetch_related('productionsiteinput_set', 'productionsiteoutput_set', 'productionsitecertificate_set').filter(producer=entity)}
         # get all my linked certificates
-        d['my_vendor_certificates'] = [c.certificate.certificate_id for c in EntityISCCTradingCertificate.objects.filter(entity=entity)]
+        d['my_vendor_certificates'] = [c.certificate.certificate_id for c in EntityCertificate.objects.filter(entity=entity)]
     else:
         d['production_sites'] = {ps.name: ps for ps in ProductionSite.objects.prefetch_related('productionsiteinput_set', 'productionsiteoutput_set', 'productionsitecertificate_set').all()}
     d['depots'] = {d.depot_id.lstrip('0').upper(): d for d in Depot.objects.all()}
@@ -750,7 +750,7 @@ def get_prefetched_data(entity=None):
     for obj in EntityDepot.objects.all():
         entitydepots.setdefault(obj.entity.id, []).append(obj.depot.id)
     d['depotsbyentity'] = entitydepots
-    d['clients'] = {c.name.upper(): c for c in Entity.objects.filter(entity_type__in=['Producteur', 'Opérateur', 'Trader'])}
+    d['clients'] = {c.name.upper(): c for c in Entity.objects.filter(entity_type__in=[Entity.PRODUCER, Entity.OPERATOR, Entity.TRADER])}
     d['certificates'] = {c.certificate_id.upper(): c for c in GenericCertificate.objects.filter(valid_until__gte=lastyear)}
     d['double_counting_certificates'] = {c.certificate_id: c for c in DoubleCountingRegistration.objects.all()}
     return d
