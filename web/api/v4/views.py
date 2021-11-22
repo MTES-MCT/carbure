4e0956dafd87f39afedd299054acb77e33b34c4c
jfalxa <jfalxa.sta@gmail.com>
2021-11-22 18:30:42 +0100

    [api] typos

--- web/api/v4/views.py
+++ web/api/v4/views.py
@@ -7,7 +7,7 @@ from django.db.models.aggregates import Count
 from django.http.response import JsonResponse
 from django.db.models.query_utils import Q
 from core.decorators import check_user_rights
-from api.v4.helpers import filter_lots, filter_stock, get_entity_lots_by_status, get_lot_comments, get_lot_errors, get_lot_updates, get_lots_summary_data, get_lots_with_metadata, get_lots_filters_data, get_entity_stock, 
+from api.v4.helpers import filter_lots, filter_stock, get_entity_lots_by_status, get_lot_comments, get_lot_errors, get_lot_updates, get_lots_summary_data, get_lots_with_metadata, get_lots_filters_data, get_entity_stock
 from api.v4.helpers import get_prefetched_data, get_stock_events, get_stock_with_metadata, get_stock_filters_data, get_stocks_summary_data, get_transaction_distance, handle_eth_to_etbe_transformation
 from api.v4.helpers import send_email_declaration_invalidated, send_email_declaration_validated
 from api.v4.sanity_checks import sanity_check
