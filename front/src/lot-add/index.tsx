57f18a3ad672ff06ce95078ca3a2ee42e6ec3a08
Martin Planes <martin.planes@gmail.com>
2021-11-25 17:10:16 +0100

    [front] add-lot plugged

--- front/src/lot-add/index.tsx
+++ front/src/lot-add/index.tsx
@@ -1,22 +1,38 @@
 import { useNavigate, useLocation } from "react-router-dom"
 import { useTranslation } from "react-i18next"
+import useEntity from 'carbure/hooks/entity'
+import { useNotify } from 'common-v2/components/notifications'
+import { useMutation } from 'common-v2/hooks/async'
 import Dialog from "common-v2/components/dialog"
 import Tag from "common-v2/components/tag"
 import Button from "common-v2/components/button"
 import { Plus, Return } from "common-v2/components/icons"
 import LotForm from "./components/lot-form"
+import * as api from './api'
 
 export const LotAdd = () => {
   const { t } = useTranslation()
+  const notify = useNotify()
+
+  const entity = useEntity()
 
   const navigate = useNavigate()
-  const location = useLocation()
+  const { search } = useLocation()
 
-  const close = () =>
-    navigate({
-      pathname: `../drafts`,
-      search: location.search,
-    })
+  const addLot = useMutation(api.addLot, {
+    invalidates: ['lots', 'snapshot'],
+
+    onSuccess: (res) => {
+      navigate({ pathname: `../drafts/${res.data.data?.id}`, search })
+      notify(t("Le lot a bien été créé"), { variant: 'success' })
+    },
+
+    onError: (res) => {
+      notify(t("La création du lot a échoué"), { variant: 'danger' })
+    }
+  })
+
+  const close = () => navigate({ pathname: `../drafts`, search })
 
   return (
     <Dialog onClose={close}>
@@ -27,12 +43,13 @@ export const LotAdd = () => {
 
       <main>
         <section>
-          <LotForm onSubmit={(form) => console.log(form)} />
+          <LotForm onSubmit={(lot) => addLot.execute(entity.id, lot!)} />
         </section>
       </main>
 
       <footer>
         <Button
+          loading={addLot.loading}
           variant="primary"
           icon={Plus}
           submit="lot-form"
