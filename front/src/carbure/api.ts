b7887f5f0bc381aa6d5129fdf882e12df20ab44d
jfalxa <jfalxa.sta@gmail.com>
2021-11-26 20:04:08 +0100

    [front] css tweaks

--- front/src/carbure/api.ts
+++ front/src/carbure/api.ts
@@ -2,5 +2,5 @@ import api, { Api } from "common-v2/services/api"
 import { User } from "./types"
 
 export function getUserSettings() {
-  return api.get<Api<User>>("v3/settings")
+  return api.get<Api<User>>("v3/settings/")
 }
