47851a862e4415fdf1185051b8c601b373ffe4f0
jfalxa <jfalxa.sta@gmail.com>
2021-12-13 17:40:24 +0100

    [api] filter deleted lots from snapshot

--- web/api/v4/views.py
+++ web/api/v4/views.py
@@ -42,12 +42,12 @@ def get_snapshot(request, *args, **kwargs):
     lots = CarbureLot.objects.filter(year=year)
     data = {}
     drafts = lots.filter(added_by_id=entity_id, lot_status=CarbureLot.DRAFT)
-    lots_in = lots.filter(carbure_client_id=entity_id)
+    lots_in = lots.filter(carbure_client_id=entity_id).exclude(lot_status__in=[CarbureLot.DELETED, CarbureLot.DRAFT])
     lots_in_pending = lots_in.filter(lot_status=CarbureLot.PENDING)
     lots_in_tofix = lots_in.exclude(correction_status=CarbureLot.NO_PROBLEMO)
     stock = CarbureStock.objects.filter(carbure_client_id=entity_id).filter(Q(parent_lot__year=year) | Q(parent_transformation__transformation_dt__year=year))
     stock_not_empty = stock.filter(remaining_volume__gt=0)
-    lots_out = lots.filter(carbure_supplier_id=entity_id)
+    lots_out = lots.filter(carbure_supplier_id=entity_id).exclude(lot_status__in=[CarbureLot.DELETED, CarbureLot.DRAFT])
     lots_out_pending = lots_out.filter(lot_status=CarbureLot.PENDING)
     lots_out_tofix = lots_out.exclude(correction_status=CarbureLot.NO_PROBLEMO)
     data['lots'] = {'draft': drafts.count(),
