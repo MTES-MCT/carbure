e03ab9871379b98655df5cf0d8704b1cbb8f393b
Martin Planes <martin.planes@gmail.com>
2021-12-16 17:36:13 +0100

    [scripts] show lot history in cli

--- scripts/show_lot_history.py
+++ scripts/show_lot_history.py
@@ -11,22 +11,30 @@ django.setup()
 from core.models import *
 
 def pretty_print(tx):
-    print('Tx [%d] Lot [%d] %s %s %d (remaining %d) From %s To %s [Period %s - %s]' % (tx.id, tx.lot.id, tx.lot.biocarburant.name, tx.lot.matiere_premiere.name, tx.lot.volume, tx.lot.remaining_volume,
+    print('Tx [%d] Lot [%d] Parent Lot [%s] %s %s %d (remaining %d) From %s To %s [Period %s - %s] Forwarded: %s' % (tx.id, tx.lot.id, tx.lot.parent_lot_id, tx.lot.biocarburant.name, tx.lot.matiere_premiere.name, tx.lot.volume, tx.lot.remaining_volume,
                                                                                        tx.lot.carbure_producer.name if tx.lot.carbure_producer else tx.lot.unknown_producer, tx.carbure_client.name if tx.carbure_client else tx.unknown_client,
-                                                                                       tx.lot.period, tx.delivery_date))
+                                                                                                     tx.lot.period, tx.delivery_date, tx.is_forwarded))
 
+def show_tx_and_parents(tx):
+    if tx.parent_tx:
+        show_tx_and_parents(tx.parent_tx)
+    pretty_print(tx)
+
+def show_tx_and_child(tx):
+    pretty_print(tx)
+    
+    child = LotTransaction.objects.filter(parent_tx=tx)
+    if child:
+        for c in child:
+            show_tx_and_child(c)
+        
 def show_lot_and_child(id):
     tx = LotTransaction.objects.get(id=id)
-    pretty_print(tx)
+
+    show_tx_and_parents(tx)
     child = LotTransaction.objects.filter(parent_tx=tx)
-    vsum = 0
     for c in child:
-        pretty_print(c)
-        vsum += c.lot.volume
-    print('Child Volume SUM: %d' % (vsum))
-    print('Actual Remaining Volume %d' % (tx.lot.remaining_volume))
-    print('Theo Remaining Volume %d' % (tx.lot.volume - vsum))    
-    print('Diff %d' % (tx.lot.remaining_volume - (tx.lot.volume - vsum)))
+        show_tx_and_child(c)
     
 if __name__ == '__main__':
     show_lot_and_child(id=int(sys.argv[1]))
