c96e89e077e0ba929189c085aed62e4fc3db6b56
Martin Planes <martin.planes@gmail.com>
2021-12-16 17:58:42 +0100

    [migration] final touch

--- scripts/migration_v4.py
+++ scripts/migration_v4.py
@@ -400,105 +400,19 @@ def migrate_old_data(quick=False):
         # errors
         errors = GenericError.objects.filter(tx=tx).update(lot=new_tx)
 
-def migrate_old_certificates():
-    # alter column character set
-    cursor = connection.cursor()
-    sql1 = "ALTER TABLE carbure_certificates CHANGE certificate_holder certificate_holder VARCHAR(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-    cursor.execute(sql1)
-    sql2 = "ALTER TABLE carbure_certificates CHANGE address address VARCHAR(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-    cursor.execute(sql2)
-
-    # SN
-    sn = SNCertificate.objects.all()
-    sne = EntitySNTradingCertificate.objects.all()
-    for s in sn:
-        print(s.certificate_id)
-        year_from = s.certificate_id.split('_')[2]
-        dt_from = datetime.date(year=int(year_from), month=1, day=1)
-
-        d = {'certificate_type': GenericCertificate.SYSTEME_NATIONAL,
-             'certificate_holder': s.certificate_holder,
-             'valid_from': s.valid_from if s.valid_from else dt_from,
-             'valid_until': s.valid_until,
-             'download_link': s.download_link,}
-        GenericCertificate.objects.update_or_create(certificate_id=s.certificate_id, defaults=d)
-    for se in sne:
-        cert = GenericCertificate.objects.filter(certificate_id=se.certificate.certificate_id)
-        if cert.count() > 0:
-            EntityCertificate.objects.update_or_create(certificate=cert[0], entity=se.entity)
-        else:
-            print('Could not migrate certificate %s' % (se.certificate.certificate_id))
-
-    # ISCC
-    certs = ISCCCertificate.objects.all()
-    iscc_links = EntityISCCTradingCertificate.objects.all()
-    for c in certs:
-        print(c.certificate_id)
-        d = {'certificate_type': GenericCertificate.ISCC,
-             'certificate_holder': c.certificate_holder,
-             'certificate_issuer': c.issuing_cb,
-             'address': c.location,
-             'valid_from': c.valid_from,
-             'valid_until': c.valid_until,
-             'download_link': c.download_link,}
-        GenericCertificate.objects.update_or_create(certificate_id=c.certificate_id, defaults=d)
-    for link in iscc_links:
-        cert = GenericCertificate.objects.filter(certificate_type=GenericCertificate.ISCC, certificate_id=link.certificate.certificate_id)
-        if cert.count() > 0:
-            EntityCertificate.objects.update_or_create(certificate=cert[0], entity=link.entity)
-        else:
-            print('Could not migrate certificate %s' % (link.certificate.certificate_id))        
-    # REDCERT
-    certs = REDCertCertificate.objects.all()
-    red_links = EntityREDCertTradingCertificate.objects.all()
-    for c in certs:
-        print(c.certificate_id)
-        d = {'certificate_type': GenericCertificate.REDCERT,
-             'certificate_holder': c.certificate_holder,
-             'certificate_issuer': c.certificator,
-             'address': "%s %s %s" % (c.city, c.zip_code, c.country_raw),
-             'valid_from': c.valid_from,
-             'valid_until': c.valid_until,
-             'download_link': None}
-        GenericCertificate.objects.update_or_create(certificate_id=c.certificate_id, defaults=d)
-    for link in red_links:
-        cert = GenericCertificate.objects.filter(certificate_type=GenericCertificate.REDCERT, certificate_id=link.certificate.certificate_id)
-        if cert.count() > 0:
-            EntityCertificate.objects.update_or_create(certificate=cert[0], entity=link.entity)
-        else:
-            print('Could not migrate certificate %s' % (link.certificate.certificate_id))   
-    # 2BS
-    certs = DBSCertificate.objects.all()
-    dbs_links = EntityDBSTradingCertificate.objects.all()
-    for c in certs:
-        print(c.certificate_id)
-        d = {'certificate_type': GenericCertificate.DBS,
-             'certificate_holder': c.certificate_holder,
-             'certificate_issuer': None,
-             'address': c.holder_address,
-             'valid_from': c.valid_from,
-             'valid_until': c.valid_until,
-             'download_link': c.download_link,}
-        GenericCertificate.objects.update_or_create(certificate_id=c.certificate_id, defaults=d)
-    for link in dbs_links:
-        cert = GenericCertificate.objects.filter(certificate_type=GenericCertificate.DBS, certificate_id=link.certificate.certificate_id)
-        if cert.count() > 0:
-            EntityCertificate.objects.update_or_create(certificate=cert[0], entity=link.entity)
-        else:
-            print('Could not migrate certificate %s' % (link.certificate.certificate_id))   
-    
 
 if __name__ == '__main__':
-    # ensure transformation transactions have the right parent_tx
-    transformations = ETBETransformation.objects.all()
-    for t in transformations:
-        if t.new_stock.parent_tx != t.previous_stock:
-            print('Updating ETBE Transformation %d. For dest tx %d Previous parent_tx %d new %d ' % (t.id, t.new_stock.id, t.new_stock.parent_tx.id if t.new_stock.parent_tx else 0, t.previous_stock.id))
-            t.new_stock.parent_tx = t.previous_stock
-            t.new_stock.save()
-        else:
-            print('All good')
-    LotTransaction.objects.filter(id=144653).update(parent_tx_id=144535)
+    # fix links between sublots and lots
+    txs = LotTransaction.objects.filter(lot__parent_lot__isnull=False)
+    for tx in txs:
+        if tx.parent_tx is None:
+            potential_parents = LotTransaction.objects.filter(lot_id=tx.lot.parent_lot.id)
+            if potential_parents.count() == 1:
+                tx.parent_tx = potential_parents[0]
+                tx.save()
+                print('Fix link between parent %s and child %s' % (potential_parents[0].id, tx.id))
+
+    #LotTransaction.objects.filter(id=144653).update(parent_tx_id=144535)
 
     # ensure forwarded lots are marked as such
     lots_with_parent = LotV2.objects.filter(parent_lot__isnull=False)
@@ -519,6 +433,10 @@ if __name__ == '__main__':
                     if x == 'y':
                         t.parent_tx_id = parent.id
                         t.save()
+                        transformed = input('mark parent lot as transformed ?')
+                        if transformed == 'y':
+                            parent.lot.is_transformed = True
+                            parent.lot.save()
                     else:
                         print('You said NO')
                         assert(False)
@@ -539,5 +457,4 @@ if __name__ == '__main__':
                 ctx.save()
 
 
-    migrate_old_data(quick=True)
-    #migrate_old_certificates()
+    migrate_old_data()
