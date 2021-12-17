d599ef432fdd78c297220829075428a6f7a77293
Martin Planes <martin.planes@gmail.com>
2021-12-17 17:30:58 +0100

    [migration] reduce script footprint

--- scripts/show_lot_history.py
+++ scripts/show_lot_history.py
@@ -11,9 +11,9 @@ django.setup()
 from core.models import *
 
 def pretty_print(tx):
-    print('Tx [%d] Lot [%d] Parent Lot [%s] %s %s %d (remaining %d) From %s To %s [Period %s - %s] Forwarded: %s' % (tx.id, tx.lot.id, tx.lot.parent_lot_id, tx.lot.biocarburant.name, tx.lot.matiere_premiere.name, tx.lot.volume, tx.lot.remaining_volume,
+    print('Tx [%d] Parent Tx [%s] Lot [%d] Parent Lot [%s] %s %s %d (remaining %d) From %s To %s [Period %s - %s] Forwarded: %s Stock: %s' % (tx.id, tx.parent_tx_id, tx.lot.id, tx.lot.parent_lot_id, tx.lot.biocarburant.name, tx.lot.matiere_premiere.name, tx.lot.volume, tx.lot.remaining_volume,
                                                                                        tx.lot.carbure_producer.name if tx.lot.carbure_producer else tx.lot.unknown_producer, tx.carbure_client.name if tx.carbure_client else tx.unknown_client,
-                                                                                                     tx.lot.period, tx.delivery_date, tx.is_forwarded))
+                                                                                                                               tx.lot.period, tx.delivery_date, tx.is_forwarded, tx.is_stock))
 
 def show_tx_and_parents(tx):
     if tx.parent_tx:
@@ -32,9 +32,13 @@ def show_lot_and_child(id):
     tx = LotTransaction.objects.get(id=id)
 
     show_tx_and_parents(tx)
-    child = LotTransaction.objects.filter(parent_tx=tx)
+    child = LotTransaction.objects.filter(parent_tx=tx, lot__status='Validated')
+    child_volume = 0
     for c in child:
         show_tx_and_child(c)
+        child_volume += c.lot.volume
+
+    print('Parent volume [%d] remaining [%d] theo remaining [%d] diff [%d] child volume [%d]' % (tx.lot.volume, tx.lot.remaining_volume, tx.lot.volume - child_volume, tx.lot.remaining_volume - (tx.lot.volume - child_volume), child_volume))
     
 if __name__ == '__main__':
     show_lot_and_child(id=int(sys.argv[1]))
