2ec60b3b25d362d31f9bd20a70446e85f0bc84de
jfalxa <jfalxa.sta@gmail.com>
2022-01-14 13:17:17 +0100

    [front] show delivery types in lot tag

--- front/src/transactions/components/lots/lot-tag.tsx
+++ front/src/transactions/components/lots/lot-tag.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "react-i18next"
 import { Tag, TagProps, TagVariant } from "common-v2/components/tag"
-import { CorrectionStatus, Lot, LotStatus } from "../../types"
+import { Lot, LotStatus, CorrectionStatus, DeliveryType } from "../../types"
 
 export interface LotTagProps extends TagProps {
   lot: Lot
@@ -12,29 +12,46 @@ export const LotTag = ({ lot, ...props }: LotTagProps) => {
   let label = t("N/A")
   let variant: TagVariant | undefined = undefined
 
-  if (lot.lot_status === LotStatus.Draft) {
+  const status = lot.lot_status
+  const correction = lot.correction_status
+  const delivery = lot.delivery_type
+
+  if (status === LotStatus.Draft) {
     label = t("Brouillon")
-  } else if (lot.lot_status === LotStatus.Rejected) {
+  } else if (status === LotStatus.Rejected) {
     label = t("Refusé")
     variant = "danger"
-  } else if (lot.correction_status === CorrectionStatus.InCorrection) {
+  } else if (correction === CorrectionStatus.InCorrection) {
     label = t("En correction")
     variant = "warning"
-  } else if (lot.correction_status === CorrectionStatus.Fixed) {
+  } else if (correction === CorrectionStatus.Fixed) {
     label = t("Corrigé")
     variant = "success"
-  } else if (lot.lot_status === LotStatus.Pending) {
+  } else if (status === LotStatus.Pending) {
     label = t("En attente")
     variant = "info"
-  } else if (lot.lot_status === LotStatus.Accepted) {
-    label = t("Accepté")
-    variant = "success"
-  } else if (lot.lot_status === LotStatus.Frozen) {
-    label = t("Déclaré")
-    variant = "success"
-  } else if (lot.lot_status === LotStatus.Deleted) {
+  } else if (status === LotStatus.Deleted) {
     label = t("Supprimé")
     variant = "danger"
+  } else {
+    variant = "success"
+    if (delivery === DeliveryType.Blending) {
+      label = t("Incorporation")
+    } else if (delivery === DeliveryType.Direct) {
+      label = t("Livraison directe")
+    } else if (delivery === DeliveryType.Export) {
+      label = t("Exportation")
+    } else if (delivery === DeliveryType.Processing) {
+      label = t("Processing")
+    } else if (delivery === DeliveryType.RFC) {
+      label = t("Mise à conso.")
+    } else if (delivery === DeliveryType.Stock) {
+      label = t("Stocké")
+    } else if (status === LotStatus.Accepted) {
+      label = t("Accepté")
+    } else if (status === LotStatus.Frozen) {
+      label = t("Déclaré")
+    }
   }
 
   return <Tag {...props} variant={variant} label={label} />
