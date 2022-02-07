3ca9392206736effaae1e6d0a5e8607139e22a13
Martin Planes <martin.planes@gmail.com>
2022-02-07 13:20:12 +0100

    [migrations] configure entities with stock

---
+++ scripts/flag_entities.py
@@ -0,0 +1,17 @@
+import os
+import django
+import argparse
+from django.db.models import Sum
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carbure.settings")
+django.setup()
+
+
+from core.models import *
+
+if __name__ == '__main__':
+    entites_with_stock = CarbureStock.objects.values('carbure_client').distinct()
+    for e in entites_with_stock:
+        entity = Entity.objects.get(id=e['carbure_client'])
+        entity.has_stocks = True
+        entity.save()
