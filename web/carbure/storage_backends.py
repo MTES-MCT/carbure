c9c581e4af047ab517504715d6409ef1b4974639
Martin Planes <martin.planes@gmail.com>
2022-02-10 15:25:01 +0100

    [pip] upgrade

--- web/carbure/storage_backends.py
+++ web/carbure/storage_backends.py
@@ -1,4 +1,9 @@
 from django.conf import settings
+import django
+from django.utils.encoding import force_str, smart_str
+django.utils.encoding.force_text = force_str
+django.utils.encoding.smart_text = smart_str
+
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class AWSStorage(S3Boto3Storage):
