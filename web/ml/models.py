a59dd114be89758cd7789433f578b3b1b137aaff
Martin Planes <martin.planes@gmail.com>
2022-03-15 14:11:50 +0100

    [ml] initial script

--- web/ml/models.py
+++ web/ml/models.py
@@ -6,7 +6,7 @@ class EECStats(models.Model):
     feedstock = models.ForeignKey(MatierePremiere, null=False, blank=False, on_delete=models.CASCADE)
     origin = models.ForeignKey(Pays, null=False, blank=False, on_delete=models.CASCADE)
     nb_lots = models.IntegerField(null=False, blank=False)
-    default_value = models.FloatField(null=False, blank=False)
+    default_value = models.FloatField(null=False, blank=False, default=0.0)
     stddev = models.FloatField(null=False, blank=False)
     average = models.FloatField(null=False, blank=False)
 
@@ -20,8 +20,8 @@ class EPStats(models.Model):
     feedstock = models.ForeignKey(MatierePremiere, null=False, blank=False, on_delete=models.CASCADE)
     biofuel = models.ForeignKey(Biocarburant, null=False, blank=False, on_delete=models.CASCADE)
     nb_lots = models.IntegerField(null=False, blank=False)
-    default_value_min_eec = models.FloatField(null=False, blank=False)
-    default_value_max_eec = models.FloatField(null=False, blank=False)
+    default_value_min_eec = models.FloatField(null=False, blank=False, default=0.0)
+    default_value_max_eec = models.FloatField(null=False, blank=False, default=0.0)
     stddev = models.FloatField(null=False, blank=False)
     average = models.FloatField(null=False, blank=False)
 
@@ -33,7 +33,7 @@ class EPStats(models.Model):
 
 class ETDStats(models.Model):
     feedstock = models.ForeignKey(MatierePremiere, null=False, blank=False, on_delete=models.CASCADE)
-    default_value = models.FloatField(null=False, blank=False)
+    default_value = models.FloatField(null=False, blank=False, default=0.0)
 
     class Meta:
         db_table = 'ml_etd_stats'
--- web/ml/scripts/load_data.py
+++ web/ml/scripts/load_data.py
@@ -1,16 +1,42 @@
 import os
 import django
+from django.db.models.aggregates import StdDev
+from django.db.models import Avg, Count, Min, Max
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carbure.settings")
 django.setup()
 
-from core.models import Entity, MatierePremiere, Pays, Biocarburant, CarbureLot
+from ml.models import EECStats, EPStats
+from core.models import Entity, MatierePremiere, Biocarburant, CarbureLot
 
 def load_eec_data():
-    pass
+    data = CarbureLot.objects.filter(feedstock__category=MatierePremiere.CONV, lot_status__in=[CarbureLot.ACCEPTED, CarbureLot.FROZEN], country_of_origin__isnull=False)\
+    .exclude(eec=0)\
+    .values('feedstock', 'country_of_origin')\
+    .annotate(nb_lots=Count('eec'), average=Avg('eec'), stddev=StdDev('eec'), min=Min('eec'), max=Max('eec'))\
+    .exclude(nb_lots__lt=10)
+    for entry in data:
+        d = {
+            'nb_lots': entry['nb_lots'],
+            'stddev': round(entry['stddev'], 2),
+            'average': round(entry['average'], 2),
+        }
+        EECStats.objects.update_or_create(feedstock_id=entry['feedstock'], origin_id=entry['country_of_origin'], defaults=d)
 
 def load_ep_data():
-    pass
+    data = CarbureLot.objects.filter(lot_status__in=[CarbureLot.ACCEPTED, CarbureLot.FROZEN], country_of_origin__isnull=False)\
+    .exclude(ep=0)\
+    .values('biofuel', 'feedstock',)\
+    .annotate(nb_lots=Count('ep'), average=Avg('ep'), stddev=StdDev('ep'), min=Min('ep'), max=Max('ep'))\
+    .exclude(nb_lots__lt=10)
+    for entry in data:
+        d = {
+            'nb_lots': entry['nb_lots'],
+            'stddev': round(entry['stddev'], 2),
+            'average': round(entry['average'], 2),
+        }
+        EPStats.objects.update_or_create(biofuel_id=entry['biofuel'], feedstock_id=entry['feedstock'], defaults=d)
 
 def load_etd_data():
     pass
