441e8a970257629fb214f045c082426d7968414e
Martin Planes <martin.planes@gmail.com>
2022-03-24 10:06:13 +0100

    [notifications] accumulate unread notifications

--- web/core/notifications.py
+++ web/core/notifications.py
@@ -1,4 +1,6 @@
 from calendar import calendar
+from pydoc import cli
+from unittest.util import three_way_cmp
 from core.models import CarbureLot, CarbureNotification, SustainabilityDeclaration
 import datetime
 from django.db.models import Count
@@ -9,12 +11,16 @@ def notify_correction_request(lots):
         .values('year', 'carbure_client__name', 'carbure_supplier')\
         .annotate(count=Count('id'), count_frozen=Count('id', filter=Q(lot_status=CarbureLot.FROZEN)))
     for supplier in lots_by_supplier:
-        notif = CarbureNotification()
-        notif.type = CarbureNotification.CORRECTION_REQUEST
-        notif.dest_id = supplier['carbure_supplier']
-        notif.send_by_email = True
-        notif.notify_administrator = supplier['count_frozen'] > 0
-        notif.meta = {'client': supplier['carbure_client__name'], 'count': supplier['count'], 'year': supplier['year']}
+        try:
+            notif = CarbureNotification.objects.get(dest_id=supplier['carbure_supplier'], type=CarbureNotification.CORRECTION_REQUEST, acked=False, email_sent=False, meta__contains={'client': supplier['carbure_client__name']})
+            notif.meta['count'] = notif.meta['count'] + supplier['count']
+        except:
+            notif = CarbureNotification()
+            notif.type = CarbureNotification.CORRECTION_REQUEST
+            notif.dest_id = supplier['carbure_supplier']
+            notif.send_by_email = True
+            notif.notify_administrator = supplier['count_frozen'] > 0
+            notif.meta = {'client': supplier['carbure_client__name'], 'count': supplier['count'], 'year': supplier['year']}
         notif.save()
 
 def notify_correction_done(lots):
@@ -22,12 +28,16 @@ def notify_correction_done(lots):
         .values('year', 'carbure_client', 'carbure_supplier__name')\
         .annotate(count=Count('id'), count_frozen=Count('id', filter=Q(lot_status=CarbureLot.FROZEN)))
     for client in lots_by_client:
-        notif = CarbureNotification()
-        notif.type = CarbureNotification.CORRECTION_DONE
-        notif.dest_id = client['carbure_client']
-        notif.send_by_email = True
-        notif.notify_administrator = client['count_frozen'] > 0
-        notif.meta = {'supplier': client['carbure_supplier__name'], 'count': client['count'], 'year': client['year']}
+        try:
+            notif = CarbureNotification.objects.get(dest_id=client['carbure_client'], type=CarbureNotification.CORRECTION_DONE, acked=False, email_sent=False, meta__contains={'supplier': client['carbure_supplier__name']})
+            notif.meta['count'] = notif.meta['count'] + client['count']
+        except:
+            notif = CarbureNotification()
+            notif.type = CarbureNotification.CORRECTION_DONE
+            notif.dest_id = client['carbure_client']
+            notif.send_by_email = True
+            notif.notify_administrator = client['count_frozen'] > 0
+            notif.meta = {'supplier': client['carbure_supplier__name'], 'count': client['count'], 'year': client['year']}
         notif.save()
 
 def notify_lots_rejected(lots):
@@ -35,12 +45,16 @@ def notify_lots_rejected(lots):
         .values('year', 'carbure_client__name', 'carbure_supplier')\
         .annotate(count=Count('id'), count_frozen=Count('id', filter=Q(lot_status=CarbureLot.FROZEN)))
     for supplier in lots_by_supplier:
-        notif = CarbureNotification()
-        notif.type = CarbureNotification.LOTS_REJECTED
-        notif.dest_id = supplier['carbure_supplier']
-        notif.send_by_email = True
-        notif.notify_administrator = supplier['count_frozen'] > 0
-        notif.meta = {'client': supplier['carbure_client__name'], 'count': supplier['count'], 'year': supplier['year']}
+        try:
+            notif = CarbureNotification.objects.get(dest_id=supplier['carbure_supplier'], type=CarbureNotification.LOTS_REJECTED, acked=False, email_sent=False, meta__contains={'client': supplier['carbure_client__name']})
+            notif.meta['count'] = notif.meta['count'] + supplier['count']
+        except:
+            notif = CarbureNotification()
+            notif.type = CarbureNotification.LOTS_REJECTED
+            notif.dest_id = supplier['carbure_supplier']
+            notif.send_by_email = True
+            notif.notify_administrator = supplier['count_frozen'] > 0
+            notif.meta = {'client': supplier['carbure_client__name'], 'count': supplier['count'], 'year': supplier['year']}
         notif.save()
 
 def notify_lots_received(lots):
@@ -52,11 +66,16 @@ def notify_lots_received(lots):
             continue
         if client['carbure_supplier__name'] is None: ### batches added to stock
             continue
-        notif = CarbureNotification()
-        notif.type = CarbureNotification.LOTS_RECEIVED
-        notif.dest_id = client['carbure_client']
-        notif.send_by_email = True
-        notif.meta = {'supplier': client['carbure_supplier__name'], 'count': client['count'], 'year': client['year']}
+        try:
+            # if the same notif exists 
+            notif = CarbureNotification.objects.get(dest_id=client['carbure_client'], type=CarbureNotification.LOTS_RECEIVED, acked=False, email_sent=False, meta__contains={'supplier': client['carbure_supplier__name']})
+            notif.meta['count'] = notif.meta['count'] + client['count']
+        except:
+            notif = CarbureNotification()
+            notif.type = CarbureNotification.LOTS_RECEIVED
+            notif.dest_id = client['carbure_client']
+            notif.send_by_email = True
+            notif.meta = {'supplier': client['carbure_supplier__name'], 'count': client['count'], 'year': client['year']}
         notif.save()
 
 def notify_lots_recalled(lots):
@@ -64,11 +83,15 @@ def notify_lots_recalled(lots):
         .values('year', 'carbure_client', 'carbure_supplier__name')\
         .annotate(count=Count('id'))
     for client in lots_by_client:
-        notif = CarbureNotification()
-        notif.type = CarbureNotification.LOTS_RECALLED
-        notif.dest_id = client['carbure_client']
-        notif.send_by_email = True
-        notif.meta = {'supplier': client['carbure_supplier__name'], 'count': client['count'], 'year': client['year']}
+        try:
+            notif = CarbureNotification.objects.get(dest_id=client['carbure_client'], type=CarbureNotification.LOTS_RECALLED, acked=False, email_sent=False, meta__contains={'supplier': client['carbure_supplier__name']})
+            notif.meta['count'] = notif.meta['count'] + client['count']
+        except:
+            notif = CarbureNotification()
+            notif.type = CarbureNotification.LOTS_RECALLED
+            notif.dest_id = client['carbure_client']
+            notif.send_by_email = True
+            notif.meta = {'supplier': client['carbure_supplier__name'], 'count': client['count'], 'year': client['year']}
         notif.save()
 
 def notify_declaration_cancelled(declaration):
