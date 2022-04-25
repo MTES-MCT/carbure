bd2c584ea028b48f78264209ca9756b3f9755a79
Martin Planes <martin.planes@gmail.com>
2022-04-25 16:04:16 +0200

    [urls] remove internal redirect of /stats

--- web/public/views.py
+++ web/public/views.py
@@ -1,15 +1,4 @@
-from django.shortcuts import render
 from django.shortcuts import redirect
-
 
 def index(request):
     return redirect('/app/')
-
-
-def stats(request):
-    return redirect('https://metabase.carbure.beta.gouv.fr/public/dashboard/d2da144c-9674-41bd-83e5-4bc9cbb98b50')
-
-
-def annuaire(request):
-    context = {}
-    return render(request, 'public/index.html', context)
