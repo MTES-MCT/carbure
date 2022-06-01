deab1134ba06891072c3c8cb493bb09a2fd3d7a3
jfalxa <jfalxa.sta@gmail.com>
2022-06-01 23:14:57 +0200

    [front] migrate production site settings to new DS

--- front/src/entities/routes/entity-details.tsx
+++ front/src/entities/routes/entity-details.tsx
@@ -1,29 +1,20 @@
-import { useEffect } from "react"
 import { useParams } from "react-router-dom"
-
-import useAPI from "common/hooks/use-api"
 import useClose from "common/hooks/use-close"
-
 import { ChevronLeft } from "common-v2/components/icons"
 import { Button } from "common-v2/components/button"
 import UserRights from "../components/user-rights"
 import * as api from "../api"
 import DeliverySitesSettings from "settings/components/delivery-site"
-import { ProductionSiteSettingsHook } from "settings/hooks/use-production-sites"
 import ProductionSitesSettings from "settings/components/production-site"
 import { EntityType } from "carbure/types"
-import { ProductionSiteDetails } from "common/types"
-import { ProductionSitePrompt } from "settings/components/production-site"
 import Certificates from "entities/components/certificates"
 import { Main, Row } from "common-v2/components/scaffold"
 import Tabs from "common-v2/components/tabs"
 import { compact } from "common-v2/utils/collection"
-import { usePortal } from "common-v2/components/portal"
 import { useQuery } from "common-v2/hooks/async"
 
 const EntityDetails = () => {
   const close = useClose("..")
-  const portal = usePortal()
   const { id = "" } = useParams<"id">()
   const entityID = parseInt(id, 10)
 
@@ -32,35 +23,8 @@ const EntityDetails = () => {
     params: [entityID],
   })
 
-  const productionSites = useQuery(api.getEntityProductionSites, {
-    key: "production-sites",
-    params: [entityID],
-  })
-
   const entityData = entity.result?.data.data
   const isProducer = entityData?.entity_type === EntityType.Producer
-
-  async function editProductionSite(ps: ProductionSiteDetails) {
-    // "Détails du site de production",
-    portal((close) => (
-      <ProductionSitePrompt
-        readOnly
-        title="Détails du site de production"
-        entity={entityData}
-        productionSite={ps}
-        onResolve={close}
-      />
-    ))
-  }
-
-  const productionSitesData = productionSites.result?.data.data
-
-  const productionSiteSettings: ProductionSiteSettingsHook = {
-    productionSites: productionSitesData ?? [],
-    isLoading: productionSites.loading,
-    isEmpty: (productionSitesData ?? []).length === 0,
-    editProductionSite,
-  }
 
   return (
     <Main>
@@ -90,8 +54,12 @@ const EntityDetails = () => {
             getDepots={api.getEntityDepots}
           />
         )}
-        {isProducer && (
-          <ProductionSitesSettings settings={productionSiteSettings} />
+        {entityData && isProducer && (
+          <ProductionSitesSettings
+            readOnly
+            entity={entityData}
+            getProductionSites={api.getEntityProductionSites}
+          />
         )}
         <Certificates entity={entityID} />
       </section>
