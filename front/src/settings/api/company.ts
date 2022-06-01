d5269bcc2b107cbde44fe1a2420d4f1e51876fb0
jfalxa <jfalxa.sta@gmail.com>
2022-06-01 19:51:18 +0200

    [front[ separate settings api in many files

---
+++ front/src/settings/api/company.ts
@@ -0,0 +1,51 @@
+import { api, Api } from "common-v2/services/api"
+import { Certificate, CertificateType, EntityCertificate } from "common/types"
+
+export function toggleMAC(entity_id: number, shouldEnable: boolean) {
+  const endpoint = shouldEnable
+    ? "/v3/settings/enable-mac"
+    : "/v3/settings/disable-mac"
+  return api.post(endpoint, { entity_id })
+}
+
+export function toggleTrading(entity_id: number, shouldEnable: boolean) {
+  const endpoint = shouldEnable
+    ? "/v3/settings/enable-trading"
+    : "/v3/settings/disable-trading"
+  return api.post(endpoint, { entity_id })
+}
+
+export function toggleStocks(entity_id: number, shouldEnable: boolean) {
+  const endpoint = shouldEnable
+    ? "/v3/settings/enable-stocks"
+    : "/v3/settings/disable-stocks"
+  return api.post(endpoint, { entity_id })
+}
+
+export function toggleDirectDeliveries(
+  entity_id: number,
+  shouldEnable: boolean
+) {
+  const endpoint = shouldEnable
+    ? "/v3/settings/enable-direct-deliveries"
+    : "/v3/settings/disable-direct-deliveries"
+  return api.post(endpoint, { entity_id })
+}
+
+export function updateEntity(
+  entity_id: number,
+  legal_name: string,
+  registration_id: string,
+  registered_address: string,
+  sustainability_officer: string,
+  sustainability_officer_phone_number: string
+) {
+  return api.post("/update-entity", {
+    entity_id,
+    legal_name,
+    registration_id,
+    registered_address,
+    sustainability_officer,
+    sustainability_officer_phone_number,
+  })
+}
