2270ae795dc7d4ffde8838e70a05294a25636337
jfalxa <jfalxa.sta@gmail.com>
2022-06-01 16:44:21 +0200

    [front] migrate private stats to new DS

--- front/src/stats/index.tsx
+++ front/src/stats/index.tsx
@@ -1,17 +1,29 @@
-import { useEffect } from "react"
 import { Route, Routes } from "react-router-dom"
-import { Entity } from "carbure/types"
 import { Trans, useTranslation } from "react-i18next"
-import { Main } from "common/components"
-import { Section } from "common/components/section"
 import IframeResizer from "iframe-resizer-react"
-import useAPI from "common/hooks/use-api"
-import api from "common/services/api"
+import { Entity } from "carbure/types"
+import { Main, Panel } from "common-v2/components/scaffold"
+import { useQuery } from "common-v2/hooks/async"
+import api, { Api } from "common-v2/services/api"
 import { EntityType } from "carbure/types"
 import useTitle from "common-v2/hooks/title"
+import useEntity from "carbure/hooks/entity"
 
-function getHash(entityId: number) {
-  return api.get("/settings/entity-hash", { entity_id: entityId })
+function getHash(entity_id: number) {
+  return api.get<Api<string>>("/settings/entity-hash", {
+    params: { entity_id },
+  })
+}
+
+function useEntityHash() {
+  const entity = useEntity()
+
+  const entityHash = useQuery(getHash, {
+    key: "entity-hash",
+    params: [entity.id],
+  })
+
+  return entityHash.result?.data.data
 }
 
 type StatsProps = {
@@ -19,20 +31,10 @@ type StatsProps = {
 }
 
 const Stats = ({ entity }: StatsProps) => {
-  const [entityHash, getEntityHash] = useAPI(getHash)
-  useEffect(() => {
-    if (entity) {
-      getEntityHash(entity.id)
-    }
-  }, [entity, getEntityHash])
-
   const { t } = useTranslation()
-
   useTitle(t("Statistiques"))
 
-  if (entityHash.data === null) {
-    return null
-  }
+  const hash = useEntityHash()
 
   const textWidth = 550
   const textAngle = 15
@@ -49,28 +51,28 @@ const Stats = ({ entity }: StatsProps) => {
   let text6 = ""
 
   if (entity?.entity_type === EntityType.Operator) {
-    link1 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/4fe4f013-188e-4693-81ec-963daeb758eb?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link2 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/f184cea4-967a-4284-902c-4a87acff5ca2?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link3 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/04601b75-225a-4a51-9491-9c9f40544f3e?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link4 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/2a697167-ddee-4fdf-aa59-43c981c71b53?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link5 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/2a481cb9-8db9-4ef3-bb05-4721c0a9d497?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link6 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/e7f0eacb-1034-4173-8634-ec4e000cd027?hash=${entityHash.data?.hash}#hide_parameters=hash`
+    link1 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/4fe4f013-188e-4693-81ec-963daeb758eb?hash=${hash}#hide_parameters=hash`
+    link2 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/f184cea4-967a-4284-902c-4a87acff5ca2?hash=${hash}#hide_parameters=hash`
+    link3 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/04601b75-225a-4a51-9491-9c9f40544f3e?hash=${hash}#hide_parameters=hash`
+    link4 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/2a697167-ddee-4fdf-aa59-43c981c71b53?hash=${hash}#hide_parameters=hash`
+    link5 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/2a481cb9-8db9-4ef3-bb05-4721c0a9d497?hash=${hash}#hide_parameters=hash`
+    link6 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/e7f0eacb-1034-4173-8634-ec4e000cd027?hash=${hash}#hide_parameters=hash`
     text6 = t("En savoir plus sur vos fournisseurs")
   } else if (entity?.entity_type === EntityType.Producer) {
-    link1 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/a960a32f-c14f-4835-9f6f-2553e951620c?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link2 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/7aa76cea-b60a-4e89-9bde-a116abd86018?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link3 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/11d88f12-22c4-467a-ad36-f6bf3a924717?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link4 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/e3b75456-7df2-4afe-89ea-ac9601abe349?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link5 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/d3722672-2e9f-48ad-beb0-29c3864b61ab?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link6 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/765ad219-d854-40e9-8f78-e32dedd28c54?hash=${entityHash.data?.hash}#hide_parameters=hash`
+    link1 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/a960a32f-c14f-4835-9f6f-2553e951620c?hash=${hash}#hide_parameters=hash`
+    link2 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/7aa76cea-b60a-4e89-9bde-a116abd86018?hash=${hash}#hide_parameters=hash`
+    link3 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/11d88f12-22c4-467a-ad36-f6bf3a924717?hash=${hash}#hide_parameters=hash`
+    link4 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/e3b75456-7df2-4afe-89ea-ac9601abe349?hash=${hash}#hide_parameters=hash`
+    link5 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/d3722672-2e9f-48ad-beb0-29c3864b61ab?hash=${hash}#hide_parameters=hash`
+    link6 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/765ad219-d854-40e9-8f78-e32dedd28c54?hash=${hash}#hide_parameters=hash`
     text6 = t("En savoir plus sur vos clients")
   } else if (entity?.entity_type === EntityType.Trader) {
-    link1 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/a960a32f-c14f-4835-9f6f-2553e951620c?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link2 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/f87e0e09-f82f-4ed1-82c6-2a8d85a1a8e3?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link3 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/801e5d6c-fb19-4f4c-82bb-06a7d0c7e5a8?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link4 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/8eb16042-a39e-469a-b357-10bcb3c4190d?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link5 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/70adcd33-801b-4ec4-a135-8cc5d33a6306?hash=${entityHash.data?.hash}#hide_parameters=hash`
-    link6 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/8d1e621d-f005-4904-a19d-e74305d3ce14?hash=${entityHash.data?.hash}#hide_parameters=hash`
+    link1 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/a960a32f-c14f-4835-9f6f-2553e951620c?hash=${hash}#hide_parameters=hash`
+    link2 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/f87e0e09-f82f-4ed1-82c6-2a8d85a1a8e3?hash=${hash}#hide_parameters=hash`
+    link3 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/801e5d6c-fb19-4f4c-82bb-06a7d0c7e5a8?hash=${hash}#hide_parameters=hash`
+    link4 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/8eb16042-a39e-469a-b357-10bcb3c4190d?hash=${hash}#hide_parameters=hash`
+    link5 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/70adcd33-801b-4ec4-a135-8cc5d33a6306?hash=${hash}#hide_parameters=hash`
+    link6 = `https://metabase.carbure.beta.gouv.fr/public/dashboard/8d1e621d-f005-4904-a19d-e74305d3ce14?hash=${hash}#hide_parameters=hash`
     text6 = t("En savoir plus sur vos clients et vos fournisseurs")
   } else {
     link6 = `about:blank`
@@ -79,7 +81,7 @@ const Stats = ({ entity }: StatsProps) => {
 
   return (
     <Main style={{ padding: "32px var(--main-spacing)" }}>
-      <Section
+      <Panel
         style={{
           boxShadow: textShadow,
           alignSelf: "center",
@@ -97,16 +99,16 @@ const Stats = ({ entity }: StatsProps) => {
             </a>
           </h1>
         </div>
-      </Section>
-      <Section style={{ boxShadow: iframeShadow }}>
+      </Panel>
+      <Panel style={{ boxShadow: iframeShadow }}>
         <IframeResizer
           title="Votre empreinte carbone"
           src={link1}
           frameBorder="0"
           allowTransparency
         />
-      </Section>
-      <Section
+      </Panel>
+      <Panel
         style={{
           boxShadow: textShadow,
           alignSelf: "center",
@@ -125,16 +127,16 @@ const Stats = ({ entity }: StatsProps) => {
             </a>
           </h1>
         </div>
-      </Section>
-      <Section style={{ boxShadow: iframeShadow }}>
+      </Panel>
+      <Panel style={{ boxShadow: iframeShadow }}>
         <IframeResizer
           title="Statistiques globales depuis 2019"
           src={link2}
           frameBorder="0"
           allowTransparency
         />
-      </Section>
-      <Section
+      </Panel>
+      <Panel
         style={{
           boxShadow: textShadow,
           alignSelf: "center",
@@ -152,16 +154,16 @@ const Stats = ({ entity }: StatsProps) => {
             </a>
           </h1>
         </div>
-      </Section>
-      <Section style={{ boxShadow: iframeShadow }}>
+      </Panel>
+      <Panel style={{ boxShadow: iframeShadow }}>
         <IframeResizer
           title="Le mois dernier"
           src={link3}
           frameBorder="0"
           allowTransparency
         />
-      </Section>
-      <Section
+      </Panel>
+      <Panel
         style={{
           boxShadow: textShadow,
           alignSelf: "center",
@@ -179,8 +181,8 @@ const Stats = ({ entity }: StatsProps) => {
             </a>
           </h1>
         </div>
-      </Section>
-      <Section style={{ background: "#d9edf7", borderColor: "#bce8f1" }}>
+      </Panel>
+      <Panel style={{ background: "#d9edf7", borderColor: "#bce8f1" }}>
         <span style={{ alignSelf: "center" }}>
           <Trans>
             <p>
@@ -188,16 +190,16 @@ const Stats = ({ entity }: StatsProps) => {
             </p>
           </Trans>
         </span>
-      </Section>
-      <Section>
+      </Panel>
+      <Panel>
         <IframeResizer
           title="Statistiques globale par année"
           src={link4}
           frameBorder="0"
           allowTransparency
         />
-      </Section>
-      <Section
+      </Panel>
+      <Panel
         style={{
           boxShadow: textShadow,
           alignSelf: "center",
@@ -215,8 +217,8 @@ const Stats = ({ entity }: StatsProps) => {
             </a>
           </h1>
         </div>
-      </Section>
-      <Section style={{ background: "#d9edf7", borderColor: "#bce8f1" }}>
+      </Panel>
+      <Panel style={{ background: "#d9edf7", borderColor: "#bce8f1" }}>
         <span style={{ alignSelf: "center" }}>
           <Trans>
             <p>
@@ -225,16 +227,16 @@ const Stats = ({ entity }: StatsProps) => {
             </p>
           </Trans>
         </span>
-      </Section>
-      <Section style={{ boxShadow: iframeShadow }}>
+      </Panel>
+      <Panel style={{ boxShadow: iframeShadow }}>
         <IframeResizer
           title="Statistiques détaillées"
           src={link5}
           frameBorder="0"
           allowTransparency
         />
-      </Section>
-      <Section
+      </Panel>
+      <Panel
         style={{
           boxShadow: textShadow,
           alignSelf: "center",
@@ -252,28 +254,23 @@ const Stats = ({ entity }: StatsProps) => {
             </a>
           </h1>
         </div>
-      </Section>
-      <Section style={{ boxShadow: iframeShadow }}>
+      </Panel>
+      <Panel style={{ boxShadow: iframeShadow }}>
         <IframeResizer
           title={text6}
           src={link6}
           frameBorder="0"
           allowTransparency
         />
-      </Section>
+      </Panel>
     </Main>
   )
 }
 
 const StatsRoutes = ({ entity }: StatsProps) => {
-  const [entityHash, getEntityHash] = useAPI(getHash)
-  useEffect(() => {
-    if (entity) {
-      getEntityHash(entity.id)
-    }
-  }, [entity, getEntityHash])
+  const hash = useEntityHash()
 
-  if (entityHash.data === null) {
+  if (hash === undefined) {
     return null
   }
 
@@ -290,14 +287,14 @@ const StatsRoutes = ({ entity }: StatsProps) => {
         element={
           <Main style={{ padding: "32px 160px" }}>
             <h1>{period}</h1>
-            <Section style={{ boxShadow: iframeShadow }}>
+            <Panel style={{ boxShadow: iframeShadow }}>
               <IframeResizer
                 title="period_details"
-                src={`https://metabase.carbure.beta.gouv.fr/public/dashboard/8d158d55-0adf-41e2-b711-6f8e0419b824?hash=${entityHash.data?.hash},period=${period}#hide_parameters=hash,period`}
+                src={`https://metabase.carbure.beta.gouv.fr/public/dashboard/8d158d55-0adf-41e2-b711-6f8e0419b824?hash=${hash},period=${period}#hide_parameters=hash,period`}
                 frameBorder="0"
                 allowTransparency
               />
-            </Section>
+            </Panel>
           </Main>
         }
       />
