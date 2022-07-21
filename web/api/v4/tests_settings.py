25585892c1eee9116712e14cae6ff03a650e5f1e
jfalxa <jfalxa.sta@gmail.com>
2022-07-21 17:27:41 +0200

    [api] actually update role when calling endpoint

--- web/api/v4/tests_settings.py
+++ web/api/v4/tests_settings.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
-from core.models import Entity
+from core.models import Entity, UserRights
 from api.v4.tests_utils import setup_current_user
 
 User = get_user_model()
@@ -37,8 +37,12 @@ class SettingsTest(TestCase):
         self.assertEqual(data['error'], 'NO_PRIOR_RIGHTS')
 
     def test_change_user_role_ok(self):
-        res = self.client.post(Endpoint.change_user_role, {'email': self.user.email, 'entity_id': self.entity.id, 'role': 'ADMIN'})
+        user_right = UserRights.objects.get(user=self.user, entity=self.entity)
+        self.assertEqual(user_right.role, 'ADMIN')
+        res = self.client.post(Endpoint.change_user_role, {'email': self.user.email, 'entity_id': self.entity.id, 'role': 'RW'})
         self.assertEqual(res.status_code, 200)
+        user_right = UserRights.objects.get(user=self.user, entity=self.entity)
+        self.assertEqual(user_right.role, 'RW')
 
 
 
