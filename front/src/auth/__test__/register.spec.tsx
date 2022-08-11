c6cb9386a8a63bc82b503eefe1b80086c3e269b0
Damien <damien@romito.fr>
2022-08-11 15:14:24 +0200

    [front] test registration

---
+++ front/src/auth/__test__/register.spec.tsx
@@ -0,0 +1,61 @@
+import { Route } from "react-router-dom"
+import { TestRoot } from "setupTests"
+
+import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+import { ActivateRequest } from "auth/components/activate"
+import { getField, waitWhileLoading } from "carbure/__test__/helpers"
+import { Register, RegisterPending } from "../components/register"
+import server from "./api"
+
+beforeAll(() => server.listen({ onUnhandledRequest: "warn" }))
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
+
+const AuthWithRouter = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <TestRoot url={`/app/auth/register`}>
+      <Route path={`/app/auth/register`} element={<Register />} />
+      {children}
+    </TestRoot>
+  )
+}
+
+test("display the register form", async () => {
+  render(<AuthWithRouter />)
+  await waitWhileLoading()
+  await screen.findByText("Adresse email")
+  await screen.findByText("Nom")
+  await screen.findByText("Mot de passe")
+  await screen.findByText("Répéter le mot de passe")
+})
+
+test("fill and submit the register form", async () => {
+  render(
+    <AuthWithRouter>
+      <Route path={`/register-pending`} element={<RegisterPending />} />
+    </AuthWithRouter>
+  )
+
+  const user = userEvent.setup()
+  await user.type(getField("Adresse email"), "test@company.com")
+  await user.type(getField("Nom"), "Jean-Pierre Champollion")
+  await user.type(getField("Mot de passe"), "azerty")
+  await user.type(getField("Répéter le mot de passe"), "azerty")
+  await user.click(await screen.findByText("Créer un nouveau compte"))
+  await waitWhileLoading()
+  await screen.findByText(/Votre demande d'inscription a bien été envoyée/)
+})
+
+test("resend activation link", async () => {
+  render(
+    <AuthWithRouter>
+      <Route path={`/activate-request`} element={<ActivateRequest />} />
+    </AuthWithRouter>
+  )
+  const user = userEvent.setup()
+  await user.click(await screen.findByText("Je n'ai pas reçu le lien d'activation")) // prettier-ignore
+  await user.type(getField("Adresse email du compte"), "test@company.com")
+  await user.click(await screen.findByText("Renvoyer le lien d'activation"))
+  await screen.findByText(/Veuillez renseigner votre adresse email/)
+})
