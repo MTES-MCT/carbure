58485133e1d86fae826549712c7fd682990a9f6c
Julien Falxa <jfalxa.sta@gmail.com>
2022-08-22 16:45:26 +0200

    [scripts] fix scripts

--- scripts/freeze_old_declarations.py
+++ scripts/freeze_old_declarations.py
@@ -1,8 +1,5 @@
-import sys
 import os
 import django
-import argparse
-from django.db.models import Sum
 from tqdm import tqdm
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carbure.settings")
@@ -10,25 +7,6 @@ django.setup()
 
 from core.models import *
 
-def recalc_all_stocks():
-    stocks = CarbureStock.objects.all()
-    for stock in tqdm(stocks):
-        child_volume = CarbureLot.objects.filter(parent_stock=stock).exclude(lot_status=CarbureLot.DELETED).aggregate(child_volume=Sum('volume'))
-        vol = child_volume['child_volume']
-        if vol is None:
-            vol = 0
-        transformations_volume = CarbureStockTransformation.objects.filter(source_stock=stock).aggregate(vol=Sum('volume_deducted_from_source'))
-        if transformations_volume['vol'] is not None:
-            vol += transformations_volume['vol']
-        initial_volume = stock.parent_lot.volume if stock.parent_lot else stock.parent_transformation.volume_destination
-        theo_remaining = initial_volume - vol
-        diff = stock.remaining_volume - theo_remaining
-        if abs(diff) > 0.1:
-            stock.remaining_volume = round(theo_remaining, 2)
-            stock.remaining_weight = stock.get_weight()
-            stock.remaining_lhv_amount = stock.get_lhv_amount()
-            stock.save()
-    
 def freeze_old_declarations():
     declarations = SustainabilityDeclaration.objects.filter(period__lt='2022-01-01', declared=False)
     for declaration in tqdm(declarations):
@@ -50,7 +28,6 @@ def freeze_old_declarations():
         lots_received.update(declared_by_client=True)
         lots_sent = CarbureLot.objects.filter(carbure_supplier=declaration.entity, period=period_int)
         lots_sent.update(declared_by_supplier=True)
-        bulk_events = []
         # freeze lots
         lots_to_freeze = CarbureLot.objects.filter(carbure_client=declaration.entity, period=period_int, declared_by_client=True, declared_by_supplier=True)
         lots_to_freeze.update(lot_status=CarbureLot.FROZEN)
@@ -60,14 +37,5 @@ def freeze_old_declarations():
         declaration.declared = True
         declaration.save()
 
-def recalc_old_pci_values():
-    lots = CarbureLot.objects.filter(lhv_amount=0)
-    for l in tqdm(lots):
-        l.lhv_amount = l.get_lhv_amount()
-        l.weight = l.get_weight()
-        l.save()
-
 if __name__ == '__main__':
-    recalc_all_stocks()
     freeze_old_declarations()
-    recalc_old_pci_values()
