fd7147b22e02aa14eca75cfca56787df01015857
Julien Falxa <jfalxa.sta@gmail.com>
2022-08-29 01:23:34 +0200

    [back] calling / will return frontend

--- web/carbure/settings.py
+++ web/carbure/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    "whitenoise.runserver_nostatic",
     'django.contrib.staticfiles',
     'django_otp',
     'django_otp.plugins.otp_email',
@@ -69,6 +70,7 @@ AUTH_USER_MODEL = 'authtools.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "spa.middleware.SPAMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
 #    'django.middleware.csrf.CsrfViewMiddleware',
@@ -162,6 +164,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = "spa.storage.SPAStaticFilesStorage"
 
 SESSION_COOKIE_AGE = 60*60*24*30 # 30 days
 
