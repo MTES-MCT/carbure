0921a795fe6858236bd87613d38c9eb9e8ba7be4
Julien Falxa <jfalxa.sta@gmail.com>
2022-08-29 00:37:10 +0200

    [dev] update docker dev env

---
+++ web/entrypoint.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# collect frontend static files
+python3 web/manage.py collectstatic --noinput
+
+# apply new migrations
+python3 web/manage.py migrate --noinput
+
+# fill database with static fixtures
+python3 /app/web/fixtures/load_biocarburants.py
+python3 /app/web/fixtures/load_countries.py
+python3 /app/web/fixtures/load_matierespremieres.py
+python3 /app/web/fixtures/load_sn_certificates.py
+
+# start dev server
+python3 ./web/manage.py runserver 0.0.0.0:8000
