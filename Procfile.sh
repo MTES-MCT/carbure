c2feb60fea392bb8500afa51cbec4021783aead4
Julien Falxa <jfalxa.sta@gmail.com>
2022-08-28 22:51:51 +0200

    [scalingo] add procfile + start script

---
+++ Procfile.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# collect backend static files
+python web/manage.py collectstatic --noinput
+
+# apply new migrations
+python web/manage.py migrate --noinput
+
+# fill database with static fixtures
+python /app/web/fixtures/load_biocarburants.py
+python /app/web/fixtures/load_countries.py
+python /app/web/fixtures/load_matierespremieres.py
+python /app/web/fixtures/load_sn_certificates.py
+
+# run server with gunicorn
+gunicorn --chdir ./web server.wsgi --log-file -
