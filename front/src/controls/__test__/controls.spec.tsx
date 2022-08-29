fdc0d257077fde8ff5971557f13c5a409f41dea5
macbook2 <macbook2@MacBook-Air-Damien-vivlab.local>
2022-08-29 17:57:17 +0200

    clean and stop adding more tests

--- front/src/controls/__test__/controls.spec.tsx
+++ front/src/controls/__test__/controls.spec.tsx
@@ -1,14 +1,13 @@
-import { render, TestRoot } from "setupTests"
 import { screen } from "@testing-library/react"
-import { Route } from "react-router-dom"
-import { AdminStatus } from "controls/types"
 import userEvent from "@testing-library/user-event"
+import { AdminStatus } from "controls/types"
+import { Route } from "react-router-dom"
+import { render, TestRoot } from "setupTests"
 
-import { waitWhileLoading } from "carbure/__test__/helpers"
 import Controls from "../index"
 
-import server from "./api"
 import useEntity from "carbure/hooks/entity"
+import server from "./api"
 
 const ControlsWithUser = () => {
   const entity = useEntity()
@@ -29,49 +28,43 @@ beforeAll(() => server.listen({ onUnhandledRequest: "warn" }))
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
 
-// test("admin: display an empty list of transactions", async () => {
-//   render(<ControlsWithRouter status="alerts" />)
-//   await waitWhileLoading()
-//   await screen.findByText("Signalement")
-//   screen.getByText("Lot")
-//   screen.getByText("Stock")
-//   screen.getByText("Statut")
-//   screen.getByText("Corrections")
-//   screen.getByText("Périodes")
-//   screen.getByText("Biocarburants")
-//   screen.getByText("Matières Premières")
-//   screen.getByText("Pays d'origine")
-//   screen.getByText("Fournisseurs")
-//   screen.getByText("Clients")
-//   screen.getByText("Types de client")
-//   screen.getByText("Sites de production")
-//   screen.getByText("Sites de livraison")
-//   screen.getByText("Ajouté par")
-//   screen.getByText("Incohérences")
-//   screen.getByText("Conformité")
+test("admin: display an empty list of transactions", async () => {
+  render(<ControlsWithRouter status="alerts" />)
+  await screen.findByText("Signalement")
+  screen.getByText("Lot")
+  screen.getByText("Stock")
+  screen.getByText("Statut")
+  screen.getByText("Corrections")
+  screen.getByText("Périodes")
+  screen.getByText("Biocarburants")
+  screen.getByText("Matières Premières")
+  screen.getByText("Pays d'origine")
+  screen.getByText("Fournisseurs")
+  screen.getByText("Clients")
+  screen.getByText("Types de client")
+  screen.getByText("Sites de production")
+  screen.getByText("Sites de livraison")
+  screen.getByText("Ajouté par")
+  screen.getByText("Incohérences")
+  screen.getByText("Conformité")
 
-//   screen.getByPlaceholderText("Rechercher...")
+  screen.getByPlaceholderText("Rechercher...")
 
-//   screen.getByText("Aucun résultat trouvé pour cette recherche")
-// })
+  screen.getByText("Aucun résultat trouvé pour cette recherche")
+})
 
 test("admin: display a list of 3 lots", async () => {
   render(<ControlsWithRouter status="alerts" />)
   const user = userEvent.setup()
-  await waitWhileLoading()
   const link = await screen.findByText("Lot")
   await user.click(link.closest("a")!)
-  await waitWhileLoading()
   screen.getByText("3 lots")
-  await waitWhileLoading()
 })
 
 test("admin: display an empty list of stocks", async () => {
   render(<ControlsWithRouter status="alerts" />)
   const user = userEvent.setup()
-  await waitWhileLoading()
   const link = await screen.findByText("Stock")
   await user.click(link.closest("a")!)
   screen.getByText("Aucun résultat trouvé pour cette recherche")
-  await waitWhileLoading()
 })
