eb9bf489f2e9f292872bf31a61c66d8e2b5529a8
Julien Falxa <jfalxa.sta@gmail.com>
2022-09-20 20:20:51 +0200

    [backend] in periodic tasks directly call python functions when available

--- web/carbure/tasks.py
+++ web/carbure/tasks.py
@@ -4,7 +4,10 @@ from huey import crontab
 from huey.contrib.djhuey import periodic_task, db_periodic_task, db_task
 
 from api.v4.sanity_checks import bulk_sanity_checks, bulk_scoring
-
+from cron.declaration_reminder import send_declaration_reminder
+from scripts.emails.send_notifications_v3 import send_notification_emails
+from ml.scripts.calc_ml_score import calc_ml_score
+from ml.scripts.load_data import load_ml_data
 
 @db_task()
 def background_bulk_sanity_checks(lots, prefetched_data=None):
@@ -19,6 +22,11 @@ def background_bulk_scoring(lots, prefetched_data=None):
 @periodic_task(crontab(hour=23, minute=45))
 def backup_prod_db() -> None:
     subprocess.run(["bash", "/app/scripts/backup/backup_prod_db.sh"])
+
+
+@periodic_task(crontab(hour=0, minute=45))
+def restore_prod_db() -> None:
+    subprocess.run(["bash", "/app/scripts/recovery/restore_db.sh"])
 
 
 @db_periodic_task(crontab(day_of_week="Sun", hour=4, minute=0))
@@ -36,25 +44,27 @@ def download_iscc_certificates() -> None:
 #     subprocess.run(["bash", "/app/cron/download_redcert_certificates.sh"])
 
 
-@db_periodic_task(crontab(hour="19,20,21,22,23", minute=30))
-def send_notification_emails() -> None:
-    subprocess.run(["python3", "/app/scripts/emails/send_notifications_v3.py"])
+# @db_periodic_task(crontab(hour="19,20,21,22,23", minute=30))
+@db_periodic_task(crontab(hour=18, minute="16,17,18,19,20"))
+def periodic_send_notification_emails() -> None:
+    send_notification_emails()
 
 
 @db_periodic_task(crontab(day=23, hour=8, minute=0))
-def send_declaration_reminder() -> None:
-    subprocess.run(["python3", "/app/cron/declaration_reminder.py"])
+def periodic_send_declaration_reminder() -> None:
+    send_declaration_reminder()
 
 
 @db_periodic_task(crontab(day=14, hour=1, minute=30))
-def load_ml_data() -> None:
-    subprocess.run(["python3", "/app/web/ml/scripts/load_data.py"])
+def periodic_load_ml_data() -> None:
+    load_ml_data()
 
 
 @db_periodic_task(crontab(day=15, hour=1, minute=30))
-def calculate_ml_scores() -> None:
+def periodic_calculate_ml_scores() -> None:
     today = datetime.datetime.today()
     first = today.replace(day=1)
     last_month = first - datetime.timedelta(days=1)
     period = last_month.year * 100 + last_month.month
-    subprocess.run(["python3", "/app/web/ml/scripts/calc_ml_score.py", "--period", str(period)])
+    calc_ml_score(period=period)
+
