c5fbfe4cab379651266cc76afb6f085763d80909
Damien Romito <damien@maracuja.ac>
2022-10-03 18:39:23 +0200

    forgot to add ^^

---
+++ front/src/saf-certificates/hooks/query-params-store.ts
@@ -0,0 +1,147 @@
+import { Entity } from "carbure/types"
+import { useLimit } from "common/components/pagination"
+import { Order } from "common/components/table"
+import useStore from "common/hooks/store"
+import { useFilterParams } from "saf-certificates/components/filters"
+import { FilterSelection, QueryParams, SafCertificateQueryStatus, SafSnapshot } from "saf-certificates/types"
+
+export function useQueryParamsStore(
+  entity: Entity,
+  year: number,
+  status: string | undefined,
+  snapshot?: SafSnapshot | undefined
+) {
+  const [limit, saveLimit] = useLimit()
+  const [filtersParams, setFiltersParams] = useFilterParams()
+
+  const [state, actions] = useStore(
+    {
+      entity,
+      year,
+      snapshot,
+      status,
+      filters: filtersParams,
+      search: undefined,
+      invalid: false,
+      deadline: false,
+      order: undefined,
+      selection: [],
+      page: 0,
+      limit,
+    } as QueryParams,
+    {
+      setEntity: (entity: Entity) => (state) => ({
+        entity,
+        filters: filtersParams,
+        invalid: false,
+        deadline: false,
+        selection: [],
+        page: 0,
+      }),
+
+      setYear: (year: number) => (state) => ({
+        year,
+        filters: filtersParams,
+        invalid: false,
+        deadline: false,
+        selection: [],
+        page: 0,
+      }),
+
+      setSnapshot: (snapshot: SafSnapshot) => (state) => {
+        return {
+          snapshot,
+          filters: filtersParams,
+          invalid: false,
+          deadline: false,
+          selection: [],
+          page: 0,
+        }
+      },
+
+      setStatus: (status: SafCertificateQueryStatus) => (state) => ({
+        status,
+        filters: filtersParams,
+        invalid: false,
+        deadline: false,
+        selection: [],
+        page: 0,
+      }),
+
+      setFilters: (filters: FilterSelection) => {
+        setTimeout(() => {
+          setFiltersParams(filters)
+        })
+        return {
+          filters,
+          selection: [],
+          page: 0,
+        }
+      },
+
+      setSearch: (search: string | undefined) => ({
+        search,
+        selection: [],
+        page: 0,
+      }),
+
+      setInvalid: (invalid: boolean) => ({
+        invalid,
+        selection: [],
+        page: 0,
+      }),
+
+      setDeadline: (deadline: boolean) => ({
+        deadline,
+        selection: [],
+        page: 0,
+      }),
+
+      setOrder: (order: Order | undefined) => ({
+        order,
+      }),
+
+      setSelection: (selection: number[]) => ({
+        selection,
+      }),
+
+      setPage: (page?: number) => ({
+        page,
+        selection: [],
+      }),
+
+      setLimit: (limit?: number) => {
+        saveLimit(limit)
+        return {
+          limit,
+          selection: [],
+          page: 0,
+        }
+      },
+    }
+  )
+
+  // sync tab title with current state
+  // useLotTitle(state)
+
+  // sync store state with entity set from above
+  if (state.entity.id !== entity.id) {
+    actions.setEntity(entity)
+  }
+
+  // sync store state with year set from above
+  if (state.year !== year) {
+    actions.setYear(year)
+  }
+
+  // sync store state with status set in the route
+  if (state.status !== status) {
+    actions.setStatus(status as SafCertificateQueryStatus)
+  }
+
+  if (snapshot && state.snapshot !== snapshot) {
+    actions.setSnapshot(snapshot)
+  }
+
+  return [state, actions] as [typeof state, typeof actions]
+}
