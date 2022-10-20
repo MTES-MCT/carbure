5ee9f40b1a083b2a43c5dbe5253b9f3ccf72c321
Julien Falxa <jfalxa.sta@gmail.com>
2022-10-20 17:29:22 +0200

    [api] add saf/tickets endpoint

--- web/api/v5/saf/__init__.py
+++ web/api/v5/saf/__init__.py
@@ -3,9 +3,11 @@ from django.urls import path
 from .years import get_years
 from .snapshot import get_snapshot
 from .ticket_sources import get_ticket_sources
+from .tickets import get_tickets
 
 urlpatterns = [
     path("years", get_years, name="api-v5-saf-years"),
     path("snapshot", get_snapshot, name="api-v5-saf-snapshot"),
     path("ticket-sources", get_ticket_sources, name="api-v5-saf-ticket-sources"),
+    path("tickets", get_tickets, name="api-v5-saf-tickets"),
 ]
--- web/saf/serializers/saf_ticket.py
+++ web/saf/serializers/saf_ticket.py
@@ -1,6 +1,33 @@
 from rest_framework import serializers
 
 from saf.models import SafTicket
+from doublecount.serializers import BiofuelSerializer, CountrySerializer, FeedStockSerializer
+
+
+class SafTicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SafTicket
+        fields = [
+            "id",
+            "carbure_id",
+            "year",
+            "period",
+            "status",
+            "agreement_date",
+            "supplier",
+            "client",
+            "volume",
+            "feedstock",
+            "biofuel",
+            "country_of_origin",
+            "ghg_reduction",
+        ]
+
+    feedstock = FeedStockSerializer(read_only=True)
+    biofuel = BiofuelSerializer(read_only=True)
+    country_of_origin = CountrySerializer(read_only=True)
+    supplier = serializers.SlugRelatedField(read_only=True, slug_field="name")
+    client = serializers.SlugRelatedField(read_only=True, slug_field="name")
 
 
 class SafTicketPreviewSerializer(serializers.ModelSerializer):
