ab166de896e51dfba1c34ebc2927295a7b92a7e3
Julien Falxa <jfalxa.sta@gmail.com>
2022-10-25 16:39:36 +0200

    [api] cleanup

--- web/api/v5/saf/snapshot.py
+++ web/api/v5/saf/snapshot.py
@@ -21,7 +21,7 @@ def get_snapshot(request, *args, **kwargs):
         return ErrorResponse(400, SafSnapshotError.PARAMS_MALFORMED)
 
     sources = SafTicketSource.objects.filter(year=year, added_by_id=entity_id)
-    tickets = SafTicket.objects.filter(year=year, added_by_id=entity_id)
+    tickets = SafTicket.objects.filter(year=year, supplier_id=entity_id)
 
     try:
         return SuccessResponse(
