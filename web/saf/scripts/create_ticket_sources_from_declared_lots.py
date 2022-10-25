5cd713d4840bde2441b62d728a768785b20803a4
Julien Falxa <jfalxa.sta@gmail.com>
2022-10-25 16:40:05 +0200

    [api] automatically create ticket sources when lots are declared

--- web/saf/scripts/create_ticket_sources_from_declared_lots.py
+++ web/saf/scripts/create_ticket_sources_from_declared_lots.py
@@ -6,26 +6,18 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carbure.settings")
 django.setup()
 
 from core.models import CarbureLot
-from saf.models.saf_ticket_source import SafTicketSource, create_ticket_source_from_lot
-
-
-# list of accepted SAF
-SAF = ("HVOC", "HOC", "HCC")
+from saf.models.saf_ticket_source import create_ticket_sources_from_lots
 
 
 def create_ticket_sources_from_declared_lots(year):
     lots = CarbureLot.objects.select_related("biofuel").filter(
-        lot_status=CarbureLot.FROZEN, year=year, biofuel__code__in=SAF, carbure_client_id__isnull=False
+        lot_status=CarbureLot.FROZEN, year=year, carbure_client_id__isnull=False
     )
 
+    ticket_sources = create_ticket_sources_from_lots(lots)
     entities = lots.values_list("carbure_client__name", flat=True).distinct()
 
-    ticket_sources = []
-    for lot in lots.iterator():
-        ticket_sources.append(create_ticket_source_from_lot(lot))
-
-    created = SafTicketSource.objects.bulk_create(ticket_sources)
-    print("[SAF] %d ticket sources created for %s" % (len(created), ", ".join(list(entities))))
+    print("[SAF] %d ticket sources created for %s" % (len(ticket_sources), ", ".join(list(entities))))
 
 
 if __name__ == "__main__":
