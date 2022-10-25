71ea8aa6f91750433b3b23b5542d58995f48763a
Julien Falxa <jfalxa.sta@gmail.com>
2022-10-25 17:24:22 +0200

    [api] add endpoint to create tickets from sources

--- web/api/v5/saf/__init__.py
+++ web/api/v5/saf/__init__.py
@@ -6,6 +6,7 @@ from .ticket_sources import get_ticket_sources
 from .ticket_source_details import get_ticket_source_details
 from .tickets import get_tickets
 from .ticket_details import get_ticket_details
+from .assign_ticket import assign_ticket
 
 urlpatterns = [
     # overview
@@ -15,6 +16,7 @@ urlpatterns = [
     path("ticket-sources", get_ticket_sources, name="api-v5-saf-ticket-sources"),
     path("ticket-sources/details", get_ticket_source_details, name="api-v5-saf-ticket-source-details"),
     # tickets
+    path("assign-ticket", assign_ticket, name="api-v5-saf-assign-ticket"),
     path("tickets", get_tickets, name="api-v5-saf-tickets"),
     path("tickets/details", get_ticket_details, name="api-v5-saf-ticket-details"),
 ]
