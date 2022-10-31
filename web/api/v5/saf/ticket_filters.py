b0fbe8bc8e73ee80ea0cef5c6569421e891838fb
Julien Falxa <jfalxa.sta@gmail.com>
2022-10-31 17:23:25 +0100

    [api] add filter endpoint for tickets + allow filtering

--- web/api/v5/saf/ticket_filters.py
+++ web/api/v5/saf/ticket_filters.py
@@ -1,49 +1,49 @@
-# /api/v5/saf/ticket-sources/filters
+# /api/v5/saf/tickets/filters
 
 import traceback
 from core.common import SuccessResponse, ErrorResponse
 from core.decorators import check_user_rights
-from .ticket_sources import parse_ticket_source_query, find_ticket_sources
+from .tickets import parse_ticket_query, find_tickets
 
 
-class SafTicketSourceFiltersError:
+class SafTicketFiltersError:
     MALFORMED_PARAMS = "MALFORMED_PARAMS"
     FILTER_LISTING_FAILED = "FILTERS_LISTING_FAILED"
 
 
 @check_user_rights()
-def get_ticket_source_filters(request, *args, **kwargs):
+def get_ticket_filters(request, *args, **kwargs):
     try:
-        query = parse_ticket_source_query(request.GET)
+        query = parse_ticket_query(request.GET)
         filter = request.GET.get("filter")
     except:
         traceback.print_exc()
-        return ErrorResponse(400, SafTicketSourceFiltersError.MALFORMED_PARAMS)
+        return ErrorResponse(400, SafTicketFiltersError.MALFORMED_PARAMS)
 
     try:
         # do not apply the filter we are listing so we can get all its possible values in the current context
         query[filter] = None
         # find all ticket sources matching the rest of the query
-        ticket_sources = find_ticket_sources(**query)
+        tickets = find_tickets(**query)
         # get the available values for the selected filter
-        data = get_filter_values(ticket_sources, filter)
+        data = get_filter_values(tickets, filter)
 
         return SuccessResponse(list(set(data)))
     except:
         traceback.print_exc()
-        return ErrorResponse(400, SafTicketSourceFiltersError.FILTER_LISTING_FAILED)
+        return ErrorResponse(400, SafTicketFiltersError.FILTER_LISTING_FAILED)
 
 
-def get_filter_values(ticket_sources, filter):
+def get_filter_values(tickets, filter):
     if not filter:
         raise Exception("No filter was specified")
 
     if filter == "client":
-        column = "saf_tickets__client__name"
+        column = "client__name"
     elif filter == "period":
         column = "period"
     elif filter == "feedstock":
         column = "feedstock__code"
 
-    values = ticket_sources.values_list(column, flat=True).distinct()
+    values = tickets.values_list(column, flat=True).distinct()
     return [v for v in values if v]
