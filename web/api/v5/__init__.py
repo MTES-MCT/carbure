d04e24627900bb3a19c0099301feadd149a605f6
Julien Falxa <jfalxa.sta@gmail.com>
2022-11-24 19:20:18 +0100

    [api] add new endpoint check many dc files at once

--- web/api/v5/__init__.py
+++ web/api/v5/__init__.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path("saf/", include("api.v5.saf")),
     path("stats/", include("api.v5.stats")),
+    path("double-counting/", include("api.v5.double_counting")),
 ]
