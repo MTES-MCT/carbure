ad3571e5e0bf8fee13ecebe818d08f34b9072691
Julien Falxa <jfalxa.sta@gmail.com>
2023-01-04 20:15:52 +0100

    [api] add endpoint to credit ticket sources when an operator accepts a ticket

--- web/api/v5/saf/operator/__init__.py
+++ web/api/v5/saf/operator/__init__.py
@@ -11,8 +11,9 @@ from .ticket_source_filters import get_ticket_source_filters
 from .ticket_filters import get_ticket_filters
 from .assign_ticket import assign_ticket
 from .grouped_assign_ticket import grouped_assign_ticket
-from .reject_ticket import reject_ticket
 from .cancel_ticket import cancel_ticket
+from .reject_ticket import reject_ticket
+from .credit_ticket_source import credit_ticket_source
 
 urlpatterns = [
     # overview
@@ -31,6 +32,7 @@ urlpatterns = [
     path("grouped-assign-ticket", grouped_assign_ticket, name="api-v5-saf-operator-grouped-assign-ticket"),
     path("cancel-ticket", cancel_ticket, name="api-v5-saf-operator-cancel-ticket"),
     path("reject-ticket", reject_ticket, name="api-v5-saf-operator-reject-ticket"),
+    path("credit-ticket-source", credit_ticket_source, name="api-v5-saf-operator-credit-ticket-source"),
     # autocomplete helpers
     path("clients", get_clients, name="api-v5-saf-operator-clients"),
 ]
