fa96b61b0bfeb128636d4abbbc81653bb57fb5d7
Julien Falxa <jfalxa.sta@gmail.com>
2023-01-16 16:56:36 +0100

    [api] fix check_admin_rights decorator

--- web/core/decorators.py
+++ web/core/decorators.py
@@ -134,20 +134,21 @@ class AdminRightsError:
     USER_NOT_VERIFIED = "USER_NOT_VERIFIED"
     ENTITY_NOT_FOUND = "ENTITY_NOT_FOUND"
     ENTITY_HAS_NO_RIGHT = "ENTITY_HAS_NO_RIGHT"
+    ENTITY_NOT_ADMIN = "ENTITY_NOT_ADMIN"
     USER_NOT_ADMIN = "USER_NOT_ADMIN"
     USER_HAS_NO_RIGHT = "USER_HAS_NO_RIGHT"
 
 
-def check_admin_rights(external_rights=None, user_rights=None):
+def check_admin_rights(allow_external=[], allow_role=None):
     def decorator(function_to_decorate):
         @wraps(function_to_decorate)
         def wrap(request, *args, **kwargs):
             if not request.user.is_authenticated:
                 return ErrorResponse(403, AdminRightsError.USER_NOT_AUTHENTICATED)
-            
+
             if not request.user.is_verified():
                 return ErrorResponse(403, AdminRightsError.USER_NOT_VERIFIED)
-        
+
             entity_id = request.POST.get('entity_id', request.GET.get('entity_id', None))
             if entity_id is None:
                 return ErrorResponse(400, AdminRightsError.MISSING_ENTITY_ID)
@@ -157,25 +158,32 @@ def check_admin_rights(external_rights=None, user_rights=None):
                 entity = Entity.objects.get(id=entity_id, entity_type__in=(Entity.ADMIN, Entity.EXTERNAL_ADMIN))
             except:
                 return ErrorResponse(404, AdminRightsError.ENTITY_NOT_FOUND)
-            
+
             try:
                 # check that user has access to entity and refine by role if specified
-                if user_rights is not None:
-                    UserRights.objects.get(entity=entity, user=request.user, role__in=user_rights)
+                if allow_role is not None:
+                    UserRights.objects.get(entity=entity, user=request.user, role__in=allow_role)
                 else:
                     UserRights.objects.get(entity=entity, user=request.user)
             except:
                 return ErrorResponse(403, AdminRightsError.USER_HAS_NO_RIGHT)
 
-            # if the user tries to access an admin page, check that they're also part of the staff
-            if entity.entity_type == Entity.ADMIN:
+            # find out if the decorated function is accessible only by admins
+            is_admin_only = len(allow_external) == 0
+
+            # if the user tries to access an admin page
+            if is_admin_only:
+                # confirm that the current entity is the administration
+                if entity.entity_type != Entity.ADMIN:
+                    return ErrorResponse(403, AdminRightsError.ENTITY_NOT_ADMIN)
+                # and confirm that the current user is part of the staff
                 if not request.user.is_staff:
                     return ErrorResponse(403, AdminRightsError.USER_NOT_ADMIN)
 
             # for an external admin page, check that this entity can actually see it
-            if entity.entity_type == Entity.EXTERNAL_ADMIN and external_rights is not None:
+            elif entity.entity_type == Entity.EXTERNAL_ADMIN:
                 try:
-                    ExternalAdminRights.objects.get(entity=entity, right__in=external_rights)
+                    ExternalAdminRights.objects.get(entity=entity, right__in=allow_external)
                 except:
                     return ErrorResponse(403, AdminRightsError.ENTITY_HAS_NO_RIGHT)
 
