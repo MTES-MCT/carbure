2765e7b11818d7aaf5004ff35f4f14689b0d4793
Julien Falxa <jfalxa.sta@gmail.com>
2023-02-02 18:41:28 +0100

    [api] fix flushed lots with parent lots

---
+++ scripts/patches/2023-02-02_fix_flushed_lot_parents.py
@@ -0,0 +1,19 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carbure.settings")
+django.setup()
+
+from core.models import CarbureLot
+
+
+def fix_flushed_lot_parents():
+    lots = CarbureLot.objects.filter(delivery_type=CarbureLot.FLUSHED)
+    print("> Found %d flushed lots" % lots.count())
+
+    lots.update(parent_lot_id=None)
+    print("> Removed parent lot from these lots")
+
+
+if __name__ == "__main__":
+    fix_flushed_lot_parents()
