3c4a521df6c263926c051e76e3a95cde38ec2053
Julien Falxa <jfalxa.sta@gmail.com>
2023-02-14 16:17:34 +0100

    [backend] run migrations in postdeploy hook

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,23 +1,7 @@
 #! /usr/bin/env bash
 
-# collect frontend static files
-python3 ./web/manage.py collectstatic --noinput
+bash ./web/setup.sh
 
-# apply new migrations
-python3 ./web/manage.py migrate --noinput
-
-# fill database with static fixtures
-python3 ./web/fixtures/load_biocarburants.py
-python3 ./web/fixtures/load_countries.py
-python3 ./web/fixtures/load_matierespremieres.py
-python3 ./web/fixtures/load_sn_certificates.py
-
-if [ "$IMAGE_TAG" = "local" ] ; then
-  # start dev server and huey consumer
-  python3 ./web/manage.py run_huey --simple --scheduler-interval 10 &
-  python3 ./web/manage.py runserver 0.0.0.0:8000 &
-  wait
-else
-  # start prod server with gunicorn
-  gunicorn --chdir ./web carbure.wsgi --log-file -
-fi
+# start dev server and huey consumer
+python3 ./web/manage.py run_huey --simple --scheduler-interval 10 &
+python3 ./web/manage.py runserver 0.0.0.0:8000 &
