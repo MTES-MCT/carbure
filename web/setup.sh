de5b95cc1b26ffb3d7099044235dc03704382f79
Julien Falxa <jfalxa.sta@gmail.com>
2023-02-14 16:17:34 +0100

    [backend] run migrations in postdeploy hook

---
+++ web/setup.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+# collect frontend static files
+python3 ./web/manage.py collectstatic --noinput
+
+# apply new migrations
+python3 ./web/manage.py migrate --noinput
+
+# fill database with static fixtures
+python3 ./web/fixtures/load_biocarburants.py
+python3 ./web/fixtures/load_countries.py
+python3 ./web/fixtures/load_matierespremieres.py
+python3 ./web/fixtures/load_sn_certificates.py
