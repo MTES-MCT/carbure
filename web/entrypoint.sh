ea9e43a7472950ddc0aec7f7429bf9c0be55a343
Julien Falxa <jfalxa.sta@gmail.com>
2023-02-14 16:19:12 +0100

    [backend] fix last commit

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
 
+# setup static files and database
 bash ./web/setup.sh
 
 # start dev server and huey consumer
 python3 ./web/manage.py run_huey --simple --scheduler-interval 10 &
-python3 ./web/manage.py runserver 0.0.0.0:8000 &
+python3 ./web/manage.py runserver 0.0.0.0:8000
