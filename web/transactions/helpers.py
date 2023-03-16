08cbbb5fb86f0c6c54a31787491a05dd4d87414a
Damien Romito <damien@maracuja.ac>
2023-03-16 10:01:34 +0100

    [api] simplify check_locked_year

--- web/transactions/helpers.py
+++ web/transactions/helpers.py
@@ -2,11 +2,10 @@
 from transactions.models import LockedYear
 
 def check_locked_year(current_year: int):
-    locked_years = LockedYear.objects.order_by('year').filter(locked=True)
-    if locked_years.count() == 0 : return False
-
+    
+    if(current_year <= 2015): return True
     try:
-        locked_years.get(year = current_year)
+        LockedYear.objects.get(year = current_year, locked = True)
         return True
     except:
-        return (True if current_year < locked_years.first().year else False)
+        return False
