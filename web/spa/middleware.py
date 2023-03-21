1775a9cb2cd036cd8426aac1f5db85fd6108958c
Julien Falxa <jfalxa.sta@gmail.com>
2023-03-21 12:10:48 +0100

    [backend] rollback whitenoise update

--- web/spa/middleware.py
+++ web/spa/middleware.py
@@ -13,45 +13,41 @@ class SPAMiddleware(WhiteNoiseMiddleware):
 
     index_name = os.path.join(settings.STATIC_URL, "index.html")
 
-    def __call__(self, request):
+    def process_request(self, request):
         # First try to serve the static files (on /static/ and on /)
         # which is relatively fast as files are stored in a self.files dict
         if self.autorefresh:  # debug mode
             static_file = self.find_file(request.path_info)
         else:  # from the collected static files
             static_file = self.files.get(request.path_info)
-
         if static_file is not None:
             return self.serve(static_file, request)
+        else:
+            # if no file was found there are two options:
 
-        # if no file was found there are two options:
+            # 1) the file is in one of the Django urls
+            # (e.g. a template or the Djangoadmin)
+            # so we'll let Django handle this
+            # (just return and let the normal middleware take its course)
+            urlconf = getattr(request, "urlconf", None)
+            if is_valid_path(request.path_info, urlconf):
+                return
+            if (
+                settings.APPEND_SLASH
+                and not request.path_info.endswith("/")
+                and is_valid_path("%s/" % request.path_info, urlconf)
+            ):
+                return
 
-        # 1) the file is in one of the Django urls
-        # (e.g. a template or the Djangoadmin)
-        # so we'll let Django handle this
-        # (just return and let the normal middleware take its course)
-
-        urlconf = getattr(request, "urlconf", None)
-        if is_valid_path(request.path_info, urlconf):
-            return self.get_response(request)
-
-        if (
-            settings.APPEND_SLASH
-            and not request.path_info.endswith("/")
-            and is_valid_path("%s/" % request.path_info, urlconf)
-        ):
-            return self.get_response(request)
-
-        # 2) the url is handled by frontend routing
-        # redirect all unknown files to the SPA root
-        try:
-            return self.serve(self.spa_root, request)
-        except AttributeError:  # no SPA page stored yet
-            self.spa_root = self.find_file("/")
-            if self.spa_root:
+            # 2) the url is handled by frontend routing
+            # redirect all unknown files to the SPA root
+            try:
                 return self.serve(self.spa_root, request)
-
-        return self.get_response(request)
+            except AttributeError:  # no SPA page stored yet
+                self.spa_root = self.find_file("/")
+                if self.spa_root:
+                    return self.serve(self.spa_root, request)
+                # TODO: else return a Django 404 (maybe?)
 
     def update_files_dictionary(self, *args):
         super(SPAMiddleware, self).update_files_dictionary(*args)
