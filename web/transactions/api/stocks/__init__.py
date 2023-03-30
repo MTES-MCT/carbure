ee4290719f4a07f57b1b94b875208abccd00b230
Damien Romito <damien@maracuja.ac>
2023-03-30 15:30:43 +0200

    [api] start split stock

--- web/transactions/api/stocks/__init__.py
+++ web/transactions/api/stocks/__init__.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .stocks import get_stocks
 from .summary import get_stocks_summary
 from .details import get_stock_details
-from .cancel_transformation import stock_cancel_transformation
 from .transform import stock_transform
+from .cancel_transformation import stock_cancel_transformation
+from .split import stock_split
 
 urlpatterns = [
     path("", get_stocks, name="transactions-stocks"),
@@ -11,4 +12,5 @@ urlpatterns = [
     path("details", get_stock_details, name="transactions-stocks-details"),
     path("transform", stock_transform, name="transactions-stocks-transform"),
     path("cancel-transformation", stock_cancel_transformation, name="transactions-stocks-cancel-transformation"),
+    path("split", stock_split, name="transactions-stocks-split"),
 ]
