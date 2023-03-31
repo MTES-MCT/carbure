4e736b5893281c3d3d61a4e691e1768070aaa3d5
Damien Romito <damien@maracuja.ac>
2023-03-31 15:55:45 +0200

    [api] admin entities

--- web/api/v3/admin/views.py
+++ web/api/v3/admin/views.py
@@ -26,8 +26,8 @@ logger = logging.getLogger(__name__)
 
 @is_admin
 def get_users(request):
-    q = request.GET.get('q', False)
-    entity_id = request.GET.get('entity_id', False)
+    q = request.GET.get("q", False)
+    entity_id = request.GET.get("entity_id", False)
     user_model = get_user_model()
     users = user_model.objects.all()
 
@@ -36,24 +36,13 @@ def get_users(request):
     if entity_id:
         users = users.filter(userrights__entity__id=entity_id)
 
-    users_sez = [{'email': u.email, 'name': u.name, 'id': u.id} for u in users]
+    users_sez = [{"email": u.email, "name": u.name, "id": u.id} for u in users]
     return JsonResponse({"status": "success", "data": users_sez})
-
-
-@check_admin_rights(allow_external=[ExternalAdminRights.AIRLINE])
-def get_entity_details(request):
-    company_id = request.GET.get('company_id', False)
-
-    try:
-        e = Entity.objects.get(pk=company_id)
-        return JsonResponse({"status": "success", "data": e.natural_key()})
-    except Exception:
-        return JsonResponse({"status": "error", "message": "Could not find entity"}, status=400)
 
 
 @is_admin
 def get_entity_production_sites(request):
-    entity_id = request.GET.get('entity_id', False)
+    entity_id = request.GET.get("entity_id", False)
 
     try:
         psites = ProductionSite.objects.filter(producer__id=entity_id)
@@ -66,22 +55,22 @@ def get_entity_production_sites(request):
 
         for ps in psites:
             psite_data = ps.natural_key()
-            psite_data['inputs'] = [i.natural_key() for i in ps.productionsiteinput_set.all()]
-            psite_data['outputs'] = [o.natural_key() for o in ps.productionsiteoutput_set.all()]
+            psite_data["inputs"] = [i.natural_key() for i in ps.productionsiteinput_set.all()]
+            psite_data["outputs"] = [o.natural_key() for o in ps.productionsiteoutput_set.all()]
             certificates = []
             for pc in ps.productionsitecertificate_set.all():
                 certificates.append(pc.natural_key())
-            psite_data['certificates'] = certificates
+            psite_data["certificates"] = certificates
             data.append(psite_data)
 
-        return JsonResponse({'status': 'success', 'data': data})
+        return JsonResponse({"status": "success", "data": data})
     except Exception:
         return JsonResponse({"status": "error", "message": "Could not find production sites"}, status=400)
 
 
 @is_admin
 def get_entity_depots(request):
-    entity_id = request.GET.get('entity_id', False)
+    entity_id = request.GET.get("entity_id", False)
 
     try:
         e = Entity.objects.get(pk=entity_id)
@@ -93,7 +82,7 @@ def get_entity_depots(request):
 
 @is_admin
 def get_entity_certificates(request):
-    entity_id = request.GET.get('entity_id', False)
+    entity_id = request.GET.get("entity_id", False)
 
     try:
         e = Entity.objects.get(pk=entity_id)
@@ -106,96 +95,32 @@ def get_entity_certificates(request):
 
 
 @check_admin_rights(allow_external=[ExternalAdminRights.AIRLINE])
-def get_entities(request):
-    q = request.GET.get('q', False)
-    entity_id = request.GET.get('entity_id', None)
-    has_requests = request.GET.get('has_requests', None)
-
-    entity = Entity.objects.get(id=entity_id)
-
-    if entity.entity_type == entity.ADMIN:
-        entities = Entity.objects.all().order_by('name').prefetch_related('userrights_set', 'userrightsrequests_set', 'entitydepot_set', 'productionsite_set').annotate(
-            users=Count('userrights', distinct=True),
-            requests=Count('userrightsrequests', filter=Q(userrightsrequests__status='PENDING'), distinct=True),
-            depots=Count('entitydepot', distinct=True),
-            production_sites=Count('productionsite', distinct=True),
-            certificates=Count('entitycertificate', distinct=True),
-            double_counting=Count('doublecountingagreement', filter=Q(
-                doublecountingagreement__status=DoubleCountingAgreement.ACCEPTED), distinct=True),
-            double_counting_requests=Count('doublecountingagreement', filter=Q(
-                doublecountingagreement__status=DoubleCountingAgreement.PENDING), distinct=True),
-        )
-    elif entity.has_external_admin_right("AIRLINE"):
-        entities = Entity.objects.all().order_by('name').filter(entity_type=Entity.AIRLINE).prefetch_related('userrights_set', 'userrightsrequests_set').annotate(
-            users=Count('userrights', distinct=True),
-            requests=Count('userrightsrequests', filter=Q(userrightsrequests__status='PENDING'), distinct=True),
-            depots=Value(0),
-            production_sites=Value(0),
-            certificates=Value(0),
-            double_counting=Value(0),
-            double_counting_requests=Value(0),
-        )
-
-    if q:
-        entities = entities.filter(name__icontains=q)
-    if has_requests == "true":
-        entities = entities.filter(requests__gt=0)
-
-    entities_sez = []
-    for e in entities.iterator():
-        entities_sez.append({
-            'entity': e.natural_key(),
-            'users': e.users,
-            'requests': e.requests,
-            'depots': e.depots,
-            'production_sites': e.production_sites,
-            'certificates': e.certificates,
-            'double_counting': e.double_counting,
-            'double_counting_requests': e.double_counting_requests
-        })
-    return JsonResponse({"status": "success", "data": entities_sez})
-
-
-@check_admin_rights(allow_external=[ExternalAdminRights.AIRLINE])
 def add_entity(request):
-    name = request.POST.get('name', False)
-    entity_type = request.POST.get('category', False)
+    name = request.POST.get("name", False)
+    entity_type = request.POST.get("category", False)
 
     if not name:
-        return JsonResponse({'status': 'error', 'message': "Please provide a value in field name"}, status=400)
+        return JsonResponse({"status": "error", "message": "Please provide a value in field name"}, status=400)
     if not entity_type:
-        return JsonResponse({'status': 'error', 'message': "Please provide a value in field Category"}, status=400)
+        return JsonResponse({"status": "error", "message": "Please provide a value in field Category"}, status=400)
 
     if entity_type not in [c[0] for c in Entity.ENTITY_TYPES]:
-        return JsonResponse({'status': 'error', 'message': "Unknown Category"}, status=400)
+        return JsonResponse({"status": "error", "message": "Unknown Category"}, status=400)
 
     try:
         obj, created = Entity.objects.update_or_create(name=name, entity_type=entity_type)
     except Exception:
-        return JsonResponse({'status': 'error', 'message': "Unknown error. Please contact an administrator"}, status=400)
-    return JsonResponse({'status': 'success', 'data': 'Entity created'})
-
-
-@is_admin
-def delete_entity(request):
-    entity_id = request.POST.get('entity_id', False)
-
-    if not entity_id:
-        return JsonResponse({'status': 'error', 'message': "Please provide an entity_id"}, status=400)
-    try:
-        entity = Entity.objects.get(id=entity_id)
-    except Exception:
-        return JsonResponse({'status': 'error', 'message': "Could not find entity"}, status=400)
-
-    entity.delete()
-    return JsonResponse({"status": "success", "data": "success"})
+        return JsonResponse(
+            {"status": "error", "message": "Unknown error. Please contact an administrator"}, status=400
+        )
+    return JsonResponse({"status": "success", "data": "Entity created"})
 
 
 @check_admin_rights(allow_external=[ExternalAdminRights.AIRLINE])
 def get_rights_requests(request):
-    q = request.GET.get('q', False)
-    statuses = request.GET.getlist('statuses', False)
-    company_id = request.GET.get('company_id', False)
+    q = request.GET.get("q", False)
+    statuses = request.GET.getlist("statuses", False)
+    company_id = request.GET.get("company_id", False)
     requests = UserRightsRequests.objects.all()
 
     if company_id:
@@ -211,24 +136,27 @@ def get_rights_requests(request):
 
 @check_admin_rights(allow_external=[ExternalAdminRights.AIRLINE])
 def update_right_request(request):
-    urr_id = request.POST.get('id', False)
-    status = request.POST.get('status', False)
+    urr_id = request.POST.get("id", False)
+    status = request.POST.get("status", False)
     if not urr_id:
-        return JsonResponse({'status': 'error', 'message': "Please provide an id"}, status=400)
+        return JsonResponse({"status": "error", "message": "Please provide an id"}, status=400)
     if not status:
-        return JsonResponse({'status': 'error', 'message': "Please provide a status"}, status=400)
+        return JsonResponse({"status": "error", "message": "Please provide a status"}, status=400)
 
     try:
         right_request = UserRightsRequests.objects.get(id=urr_id)
     except:
-        return JsonResponse({'status': 'error', 'message': "Could not find request"}, status=400)
+        return JsonResponse({"status": "error", "message": "Could not find request"}, status=400)
 
     right_request.status = status
     right_request.save()
 
-    if status == 'ACCEPTED':
-        UserRights.objects.update_or_create(entity=right_request.entity, user=right_request.user, defaults={
-                                            'role': right_request.role, 'expiration_date': right_request.expiration_date})
+    if status == "ACCEPTED":
+        UserRights.objects.update_or_create(
+            entity=right_request.entity,
+            user=right_request.user,
+            defaults={"role": right_request.role, "expiration_date": right_request.expiration_date},
+        )
         # send_mail
         email_subject = "Carbure - Demande acceptée"
         message = """
@@ -236,7 +164,9 @@ def update_right_request(request):
 
         Votre demande d'accès à la Société %s vient d'être validée par l'administration.
 
-        """ % (right_request.entity.name)
+        """ % (
+            right_request.entity.name
+        )
 
         send_mail(
             subject=email_subject,
@@ -538,13 +468,46 @@ def update_right_request(request):
 
 # not an api endpoint
 def couleur():
-    liste = ["#FF0040", "#DF01A5", "#BF00FF", "#4000FF", "#0040FF", "#00BFFF",
-             "#00FFBF", "#00FF40", "#40FF00", "#BFFF00", "#FFBF00", "#FF4000",
-             "#FA5858", "#FAAC58", "#F4FA58", "#ACFA58", "#58FA58", "#58FAAC",
-             "#58FAF4", "#58ACFA", "#5858FA", "#AC58FA", "#FA58F4", "#FA58AC",
-             "#B40404", "#B45F04", "#AEB404", "#5FB404", "#04B45F", "#04B4AE",
-             "#045FB4", "#0404B4", "#5F04B4", "#B404AE", "#B4045F", "#8A0829"]
+    liste = [
+        "#FF0040",
+        "#DF01A5",
+        "#BF00FF",
+        "#4000FF",
+        "#0040FF",
+        "#00BFFF",
+        "#00FFBF",
+        "#00FF40",
+        "#40FF00",
+        "#BFFF00",
+        "#FFBF00",
+        "#FF4000",
+        "#FA5858",
+        "#FAAC58",
+        "#F4FA58",
+        "#ACFA58",
+        "#58FA58",
+        "#58FAAC",
+        "#58FAF4",
+        "#58ACFA",
+        "#5858FA",
+        "#AC58FA",
+        "#FA58F4",
+        "#FA58AC",
+        "#B40404",
+        "#B45F04",
+        "#AEB404",
+        "#5FB404",
+        "#04B45F",
+        "#04B4AE",
+        "#045FB4",
+        "#0404B4",
+        "#5F04B4",
+        "#B404AE",
+        "#B4045F",
+        "#8A0829",
+    ]
     return random.choice(liste)
+
 
 # not an api endpoint
 
@@ -563,21 +526,33 @@ def map(request):
     admin_entities_rights = UserRights.objects.filter(user=request.user, entity__entity_type=Entity.ADMIN)
     entity = admin_entities_rights[0].entity
     lots = CarbureLot.objects.select_related(
-        'carbure_producer', 'carbure_production_site', 'production_country',
-        'feedstock', 'biofuel', 'country_of_origin', 'added_by',
-        'carbure_supplier', 'carbure_client', 'carbure_delivery_site', 'delivery_site_country',
+        "carbure_producer",
+        "carbure_production_site",
+        "production_country",
+        "feedstock",
+        "biofuel",
+        "country_of_origin",
+        "added_by",
+        "carbure_supplier",
+        "carbure_client",
+        "carbure_delivery_site",
+        "delivery_site_country",
     ).filter(lot_status__in=[CarbureLot.ACCEPTED, CarbureLot.FROZEN, CarbureLot.PENDING])
     lots = filter_lots(lots, request.GET, entity)
 
     # on veut: nom site de depart, gps depart, nom site arrivee, gps arrivee, volume
     lots = lots.filter(carbure_production_site__isnull=False, carbure_delivery_site__isnull=False)
-    values = lots.values('carbure_production_site__name', 'carbure_production_site__gps_coordinates',
-                         'carbure_delivery_site__name', 'carbure_delivery_site__gps_coordinates').annotate(volume=Sum('volume'))
+    values = lots.values(
+        "carbure_production_site__name",
+        "carbure_production_site__gps_coordinates",
+        "carbure_delivery_site__name",
+        "carbure_delivery_site__gps_coordinates",
+    ).annotate(volume=Sum("volume"))
 
     volume_min = 999999
     volume_max = 0
     for v in values:
-        volume = v['volume']
+        volume = v["volume"]
         if volume < volume_min:
             volume_min = volume
         if volume > volume_max:
@@ -589,39 +564,54 @@ def map(request):
     for v in values:
         try:
             # start coordinates
-            slat, slon = v['carbure_production_site__gps_coordinates'].split(',')
+            slat, slon = v["carbure_production_site__gps_coordinates"].split(",")
             # end coordinates
-            elat, elon = v['carbure_delivery_site__gps_coordinates'].split(',')
+            elat, elon = v["carbure_delivery_site__gps_coordinates"].split(",")
         except:
-            print('Missing start or end gps coordinates')
-            print('Start %s : %s' % (v['carbure_production_site__name'].encode(
-                'utf-8'), v['carbure_production_site__gps_coordinates']))
-            print('End %s : %s' % (v['carbure_delivery_site__name'].encode(
-                'utf-8'), v['carbure_delivery_site__gps_coordinates']))
+            print("Missing start or end gps coordinates")
+            print(
+                "Start %s : %s"
+                % (v["carbure_production_site__name"].encode("utf-8"), v["carbure_production_site__gps_coordinates"])
+            )
+            print(
+                "End %s : %s"
+                % (v["carbure_delivery_site__name"].encode("utf-8"), v["carbure_delivery_site__gps_coordinates"])
+            )
             continue
 
-        if v['carbure_production_site__gps_coordinates'] not in known_prod_sites:
-            known_prod_sites.append(v['carbure_production_site__gps_coordinates'])
+        if v["carbure_production_site__gps_coordinates"] not in known_prod_sites:
+            known_prod_sites.append(v["carbure_production_site__gps_coordinates"])
             c = couleur()
             folium.Circle(
                 radius=50e2,
                 location=[slat, slon],
-                popup=v['carbure_production_site__name'],
+                popup=v["carbure_production_site__name"],
                 color=c,
                 fill=True,
                 fill_opacity=1,
             ).add_to(m)
-        if v['carbure_delivery_site__gps_coordinates'] not in known_depots:
-            known_depots.append(v['carbure_delivery_site__gps_coordinates'])
+        if v["carbure_delivery_site__gps_coordinates"] not in known_depots:
+            known_depots.append(v["carbure_delivery_site__gps_coordinates"])
             folium.Circle(
                 radius=50e2,
                 location=[elat, elon],
-                popup=v['carbure_delivery_site__name'],
+                popup=v["carbure_delivery_site__name"],
                 color="white",
                 fill=True,
                 fill_opacity=1,
             ).add_to(m)
-        volume = v['volume']
-        folium.PolyLine([(float(slat), float(slon)), (float(elat), float(elon))], color=c, weight=grade(volume, volume_min, volume_max), line_cap='round',
-                        opacity=0.7, popup=v['carbure_production_site__name']+' vers '+v['carbure_delivery_site__name']+' : \n'+str(volume)+' litres').add_to(m)
+        volume = v["volume"]
+        folium.PolyLine(
+            [(float(slat), float(slon)), (float(elat), float(elon))],
+            color=c,
+            weight=grade(volume, volume_min, volume_max),
+            line_cap="round",
+            opacity=0.7,
+            popup=v["carbure_production_site__name"]
+            + " vers "
+            + v["carbure_delivery_site__name"]
+            + " : \n"
+            + str(volume)
+            + " litres",
+        ).add_to(m)
     return HttpResponse(m._repr_html_())
