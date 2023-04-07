6f0af5cb86460944120580f1c781244e86013311
Damien Romito <damien@maracuja.ac>
2023-04-07 17:11:38 +0200

    [api] script to update dc certificate with linked production site

--- scripts/patches/2023-04-7_update_dc_registrations.py
+++ scripts/patches/2023-04-7_update_dc_registrations.py
@@ -12,13 +12,12 @@ from certificates.models import DoubleCountingRegistration
 
 @transaction.atomic
 def update_dc_registrations():
-    print("TEST")
-    # dc_production_sites = ProductionSite.objects.filter(dc_reference__isnull=False, dc_reference__regex=r"\S+")
-    # for production_site in dc_production_sites:
-    #     certificate_id = production_site.dc_reference
-    #     try:
-    #         certificate = DoubleCountingRegistration.objects.get(certificate_id=certificate_id)
-    #         certificate.production_site = production_site
-    #         certificate.save()
-    #     except:
-    #         print("DC Certificate not found")
+    dc_production_sites = ProductionSite.objects.filter(dc_reference__isnull=False, dc_reference__regex=r"\S+")
+    for production_site in dc_production_sites:
+        certificate_id = production_site.dc_reference
+        try:
+            certificate = DoubleCountingRegistration.objects.get(certificate_id=certificate_id)
+            certificate.production_site = production_site
+            certificate.save()
+        except:
+            print("DC Certificate not found")
