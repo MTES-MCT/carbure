c86e5fbd6402a0eaa29d2dfa3fd00a8db96faee0
Damien Romito <damien@maracuja.ac>
2023-04-13 15:57:43 +0200

    [api] clean v3/admin

--- web/admin/api/entities/depots.py
+++ web/admin/api/entities/depots.py
@@ -5,10 +5,10 @@ from core.models import Entity
 
 @check_admin_rights()
 def get_entity_depots(request):
-    entity_id = request.GET.get("entity_id", False)
+    company_id = request.GET.get("company_id", False)
 
     try:
-        e = Entity.objects.get(pk=entity_id)
+        e = Entity.objects.get(pk=company_id)
         data = [ps.natural_key() for ps in e.entitydepot_set.all()]
         return JsonResponse({"status": "success", "data": data})
     except Exception:
