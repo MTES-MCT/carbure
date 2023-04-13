ffcba83f5f0bd2055516f1da691224c0d2d9c5a3
Damien Romito <damien@maracuja.ac>
2023-04-13 16:39:30 +0200

    [api] refacto notifications/ endpoints

---
+++ web/notifications/api/__init__.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .notifications import get_notifications
+from .ack import ack_notifications
+
+urlpatterns = [
+    path("", get_notifications, name="notifications"),
+    path("ack", ack_notifications, name="notifications.ack"),
+]
