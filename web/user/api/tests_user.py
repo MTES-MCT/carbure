5eee70a29d44e19eb800131dcf77e06c58ffae61
Damien Romito <damien@maracuja.ac>
2023-05-03 17:44:50 +0200

    [api] remove unused file in v3

--- web/user/api/tests_user.py
+++ web/user/api/tests_user.py
@@ -28,3 +28,23 @@ class UserTest(TestCase):
         self.assertIn("rights", data)
         self.assertIn("email", data)
         self.assertIn("requests", data)
+
+    def test_entity_access_request(self):
+        # get settings - 0 pending requests
+        url = "user"
+        response = self.client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+        data = response.json()["data"]
+        self.assertIn("requests", data)
+        prev_len = len(data["requests"])
+
+        e, _ = Entity.objects.update_or_create(name="Entity test", entity_type="Producteur")
+        postdata = {"entity_id": e.id, "comment": "", "role": "RO"}
+        response = self.client.post(reverse("user-request-access"), postdata)
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+        data = response.json()["data"]
+        new_len = len(data["requests"])
+        self.assertEqual(prev_len + 1, new_len)
