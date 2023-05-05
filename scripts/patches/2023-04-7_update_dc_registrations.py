0e4679f3fc2343b0826f0345d1a8e48ea427384b
Damien Romito <damien@maracuja.ac>
2023-05-05 15:53:34 +0200

    execute script 2023-04-7_update_dc_registrations in production

--- scripts/patches/2023-04-7_update_dc_registrations.py
+++ scripts/patches/2023-04-7_update_dc_registrations.py
@@ -16,10 +16,7 @@ def update_dc_registrations():
     dc_lots = CarbureLot.objects.filter(production_site_double_counting_certificate__isnull=False).exclude(
         production_site_double_counting_certificate__exact=""
     )
-
     dc_certificate_not_found = 0
-    lot_without_valid_dc_certificate = 0
-
     for dc_lot in dc_lots:
         # 1 - on met à jour le site de production dans le certificat du lot
         dc_cert_id = dc_lot.production_site_double_counting_certificate
@@ -29,12 +26,15 @@ def update_dc_registrations():
             if dc_cert.production_site != dc_production_site:
                 dc_cert.production_site = dc_production_site
                 dc_cert.save()
+                print(f"Production site {dc_production_site} associate with DC certificate {dc_cert_id}")
         except:
-            print(f"DC Certificate not found for lot {dc_lot} {dc_lot.delivery_date} {dc_lot.carbure_producer} ")
-            dc_certificate_not_found += 1
+            # print(f"DC Certificate not found for lot {dc_lot} {dc_lot.delivery_date} {dc_lot.carbure_producer} ")
+            dc_certificate_not_found = dc_certificate_not_found + 1
+    print(f"DC Certificate not found for {dc_certificate_not_found} lots")
 
+    # 2 - on récupere le certificat valide pour le site de production du lot et on le met à jour dans le lot s'il existe
+    lot_without_valid_dc_certificate = 0
     for dc_lot in dc_lots:
-        # 2 - on récupere le certificat valide pour le site de production du lot et on le met à jour dans le lot s'il existe
         try:
             valid_certificate = DoubleCountingRegistration.objects.filter(
                 production_site_id=dc_lot.carbure_production_site.id,
@@ -44,11 +44,15 @@ def update_dc_registrations():
             if dc_lot.production_site_double_counting_certificate != valid_certificate.certificate_id:
                 dc_lot.production_site_double_counting_certificate = valid_certificate.certificate_id
                 dc_lot.save()
+                print(f"Valid DC certificate {valid_certificate.certificate_id} found for {dc_lot}")
         except:
             print(f"not valid certificate found for {dc_lot}")
             lot_without_valid_dc_certificate += 1
 
+    print(f"lots without valid dc certificate: {lot_without_valid_dc_certificate}")
+
     # 3 - Au cas où, on met à jour le site de production dans le certificat
+
     dc_production_sites = ProductionSite.objects.filter(eligible_dc=True)
     for production_site in dc_production_sites:
         certificate_id = production_site.dc_reference
