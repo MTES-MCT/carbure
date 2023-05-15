5fbc0d1c020139d56a70ec4a194c7509083d58c1
Julien Falxa <jfalxa.sta@gmail.com>
2023-05-15 17:02:13 +0200

    [api] move entity stats endpoint

--- web/api/v5/__init__.py
+++ web/api/v5/__init__.py
@@ -2,14 +2,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", include("admin.api")),
-    path("admin/", include("api.v5.admin")),
     path("auth/", include("auth.api")),
     path("double-counting/", include("api.v5.double_counting")),
     path("entity/", include("entity.api")),
-    path("saf/", include("api.v5.saf")),
+    path("saf/", include("saf.api")),
     path("resources/", include("resources.api")),
-    path("settings/", include("api.v5.settings")),
-    path("stats/", include("api.v5.stats")),
     path("stats/", include("stats.api")),
     path("transactions/", include("transactions.api")),
     path("user/", include("user.api")),
--- web/api/v5/stats/__init__.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .entity import get_entity
-
-urlpatterns = [
-    path("entity", get_entity, name="api-v5-stats-entity"),
-]
