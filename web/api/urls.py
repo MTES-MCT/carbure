be5c44edc1a149e473d04bbcfb40e36dee58b6dd
Julien Falxa <jfalxa.sta@gmail.com>
2023-05-15 18:33:30 +0200

    [api] move all api setup inside the carbure module

--- web/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path("v3/doublecount/", include("doublecount.api.urls")),
-    path("", include("api.v4.urls")),
-    path("v5/", include("api.v5")),
-]
