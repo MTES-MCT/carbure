4e61dffdd5f1a2910f4c68e1b26b49359484324a
Julien Falxa <jfalxa.sta@gmail.com>
2023-05-15 14:45:20 +0200

    [api] move admin v5 api endpoints to admin module

--- web/api/v5/admin/lots/__init__.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .update_many import update_many
-from .delete_many import delete_many
-
-urlpatterns = [
-    path("update-many", update_many, name="api-v5-admin-lots-update-many"),
-    path("delete-many", delete_many, name="api-v5-admin-lots-delete-many"),
-]
