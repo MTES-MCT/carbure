2198d5c6d245a93fb510e3d3ac8f445695fa7da0
Julien Falxa <jfalxa.sta@gmail.com>
2023-05-15 14:45:20 +0200

    [api] move admin v5 api endpoints to admin module

--- web/api/v5/admin/__init__.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .create_entity import create_entity
-
-urlpatterns = [
-    path("lots/", include("api.v5.admin.lots")),
-    path("create-entity", create_entity, name="api-v5-admin-create-entity"),
-]
