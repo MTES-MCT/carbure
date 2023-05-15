6f80ef9d851ac22cb174515cc2295e4f18ccb739
Julien Falxa <jfalxa.sta@gmail.com>
2023-05-15 17:07:37 +0200

    [api] cleanup api v5 folder

--- web/api/v5.py
+++ web/api/v5.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", include("admin.api")),
     path("auth/", include("auth.api")),
-    path("double-counting/", include("api.v5.double_counting")),
+    path("double-counting/", include("doublecount.api")),
     path("entity/", include("entity.api")),
     path("saf/", include("saf.api")),
     path("resources/", include("resources.api")),
