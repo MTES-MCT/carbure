db8cd9b7376b77f99675008bb17b69720137a2d8
Julien Falxa <jfalxa.sta@gmail.com>
2023-05-16 17:19:08 +0200

    [api] refactor ghg sanity checks

--- web/transactions/sanity_checks/sanity_checks.py
+++ web/transactions/sanity_checks/sanity_checks.py
@@ -1,12 +1,14 @@
 from core.models import CarbureLot, GenericError
 from .mandatory import *
+from .ghg import *
 
 
-def sanity_checks(lot: CarbureLot):
+def sanity_checks(lot: CarbureLot, prefetched_data):
     if lot.lot_status == CarbureLot.FLUSHED:
         return []
 
     errors: list[GenericError] = [
+        # mandatory fields
         check_missing_volume(lot),
         check_missing_biofuel(lot),
         check_missing_feedstock(lot),
@@ -20,6 +22,21 @@ def sanity_checks(lot: CarbureLot):
         check_missing_delivery_site_country(lot),
         check_missing_feedstock_country_of_origin(lot),
         check_missing_supplier_certificate(lot),
+        # ghg errors
+        check_etd_anormal_high(lot, prefetched_data),
+        check_etd_no_eu_too_low(lot, prefetched_data),
+        check_etd_eu_default_value(lot, prefetched_data),
+        check_eec_anormal_low(lot, prefetched_data),
+        check_eec_anormal_high(lot, prefetched_data),
+        check_ep_anormal_low(lot, prefetched_data),
+        check_ep_anormal_high(lot, prefetched_data),
+        check_ghg_etd_0(lot),
+        check_ghg_ep_0(lot),
+        check_ghg_el_neg(lot),
+        check_ghg_eec_0(lot),
+        check_eec_with_residue(lot),
+        check_ghg_reduc(lot),
+        check_ghg_reduc_for_production_site(lot),
     ]
 
     # remove empty values from error list
