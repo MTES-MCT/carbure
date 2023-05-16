4df12bc73d0b3b935afaec8ce97ff3d12b74e720
Julien Falxa <jfalxa.sta@gmail.com>
2023-05-16 17:19:08 +0200

    [api] refactor ghg sanity checks

--- web/transactions/sanity_checks/tests_sanity_checks.py
+++ web/transactions/sanity_checks/tests_sanity_checks.py
@@ -1,7 +1,6 @@
 import datetime
 from django.test import TestCase
 
-from api.v4.tests_utils import setup_current_user
 from core.carburetypes import CarbureSanityCheckErrors, CarbureCertificatesErrors, CarbureMLGHGErrors
 from core.models import Entity, CarbureLot, MatierePremiere, Biocarburant, Depot, EntityDepot, Pays
 from ml.models import ETDStats, EECStats, EPStats
@@ -10,7 +9,7 @@ from transactions.factories import CarbureLotFactory
 from transactions.models import LockedYear
 from producers.models import ProductionSite, ProductionSiteInput, ProductionSiteOutput
 from resources.factories import ProductionSiteFactory
-from .helpers import enrich_lot
+from .helpers import enrich_lot, has_error
 
 
 class SanityChecksTest(TestCase):
@@ -36,206 +35,6 @@ class SanityChecksTest(TestCase):
     def create_lot(self, **kwargs):
         lot = CarbureLotFactory.create(**kwargs)
         return enrich_lot(lot)
-
-    def test_ghg_reduc_sup_100(self):
-        error = CarbureSanityCheckErrors.GHG_REDUC_SUP_100
-
-        # prepare a lot for RED I values
-        lot_red_i = self.create_lot(
-            delivery_date=july1st2021 - datetime.timedelta(days=15),
-            ghg_reduction=95,
-            ghg_reduction_red_ii=105,  # put a wrong RED II value to confirm it will be ignored
-        )
-
-        error_list = self.run_checks(lot_red_i)
-        self.assertFalse(has_error(error, error_list))
-
-        lot_red_i.ghg_reduction = 105
-
-        error_list = self.run_checks(lot_red_i)
-        self.assertTrue(has_error(error, error_list))
-
-        # prepare a lot for RED II values
-        lot_red_ii = self.create_lot(
-            delivery_date=july1st2021 + datetime.timedelta(days=15),
-            ghg_reduction_red_ii=95,
-            ghg_reduction=105,  # put a wrong RED I value to confirm it will be ignored
-        )
-
-        error_list = self.run_checks(lot_red_ii)
-        self.assertFalse(has_error(error, error_list))
-
-        lot_red_ii.ghg_reduction_red_ii = 105
-
-        error_list = self.run_checks(lot_red_ii)
-        self.assertTrue(has_error(error, error_list))
-
-    def test_ghg_reduc_sup_99(self):
-        error = CarbureSanityCheckErrors.GHG_REDUC_SUP_99
-
-        # prepare a lot for RED II values
-        lot = self.create_lot(
-            delivery_date=july1st2021 + datetime.timedelta(days=15),
-            ghg_reduction_red_ii=95,
-            ghg_reduction=105,  # put a wrong RED I value to confirm it will be ignored
-        )
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.ghg_reduction_red_ii = 99.9
-
-        error_list = self.run_checks(lot)
-        self.assertTrue(has_error(error, error_list))
-
-    def test_ghg_reduc_inf_50(self):
-        error = CarbureSanityCheckErrors.GHG_REDUC_INF_50
-
-        # prepare a lot for RED II values
-        lot = self.create_lot(
-            delivery_date=july1st2021 + datetime.timedelta(days=15),
-            ghg_reduction_red_ii=95,
-            ghg_reduction=105,  # put a wrong RED I value to confirm it will be ignored
-        )
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.ghg_reduction_red_ii = 5
-
-        error_list = self.run_checks(lot)
-        self.assertTrue(has_error(error, error_list))
-
-    def test_ghg_etd_0(self):
-        error = CarbureSanityCheckErrors.GHG_ETD_0
-
-        lot = self.create_lot(etd=1)
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.etd = -1
-
-        error_list = self.run_checks(lot)
-        self.assertTrue(has_error(error, error_list))
-
-    def test_ghg_ep_0(self):
-        error = CarbureSanityCheckErrors.GHG_EP_0
-
-        lot = self.create_lot(ep=1)
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.ep = -1
-
-        error_list = self.run_checks(lot)
-        self.assertTrue(has_error(error, error_list))
-
-    def test_ghg_el_neg(self):
-        error = CarbureSanityCheckErrors.GHG_EL_NEG
-
-        lot = self.create_lot(el=1)
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.el = -1
-
-        error_list = self.run_checks(lot)
-        self.assertTrue(has_error(error, error_list))
-
-    def test_ghg_eec_0(self):
-        error = CarbureSanityCheckErrors.GHG_EEC_0
-
-        conv_feedstock = MatierePremiere.objects.filter(category="CONV").first()
-        other_feedstock = MatierePremiere.objects.exclude(category="CONV").first()
-
-        lot = self.create_lot(eec=0, feedstock=other_feedstock)
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.eec = 1
-        lot.feedstock = conv_feedstock
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.eec = 0
-
-        error_list = self.run_checks(lot)
-        self.assertTrue(has_error(error, error_list))
-
-    def test_eec_with_residue(self):
-        error = CarbureSanityCheckErrors.EEC_WITH_RESIDUE
-
-        conv_feedstock = MatierePremiere.objects.filter(category="CONV").exclude(code="EP2").first()
-        other_feedstock = MatierePremiere.objects.exclude(category="CONV", code="EP2").first()
-        ep2 = MatierePremiere.objects.get(code="EP2")
-
-        lot = self.create_lot(feedstock=other_feedstock, eec=1)
-
-        error_list = self.run_checks(lot)
-        self.assertTrue(has_error(error, error_list))
-
-        lot.eec = 0
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.eec = 1
-        lot.feedstock = conv_feedstock
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.eec = 1
-        lot.feedstock = ep2
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-    def test_ghg_reduc_inf_60(self):
-        error = CarbureSanityCheckErrors.GHG_REDUC_INF_60
-
-        lot = self.create_lot(production_site_commissioning_date=None)
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.production_site_commissioning_date = oct2015 + datetime.timedelta(days=15)
-        lot.ghg_reduction_red_ii = 61
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.ghg_reduction_red_ii = 59
-
-        error_list = self.run_checks(lot)
-        self.assertTrue(has_error(error, error_list))
-
-    def x_test_ghg_reduc_inf_65(self):
-        error = CarbureSanityCheckErrors.GHG_REDUC_INF_65
-
-        lot = self.create_lot(production_site_commissioning_date=None)
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.production_site_commissioning_date = jan2021 + datetime.timedelta(days=15)
-        lot.ghg_reduction_red_ii = 64
-
-        error_list = self.run_checks(lot)
-        self.assertFalse(has_error(error, error_list))
-
-        lot.ghg_reduction_red_ii = 66
-
-        error_list = self.run_checks(lot)
-        self.assertTrue(has_error(error, error_list))
-
-        error_list = self.run_checks(lot)
-        self.assertTrue(has_error(error, error_list))
 
     def test_mac_bc_wrong(self):
         error = CarbureSanityCheckErrors.MAC_BC_WRONG
@@ -611,10 +410,3 @@ class SanityChecksTest(TestCase):
 
     def x_test_unknown_client(self):
         error = "UNKNOWN_CLIENT"
-
-
-def has_error(error, error_list):
-    for e in error_list:
-        if e.error == error:
-            return True
-    return False
