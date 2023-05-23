2520143d020b740e40ea83b0515846d53cb425f0
Damien Romito <damien@maracuja.ac>
2023-05-23 14:06:17 +0200

    [api] refacto admin/controls/update(delete)-many

--- web/admin/api/lots/__init__.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .update_many import update_many
-from .delete_many import delete_many
-
-urlpatterns = [
-    path("update-many", update_many, name="admin-lots-update-many"),
-    path("delete-many", delete_many, name="admin-lots-delete-many"),
-]
