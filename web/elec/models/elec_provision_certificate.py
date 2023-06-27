60750c522bad428ef8e0a28f7915f3260af6446b
Julien Falxa <jfalxa.sta@gmail.com>
2023-06-27 11:38:24 +0200

    [api] setup elec provision cert model

--- web/elec/models/elec_provision_certificate.py
+++ web/elec/models/elec_provision_certificate.py
@@ -1,5 +1,4 @@
 from django.db import models
-from math import floor
 
 
 class ElecProvisionCertificate(models.Model):
@@ -8,8 +7,16 @@ class ElecProvisionCertificate(models.Model):
         verbose_name = "Certificat de Fourniture (elec)"
         verbose_name_plural = "Certificats de Fourniture (elec)"
 
+    QUARTERS = (
+        (1, "T1"),
+        (2, "T2"),
+        (3, "T3"),
+        (4, "T4"),
+    )
+
     entity = models.ForeignKey("core.Entity", null=True, blank=True, on_delete=models.CASCADE)
-    quarter = models.CharField(max_length=64)
-    oeprating_unit = models.CharField(max_length=64)
+    quarter = models.IntegerField(choices=QUARTERS)
+    year = models.IntegerField()
+    operating_unit = models.CharField(max_length=64)
     energy_amount = models.IntegerField()
     remaining_energy_amount = models.IntegerField()
