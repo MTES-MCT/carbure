951804f1724fe4e171026fc8f78f07bc36a82dab
Julien Falxa <jfalxa.sta@gmail.com>
2023-06-27 11:38:24 +0200

    [api] setup elec provision cert model

--- web/elec/admin.py
+++ web/elec/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from elec.models.elec_provision_certificate import ElecProvisionCertificate
+
+
+@admin.register(ElecProvisionCertificate)
+class ElecProvisionCertificateAdmin(admin.ModelAdmin):
+    list_display = (
+        "entity",
+        "quarter",
+        "year",
+        "operating_unit",
+        "energy_amount",
+        "remaining_energy_amount",
+    )
+    list_filter = (
+        "entity",
+        "quarter",
+        "year",
+        "operating_unit",
+    )
