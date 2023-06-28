e9e8b27b961627e0c68b098b093546be65dd1792
Julien Falxa <jfalxa.sta@gmail.com>
2023-06-28 18:04:59 +0200

    [udb] reduce ram allocation of domibus

--- udb/setenv.sh
+++ udb/setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #Please change CATALINA_HOME to the right folder
 export CATALINA_HOME=/app/domibus
-JAVA_OPTS="$JAVA_OPTS -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
+JAVA_OPTS="$JAVA_OPTS -Xms128m -Xmx1024m -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
