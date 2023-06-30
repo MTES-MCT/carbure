9fe5297a7e720ee13077bd3a64d2759613358e47
Julien Falxa <jfalxa.sta@gmail.com>
2023-06-30 16:45:46 +0200

    [udb] simplify domibus config generation from env vars

--- udb/download_domibus.sh
+++ udb/download_domibus.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+# Download domibus binary
+
 wget https://ec.europa.eu/digital-building-blocks/artifact/content/repositories/public/eu/domibus/domibus-distribution/5.0.4/domibus-distribution-5.0.4-tomcat-full.zip
 wget https://ec.europa.eu/digital-building-blocks/artifact/content/repositories/public/eu/domibus/domibus-distribution/5.0.4/domibus-distribution-5.0.4-sample-configuration-and-testing.zip
 wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-j-8.0.32.zip
@@ -11,12 +14,3 @@ mv /tmp/mysql-connector-j-8.0.32/mysql-connector-j-8.0.32.jar ./domibus/lib
 rm *.zip
 
 chmod +x ./domibus/bin/*.sh
-
-# bind properties file with env vars and move it to domibus
-bash properties.sh
-mv carbure-domibus.properties ./domibus/conf/domibus/domibus.properties
-
-# add the setenv.sh file
-mv setenv.sh ./domibus/bin/setenv.sh
-
-bash ./domibus/bin/catalina.sh run
--- udb/setenv.sh
+++ udb/setenv.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-#Please change CATALINA_HOME to the right folder
+# script to setup env variables for domibus
+
 export CATALINA_HOME=/app/domibus
 JAVA_OPTS="$JAVA_OPTS -Xms128m -Xmx1024m -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
