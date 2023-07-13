058d9d045b1b5b14b6b9dccb8219cb75fc30641e
Damien Romito <damien@maracuja.ac>
2023-07-13 18:35:03 +0200

    [front] add dc translations

--- front/src/double-counting/components/dc-tables.tsx
+++ front/src/double-counting/components/dc-tables.tsx
@@ -126,7 +126,7 @@ export const SourcingFullTable = ({ sourcing }: { sourcing: DoubleCountingSourci
   return <>
 
     <Checkbox readOnly value={aggregateSourcing} onChange={() => setAggregateSourcing(!aggregateSourcing)}>
-      {t("Agréger les données d’approvisionnement par matière première ")}
+      {t("Agréger les données d’approvisionnement par matière première")}
     </Checkbox>
     {!aggregateSourcing &&
       <SourcingTable
