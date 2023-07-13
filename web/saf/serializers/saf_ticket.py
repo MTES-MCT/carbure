3476d403be1574b7cd72a8f01f8d13da7feea7da
Julien Falxa <jfalxa.sta@gmail.com>
2023-07-13 14:53:06 +0200

    [api] add ability to export saf tickets as excel

--- web/saf/serializers/saf_ticket.py
+++ web/saf/serializers/saf_ticket.py
@@ -1,4 +1,6 @@
+import datetime
 from rest_framework import serializers
+from core.excel import export_to_excel
 
 from doublecount.serializers import BiofuelSerializer, CountrySerializer, FeedStockSerializer
 from core.serializers import EntityPreviewSerializer, ProductionSiteSerializer
@@ -97,3 +99,61 @@ class SafTicketPreviewSerializer(serializers.ModelSerializer):
         fields = ["id", "carbure_id", "client", "agreement_date", "volume", "status", "created_at"]
 
     client = serializers.SlugRelatedField(read_only=True, slug_field="name")
+
+
+def export_tickets_to_excel(tickets):
+    today = datetime.datetime.today()
+    location = "/tmp/carbure_saf_tickets_%s.xlsx" % (today.strftime("%Y%m%d_%H%M"))
+
+    return export_to_excel(
+        location,
+        [
+            {
+                "label": "tickets",
+                "rows": SafTicketDetailsSerializer(tickets, many=True).data,
+                "columns": [
+                    {"label": "carbure_id", "value": "carbure_id"},
+                    {"label": "year", "value": "year"},
+                    {"label": "assignment_period", "value": "assignment_period"},
+                    {"label": "agreement_reference", "value": "agreement_reference"},
+                    {"label": "agreement_date", "value": "agreement_date"},
+                    {"label": "volume", "value": "volume"},
+                    {"label": "biofuel", "value": "biofuel.name"},
+                    {"label": "feedstock", "value": "feedstock.name"},
+                    {"label": "country_of_origin", "value": "country_of_origin.name"},
+                    {"label": "supplier", "value": "supplier"},
+                    {"label": "client", "value": "client"},
+                    {"label": "producer", "value": get_producer},
+                    {"label": "production_site", "value": get_production_site},
+                    {"label": "production_site_commissioning_date", "value": "production_site_commissioning_date"},
+                    {"label": "eec", "value": "eec"},
+                    {"label": "el", "value": "el"},
+                    {"label": "ep", "value": "ep"},
+                    {"label": "etd", "value": "etd"},
+                    {"label": "eu", "value": "eu"},
+                    {"label": "esca", "value": "esca"},
+                    {"label": "eccs", "value": "eccs"},
+                    {"label": "eccr", "value": "eccr"},
+                    {"label": "eee", "value": "eee"},
+                    {"label": "ghg_total", "value": "ghg_total"},
+                    {"label": "ghg_reference", "value": "ghg_reference"},
+                    {"label": "ghg_reduction", "value": "ghg_reduction"},
+                    {"label": "free_field", "value": "free_field"},
+                ],
+            }
+        ],
+    )
+
+
+def get_producer(obj):
+    if "carbure_producer" in obj:
+        return obj["carbure_producer"]["name"]
+    elif "unknown_producer" in obj:
+        return obj["unknown_producer"]
+
+
+def get_production_site(obj):
+    if "carbure_production_site" in obj:
+        return obj["carbure_production_site"]["name"]
+    elif "unknown_production_site" in obj:
+        return obj["unknown_production_site"]
