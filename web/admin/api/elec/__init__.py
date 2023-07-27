0f5655416e730468a8d91c952961f50f6d2c6e02
Julien Falxa <jfalxa.sta@gmail.com>
2023-07-27 17:07:55 +0200

    [api+front] add drag and drop area to upload a list of provision certificates as csv
    Co-authored-by: theo-santos <theo-santos@users.noreply.github.com>

--- web/admin/api/elec/__init__.py
+++ web/admin/api/elec/__init__.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
+
 from .years import get_years
 from .snapshot import get_snapshot
+from admin.api.elec.import_certificates import import_certificate_excel
 
 urlpatterns = [
     path("years", get_years, name="admin-elec-years"),
     path("snapshot", get_snapshot, name="admin-elec-snapshot"),
+    path("import-certificates", import_certificate_excel, name="admin-elec-import-certificates"),
 ]
