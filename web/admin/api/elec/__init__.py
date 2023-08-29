9ff2851f2b11ed8960fb4c0ef9d6ddde17ff264f
Julien Falxa <jfalxa.sta@gmail.com>
2023-08-29 15:55:56 +0200

    [api] add filter listing endpoint for elec provision certificates

--- web/admin/api/elec/__init__.py
+++ web/admin/api/elec/__init__.py
@@ -1,14 +1,19 @@
 from django.urls import path, include
 
+from admin.api.elec.provision_certificate_filters import get_provision_certificate_filters
+
 
 from .years import get_years
 from .snapshot import get_snapshot
-from .import_certificates import import_certificate_excel
+from .import_provision_certificates import import_provision_certificate_excel
 from .provision_certificates import get_provision_certificates
 
 urlpatterns = [
     path("years", get_years, name="admin-elec-years"),
     path("snapshot", get_snapshot, name="admin-elec-snapshot"),
-    path("import-certificates", import_certificate_excel, name="admin-elec-import-certificates"),
+    path("import-provision-certificates", import_provision_certificate_excel, name="admin-elec-import-provision-certs"),
     path("provision-certificates", get_provision_certificates, name="admin-elec-get-provision-certificates"),
+    path(
+        "provision-certificate-filters", get_provision_certificate_filters, name="admin-elec-provision-certificates-filters"
+    ),
 ]
