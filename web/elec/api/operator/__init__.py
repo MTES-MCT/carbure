321e663a6a88a2f5680ee8baa5ebbe36140c01c0
Julien Falxa <jfalxa.sta@gmail.com>
2023-08-30 18:39:22 +0200

    [api] elec: fix endpoint paths

--- web/elec/api/operator/__init__.py
+++ web/elec/api/operator/__init__.py
@@ -3,5 +3,5 @@ from .snapshot import get_snapshot
 
 urlpatterns = [
     # overview
-    path("snapshot", get_snapshot, name="elec-snapshot"),
+    path("snapshot", get_snapshot, name="elec-operator-snapshot"),
 ]
