789c9291fcc98f86bf7fd5fbdbf1ba8094a2c373
Julien Falxa <jfalxa.sta@gmail.com>
2023-09-05 11:03:43 +0200

    [api] add endpoint to reject transfer certificate as operator

--- web/elec/api/operator/__init__.py
+++ web/elec/api/operator/__init__.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from .snapshot import get_snapshot
+from .reject_transfer_certificate import reject_transfer_certificate
 
 urlpatterns = [
     # overview
     path("snapshot", get_snapshot, name="elec-operator-snapshot"),
+    path("reject-transfer-certificate", reject_transfer_certificate, name="elec-operator-reject-transfer-certificate"),
 ]
