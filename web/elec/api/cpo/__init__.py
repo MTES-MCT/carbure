4a43079dcf277e726b1ab45518146fc4f285c7e0
Julien Falxa <jfalxa.sta@gmail.com>
2023-09-05 11:03:19 +0200

    [api] add endpoint to cancel transfer certificate as CPO
    Co-authored-by: theo-santos <theo-santos@users.noreply.github.com>

--- web/elec/api/cpo/__init__.py
+++ web/elec/api/cpo/__init__.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
+
 from .snapshot import get_snapshot
 from .transfer_provision_certificate import transfer_provision_certificate
+from .cancel_transfer_certificate import cancel_transfer_certificate
 
 urlpatterns = [
     # overview
     path("snapshot", get_snapshot, name="elec-cpo-snapshot"),
     path("transfer-provision-certificate", transfer_provision_certificate, name="elec-cpo-transfer-provision-certificate"),
+    path("cancel-transfer-certificate", cancel_transfer_certificate, name="elec-cpo-cancel-transfer-certificate"),
 ]
