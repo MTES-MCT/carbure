41b942de822a92733d0857e0769510f81968080b
Julien Falxa <jfalxa.sta@gmail.com>
2023-09-07 00:59:43 +0200

    [api] elec: add transfer cert filter endpoint for cpo

--- web/elec/api/cpo/transfer_certificate_filters.py
+++ web/elec/api/cpo/transfer_certificate_filters.py
@@ -1,25 +1,26 @@
+from django.views.decorators.http import require_GET
+from admin.api.elec.transfer_certificate_filters import CertificateFilterError
 from core.common import ErrorResponse, SuccessResponse
-from core.decorators import check_admin_rights
+from core.decorators import check_user_rights
 from core.models import ExternalAdminRights
 from elec.models.elec_transfer_certificate import ElecTransferCertificate
 from .transfer_certificates import TransferCertificatesFilterForm, find_transfer_certificates
 
 
-class CertificateFilterError:
-    BAD_FILTER = "BAD_FILTER"
-
-
-@check_admin_rights(allow_external=[ExternalAdminRights.ELEC])
+@require_GET
+@check_user_rights(allow_external=[ExternalAdminRights.ELEC])
 def get_transfer_certificate_filters(request):
     filters = TransferCertificatesFilterForm(request.GET)
-    current_filter = request.GET.get("filter")
 
     if not filters.is_valid():
         return ErrorResponse(400, CertificateFilterError.BAD_FILTER, filters.errors)
 
+    entity_id = request.GET.get("entity_id")
+    current_filter = request.GET.get("filter")
+
     filters.cleaned_data[current_filter] = None
 
-    transfer_certificates = ElecTransferCertificate.objects.all()
+    transfer_certificates = ElecTransferCertificate.objects.filter(supplier_id=entity_id)
     transfer_certificates = find_transfer_certificates(transfer_certificates, **filters.cleaned_data)
 
     remaining_filter_values = transfer_certificates.values_list(filter_to_column[current_filter], flat=True).distinct()
