58b51617af01eb4a8cf8d66d769ffe63f4e29579
Julien Falxa <jfalxa.sta@gmail.com>
2023-09-06 20:57:59 +0200

    [api] elec: allow operator to accept transfer certificate

--- web/elec/api/operator/__init__.py
+++ web/elec/api/operator/__init__.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from .snapshot import get_snapshot
+from .accept_transfer_certificate import accept_transfer_certificate
 from .reject_transfer_certificate import reject_transfer_certificate
 
 urlpatterns = [
     # overview
     path("snapshot", get_snapshot, name="elec-operator-snapshot"),
+    path("accept-transfer-certificate", accept_transfer_certificate, name="elec-operator-accept-transfer-certificate"),
     path("reject-transfer-certificate", reject_transfer_certificate, name="elec-operator-reject-transfer-certificate"),
 ]
