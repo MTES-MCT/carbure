a853e37f0a859dadad43ed080f6102a45b07d275
Julien Falxa <jfalxa.sta@gmail.com>
2023-09-07 00:59:23 +0200

    [api] elec: add provision cert filter endpoint for cpo

--- web/elec/api/cpo/provision_certificate_filters.py
+++ web/elec/api/cpo/provision_certificate_filters.py
@@ -1,29 +1,29 @@
+from django.views.decorators.http import require_GET
+from admin.api.elec.provision_certificate_filters import CertificateFilterError
 from core.common import ErrorResponse, SuccessResponse
-from core.decorators import check_admin_rights
-from core.models import ExternalAdminRights
+from core.decorators import check_user_rights
 from elec.models.elec_provision_certificate import ElecProvisionCertificate
 from .provision_certificates import ProvisionCertificatesFilterForm, find_provision_certificates
 
 
-class CertificateFilterError:
-    BAD_FILTER = "BAD_FILTER"
-
-
-@check_admin_rights(allow_external=[ExternalAdminRights.ELEC])
+@require_GET
+@check_user_rights()
 def get_provision_certificate_filters(request):
     filters = ProvisionCertificatesFilterForm(request.GET)
-    current_filter = request.GET.get("filter")
 
     if not filters.is_valid():
         return ErrorResponse(400, CertificateFilterError.BAD_FILTER, filters.errors)
+
+    entity_id = request.GET.get("entity_id")
+    current_filter = request.GET.get("filter")
 
     if current_filter == "status":
         return SuccessResponse({"filter_values": ["FULL", "AVAILABLE", "EMPTY"]})
 
     filters.cleaned_data[current_filter] = None
 
-    provision_certificates = ElecProvisionCertificate.objects.all()
-    provision_certificates = find_provision_certificates(provision_certificates, **filters.cleaned_data)
+    provision_certificates = ElecProvisionCertificate.objects.filter(cpo_id=entity_id)
+    provision_certificates = find_provision_certificates(**filters.cleaned_data)
 
     remaining_filter_values = provision_certificates.values_list(filter_to_column[current_filter], flat=True).distinct()
 
