3134f20d56a9f82652fb7537530f539ed10dcd05
Julien Falxa <jfalxa.sta@gmail.com>
2023-09-06 21:49:37 +0200

    [api] rename transfer-provision-certificate into create-transfer-certificate

--- web/elec/api/cpo/__init__.py
+++ web/elec/api/cpo/__init__.py
@@ -2,12 +2,12 @@ from django.urls import path
 
 
 from .snapshot import get_snapshot
-from .transfer_provision_certificate import transfer_provision_certificate
+from .create_transfer_certificate import create_transfer_certificate
 from .cancel_transfer_certificate import cancel_transfer_certificate
 
 urlpatterns = [
     # overview
     path("snapshot", get_snapshot, name="elec-cpo-snapshot"),
-    path("transfer-provision-certificate", transfer_provision_certificate, name="elec-cpo-transfer-provision-certificate"),
+    path("create-transfer-certificate", create_transfer_certificate, name="elec-cpo-create-transfer-certificate"),
     path("cancel-transfer-certificate", cancel_transfer_certificate, name="elec-cpo-cancel-transfer-certificate"),
 ]
