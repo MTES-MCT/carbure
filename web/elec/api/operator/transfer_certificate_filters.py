65277a58bf29c4cfc80428994aa0a41be619d395
Julien Falxa <jfalxa.sta@gmail.com>
2023-09-12 15:17:48 +0200

    [api] elec: add operator endpoints
    Co-authored-by: theo-santos <theo-santos@users.noreply.github.com>

---
+++ web/elec/api/operator/transfer_certificate_filters.py
@@ -0,0 +1,39 @@
+from django.views.decorators.http import require_GET
+from admin.api.elec.transfer_certificate_filters import CertificateFilterError
+from core.common import ErrorResponse, SuccessResponse
+from core.decorators import check_user_rights
+from elec.models.elec_transfer_certificate import ElecTransferCertificate
+from .transfer_certificates import TransferCertificatesFilterForm, find_transfer_certificates
+
+
+@require_GET
+@check_user_rights()
+def get_transfer_certificate_filters(request, *args, **kwargs):
+    filters = TransferCertificatesFilterForm(request.GET)
+
+    if not filters.is_valid():
+        return ErrorResponse(400, CertificateFilterError.BAD_FILTER, filters.errors)
+
+    entity_id = request.GET.get("entity_id")
+    current_filter = request.GET.get("filter")
+
+    if current_filter is None:
+        return ErrorResponse(400, CertificateFilterError.MISSING_FILTER)
+
+    filters.cleaned_data[current_filter] = None
+
+    transfer_certificates = ElecTransferCertificate.objects.filter(client_id=entity_id)
+    transfer_certificates = find_transfer_certificates(transfer_certificates, **filters.cleaned_data)
+
+    remaining_filter_values = transfer_certificates.values_list(filter_to_column[current_filter], flat=True).distinct()
+
+    return SuccessResponse({"filter_values": list(remaining_filter_values)})
+
+
+filter_to_column = {
+    "year": "transfer_date__year",
+    "transfer_date": "transfer_date",
+    "cpo": "supplier__name",
+    "operator": "client__name",
+    "certificate_id": "certificate_id",
+}
