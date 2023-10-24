08bb0bde7cb2cdf8fc45075c0f7c3d719128fced
Julien Falxa <jfalxa.sta@gmail.com>
2023-10-24 20:57:26 +0200

    [udb] more tries

--- udb/setenv.sh
+++ udb/setenv.sh
@@ -2,4 +2,4 @@
 # script to setup env variables for domibus
 
 export CATALINA_HOME=/app/domibus
-JAVA_OPTS="$JAVA_OPTS -Xms128m -Xmx2048m -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
+JAVA_OPTS="$JAVA_OPTS -Xms128m -Xmx1024m -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
