8ee2455344c5633d3c32cdb96a49b60d46854fb3
Julien Falxa <jfalxa.sta@gmail.com>
2023-10-31 11:58:58 +0100

    [udb] customizable domibus path

--- udb/setup/setenv.sh
+++ udb/setup/setenv.sh
@@ -4,5 +4,5 @@
 # This script is supposed to be copied directly inside the domibus/bin directory.
 # It should follow the template provided by Domibus (at domibus/bin/setenv.sh)
 
-export CATALINA_HOME=/app/domibus
+export CATALINA_HOME=$DOMIBUS_PATH
 JAVA_OPTS="$JAVA_OPTS -Xms128m -Xmx1024m -Ddomibus.config.location=$CATALINA_HOME/conf/domibus"
