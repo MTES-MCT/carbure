36c6acf60d6cba85d586c20ce26efc563009720b
Julien Falxa <jfalxa.sta@gmail.com>
2023-11-23 18:41:46 +0100

    [api] add endpoint to list an entity's charge point applications

--- web/elec/api/cpo/charge_points/__init__.py
+++ web/elec/api/cpo/charge_points/__init__.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+from .applications import get_applications
 from .check_application import check_application
 from .add_application import add_application
 
 urlpatterns = [
+    path("applications", get_applications, name="elec-cpo-charge-points-get-applications"),
     path("check-application", check_application, name="elec-cpo-charge-points-check-application"),
     path("add-application", add_application, name="elec-cpo-charge-points-add-application"),
 ]
