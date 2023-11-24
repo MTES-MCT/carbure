b90b333fc2ba709bfeda1be303b6246da426851c
Julien Falxa <jfalxa.sta@gmail.com>
2023-11-24 17:58:32 +0100

    [api] add charge point tests

--- web/elec/api/cpo/charge_points/tests_charge_points.py
+++ web/elec/api/cpo/charge_points/tests_charge_points.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from core.tests_utils import setup_current_user
 from core.models import Entity
@@ -202,3 +203,211 @@ class ElecCharginPointsTest(TestCase):
         self.assertEqual(charge_points[4].reference_meter_id, "1122334459")
         self.assertEqual(charge_points[4].cpo, self.cpo)
         self.assertEqual(charge_points[4].application, application)
+
+    def test_get_applications_ok(self):
+        application = ElecChargePointApplication.objects.create(cpo=self.cpo)
+        application2 = ElecChargePointApplication.objects.create(cpo=self.cpo)
+
+        charge_point = ElecChargePoint.objects.create(
+            application=application,
+            cpo=self.cpo,
+            charge_point_id="ABCDE",
+            current_type="AC",
+            installation_date=datetime.date(2023, 2, 15),
+            lne_certificate="123-456",
+            meter_reading_date=datetime.date(2023, 6, 29),
+            meter_reading_energy=1000.1234,
+            reference_meter_id="123456",
+            station_name="Station",
+            station_id="FGHIJ",
+        )
+
+        charge_point2 = ElecChargePoint.objects.create(
+            application=application2,
+            cpo=self.cpo,
+            charge_point_id="ABCDE",
+            current_type="AC",
+            installation_date=datetime.date(2023, 2, 15),
+            lne_certificate="123-456",
+            meter_reading_date=datetime.date(2023, 6, 29),
+            meter_reading_energy=1000.1234,
+            reference_meter_id="123456",
+            station_name="Station",
+            station_id="FGHIJ",
+        )
+
+        response = self.client.get(
+            reverse("elec-cpo-charge-points-get-applications"),
+            {"entity_id": self.cpo.id},
+        )
+
+        data = response.json()
+
+        expected = {
+            "status": "success",
+            "data": [
+                {
+                    "id": application.id,
+                    "cpo": self.cpo.name,
+                    "status": "PENDING",
+                    "application_date": data["data"][0]["application_date"],  # timezone annoying stuff
+                    "station_count": 1,
+                    "charging_point_count": 1,
+                    "power_total": 1000.12,
+                },
+                {
+                    "id": application2.id,
+                    "cpo": self.cpo.name,
+                    "status": "PENDING",
+                    "application_date": data["data"][1]["application_date"],
+                    "station_count": 1,
+                    "charging_point_count": 1,
+                    "power_total": 1000.12,
+                },
+            ],
+        }
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data, expected)
+
+    def test_get_charge_points_ok(self):
+        application = ElecChargePointApplication.objects.create(cpo=self.cpo)
+        application2 = ElecChargePointApplication.objects.create(cpo=self.cpo)
+
+        charge_point = ElecChargePoint.objects.create(
+            application=application,
+            cpo=self.cpo,
+            charge_point_id="ABCDE",
+            current_type="AC",
+            installation_date=datetime.date(2023, 2, 15),
+            lne_certificate="123-456",
+            meter_reading_date=datetime.date(2023, 6, 29),
+            meter_reading_energy=1000.1234,
+            reference_meter_id="123456",
+            station_name="Station",
+            station_id="FGHIJ",
+        )
+
+        charge_point2 = ElecChargePoint.objects.create(
+            application=application2,
+            cpo=self.cpo,
+            charge_point_id="ABCDE",
+            current_type="AC",
+            installation_date=datetime.date(2023, 2, 15),
+            lne_certificate="123-456",
+            meter_reading_date=datetime.date(2023, 6, 29),
+            meter_reading_energy=1000.1234,
+            reference_meter_id="123456",
+            station_name="Station",
+            station_id="FGHIJ",
+        )
+
+        response = self.client.get(
+            reverse("elec-cpo-charge-points-get-charge-points"),
+            {"entity_id": self.cpo.id},
+        )
+
+        expected = {
+            "status": "success",
+            "data": [
+                {
+                    "id": charge_point.id,
+                    "cpo": self.cpo.name,
+                    "charge_point_id": "ABCDE",
+                    "current_type": "AC",
+                    "installation_date": "2023-02-15",
+                    "lne_certificate": "123-456",
+                    "meter_reading_date": "2023-06-29",
+                    "meter_reading_energy": 1000.12,
+                    "is_using_reference_meter": False,
+                    "is_auto_consumption": False,
+                    "has_article_4_regularization": False,
+                    "reference_meter_id": "123456",
+                    "station_name": "Station",
+                    "station_id": "FGHIJ",
+                },
+                {
+                    "id": charge_point2.id,
+                    "cpo": self.cpo.name,
+                    "charge_point_id": "ABCDE",
+                    "current_type": "AC",
+                    "installation_date": "2023-02-15",
+                    "lne_certificate": "123-456",
+                    "meter_reading_date": "2023-06-29",
+                    "meter_reading_energy": 1000.12,
+                    "is_using_reference_meter": False,
+                    "is_auto_consumption": False,
+                    "has_article_4_regularization": False,
+                    "reference_meter_id": "123456",
+                    "station_name": "Station",
+                    "station_id": "FGHIJ",
+                },
+            ],
+        }
+
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data, expected)
+
+    def test_get_application_details_ok(self):
+        application = ElecChargePointApplication.objects.create(cpo=self.cpo)
+        application2 = ElecChargePointApplication.objects.create(cpo=self.cpo)
+
+        charge_point = ElecChargePoint.objects.create(
+            application=application,
+            cpo=self.cpo,
+            charge_point_id="ABCDE",
+            current_type="AC",
+            installation_date=datetime.date(2023, 2, 15),
+            lne_certificate="123-456",
+            meter_reading_date=datetime.date(2023, 6, 29),
+            meter_reading_energy=1000.1234,
+            reference_meter_id="123456",
+            station_name="Station",
+            station_id="FGHIJ",
+        )
+
+        charge_point2 = ElecChargePoint.objects.create(
+            application=application2,
+            cpo=self.cpo,
+            charge_point_id="ABCDE",
+            current_type="AC",
+            installation_date=datetime.date(2023, 2, 15),
+            lne_certificate="123-456",
+            meter_reading_date=datetime.date(2023, 6, 29),
+            meter_reading_energy=1000.1234,
+            reference_meter_id="123456",
+            station_name="Station",
+            station_id="FGHIJ",
+        )
+
+        response = self.client.get(
+            reverse("elec-cpo-charge-points-get-application-details"),
+            {"entity_id": self.cpo.id, "application_id": application.id},
+        )
+
+        expected = {
+            "status": "success",
+            "data": [
+                {
+                    "id": charge_point.id,
+                    "cpo": self.cpo.name,
+                    "charge_point_id": "ABCDE",
+                    "current_type": "AC",
+                    "installation_date": "2023-02-15",
+                    "lne_certificate": "123-456",
+                    "meter_reading_date": "2023-06-29",
+                    "meter_reading_energy": 1000.12,
+                    "is_using_reference_meter": False,
+                    "is_auto_consumption": False,
+                    "has_article_4_regularization": False,
+                    "reference_meter_id": "123456",
+                    "station_name": "Station",
+                    "station_id": "FGHIJ",
+                }
+            ],
+        }
+
+        data = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data, expected)
