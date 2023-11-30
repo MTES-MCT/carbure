3403752e5906962b5506bac72591406a444b19f6
Julien Falxa <jfalxa.sta@gmail.com>
2023-11-30 19:26:52 +0100

    [api] rename some field on charge point model

--- web/elec/serializers/elec_charge_point.py
+++ web/elec/serializers/elec_charge_point.py
@@ -12,19 +12,19 @@ class ElecChargePointSerializer(serializers.ModelSerializer):
             "charge_point_id",
             "current_type",
             "installation_date",
-            "lne_certificate",
-            "meter_reading_date",
-            "meter_reading_energy",
-            "is_using_reference_meter",
+            "lne_id",
+            "measure_date",
+            "measure_energy",
+            "is_article_2",
             "is_auto_consumption",
-            "has_article_4_regularization",
-            "reference_meter_id",
+            "is_article_4",
+            "measure_reference_point_id",
             "station_name",
             "station_id",
         ]
 
     cpo = serializers.SlugRelatedField(read_only=True, slug_field="name")
-    meter_reading_energy = serializers.SerializerMethodField()
+    measure_energy = serializers.SerializerMethodField()
 
-    def get_meter_reading_energy(self, instance):
-        return round(instance.meter_reading_energy, 2)
+    def get_measure_energy(self, instance):
+        return round(instance.measure_energy, 2)
