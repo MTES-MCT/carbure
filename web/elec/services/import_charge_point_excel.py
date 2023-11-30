19919e9a25f3af3d11c7ab1f23472e17005f71fc
Julien Falxa <jfalxa.sta@gmail.com>
2023-11-30 19:27:51 +0100

    [api] add validation rules to charge point excel import

--- web/elec/services/import_charge_point_excel.py
+++ web/elec/services/import_charge_point_excel.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 import requests
 import pandas as pd
 from dateutil.parser import isoparse
@@ -8,85 +9,12 @@ from core.excel import ExcelParser
 
 def import_charge_point_excel(excel_file: UploadedFile):
     try:
-        excel_data = parse_charge_point_excel(excel_file)
-        transport_data = download_charge_point_data()
-        return validate_charge_points(excel_data, transport_data)
+        transport_data = TransportDataGouv.get_charge_point_data()
+        excel_data = ExcelChargePoints.parse_charge_point_excel(excel_file)
+        return ExcelChargePoints.validate_charge_points(excel_data, transport_data)
     except Exception as e:
+        traceback.print_exc()
         return [], [{"error": str(e)}]
-
-
-def parse_charge_point_excel(excel_file: UploadedFile):
-    EXCEL_COLUMNS = {
-        "charge_point_id": ExcelParser.id,
-        "current_type": ExcelParser.str,
-        "installation_date": ExcelParser.date,
-        "lne_certificate": ExcelParser.id,
-        "meter_reading_date": ExcelParser.date,
-        "meter_reading_energy": ExcelParser.float,
-        "is_using_reference_meter": ExcelParser.bool,
-        "is_auto_consumption": ExcelParser.bool,
-        "has_article_4_regularization": ExcelParser.bool,
-        "reference_meter_id": ExcelParser.id,
-    }
-
-    charge_point_data = pd.read_excel(excel_file, skiprows=11, usecols=list(range(1, 11)))
-    charge_point_data.rename(columns={charge_point_data.columns[i]: column for i, column in enumerate(EXCEL_COLUMNS)}, inplace=True)  # fmt: skip
-    charge_point_data.fillna("", inplace=True)
-
-    for column, parser in EXCEL_COLUMNS.items():
-        charge_point_data[column] = charge_point_data[column].apply(parser)
-
-    charge_points = charge_point_data.to_dict(orient="records")
-
-    # default template example cells
-    first_id = charge_points[0]["charge_point_id"]
-    eighteenth_id = charge_points[17]["charge_point_id"]
-
-    # the example was left in the template, so we skip it
-    if first_id == "FRUEXESTATION1P1" and eighteenth_id == "FRUEXESTATION4P4":
-        charge_points = charge_points[22:]
-
-    return charge_points
-
-
-def download_charge_point_data():
-    URL = "https://www.data.gouv.fr/api/1/datasets/5448d3e0c751df01f85d0572/"
-    api_response = requests.get(URL)
-
-    if api_response.status_code != 200:
-        raise Exception("Dataset was not found on data.gouv.fr API")
-
-    data = api_response.json()
-
-    dataset_url: str = data["resources"][0]["latest"]
-    last_modified = isoparse(data["resources"][0]["last_modified"])
-    file_path = f"/tmp/transport_charge_points_{last_modified:%Y-%m-%d_%H-%M-%S}.csv"
-
-    # cache the csv to the filesystem to avoid refetching it every time
-    if not os.path.exists(file_path):
-        csv_response = requests.get(dataset_url)
-        with open(file_path, "wb") as file:
-            file.write(csv_response.content)
-
-    EXCEL_COLUMNS = [
-        "id_station_itinerance",
-        "nom_station",
-        "id_pdc_itinerance",
-    ]
-
-    ALIAS = {
-        "id_pdc_itinerance": "charge_point_id",
-        "id_station_itinerance": "station_id",
-        "nom_station": "station_name",
-    }
-
-    charge_point_data = pd.read_csv(file_path, sep=",", header=0, usecols=EXCEL_COLUMNS)  # pyright: ignore
-    charge_point_data.rename(columns=ALIAS, inplace=True)
-    charge_point_data.fillna("", inplace=True)
-
-    data = charge_point_data.to_dict(orient="records")
-
-    return data
 
 
 class ExcelChargePointError:
@@ -94,40 +22,184 @@ class ExcelChargePointError:
     MISSING_CHARGING_POINT_DATA = "MISSING_CHARGING_POINT_DATA"
 
 
-def validate_charge_points(charge_points, transport_data):
-    transport_data_index = {row["charge_point_id"]: row for row in transport_data}
+class ExcelChargePoints:
+    @staticmethod
+    def parse_charge_point_excel(excel_file: UploadedFile):
+        EXCEL_COLUMNS = {
+            "charge_point_id": ExcelParser.id,
+            "current_type": ExcelParser.str,
+            "installation_date": ExcelParser.date,
+            "lne_id": ExcelParser.id,
+            "measure_date": ExcelParser.date,
+            "measure_energy": ExcelParser.float,
+            "is_article_2": ExcelParser.bool,
+            "is_auto_consumption": ExcelParser.bool,
+            "is_article_4": ExcelParser.bool,
+            "measure_reference_point_id": ExcelParser.id,
+        }
 
-    missing_charge_points = []
-    invalid_charge_points = []
-    valid_charge_points = []
+        charge_point_data = pd.read_excel(excel_file, skiprows=11, usecols=list(range(1, 11)))
+        charge_point_data["line"] = charge_point_data.index + 11 + 2
+        charge_point_data.rename(columns={charge_point_data.columns[i]: column for i, column in enumerate(EXCEL_COLUMNS)}, inplace=True)  # fmt: skip
+        charge_point_data.fillna("", inplace=True)
 
-    for charge_point_data in charge_points:
-        is_valid = True
-        charge_point_transport_data = transport_data_index.get(charge_point_data["charge_point_id"], {})
+        for column, parser in EXCEL_COLUMNS.items():
+            charge_point_data[column] = charge_point_data[column].apply(parser)
 
-        if len(charge_point_transport_data) == 0:
-            is_valid = False
-            missing_charge_points.append(charge_point_data)
+        charge_points = charge_point_data.to_dict(orient="records")
 
-        if not is_charge_point_valid(charge_point_data):
-            is_valid = False
-            invalid_charge_points.append(charge_point_data)
+        if len(charge_points) >= 18:
+            # default template example cells
+            first_id = charge_points[0]["charge_point_id"]
+            eighteenth_id = charge_points[17]["charge_point_id"]
 
-        if is_valid:
-            charge_point_data = {**charge_point_transport_data, **charge_point_data}
-            valid_charge_points.append(charge_point_data)
+            # the example was left in the template, so we skip it
+            if first_id == "FRUEXESTATION1P1" and eighteenth_id == "FRUEXESTATION4P4":
+                charge_points = charge_points[22:]
 
-    errors = []
-    if len(missing_charge_points) > 0:
-        meta = [charge_point["charge_point_id"] for charge_point in missing_charge_points if charge_point["charge_point_id"]]
-        errors.append({"error": ExcelChargePointError.MISSING_CHARGING_POINT_IN_DATAGOUV, "meta": meta})
-    if len(invalid_charge_points) > 0:
-        meta = [charge_point["charge_point_id"] for charge_point in invalid_charge_points if charge_point["charge_point_id"]]
-        errors.append({"error": ExcelChargePointError.MISSING_CHARGING_POINT_DATA, "meta": meta})
+        return charge_points
 
-    return valid_charge_points, errors
+    @staticmethod
+    def validate_charge_points(charge_points: list[dict], transport_data):
+        valid_charge_points = []
+        charge_points_errors = []
+
+        transport_data_index = {row["charge_point_id"]: row for row in transport_data}
+
+        for charge_point_data in charge_points:
+            line = charge_point_data.pop("line")
+            charge_point_transport_data = transport_data_index.get(charge_point_data["charge_point_id"])
+
+            errors = []
+
+            if charge_point_transport_data is None:
+                error = {"line": line, "error": ExcelChargePointError.MISSING_CHARGING_POINT_IN_DATAGOUV, "meta": charge_point_data["charge_point_id"]}  # fmt:skip
+                errors.append(error)
+            else:
+                charge_point_data["station_id"] = charge_point_transport_data["station_id"]
+                charge_point_data["station_name"] = charge_point_transport_data["station_name"]
+
+                # on override la valeur du excel si transport.data.gouv indique que le point de charge est éligible article 2
+                if charge_point_transport_data["should_be_article_2"]:
+                    charge_point_data["is_article_2"] = True
+
+            missing_fields = list(ExcelChargePoints.validate_charge_point_fields(charge_point_data))
+
+            if len(missing_fields) > 0:
+                errors.append({"line": line, "error": ExcelChargePointError.MISSING_CHARGING_POINT_DATA, "meta": missing_fields})  # fmt:skip
+
+            if len(errors) > 0:
+                charge_points_errors += errors
+            else:
+                valid_charge_points.append(charge_point_data)
+
+        return valid_charge_points, charge_points_errors
+
+    @staticmethod
+    def validate_charge_point_fields(charge_point):
+        if charge_point.get("is_article_2"):
+            if not charge_point.get("measure_reference_point_id"):
+                yield "measure_reference_point_id"
+        else:
+            if not charge_point.get("lne_id"):
+                yield "lne_id"
+            elif not charge_point.get("measure_date"):
+                yield "measure_date"
+            elif not charge_point.get("measure_energy"):
+                yield "measure_energy"
 
 
-def is_charge_point_valid(charge_point):
-    # @TODO validate field values
-    return True
+class TransportDataGouv:
+    URL = "https://www.data.gouv.fr/api/1/datasets/5448d3e0c751df01f85d0572/"
+
+    CSV_COLUMNS = [
+        "id_station_itinerance",
+        "nom_station",
+        "id_pdc_itinerance",
+        "coordonneesXY",
+        "puissance_nominale",
+        "prise_type_combo_ccs",
+        "prise_type_chademo",
+        "date_maj",
+        "last_modified",
+    ]
+
+    CSV_COLUMNS_ALIAS = {
+        "id_pdc_itinerance": "charge_point_id",
+        "id_station_itinerance": "station_id",
+        "nom_station": "station_name",
+    }
+
+    @staticmethod
+    def get_charge_point_data():
+        file_path = TransportDataGouv.download_csv()
+
+        charge_point_data = pd.read_csv(
+            file_path,
+            sep=",",
+            header=0,
+            usecols=TransportDataGouv.CSV_COLUMNS,
+            parse_dates=["last_modified", "date_maj"],
+            dayfirst=False,
+            encoding="utf-8",
+            engine="python",
+        )
+
+        charge_point_data = charge_point_data.rename(columns=TransportDataGouv.CSV_COLUMNS_ALIAS)
+        charge_point_data = TransportDataGouv.process_charge_point_data(charge_point_data)
+
+        return charge_point_data.to_dict(orient="records")
+
+    @staticmethod
+    def download_csv():
+        api_response = requests.get(TransportDataGouv.URL)
+
+        if api_response.status_code != 200:
+            raise Exception("Dataset was not found on data.gouv.fr API")
+
+        data = api_response.json()
+
+        dataset_url: str = data["resources"][0]["latest"]
+        last_modified = isoparse(data["resources"][0]["last_modified"])
+        file_path = f"/tmp/transport_charge_points_{last_modified:%Y-%m-%d_%H-%M-%S}.csv"
+
+        # cache the csv to the filesystem to avoid refetching it every time
+        if not os.path.exists(file_path):
+            csv_response = requests.get(dataset_url)
+            with open(file_path, "wb") as file:
+                file.write(csv_response.content)
+
+        return file_path
+
+    @staticmethod
+    def process_charge_point_data(charge_point_data: pd.DataFrame):
+        charge_point_data = charge_point_data.sort_values("date_maj", ascending=False)
+        charge_point_data = charge_point_data.drop_duplicates("charge_point_id")
+        charge_point_data = charge_point_data.fillna("")
+
+        # On ne se sert pas de ces données pour le moment.
+        #
+        # lon = [float(coord.replace("[", "").replace("]", "").split(",")[0]) for coord in charge_point_data.coordonneesXY]
+        # lat = [float(coord.replace("[", "").replace("]", "").split(",")[1]) for coord in charge_point_data.coordonneesXY]
+        #
+        # charge_point_data["lat"] = lat
+        # charge_point_data["lon"] = lon
+        # charge_point_data["ux"] = charge_point_data["charge_point_id"].str[:5]
+        #
+        # charge_point_data.loc[charge_point_data.puissance_nominale > 1000, "puissance_nominale"] = (
+        #     charge_point_data.loc[charge_point_data.puissance_nominale > 1000, "puissance_nominale"] / 1000
+        # )
+
+        # On détermine pour chaque point de charge s'il utilise du DC
+        charge_point_data["prise_type_combo_ccs"] = (charge_point_data["prise_type_combo_ccs"].str.lower() == "true") | (charge_point_data["prise_type_combo_ccs"] == "1")  # fmt:skip
+        charge_point_data["prise_type_chademo"] = (charge_point_data["prise_type_chademo"].str.lower() == "true") | (charge_point_data["prise_type_chademo"] == "1")  # fmt:skip
+        charge_point_data.insert(0, "DC", charge_point_data["prise_type_combo_ccs"] | charge_point_data["prise_type_chademo"])  # fmt:skip
+
+        # On regarde les stations qui comprennent un point de recharge DC
+        stations_art2 = charge_point_data[["charge_point_id", "DC"]].groupby("charge_point_id").max()
+        stations_art2 = stations_art2.rename(columns={"DC": "should_be_article_2"})
+
+        # On marque tous les points de charge de ces stations comme éligible article 2
+        return charge_point_data.merge(stations_art2, on="charge_point_id")[
+            ["charge_point_id", "should_be_article_2", "station_name", "station_id"]
+        ]
