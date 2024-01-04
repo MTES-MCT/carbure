4b787d6499c350cdda60b0f0265ff5718b89ce9c
Julien Falxa <jfalxa.sta@gmail.com>
2024-01-04 11:00:06 +0100

    [api] add models for meter reading

---
+++ web/elec/models/elec_meter_reading_application.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from core.models import Entity
+
+
+class ElecMeterReadingApplication(models.Model):
+    class Meta:
+        db_table = "elec_meter_reading_application"
+        verbose_name = "Dossier de relevés de points de recharge"
+        verbose_name_plural = "Dossiers de relevés de points de recharge"
+
+    PENDING = "PENDING"
+    ACCEPTED = "ACCEPTED"
+    REJECTED = "REJECTED"
+    STATUSES = [(PENDING, PENDING), (ACCEPTED, ACCEPTED), (REJECTED, REJECTED)]
+
+    QUARTERS = (
+        (1, "T1"),
+        (2, "T2"),
+        (3, "T3"),
+        (4, "T4"),
+    )
+
+    status = models.CharField(max_length=10, default=PENDING, choices=STATUSES)
+    quarter = models.IntegerField(choices=QUARTERS)
+    year = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    cpo = models.ForeignKey(Entity, on_delete=models.deletion.CASCADE, related_name="elec_meter_reading_applications")
