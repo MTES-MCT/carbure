04052f6caebfef8bcfe336e181701742190e65e7
Damien Romito <damien@maracuja.ac>
2024-01-05 10:40:50 +0100

    [back]clean ISCC scope parsing fix

--- web/carbure/scripts/tests_scripts.py
+++
@@ -1,8 +0,0 @@
-# test with : python web/manage.py test carbure.scripts.tests_scripts.ScriptsTest --keepdb
-from carbure.scripts.update_iscc_certificates import update_iscc_certificates
-from django.test import TestCase
-
-# UNCOMMENT THIS TO TEST THE SCRIPTS
-# class ScriptsTest(TestCase):
-#     def test_update_iscc_certificates(self):
-#         update_iscc_certificates(batch=1, test=True)
