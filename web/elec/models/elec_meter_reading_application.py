cf8f32e3c328bd1cad1239d3278a64d4e9b69e8a
Julien Falxa <jfalxa.sta@gmail.com>
2024-01-09 19:35:19 +0100

    [api] add admin controls for meter readings

--- web/elec/models/elec_meter_reading_application.py
+++ web/elec/models/elec_meter_reading_application.py
@@ -6,8 +6,8 @@ from core.models import Entity
 class ElecMeterReadingApplication(models.Model):
     class Meta:
         db_table = "elec_meter_reading_application"
-        verbose_name = "Dossier de relevés de points de recharge"
-        verbose_name_plural = "Dossiers de relevés de points de recharge"
+        verbose_name = "Inscription de relevés de points de recharge"
+        verbose_name_plural = "Inscriptions de relevés de points de recharge"
 
     PENDING = "PENDING"
     ACCEPTED = "ACCEPTED"
