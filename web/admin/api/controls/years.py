477e068103711e80a6ff21ea57fdbb9de2d73a3f
Julien Falxa <jfalxa.sta@gmail.com>
2024-01-11 12:28:08 +0100

    [api] fix wrong deleted and draft years being shown for lots and stocks

--- web/admin/api/controls/years.py
+++ web/admin/api/controls/years.py
@@ -1,16 +1,23 @@
-from core.decorators import is_admin
-from core.models import (
-    CarbureLot,
-    CarbureStockTransformation,
-)
+from core.common import SuccessResponse
+from core.decorators import check_admin_rights
+from core.models import CarbureLot, CarbureStockTransformation
 from django.http.response import JsonResponse
 
 
-@is_admin
-def get_years(request, *args, **kwargs):
-    data_lots = CarbureLot.objects.values_list("year", flat=True).distinct()
-    data_transforms = CarbureStockTransformation.objects.values_list(
-        "transformation_dt__year", flat=True
-    ).distinct()
-    data = set(list(data_transforms) + list(data_lots))
-    return JsonResponse({"status": "success", "data": list(data)})
+@check_admin_rights()
+def get_years(request):
+    lots_years = (
+        CarbureLot.objects.exclude(lot_status__in=[CarbureLot.DRAFT, CarbureLot.DELETED])
+        .values_list("year", flat=True)
+        .distinct()
+    )
+
+    transforms_years = (
+        CarbureStockTransformation.objects.select_related("source_stock__parent_lot")
+        .exclude(source_stock__parent_lot__lot_status__in=[CarbureLot.DRAFT, CarbureLot.DELETED])
+        .values_list("transformation_dt__year", flat=True)
+        .distinct()
+    )
+
+    years = list(set(list(lots_years) + list(transforms_years)))
+    return SuccessResponse(years)
--- web/transactions/api/years.py
+++ web/transactions/api/years.py
@@ -1,27 +1,26 @@
-from django.http.response import JsonResponse
 from django.db.models.query_utils import Q
+from core.common import SuccessResponse
 from core.decorators import check_user_rights
-
-from core.models import (
-    CarbureLot,
-    CarbureStockTransformation,
-)
+from core.models import CarbureLot, CarbureStockTransformation
 
 
 @check_user_rights()
-def get_years(request, *args, **kwargs):
-    entity_id = int(kwargs["context"]["entity_id"])
-    data_lots = (
-        CarbureLot.objects.filter(
-            Q(carbure_client_id=entity_id) | Q(carbure_supplier_id=entity_id) | Q(added_by_id=entity_id)
-        )
+def get_years(request, entity_id):
+    lots_years = (
+        CarbureLot.objects.exclude(lot_status=CarbureLot.DELETED)
+        .exclude(Q(lot_status=CarbureLot.DRAFT) & ~Q(added_by_id=entity_id))  # only ignore drafts created by other entities
+        .filter(Q(carbure_client_id=entity_id) | Q(carbure_supplier_id=entity_id) | Q(added_by_id=entity_id))
         .values_list("year", flat=True)
         .distinct()
     )
-    data_transforms = (
-        CarbureStockTransformation.objects.filter(entity_id=entity_id)
+
+    transforms_years = (
+        CarbureStockTransformation.objects.select_related("source_stock__parent_lot")
+        .exclude(source_stock__parent_lot__lot_status__in=[CarbureLot.DRAFT, CarbureLot.DELETED])
+        .filter(entity_id=entity_id)
         .values_list("transformation_dt__year", flat=True)
         .distinct()
     )
-    data = set(list(data_transforms) + list(data_lots))
-    return JsonResponse({"status": "success", "data": list(data)})
+
+    years = list(set(list(lots_years) + list(transforms_years)))
+    return SuccessResponse(list(years))
