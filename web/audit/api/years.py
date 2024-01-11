82ec2fd6a81d4578ac183a973d8e2da421b67029
Julien Falxa <jfalxa.sta@gmail.com>
2024-01-11 12:28:08 +0100

    [api] fix wrong deleted and draft years being shown for lots and stocks

--- web/audit/api/years.py
+++ web/audit/api/years.py
@@ -1,18 +1,32 @@
-from django.http.response import JsonResponse
-from audit.helpers import get_auditor_lots
-from core.decorators import check_user_rights, is_auditor
-
-from core.models import (
-    CarbureStockTransformation,
-)
+from django.db.models.query_utils import Q
+from audit.helpers import get_audited_entities
+from core.common import SuccessResponse
+from core.decorators import check_user_rights
+from core.models import CarbureLot, CarbureStockTransformation, Entity
 
 
-@check_user_rights()
-@is_auditor
-def get_years(request, *args, **kwargs):
-    data_lots = get_auditor_lots(request).values_list("year", flat=True).distinct()
-    data_transforms = CarbureStockTransformation.objects.values_list(
-        "transformation_dt__year", flat=True
-    ).distinct()
-    data = set(list(data_transforms) + list(data_lots))
-    return JsonResponse({"status": "success", "data": list(data)})
+@check_user_rights(entity_type=[Entity.AUDITOR])
+def get_years(request):
+    audited_entities = get_audited_entities(request.user)
+
+    lots_years = (
+        CarbureLot.objects.exclude(lot_status__in=[CarbureLot.DRAFT, CarbureLot.DELETED])
+        .filter(
+            Q(carbure_client__in=audited_entities)
+            | Q(carbure_supplier__in=audited_entities)
+            | Q(added_by__in=audited_entities)
+        )
+        .values_list("year", flat=True)
+        .distinct()
+    )
+
+    transforms_years = (
+        CarbureStockTransformation.objects.select_related("source_stock__parent_lot")
+        .exclude(source_stock__parent_lot__lot_status__in=[CarbureLot.DRAFT, CarbureLot.DELETED])
+        .filter(entity_id__in=audited_entities)
+        .values_list("transformation_dt__year", flat=True)
+        .distinct()
+    )
+
+    years = list(set(list(lots_years) + list(transforms_years)))
+    return SuccessResponse(years)
