743f6be1da2752c0e97d12ff17f96788b089ee56
Julien Falxa <jfalxa.sta@gmail.com>
2024-01-11 13:02:55 +0100

    [api] add endpoint for admin to list all meter reading applications

---
+++ web/admin/api/elec/meter_readings/applications.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.views.decorators.http import require_GET
+from core.common import ErrorResponse, SuccessResponse
+from core.decorators import check_admin_rights
+from core.models import Entity, ExternalAdminRights
+from elec.repositories.meter_reading_repository import MeterReadingRepository
+from elec.serializers.elec_meter_reading_application_serializer import ElecMeterReadingApplicationSerializer
+
+
+class ApplicationsForm(forms.Form):
+    company_id = forms.ModelChoiceField(queryset=Entity.objects.filter(entity_type=Entity.CPO))
+
+
+class ApplicationsError:
+    MALFORMED_PARAMS = "MALFORMED_PARAMS"
+
+
+@require_GET
+@check_admin_rights(allow_external=[ExternalAdminRights.ELEC])
+def get_applications(request):
+    form = ApplicationsForm(request.GET)
+
+    if not form.is_valid():
+        return ErrorResponse(400, ApplicationsError.MALFORMED_PARAMS, form.errors)
+
+    company = form.cleaned_data["company_id"]
+
+    applications = MeterReadingRepository.get_annotated_applications(company)
+    serialized = ElecMeterReadingApplicationSerializer(applications, many=True).data
+    return SuccessResponse(serialized)
