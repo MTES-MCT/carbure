2d3b519b8812e2d014cf1090e567cd67c3c4b6d0
Damien Romito <damien@maracuja.ac>
2024-01-16 15:20:50 +0100

    [back] fix last commit

--- scripts/patches/2024-01-16_update_feedstock.py
+++ scripts/patches/2024-01-16_update_feedstock.py
@@ -1,5 +1,4 @@
 import argparse
-import code
 import os
 import django
 
@@ -7,25 +6,27 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carbure.settings")
 django.setup()
 
-from tqdm import tqdm
 from django.db import transaction
-from django.core.paginator import Paginator
-from core.models import CarbureLot, MatierePremiere
-from certificates.models import DoubleCountingRegistration
-from transactions.sanity_checks import bulk_sanity_checks, get_prefetched_data
+from core.models import MatierePremiere
 
 
 @transaction.atomic
-def rename_alcool_pur_marc_raisin():
+def rename_alcool_pur_marc_raisin(id, name, code):
     try:
-        feedstock = MatierePremiere.objects.get(id=77)
+        feedstock = MatierePremiere.objects.get(id=id)
     except:
         print(f"Feedstock {id} not found")
         return
-    feedstock.name = "Ethanol pur de marc de raisin"
-    feedstock.code = "ETHANOL_PUR_MARC_RAISIN"
+    print(f"Feedstock {feedstock.code} updated by {name}")
+    feedstock.name = name
+    feedstock.code = code
     feedstock.save()
 
 
 if __name__ == "__main__":
-    rename_alcool_pur_marc_raisin()
+    parser = argparse.ArgumentParser(description="Update feedstock --id --name --code")
+    parser.add_argument("--id", dest="id", action="store", default=None, help="Feedstock ID")
+    parser.add_argument("--name", dest="name", action="store", default=None, help="Feedstock name")
+    parser.add_argument("--code", dest="code", action="store", default=None, help="Feedstock code")
+    args = parser.parse_args()
+    rename_alcool_pur_marc_raisin(id=args.id, name=args.name, code=args.code)
