b85750685d14e1ae3fc9c4c127c9d0cf9ec14f3d
Julien Falxa <jfalxa.sta@gmail.com>
2024-02-06 17:17:39 +0100

    [api] add generic error handler

---
+++ web/core/exception.py
@@ -0,0 +1,21 @@
+import traceback
+from django.conf import settings
+from core.carburetypes import CarbureError
+
+from core.common import ErrorResponse
+
+
+# general middleware that will catch any uncaught error in endpoints, and return a clean error response
+class ExceptionMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
+    def process_exception(self, request, exception):
+        if settings.DEBUG:
+            traceback.print_exc()
+            return ErrorResponse(500, CarbureError.UNKNOWN_ERROR, message=str(exception))
+        else:
+            return ErrorResponse(500, CarbureError.UNKNOWN_ERROR)
