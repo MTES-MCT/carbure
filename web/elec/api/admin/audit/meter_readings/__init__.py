e6ea11535cd8f219cc4f2e1cc040917b96f68ebe
Damien Romito <damien@maracuja.ac>
2024-02-09 12:46:22 +0100

    [api] audit relevés ok

--- web/elec/api/admin/audit/meter_readings/__init__.py
+++ web/elec/api/admin/audit/meter_readings/__init__.py
@@ -5,15 +5,15 @@ from .filters import get_meter_readings_applications_filters
 
 from .application_details import get_application_details
 
-# from .accept_application import accept_application
-# from .reject_application import reject_application
-# from .start_audit import start_audit
+from .accept_application import accept_application
+from .reject_application import reject_application
+from .start_audit import start_audit
 
 urlpatterns = [
     path("applications", get_applications, name="elec-admin-audit-meter-readings-applications"),
     path("filters", get_meter_readings_applications_filters, name="elec-admin-audit-meter-readings-filters"),
     path("application-details", get_application_details, name="elec-admin-audit-meter-readings-application-details"),
-    # path("accept-application", accept_application, name="elec-admin-audit-charge-points-accept-application"),
-    # path("reject-application", reject_application, name="elec-admin-audit-charge-points-reject-application"),
-    # path("start-audit", start_audit, name="elec-admin-audit-charge-points-start-audit"),
+    path("accept-application", accept_application, name="elec-admin-audit-meter-readings-accept-application"),
+    path("reject-application", reject_application, name="elec-admin-audit-meter-readings-reject-application"),
+    path("start-audit", start_audit, name="elec-admin-audit-meter-readings-start-audit"),
 ]
