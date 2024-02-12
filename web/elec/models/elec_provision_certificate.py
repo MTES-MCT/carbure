e59b88fa1d3a1560796a7ce1bfd9020afa686f35
Damien Romito <damien@maracuja.ac>
2024-02-12 18:10:52 +0100

    import provision certificates specifying the current type

--- web/elec/models/elec_provision_certificate.py
+++ web/elec/models/elec_provision_certificate.py
@@ -1,4 +1,7 @@
+from random import choices
 from django.db import models
+
+from elec.models.elec_charge_point import ElecChargePoint
 
 
 class ElecProvisionCertificate(models.Model):
@@ -6,7 +9,7 @@ class ElecProvisionCertificate(models.Model):
         db_table = "elec_provision_certificate"
         verbose_name = "Certificat de Fourniture (elec)"
         verbose_name_plural = "Certificats de Fourniture (elec)"
-        unique_together = ("cpo", "quarter", "year", "operating_unit")
+        unique_together = ("cpo", "quarter", "year", "current_type", "operating_unit")
 
     QUARTERS = (
         (1, "T1"),
@@ -16,8 +19,9 @@ class ElecProvisionCertificate(models.Model):
     )
 
     cpo = models.ForeignKey("core.Entity", on_delete=models.CASCADE)
-    quarter = models.IntegerField(choices=QUARTERS)
-    year = models.IntegerField()
-    operating_unit = models.CharField(max_length=64)
+    current_type = models.CharField(max_length=2, choices=ElecChargePoint.CURRENT_TYPE, null=True)
     energy_amount = models.FloatField()
+    operating_unit = models.CharField(max_length=64)
+    quarter = models.IntegerField(choices=QUARTERS)
     remaining_energy_amount = models.FloatField()
+    year = models.IntegerField()
