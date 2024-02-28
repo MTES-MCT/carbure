83957398b9d317f6a7858604e01075f59e9364b5
Damien Romito <damien@maracuja.ac>
2024-02-28 09:41:37 +0100

    [api] create TransactionsAuditLotsRepository

--- web/audit/api/lots/details.py
+++ web/audit/api/lots/details.py
@@ -1,5 +1,4 @@
 from django.http.response import JsonResponse
-from audit.helpers import get_auditor_lot_comments
 
 from core.decorators import check_user_rights, is_auditor
 from core.helpers import (
@@ -21,6 +20,7 @@ from core.serializers import (
     CarbureLotReliabilityScoreSerializer,
     CarbureStockPublicSerializer,
 )
+from transactions.repositories.audit_lots_repository import TransactionsAuditLotsRepository
 
 
 @check_user_rights()
@@ -29,9 +29,7 @@ def get_lot_details(request, *args, **kwargs):
     lot_id = request.GET.get("lot_id", False)
     entity_id = request.GET.get("entity_id", False)
     if not lot_id:
-        return JsonResponse(
-            {"status": "error", "message": "Missing lot_id"}, status=400
-        )
+        return JsonResponse({"status": "error", "message": "Missing lot_id"}, status=400)
 
     auditor = Entity.objects.get(id=entity_id)
     lot = CarbureLot.objects.get(pk=lot_id)
@@ -42,50 +40,27 @@ def get_lot_details(request, *args, **kwargs):
     has_right_to_audit_owner = False
     has_right_to_audit_client = False
     has_right_to_audit_supplier = False
-    if (
-        owner_id in request.session["rights"]
-        and request.session["rights"][owner_id] == UserRights.AUDITOR
-    ):
+    if owner_id in request.session["rights"] and request.session["rights"][owner_id] == UserRights.AUDITOR:
         has_right_to_audit_client = True
-    if (
-        client_id in request.session["rights"]
-        and request.session["rights"][client_id] == UserRights.AUDITOR
-    ):
+    if client_id in request.session["rights"] and request.session["rights"][client_id] == UserRights.AUDITOR:
         has_right_to_audit_client = True
-    if (
-        supplier_id in request.session["rights"]
-        and request.session["rights"][supplier_id] == UserRights.AUDITOR
-    ):
+    if supplier_id in request.session["rights"] and request.session["rights"][supplier_id] == UserRights.AUDITOR:
         has_right_to_audit_supplier = True
 
     if not has_right_to_audit_client and not has_right_to_audit_supplier:
-        return JsonResponse(
-            {"status": "forbidden", "message": "User not allowed"}, status=403
-        )
+        return JsonResponse({"status": "forbidden", "message": "User not allowed"}, status=403)
 
     data = {}
     data["lot"] = CarbureLotAdminSerializer(lot).data
-    data["parent_lot"] = (
-        CarbureLotAdminSerializer(lot.parent_lot).data if lot.parent_lot else None
-    )
-    data["parent_stock"] = (
-        CarbureStockPublicSerializer(lot.parent_stock).data
-        if lot.parent_stock
-        else None
-    )
-    data["children_lot"] = CarbureLotAdminSerializer(
-        CarbureLot.objects.filter(parent_lot=lot), many=True
-    ).data
-    data["children_stock"] = CarbureStockPublicSerializer(
-        CarbureStock.objects.filter(parent_lot=lot), many=True
-    ).data
+    data["parent_lot"] = CarbureLotAdminSerializer(lot.parent_lot).data if lot.parent_lot else None
+    data["parent_stock"] = CarbureStockPublicSerializer(lot.parent_stock).data if lot.parent_stock else None
+    data["children_lot"] = CarbureLotAdminSerializer(CarbureLot.objects.filter(parent_lot=lot), many=True).data
+    data["children_stock"] = CarbureStockPublicSerializer(CarbureStock.objects.filter(parent_lot=lot), many=True).data
     data["distance"] = get_transaction_distance(lot)
     data["errors"] = get_lot_errors(lot, auditor)
     data["certificates"] = get_known_certificates(lot)
     data["updates"] = get_lot_updates(lot)
     data["comments"] = get_lot_comments(lot)
-    data["control_comments"] = get_auditor_lot_comments(lot)
-    data["score"] = CarbureLotReliabilityScoreSerializer(
-        lot.carburelotreliabilityscore_set.all(), many=True
-    ).data
+    data["control_comments"] = TransactionsAuditLotsRepository.get_auditor_lot_comments(lot)
+    data["score"] = CarbureLotReliabilityScoreSerializer(lot.carburelotreliabilityscore_set.all(), many=True).data
     return JsonResponse({"status": "success", "data": data})
