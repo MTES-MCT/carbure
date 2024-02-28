243a5bcfa44a47cabda10c367dee19e5220c1b79
Damien Romito <damien@maracuja.ac>
2024-02-28 09:54:23 +0100

    [api] reorg /audit

--- web/elec/api/admin/audit/__init__.py
+++ web/elec/api/admin/audit/__init__.py
@@ -3,8 +3,8 @@ from .years import get_years
 from .snapshot import get_snapshot
 
 urlpatterns = [
-    path("years", get_years, name="elec-admin-audit-years"),
-    path("snapshot", get_snapshot, name="elec-admin-audit-snapshot"),
+    path("years", get_years, name="elec-admin-transactions-audit-years"),
+    path("snapshot", get_snapshot, name="elec-admin-transactions-audit-snapshot"),
     path("charge-points/", include("elec.api.admin.audit.charge_points")),
     path("meter-readings/", include("elec.api.admin.audit.meter_readings")),
 ]
