4eb6054abef68e9b13a40edd844c49494ae674ed
Damien Romito <damien@maracuja.ac>
2024-02-28 08:57:12 +0100

    [api] reorg admin/declarations

--- web/admin/api/__init__.py
+++ web/admin/api/__init__.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("dashboard/", include("admin.api.dashboard")),
     path("entities/", include("admin.api.entities")),
     path("controls/", include("admin.api.controls")),
 ]
--- web/transactions/api/admin/__init__.py
+++ web/transactions/api/admin/__init__.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .map import map
+from transactions.api.admin.declarations import get_declarations
 
 urlpatterns = [
     path("flow-map", map, name="transactions-admin-flow-map"),
+    path("declarations", get_declarations, name="transactions-admin-declarations"),
 ]
