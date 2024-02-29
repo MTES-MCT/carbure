97b65ddfcbaf1c0d61d8c869dfb172818b7c9e29
Julien Falxa <jfalxa.sta@gmail.com>
2024-02-29 12:48:53 +0100

    [api] update charge point refresh script to detect special non article 2 stations

--- web/elec/scripts/refresh_charge_point_data.py
+++ web/elec/scripts/refresh_charge_point_data.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import django
 import os
 from tqdm import tqdm
@@ -34,6 +35,9 @@ def refresh_charge_point_data(batch):
     charge_points.filter(current_type="CC").update(current_type="DC")
     charge_points.filter(current_type="CA").update(current_type="AC")
 
+    # group charge points by their station
+    charge_points_by_stations = defaultdict(list[ElecChargePoint])
+
     # update charge point data from TDG
     paginator = Paginator(charge_points, batch)
     for page_number in tqdm(paginator.page_range):
@@ -43,12 +47,23 @@ def refresh_charge_point_data(batch):
         charge_point_data = TransportDataGouv.find_charge_point_data(charge_point_summary, batch)
         charge_point_data_by_id = {c["charge_point_id"]: c for c in charge_point_data}
         charge_points_to_update = []
+
         for charge_point in page_charge_points:
             tdg_data = charge_point_data_by_id.get(charge_point.charge_point_id)
             if tdg_data:
                 updated_charge_point = update_charge_point(charge_point, tdg_data)
                 charge_points_to_update.append(updated_charge_point)
+                charge_points_by_stations[updated_charge_point.station_id].append(updated_charge_point)
+
         ElecChargePoint.objects.bulk_update(charge_points_to_update, update_fields)
+
+    print("> Check if applications have readings for all charge points of a same station")
+    for station, charge_points in tqdm(charge_points_by_stations.items()):
+        if all([cp.mid_id and cp.measure_date and cp.measure_energy is not None for cp in charge_points]):
+            charge_points_to_update = []
+            for charge_point in charge_points:
+                charge_points_to_update.append(update_charge_point(charge_point, {"is_article_2": False}))
+            ElecChargePoint.objects.bulk_update(charge_points_to_update, ["is_article_2"])
 
     print("> Done")
 
