333d0d1510afb63185887fa04051c83a1606a3f7
Damien Romito <damien@maracuja.ac>
2024-03-15 12:28:09 +0100

    [front] request siren api + create input

--- front/src/companies/components/registration-dialog.tsx
+++ front/src/companies/components/registration-dialog.tsx
@@ -1,23 +1,55 @@
 import useEntity from "carbure/hooks/entity"
+import Autocomplete from "common/components/autocomplete"
 import { Button } from "common/components/button"
 import { Dialog } from "common/components/dialog"
 import {
   Return
 } from "common/components/icons"
+import { TextInput } from "common/components/input"
 import Portal from "common/components/portal"
 import { Trans, useTranslation } from "react-i18next"
 import { useLocation, useNavigate } from "react-router-dom"
-
+import * as api from "companies/api"
+import { useQuery } from "common/hooks/async"
+import React, { useRef } from "react"
 
 export const CompanyRegistrationDialog = () => {
   const { t } = useTranslation()
   const navigate = useNavigate()
   const location = useLocation()
   const entity = useEntity()
-
+  const searchSirentRef = useRef<HTMLElement>(null)
+  const [sirenQuety, setSirenQuery] = React.useState<string>("")
   const closeDialog = () => {
     navigate({ search: location.search, hash: "#" })
   }
+
+
+
+  const findCompanyBySiren = (siren: string | undefined) => {
+    siren = siren?.trim() || ""
+    setSirenQuery(siren)
+
+    const sirenInput = searchSirentRef.current?.querySelector('input')
+    if (!sirenInput || siren.length < 3) return
+
+    if (siren.match(/^\d{9}$/) === null) {
+      sirenInput.setCustomValidity("Le SIREN doit contenir 9 chiffres")
+      sirenInput.reportValidity()
+      return
+    }
+
+    sirenInput.setCustomValidity("")
+    sirenInput.reportValidity()
+
+    // https://recherche-entreprises.api.gouv.fr/search?q=damien%20romito
+    // #api
+    api.searchCompanyData(siren).then((result) => {
+      console.log('result:', result)
+    })
+
+  }
+
 
   return (
     <Portal onClose={closeDialog}>
@@ -32,6 +64,17 @@ export const CompanyRegistrationDialog = () => {
             <p>
               <Trans>Rechercher votre société dans la base de donnée entreprises.data.gouv :</Trans>
             </p>
+          </section>
+          <section>
+            <TextInput
+              required
+              autoFocus
+              value={sirenQuety}
+              label={t("SIREN de votre entreprise ")}
+              onChange={findCompanyBySiren}
+              domRef={searchSirentRef}
+              name="siren"
+            />
           </section>
           <section>
             {/* #form */}
---
+++ front/src/companies/types.ts
@@ -0,0 +1,11 @@
+
+
+export interface CompanyResult {
+  siren: string
+  nom_complet: string
+  siege: {
+    adresse: string
+    code_postal: string
+  }
+
+}  
