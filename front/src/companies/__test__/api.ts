950a3ca9c1036074d93779926e4c0233d63dd387
Damien Romito <damien@maracuja.ac>
2024-03-19 18:21:43 +0100

    [front] update searchCompanyBySiren method

---
+++ front/src/companies/__test__/api.ts
@@ -0,0 +1,10 @@
+import { mockPostWithResponseData } from "carbure/__test__/helpers";
+import { companyResult } from "./data";
+import { setupServer } from "msw/lib/node";
+
+export const searchCompanyDataBySirenOk = mockPostWithResponseData("/entity/search-company", companyResult)
+
+
+export default setupServer(
+  searchCompanyDataBySirenOk
+)
