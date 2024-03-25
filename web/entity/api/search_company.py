018b80125932d06c4bbc33eeb0770bc98be5fdf2
Damien Romito <damien@maracuja.ac>
2024-03-25 14:16:27 +0100

    [api] no company found error + test

--- web/entity/api/search_company.py
+++ web/entity/api/search_company.py
@@ -29,49 +29,49 @@ def search_company(request, *args, **kwargs):
     registration_id = form.cleaned_data["registration_id"]
     print("registration_id: ", registration_id)
 
-    # url = f"https://recherche-entreprises.api.gouv.fr/search?q={registration_id}"
-    # api_response = requests.get(url)
-    # data = api_response.json()
-    # try:
-    #     company_found = data["results"][0]
-    # except:
-    #     return ErrorResponse(400, SeachCompanyFormError.NO_COMPANY_FOUND)
+    url = f"https://recherche-entreprises.api.gouv.fr/search?q={registration_id}"
+    api_response = requests.get(url)
+    data = api_response.json()
+    try:
+        company_found = data["results"][0]
+    except:
+        return ErrorResponse(400, SeachCompanyFormError.NO_COMPANY_FOUND)
 
-    # company_siege = company_found["siege"]
-    # company_city = company_siege["libelle_commune"]
-    # company_zipcode = company_siege["code_postal"]
-    # company_address = company_siege["adresse"]
-    # string_to_remove = f"{company_zipcode} {company_city}"
-    # company_address = company_address.replace(string_to_remove, "")
-
-    # company_preview = {
-    #     "name": company_found["nom_complet"],
-    #     "legal_name": company_found["nom_complet"],
-    #     "registration_id": company_found["siren"],
-    #     "registered_address": company_address,
-    #     "registered_city": company_siege["libelle_commune"],
-    #     "registered_zipcode": company_siege["code_postal"],
-    #     "registered_country": "France",
-    # }
-    # print("company_preview: ", company_preview)
-
-    # check if not already exists
-    # try:
-    #     entity = Entity.objects.get(registration_id=registration_id)
-    # except:
+    company_siege = company_found["siege"]
+    company_city = company_siege["libelle_commune"]
+    company_zipcode = company_siege["code_postal"]
+    company_address = company_siege["adresse"]
+    string_to_remove = f"{company_zipcode} {company_city}"
+    company_address = company_address.replace(string_to_remove, "")
 
     company_preview = {
-        "name": "TOTALENERGIES SE",
-        "legal_name": "TOTALENERGIES SE",
-        "registration_id": "542051180",
-        "registered_address": "LA DEFENSE 6 2 PL JEAN MILLIER ",
-        "registered_city": "COURBEVOIE",
-        "registered_zipcode": "92400",
+        "name": company_found["nom_complet"],
+        "legal_name": company_found["nom_complet"],
+        "registration_id": company_found["siren"],
+        "registered_address": company_address,
+        "registered_city": company_siege["libelle_commune"],
+        "registered_zipcode": company_siege["code_postal"],
         "registered_country": "France",
     }
+    print("company_preview: ", company_preview)
 
-    warning = {
-        "code": SeachCompanyFormError.REGISTRATION_ID_ALREADY_USED,
-        "meta": {"company_name": "Shell"},
-    }
+    response = {"company_preview": company_preview}
+    try:
+        entity = Entity.objects.get(registration_id=registration_id)
+        company_preview["warning"] = {
+            "code": SeachCompanyFormError.REGISTRATION_ID_ALREADY_USED,
+            "meta": {"company_name": entity.name},
+        }
+    except:
+        print("no registred company wit same siret")
+    # company_preview = {
+    #     "name": "TOTALENERGIES SE",
+    #     "legal_name": "TOTALENERGIES SE",
+    #     "registration_id": "542051180",
+    #     "registered_address": "LA DEFENSE 6 2 PL JEAN MILLIER ",
+    #     "registered_city": "COURBEVOIE",
+    #     "registered_zipcode": "92400",
+    #     "registered_country": "France",
+    # }
+
     return SuccessResponse({"company_preview": company_preview, "warning": warning})
