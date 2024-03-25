b55818278443a9066f77004641f930a5954532be
Julien Falxa <jfalxa.sta@gmail.com>
2024-03-25 16:19:59 +0100

    [api] when giving airline access to dgac, filter users by their email address

--- web/saf/scripts/link_dgac_to_airlines.py
+++ web/saf/scripts/link_dgac_to_airlines.py
@@ -7,7 +7,7 @@ from django.db import transaction
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carbure.settings")
 django.setup()
 
-from core.models import Entity, UserRights, UserRightsRequests
+from core.models import Entity, UserRights
 
 User = get_user_model()
 
@@ -16,20 +16,15 @@ User = get_user_model()
 def link_dgac_to_airlines():
     airlines = Entity.objects.filter(entity_type=Entity.AIRLINE)
 
-    dgac_users = list(
-        UserRights.objects.select_related("user")
-        .filter(entity__name="DGAC", user__is_staff=False)
-        .values_list("user_id", flat=True)
-        .distinct()
-    )
+    dgac_users = User.objects.filter(email__endswith="@aviation-civile.gouv.fr")
 
     # delete old rights
-    UserRights.objects.filter(entity__entity_type=Entity.AIRLINE, user__id__in=dgac_users).delete()
+    UserRights.objects.filter(entity__entity_type=Entity.AIRLINE, user__in=dgac_users).delete()
 
     dgac_rights = []
     for airline in airlines:
-        for user_id in dgac_users:
-            right = UserRights(user_id=user_id, entity=airline, role=UserRights.RO)
+        for user in dgac_users:
+            right = UserRights(user=user, entity=airline, role=UserRights.RO)
             dgac_rights.append(right)
 
     # create new rights
