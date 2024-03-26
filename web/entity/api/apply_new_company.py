e4ce93c3bf8285262246f6c26ee0e991644482ca
Damien Romito <damien@maracuja.ac>
2024-03-26 13:10:15 +0100

    [api] create new company applied

---
+++ web/entity/api/apply_new_company.py
@@ -0,0 +1,66 @@
+from django import forms
+
+from core.carburetypes import CarbureError
+from core.decorators import otp_or_403
+from core.models import EntityCertificate, GenericCertificate, Entity
+from core.common import SuccessResponse, ErrorResponse
+
+
+class ApplyForNewCompanyForm(forms.Form):
+    activity_description = forms.CharField(max_length=256, required=True)
+    certificate_id = forms.CharField(max_length=64, required=True)
+    certificate_type = forms.CharField(max_length=32, required=True)
+    entity_type = forms.CharField(max_length=64, required=True)
+    legal_name = forms.CharField(max_length=128, required=True)
+    registered_address = forms.CharField(max_length=256, required=True)
+    registered_city = forms.CharField(max_length=64, required=True)
+    registered_country = forms.CharField(max_length=64, required=True)
+    registered_zipcode = forms.CharField(max_length=64, required=True)
+    registration_id = forms.CharField(max_length=9, required=True)  # SIREN
+    sustainability_officer = forms.CharField(max_length=64, required=True)
+    sustainability_officer_email = forms.CharField(max_length=254, required=True)
+    sustainability_officer_phone_number = forms.CharField(max_length=32, required=True)
+
+
+@otp_or_403
+def apply_for_new_company(request, *args, **kwargs):
+
+    form = ApplyForNewCompanyForm(request.POST)
+
+    if not form.is_valid():
+        return ErrorResponse(400, CarbureError.MALFORMED_PARAMS, form.errors)
+
+    activity_description = form.cleaned_data["activity_description"]
+    certificate_id = form.cleaned_data["certificate_id"]
+    certificate_type = form.cleaned_data["certificate_type"]
+    entity_type = form.cleaned_data["entity_type"]
+    legal_name = form.cleaned_data["legal_name"]
+    registered_address = form.cleaned_data["registered_address"]
+    registered_city = form.cleaned_data["registered_city"]
+    registered_country = form.cleaned_data["registered_country"]
+    registered_zipcode = form.cleaned_data["registered_zipcode"]
+    registration_id = form.cleaned_data["registration_id"]
+    sustainability_officer = form.cleaned_data["sustainability_officer"]
+    sustainability_officer_email = form.cleaned_data["sustainability_officer_email"]
+    sustainability_officer_phone_number = form.cleaned_data["sustainability_officer_phone_number"]
+
+    entity = Entity.objects.create(
+        activity_description=activity_description,
+        entity_type=entity_type,
+        legal_name=legal_name,
+        name=legal_name,
+        registered_address=registered_address,
+        registered_city=registered_city,
+        registered_country=registered_country,
+        registered_zipcode=registered_zipcode,
+        registration_id=registration_id,
+        sustainability_officer=sustainability_officer,
+        sustainability_officer_email=sustainability_officer_email,
+        sustainability_officer_phone_number=sustainability_officer_phone_number,
+    )
+
+    # add certificat
+    original_certificate = GenericCertificate.objects.get(certificate_type=certificate_type, certificate_id=certificate_id)
+    EntityCertificate.objects.create(entity=entity, certificate=original_certificate)
+
+    return SuccessResponse()
---
+++ web/entity/api/tests_apply_new_company.py
@@ -0,0 +1,60 @@
+# test with : python web/manage.py test entity.api.tests_apply_new_company.EntityApplyNewCompanyTest --keepdb
+
+import datetime
+
+from core.tests_utils import setup_current_user
+from core.models import Entity, EntityCertificate, GenericCertificate
+from django.test import TestCase
+from django.urls import reverse
+
+
+class EntityApplyNewCompanyTest(TestCase):
+    def setUp(self):
+
+        self.cpo = Entity.objects.create(
+            name="CPO",
+            entity_type=Entity.CPO,
+            has_elec=True,
+        )
+
+        self.user = setup_current_user(
+            self,
+            "tester@carbure.local",
+            "Tester",
+            "gogogo",
+        )
+
+    def test_apply_new_company(self):
+        GenericCertificate.objects.create(
+            certificate_id="EU-ISCC-Cert-PL123-12345678",
+            certificate_type="ISCC",
+            valid_from=datetime.date(2020, 1, 1),
+            valid_until=datetime.date(2021, 1, 1),
+        )
+        params = {
+            "activity_description": "Du blabla dur la société",
+            "certificate_id": "EU-ISCC-Cert-PL123-12345678",
+            "certificate_type": "ISCC",
+            "entity_type": "Opérateur",
+            "legal_name": "Mon entreprise test",
+            "registered_address": "1 rue de la paix",
+            "registered_city": "Paris",
+            "registered_country": "France",
+            "registered_zipcode": "75001",
+            "registration_id": "542051180",
+            "sustainability_officer": "",
+            "sustainability_officer_email": "officer@test.com",
+            "sustainability_officer_phone_number": "",
+        }
+        response = self.client.post(
+            reverse("entity-apply-new-company"),
+            params,
+        )
+        # check new entity created
+        self.assertEqual(response.status_code, 200)
+        entity = Entity.objects.get(legal_name="Mon entreprise test", registration_id="542051180")
+        self.assertEqual(entity.registered_address, "1 rue de la paix")
+
+        # check certificate created
+        certificate = EntityCertificate.objects.get(certificate__certificate_id="EU-ISCC-Cert-PL123-12345678", entity=entity)
+        self.assertEqual(entity.id, certificate.entity.id)
