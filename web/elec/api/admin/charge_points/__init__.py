c5a01d9183dfad9751fd727cc5087fafd5a217c3
Julien Falxa <jfalxa.sta@gmail.com>
2024-04-08 18:01:56 +0200

    [api] remove accept/reject charge point related applications from company detail page

--- web/elec/api/admin/charge_points/__init__.py
+++ web/elec/api/admin/charge_points/__init__.py
@@ -4,13 +4,9 @@ from django.urls import path
 from .charge_points import get_charge_points
 from .applications import get_applications
 from .application_details import get_application_details
-from .accept_application import accept_application
-from .reject_application import reject_application
 
 urlpatterns = [
     path("", get_charge_points, name="elec-admin-charge-points-get-charge-points"),
     path("applications", get_applications, name="elec-admin-charge-points-get-applications"),
     path("application-details", get_application_details, name="elec-admin-charge-points-get-application-details"),
-    path("accept-application", accept_application, name="elec-admin-charge-points-accept-application"),
-    path("reject-application", reject_application, name="elec-admin-charge-points-reject-application"),
 ]
