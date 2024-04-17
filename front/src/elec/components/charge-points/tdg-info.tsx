bb944c180dd7bc1266f9d40bcfd6ba959cc75ef9
Julien Falxa <jfalxa.sta@gmail.com>
2024-04-17 19:17:13 +0200

    [front] add basic explanation for new columns in charge point template

--- front/src/elec/components/charge-points/tdg-info.tsx
+++ front/src/elec/components/charge-points/tdg-info.tsx
@@ -4,14 +4,6 @@ import { InfoCircle } from "common/components/icons"
 import Tooltip from "common/components/tooltip"
 import { ExternalLink } from "common/components/button"
 
-// url vers la page de présentation du projet IRVE de transport.data.gouv.fr
-const TDG_URL =
-  "https://transport.data.gouv.fr/datasets/fichier-consolide-des-bornes-de-recharge-pour-vehicules-electriques"
-
-// url vers l'explorateur de données IRVE de transport.data.gouv.fr
-const TDG_DATA_URL =
-  "https://explore.data.gouv.fr/fr/tableau?url=https%3A%2F%2Fwww.data.gouv.fr%2Ffr%2Fdatasets%2Fr%2Feb76d20a-8501-400e-b336-d85724de5435"
-
 export function TDGInfo() {
   const { t } = useTranslation()
 
@@ -19,9 +11,11 @@ export function TDGInfo() {
     <Alert icon={InfoCircle} variant="info" style={{ minWidth: "50vw" }}>
       <section>
         <p>
-          <Trans
-            defaults={`Carbure utilise les <b>données consolidées IRVE</b> de <b>transport.data.gouv.fr</b> pour déterminer certaines caractéristiques de vos points de recharge.`}
-          />
+          {t("Carbure utilise les")}{" "}
+          <ExternalLink href="https://www.data.gouv.fr/fr/datasets/5448d3e0c751df01f85d0572/">
+            <b>{t("données consolidées IRVE")}</b>
+          </ExternalLink>{" "}
+          <Trans defaults="de <b>transport.data.gouv.fr</b> pour déterminer certaines caractéristiques de vos points de recharge." />
         </p>
 
         <ul
@@ -33,13 +27,24 @@ export function TDGInfo() {
           }}
         >
           <li>
-            <Trans defaults="Vos PDR y sont identifiés par la colonne <b>id_pdc_itinerance</b>" />
+            {t("Vos PDR y sont identifiés par la colonne")}{" "}
+            <ExternalLink href="https://schema.data.gouv.fr/etalab/schema-irve-statique/2.3.1/documentation.html#propriete-id-pdc-itinerance">
+              <b>id_pdc_itinerance</b>
+            </ExternalLink>
           </li>
           <li>
-            <Trans defaults="Le type de courant d'un PDR est considéré comme <b>CC</b> si une des colonnes <b>prise_type_chademo</b> ou <b>prise_type_combo_ccs</b> vaut <b>1</b>. Sinon <b>CA</b>." />
+            <Trans defaults="Le type de courant d'un PDR est considéré comme <b>CC</b> si une des colonnes " />
+            <ExternalLink href="https://schema.data.gouv.fr/etalab/schema-irve-statique/2.3.1/documentation.html#propriete-prise-type-chademo">
+              <b>prise_type_chademo</b>
+            </ExternalLink>{" "}
+            {t("ou")}{" "}
+            <ExternalLink href="https://schema.data.gouv.fr/etalab/schema-irve-statique/2.3.1/documentation.html#propriete-prise-type-combo-ccs">
+              <b>prise_type_combo_ccs</b>
+            </ExternalLink>{" "}
+            <Trans defaults="vaut <b>1</b>. Sinon <b>CA</b>." />
           </li>
           <li>
-            <Trans defaults="Si certains PDR d'une station que vous inscrivez sont mentionnés sur <b>transport.data.gouv.fr</b> mais pas dans votre fichier excel, ces PDR seront quand même pris en compte pour déterminer si cette station est soumise ou non à " />
+            <Trans defaults="Si certains PDR d'une station que vous inscrivez sont mentionnés sur <b>transport.data.gouv.fr</b> mais pas dans votre fichier excel, ces PDR seront quand même pris en compte pour déterminer si cette station est soumise ou non à" />{" "}
             <Tooltip
               style={{
                 display: "inline",
@@ -53,22 +58,21 @@ export function TDGInfo() {
               {t("l'article 2 du décret n°2022-1330")}
             </Tooltip>
           </li>
+          <li>
+            {t(
+              "Si certaines erreurs vous suprennent (ex: MID obligatoire sur un point CC), vous pouvez remplir les 2 colonnes optionnelles à droite du fichier excel pour nous indiquer la bonne valeur à utiliser dans nos calculs."
+            )}
+          </li>
         </ul>
-
-        <p style={{ marginTop: "var(--spacing-m)" }}>
-          <ExternalLink href={TDG_DATA_URL}>
-            {t("Consulter les données consolidées IRVE")}
-          </ExternalLink>
-        </p>
 
         <p
           style={{
-            marginTop: "var(--spacing-xs)",
+            marginTop: "var(--spacing-m)",
             marginBottom: "var(--spacing-s)",
           }}
         >
-          <ExternalLink href={TDG_URL}>
-            {t("Voir la documentation IRVE de transport.data.gouv.fr")}
+          <ExternalLink href="https://explore.data.gouv.fr/fr/tableau?url=https%3A%2F%2Fwww.data.gouv.fr%2Ffr%2Fdatasets%2Fr%2Feb76d20a-8501-400e-b336-d85724de5435">
+            {t("Explorer les données consolidées IRVE")}
           </ExternalLink>
         </p>
       </section>
