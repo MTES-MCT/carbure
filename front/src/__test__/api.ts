bed5409365e55764eda396fee5195f25f3474e2b
Damien Romito <damien@maracuja.ac>
2024-04-25 18:01:56 +0200

    [front] process generation sample in process with mocked api

--- front/src/__test__/api.ts
+++ front/src/__test__/api.ts
@@ -1,7 +1,6 @@
 import * as elecAdminAuditMocks from "elec-audit-admin/__test__/api"
 import * as elecMocks from "elec/__test__/api"
 
-
 export default [
   ...Object.values(elecAdminAuditMocks),
   ...Object.values(elecMocks)
