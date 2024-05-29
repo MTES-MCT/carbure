f6eb9dfb130c832af6d6a8231576608eca521814
Julien Falxa <jfalxa.sta@gmail.com>
2024-05-29 13:18:58 +0200

    [back] cleanup

--- web/elec/api/admin/snapshot.py
+++ web/elec/api/admin/snapshot.py
@@ -1,6 +1,5 @@
 import traceback
 from django import forms
-from django.db.models import Sum
 from core.carburetypes import CarbureError
 from core.common import ErrorResponse, SuccessResponse
 from core.decorators import check_admin_rights
