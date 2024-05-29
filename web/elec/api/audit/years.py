70c6b93af209f78780fb496725043f14a89eec40
Julien Falxa <jfalxa.sta@gmail.com>
2024-05-29 13:17:09 +0200

    [back] add endpoint for elec audit years

---
+++ web/elec/api/audit/years.py
@@ -0,0 +1,11 @@
+from core.common import SuccessResponse
+from core.decorators import check_user_rights
+from core.models import Entity
+from elec.repositories.elec_audit_repository import ElecAuditRepository
+
+
+@check_user_rights(entity_type=[Entity.AUDITOR])
+def get_years(request):
+    audits = ElecAuditRepository.get_audited_applications(request.user)
+    years = audits.values_list("created_at__year", flat=True).distinct()
+    return SuccessResponse(list(years))
