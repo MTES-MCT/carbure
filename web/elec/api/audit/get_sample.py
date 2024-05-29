31f34fc1be4b4814c6b6f6d42ab8f6c372480af6
Julien Falxa <jfalxa.sta@gmail.com>
2024-05-29 13:17:51 +0200

    [back] add endpoint for elec audit application sample

---
+++ web/elec/api/audit/get_sample.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.views.decorators.http import require_GET
+from core.carburetypes import CarbureError
+from core.common import ErrorResponse, SuccessResponse
+from core.decorators import check_user_rights
+from core.excel import ExcelResponse
+from core.models import Entity
+from elec.models.elec_charge_point import ElecChargePoint
+from elec.repositories.charge_point_repository import ChargePointRepository
+from elec.repositories.elec_audit_repository import ElecAuditRepository
+from elec.serializers.elec_charge_point import ElecChargePointSampleSerializer
+from elec.services.export_charge_point_excel import export_charge_points_sample_to_excel
+
+
+class GetSampleError:
+    NO_SAMPLE_FOUND = "NO_SAMPLE_FOUND"
+
+
+class GetSampleForm(forms.Form):
+    application_id = forms.IntegerField()
+    export = forms.BooleanField(required=False)
+
+
+@require_GET
+@check_user_rights(entity_type=[Entity.AUDITOR])
+def get_sample(request):
+    form = GetSampleForm(request.GET)
+
+    if not form.is_valid():
+        return ErrorResponse(400, CarbureError.MALFORMED_PARAMS, form.errors)
+
+    application_id = form.cleaned_data["application_id"]
+    export = form.cleaned_data["export"]
+
+    audit_sample = ElecAuditRepository.get_audited_application_by_id(request.user, application_id)
+
+    if not audit_sample:
+        return ErrorResponse(404, GetSampleError.NO_SAMPLE_FOUND)
+
+    audited_charge_points = audit_sample.audited_charge_points.all()
+    charge_points = ElecChargePoint.objects.filter(charge_point_audit__in=audited_charge_points)
+
+    if export:
+        excel_file = export_charge_points_sample_to_excel(charge_points, audit_sample.cpo)
+        return ExcelResponse(excel_file)
+
+    return SuccessResponse(ElecChargePointSampleSerializer(charge_points, many=True).data)
