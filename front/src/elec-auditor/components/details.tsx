93e51e7b2db03fe8609f1e714b3e91c1e6a8761b
Damien Romito <damien@maracuja.ac>
2024-07-12 12:09:48 +0200

    [front] add stepper

--- front/src/elec-auditor/components/details.tsx
+++ front/src/elec-auditor/components/details.tsx
@@ -14,6 +14,8 @@ import { Trans, useTranslation } from "react-i18next"
 import { useLocation, useNavigate } from "react-router-dom"
 import ApplicationSummary from "./details-application-summary"
 import ApplicationStatus from "./application-status"
+import { ElecAuditorApplicationsStatus } from "elec-auditor/types"
+import ApplicationDetailsPending from "./details-pending"
 
 
 
@@ -24,11 +26,11 @@ export const ApplicationDetailsDialog = () => {
   const location = useLocation()
   const match = useHashMatch("application/:id")
 
-  const chargePointApplicationResponse = useQuery(api.getApplicationDetails, {
-    key: "elec-audit-application-details",
+  const applicationResponse = useQuery(api.getApplicationDetails, {
+    key: "elec-auditor-application-details",
     params: [entity.id, parseInt(match?.params.id!)],
   })
-  const chargePointApplication = chargePointApplicationResponse.result?.data.data
+  const application = applicationResponse.result?.data.data
 
 
   const closeDialog = () => {
@@ -37,81 +39,34 @@ export const ApplicationDetailsDialog = () => {
 
 
   const downloadSample = async () => {
-    return api.downloadSample(entity.id, chargePointApplication!.id)
+    return api.downloadSample(entity.id, application!.id)
   }
 
   return (
     <Portal onClose={closeDialog}>
       <Dialog onClose={closeDialog} >
         <header>
-          <ApplicationStatus status={chargePointApplication?.status} big />
+          <ApplicationStatus status={application?.status} big />
           <h1>{t("Audit de points de recharge")}</h1>
         </header>
 
-        {/* {chargePointApplication?.status === ElecAuditApplicationStatus.Pending && (
-          <AuditApplicationDetailsPending
-            meterReadingsApplication={meterReadingsApplication}
-            onAccept={acceptApplication}
-            onReject={rejectApplication}
+        {application?.status === ElecAuditorApplicationsStatus.AuditInProgress && (
+          <ApplicationDetailsPending
+            application={application}
             onDownloadSample={downloadSample}
           />
-        )} */}
+        )}
 
-        <main>
-          <section>
-            <ApplicationSummary application={chargePointApplication} />
-          </section>
-          <Divider />
-          {chargePointApplication?.sample ? (
-            <section>
-              <ChargePointsSampleMap chargePoints={chargePointApplication?.sample?.charge_points} />
-              <Button icon={Download} label={t("Télécharger les points à auditer")} variant="secondary" action={downloadSample} style={{ width: "min-content" }} />
-            </section>
-          ) : <p><Trans>Le fichier CSV listant l'intégralité des points de recharge à auditer vous a été envoyé par email par l'aménageur.</Trans></p>}
+        {application?.status === ElecAuditorApplicationsStatus.AuditDone && (
+          <p>Done</p>
+          // <ApplicationDetailsPending
+          //   application={application}
+          //   onDownloadSample={downloadSample}
+          // />
+        )}
 
-          <section>
 
-            <Alert icon={Edit} variant="info" label={t("Précision sur les champs du tableau à remplir :")} >
-              <p>
-                <strong><Trans>Infrastructure de recharge installée à la localisation renseignée :</Trans></strong>
-                <Trans>l'inspecteur confirme avoir trouvé le point de recharge à la localisation indiquée. Écrire "OUI" ou "NON" et passer aux étapes suivantes si l'infrastructure a été localisée ;</Trans>
-              </p>
-
-              <p>
-                <strong><Trans>Identifiant renseigné visible à proximité immédiate de l'infrastructure :</Trans></strong>
-                <Trans>l'inspecteur précise si l'identifiant renseigné est visible sur ou a proximité immédiate du point de recharge. La non visibilité de cet identifiant ne doit pas faire obstacle à la suite du contrôle si l'inspecteur est en mesure d'identifier les points de recharge par d'autres moyens (indication de l'aménageur/de l'opérateur par exemple). Écrire "OUI" ou "NON" ;</Trans>
-              </p>
-
-              <p>
-                <strong><Trans>Point de contrôle type de courant :</Trans></strong>
-                <Trans>l'inspecteur vérifie le type de courant délivré par le point de recharge (à partir du connecteur) et l'indique en écrivant "CC" ou "CA" </Trans>
-              </p>
-
-              <p>
-                <strong><Trans>Numéro du certificat d'examen du type si différent :</Trans></strong>
-                <Trans>l'inspecteur vérifie la correspondance entre le certificat d'examen du type déclaré par le demandeur (rappelé dans la colonne "no_mid") et celui du compteur installé dans le point de recharge. Il laisse la case vide en cas de correspondance ou indique le numéro du compteur installé si une différence est constatée ;</Trans>
-              </p>
-
-              <p>
-                <strong><Trans>Date du relevé par l'intervenant :</Trans></strong>
-                <Trans>la date de passage de l'inspecteur au format JJ/MM/AAAA ;</Trans>
-              </p>
-              <p>
-                <strong><Trans>Énergie active totale relevée :</Trans></strong>
-                <Trans>le relevé en kWh, au format XXXX,XX ;</Trans>
-              </p>
-              <p>
-                <strong><Trans>Limite dans la mission de contrôle :</Trans></strong>
-                <Trans>champ libre permettant d'indiquer tout circonstance ayant fait obstacle à la mission de contrôle.</Trans>
-              </p>
-            </Alert>
-          </section>
-        </main>
-
-        <footer>
-          <MailtoButton cpoName={chargePointApplication?.cpo.name} auditorName={entity?.name} chargePointCount={chargePointApplication?.charge_point_count} />
-        </footer>
-        {chargePointApplicationResponse.loading && <LoaderOverlay />}
+        {applicationResponse.loading && <LoaderOverlay />}
       </Dialog>
     </Portal >
   )
