26271330f331e099ee8323b9a6195224c288d505
Damien Romito <damien@maracuja.ac>
2024-07-12 12:09:48 +0200

    [front] add stepper

--- front/src/elec-auditor/components/details-pending.tsx
+++ front/src/elec-auditor/components/details-pending.tsx
@@ -16,13 +16,18 @@ import { useLocation, useNavigate } from "react-router-dom"
 import ApplicationSummary from "./details-application-summary"
 import { ElecChargePointsApplication, ElecMeterReadingsApplication } from "elec/types"
 import { ElecAuditorApplicationDetails } from "elec-auditor/types"
+import { useState } from "react"
+import Stepper from "@codegouvfr/react-dsfr/Stepper"
 
 
 interface ApplicationDetailsPendingProps {
-  audit: ElecAuditorApplicationDetails
+  application: ElecAuditorApplicationDetails
+  onDownloadSample: () => void
+
 }
 export const ApplicationDetailsPending = ({
-  audit
+  application,
+  onDownloadSample
 }: ApplicationDetailsPendingProps) => {
   const { t } = useTranslation()
   const entity = useEntity()
@@ -31,85 +36,107 @@ export const ApplicationDetailsPending = ({
   const match = useHashMatch("application/:id")
 
 
+  type IndicatorStep = "download-sample" | "upload-report" | "check"
+  const [currentStep, setCurrentStep] = useState(0)
 
-  // const closeDialog = () => {
-  //   navigate({ search: location.search, hash: "#" })
-  // }
+  const steps = [
+    {
+      key: "download-sample",
+      title: t("Récupération du retour de contrôle à remplir")
+    },
+    {
+      key: "upload-report",
+      title: t("Dépôt du fichier de résultat")
+    },
+    {
+      key: "check",
+      title: t("Vérification du fichier"),
+    },
+  ]
 
+  const step: IndicatorStep = steps[currentStep].key as IndicatorStep
 
-  // const downloadSample = async () => {
-  //   return api.downloadChargePointsSample(entity.id, application!.id)
-  // }
+  function setStep(key: string) {
+    setCurrentStep(steps.findIndex(step => step.key === key))
+  }
+
+  const downloadSample = () => {
+    onDownloadSample()
+    setCurrentStep(currentStep + 1)
+  }
 
   return (
     <>
-
-      {/* <main>
+      <main>
         <section>
+          <Stepper
+            title={steps[currentStep].title}
+            stepCount={steps.length}
+            currentStep={currentStep + 1}
+            nextTitle={steps[currentStep + 1]?.title}
+          />
           <ApplicationSummary application={application} />
         </section>
-        <Divider />
-        {chargePointApplication?.sample ? (
+        {application?.sample ? (
           <section>
-            <ChargePointsSampleMap chargePoints={chargePointApplication?.sample?.charge_points} />
-            <Button icon={Download} label={t("Télécharger les points à auditer")} variant="secondary" action={downloadSample} style={{ width: "min-content" }} />
+            <ChargePointsSampleMap chargePoints={application?.sample?.charge_points} />
           </section>
         ) : <p><Trans>Le fichier CSV listant l'intégralité des points de recharge à auditer vous a été envoyé par email par l'aménageur.</Trans></p>}
-
         <section>
-
-          <Alert icon={Edit} variant="info" label={t("Précision sur les champs du tableau à remplir :")} >
-            <p>
-              <strong><Trans>Infrastructure de recharge installée à la localisation renseignée :</Trans></strong>
-              <Trans>l'inspecteur confirme avoir trouvé le point de recharge à la localisation indiquée. Écrire "OUI" ou "NON" et passer aux étapes suivantes si l'infrastructure a été localisée ;</Trans>
-            </p>
-
-            <p>
-              <strong><Trans>Identifiant renseigné visible à proximité immédiate de l'infrastructure :</Trans></strong>
-              <Trans>l'inspecteur précise si l'identifiant renseigné est visible sur ou a proximité immédiate du point de recharge. La non visibilité de cet identifiant ne doit pas faire obstacle à la suite du contrôle si l'inspecteur est en mesure d'identifier les points de recharge par d'autres moyens (indication de l'aménageur/de l'opérateur par exemple). Écrire "OUI" ou "NON" ;</Trans>
-            </p>
-
-            <p>
-              <strong><Trans>Point de contrôle type de courant :</Trans></strong>
-              <Trans>l'inspecteur vérifie le type de courant délivré par le point de recharge (à partir du connecteur) et l'indique en écrivant "CC" ou "CA" </Trans>
-            </p>
-
-            <p>
-              <strong><Trans>Numéro du certificat d'examen du type si différent :</Trans></strong>
-              <Trans>l'inspecteur vérifie la correspondance entre le certificat d'examen du type déclaré par le demandeur (rappelé dans la colonne "no_mid") et celui du compteur installé dans le point de recharge. Il laisse la case vide en cas de correspondance ou indique le numéro du compteur installé si une différence est constatée ;</Trans>
-            </p>
-
-            <p>
-              <strong><Trans>Date du relevé par l'intervenant :</Trans></strong>
-              <Trans>la date de passage de l'inspecteur au format JJ/MM/AAAA ;</Trans>
-            </p>
-            <p>
-              <strong><Trans>Énergie active totale relevée :</Trans></strong>
-              <Trans>le relevé en kWh, au format XXXX,XX ;</Trans>
-            </p>
-            <p>
-              <strong><Trans>Limite dans la mission de contrôle :</Trans></strong>
-              <Trans>champ libre permettant d'indiquer tout circonstance ayant fait obstacle à la mission de contrôle.</Trans>
-            </p>
-          </Alert>
+          <ReportInfoAlert />
         </section>
       </main>
 
       <footer>
-        <MailtoButton cpoName={chargePointApplication?.cpo.name} auditorName={entity?.name} chargePointCount={chargePointApplication?.charge_point_count} />
-      </footer> */}
+        {step === "download-sample" && <>
+          <Button icon={Download} label={t("Télécharger les points à auditer")} variant="primary" action={downloadSample} style={{ width: "min-content" }} />
+        </>}
+      </footer>
     </>
   )
 }
 
 
-interface MailtoButtonProps {
-  cpoName?: string
-  auditorName?: string
-  chargePointCount?: number
-
-}
-
 
 
 export default ApplicationDetailsPending
+
+
+const ReportInfoAlert = () => {
+  const { t } = useTranslation()
+
+  return <Alert icon={Edit} variant="info" label={t("Précision sur les champs du tableau à remplir :")} >
+    <p>
+      <strong><Trans>Infrastructure de recharge installée à la localisation renseignée :</Trans></strong>
+      <Trans>l'inspecteur confirme avoir trouvé le point de recharge à la localisation indiquée. Écrire "OUI" ou "NON" et passer aux étapes suivantes si l'infrastructure a été localisée ;</Trans>
+    </p>
+
+    <p>
+      <strong><Trans>Identifiant renseigné visible à proximité immédiate de l'infrastructure :</Trans></strong>
+      <Trans>l'inspecteur précise si l'identifiant renseigné est visible sur ou a proximité immédiate du point de recharge. La non visibilité de cet identifiant ne doit pas faire obstacle à la suite du contrôle si l'inspecteur est en mesure d'identifier les points de recharge par d'autres moyens (indication de l'aménageur/de l'opérateur par exemple). Écrire "OUI" ou "NON" ;</Trans>
+    </p>
+
+    <p>
+      <strong><Trans>Point de contrôle type de courant :</Trans></strong>
+      <Trans>l'inspecteur vérifie le type de courant délivré par le point de recharge (à partir du connecteur) et l'indique en écrivant "CC" ou "CA" </Trans>
+    </p>
+
+    <p>
+      <strong><Trans>Numéro du certificat d'examen du type si différent :</Trans></strong>
+      <Trans>l'inspecteur vérifie la correspondance entre le certificat d'examen du type déclaré par le demandeur (rappelé dans la colonne "no_mid") et celui du compteur installé dans le point de recharge. Il laisse la case vide en cas de correspondance ou indique le numéro du compteur installé si une différence est constatée ;</Trans>
+    </p>
+
+    <p>
+      <strong><Trans>Date du relevé par l'intervenant :</Trans></strong>
+      <Trans>la date de passage de l'inspecteur au format JJ/MM/AAAA ;</Trans>
+    </p>
+    <p>
+      <strong><Trans>Énergie active totale relevée :</Trans></strong>
+      <Trans>le relevé en kWh, au format XXXX,XX ;</Trans>
+    </p>
+    <p>
+      <strong><Trans>Limite dans la mission de contrôle :</Trans></strong>
+      <Trans>champ libre permettant d'indiquer tout circonstance ayant fait obstacle à la mission de contrôle.</Trans>
+    </p>
+  </Alert>
+}
