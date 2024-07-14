6ee90101c82597af87e9596d3b16d6ef7044a319
Julien Falxa <jfalxa.sta@gmail.com>
2024-07-14 18:46:43 +0200

    [front+back] rename elec auditor endpoints from audit/ to auditor/

--- front/src/elec-auditor/api.ts
+++ front/src/elec-auditor/api.ts
@@ -1,27 +1,37 @@
 import { api, Api, download } from "common/services/api"
-import { ElecAuditorApplicationDetails, ElecAuditorApplicationsData, ElecAuditorApplicationsFilter, ElecAuditorApplicationsQuery, ElecAuditorApplicationsSnapshot } from "./types"
+import {
+  ElecAuditorApplicationDetails,
+  ElecAuditorApplicationsData,
+  ElecAuditorApplicationsFilter,
+  ElecAuditorApplicationsQuery,
+  ElecAuditorApplicationsSnapshot,
+} from "./types"
 import { UploadCheckReportInfo } from "carbure/types"
 
 export function getYears(entity_id: number) {
-  return api.get<Api<number[]>>("/elec/audit/years", {
+  return api.get<Api<number[]>>("/elec/auditor/years", {
     params: { entity_id },
   })
 }
 
-
 // AUDIT
 export function getSnapshot(entity_id: number, year: number) {
-  return api.get<Api<ElecAuditorApplicationsSnapshot>>("/elec/audit/snapshot", {
-    params: { entity_id, year },
-  })
+  return api.get<Api<ElecAuditorApplicationsSnapshot>>(
+    "/elec/auditor/snapshot",
+    {
+      params: { entity_id, year },
+    }
+  )
 }
-
 
 //CHARGE POINT
 export function getApplications(query: ElecAuditorApplicationsQuery) {
-  return api.get<Api<ElecAuditorApplicationsData>>("/elec/audit/applications", {
-    params: query,
-  })
+  return api.get<Api<ElecAuditorApplicationsData>>(
+    "/elec/auditor/applications",
+    {
+      params: query,
+    }
+  )
 }
 
 const QUERY_RESET: Partial<ElecAuditorApplicationsQuery> = {
@@ -31,39 +41,54 @@ const QUERY_RESET: Partial<ElecAuditorApplicationsQuery> = {
   order: undefined,
 }
 
-export async function getFilters(field: ElecAuditorApplicationsFilter, query: ElecAuditorApplicationsQuery) {
+export async function getFilters(
+  field: ElecAuditorApplicationsFilter,
+  query: ElecAuditorApplicationsQuery
+) {
   const params = { filter: field, ...query, ...QUERY_RESET }
   const result = await api
-    .get<Api<string[]>>("/elec/audit/filters", { params })
+    .get<Api<string[]>>("/elec/auditor/filters", { params })
     .then((res) => res.data.data ?? [])
   return result
 }
 
-
 export function getApplicationDetails(entityId: number, auditSampleId: number) {
-  return api.get<Api<ElecAuditorApplicationDetails>>("/elec/audit/application-details", {
-    params: { entity_id: entityId, audit_sample_id: auditSampleId },
-  })
+  return api.get<Api<ElecAuditorApplicationDetails>>(
+    "/elec/auditor/application-details",
+    {
+      params: { entity_id: entityId, audit_sample_id: auditSampleId },
+    }
+  )
 }
-
 
 export function downloadSample(entityId: number, auditSampleId: number) {
-  return download("/elec/audit/get-sample", { entity_id: entityId, audit_sample_id: auditSampleId, export: true })
-}
-
-
-export function checkAuditReport(entityId: number, auditSampleId: number, file: File) {
-  return api.post<Api<UploadCheckReportInfo>>("/elec/audit/check-report", {
+  return download("/elec/auditor/get-sample", {
     entity_id: entityId,
     audit_sample_id: auditSampleId,
-    file
+    export: true,
   })
 }
 
-export function acceptAuditReport(entityId: number, auditSampleId: number, file: File) {
-  return api.post("/elec/audit/accept-report", {
+export function checkAuditReport(
+  entityId: number,
+  auditSampleId: number,
+  file: File
+) {
+  return api.post<Api<UploadCheckReportInfo>>("/elec/auditor/check-report", {
     entity_id: entityId,
     audit_sample_id: auditSampleId,
-    file
+    file,
+  })
+}
+
+export function acceptAuditReport(
+  entityId: number,
+  auditSampleId: number,
+  file: File
+) {
+  return api.post("/elec/auditor/accept-report", {
+    entity_id: entityId,
+    audit_sample_id: auditSampleId,
+    file,
   })
 }
---
+++ web/elec/api/auditor/__init__.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+
+from .years import get_years
+from .snapshot import get_snapshot
+from .applications import get_applications
+from .application_details import get_application_details
+from .filters import get_application_filters
+from .get_sample import get_sample
+from .check_report import check_report
+
+
+urlpatterns = [
+    path("years", get_years, name="elec-auditor-years"),
+    path("snapshot", get_snapshot, name="elec-auditor-snapshot"),
+    path("applications", get_applications, name="elec-auditor-applications"),
+    path("filters", get_application_filters, name="elec-auditor-filters"),
+    path("application-details", get_application_details, name="elec-auditor-application-details"),
+    path("get-sample", get_sample, name="elec-auditor-get-sample"),
+    path("check-report", check_report, name="elec-auditor-check-report"),
+]
