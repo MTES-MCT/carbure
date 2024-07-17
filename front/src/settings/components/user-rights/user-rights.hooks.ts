7575d90dbd8b2817cadd23c445c52cf1e38f986e
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-07-17 09:41:27 +0200

    refactor: move api endpoints to a separated hook file

---
+++ front/src/settings/components/user-rights/user-rights.hooks.ts
@@ -0,0 +1,49 @@
+import useEntity from "carbure/hooks/entity"
+import { useMutation, useQuery } from "common/hooks/async"
+import * as api from "../../api/user-rights"
+import { useTranslation } from "react-i18next"
+import { useNotify } from "common/components/notifications"
+
+export const useGetEntityRights = () => {
+  const entity = useEntity()
+
+  const response = useQuery(api.getEntityRights, {
+    key: "entity-rights",
+    params: [entity.id],
+  })
+
+  return {
+    rights: response.result?.data.data?.requests ?? [],
+    response,
+  }
+}
+
+export const useChangeUserRole = () => {
+  const { t } = useTranslation()
+  const notify = useNotify()
+
+  return useMutation(api.changeUserRole, {
+    invalidates: ["entity-rights"],
+
+    onSuccess: () => {
+      notify(t("Le rôle de l'utilisateur a été modifié !"), {
+        variant: "success",
+      })
+    },
+    onError: () => {
+      notify(t("Le rôle de l'utilisateur n'a pas pu être modifié !"), {
+        variant: "danger",
+      })
+    },
+  })
+}
+
+export const useRevokeUserRights = () =>
+  useMutation(api.revokeUserRights, {
+    invalidates: ["entity-rights"],
+  })
+
+export const useAcceptUserRights = () =>
+  useMutation(api.acceptUserRightsRequest, {
+    invalidates: ["entity-rights"],
+  })
---
+++ front/src/settings/components/user-rights/user-rights.tsx
@@ -0,0 +1,40 @@
+import useEntity from "carbure/hooks/entity"
+import { UserRightsTable } from "common/components/user-rights-table"
+import {
+  useAcceptUserRights,
+  useChangeUserRole,
+  useGetEntityRights,
+  useRevokeUserRights,
+} from "./user-rights.hooks"
+
+export const EntityUserRights = () => {
+  const entity = useEntity()
+
+  const { rights } = useGetEntityRights()
+
+  const changeUserRole = useChangeUserRole()
+
+  const revokeRight = useRevokeUserRights()
+
+  const acceptRight = useAcceptUserRights()
+
+  return (
+    <UserRightsTable
+      rights={rights}
+      isLoadingEditUserRight={changeUserRole.loading}
+      onChangeUserRole={(role, request) =>
+        changeUserRole.execute(entity.id, request.user[0], role!)
+      }
+      onAcceptUser={(request) => acceptRight.execute(entity.id, request.id)}
+      onRevokeUser={(request) =>
+        revokeRight.execute(entity.id, request.user[0])
+      }
+      // Reject and revoke call the same endpoint, just UI is different
+      onRejectUser={(request) =>
+        revokeRight.execute(entity.id, request.user[0])
+      }
+    />
+  )
+}
+
+export default EntityUserRights
