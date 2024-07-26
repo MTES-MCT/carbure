7f016ae52bb09ec99f9a3232b50576d9d61f6a56
Damien Romito <damien@maracuja.ac>
2024-07-26 13:20:49 +0200

    [front] mock api error and validation

---
+++ front/src/elec-auditor/components/details-pending-report-valid.tsx
@@ -0,0 +1,56 @@
+import { UploadCheckReportInfo } from "carbure/types"
+import Button from "common/components/button"
+import { Send } from "common/components/icons"
+import { useNotify } from "common/components/notifications"
+import { useMutation } from "common/hooks/async"
+import { useTranslation } from "react-i18next"
+import * as api from "elec-auditor/api"
+import useEntity from "carbure/hooks/entity"
+
+const ReportValidSection = ({ applicationId, header, file, fileName, onReportAccepted }: {
+  applicationId: number,
+  header: JSX.Element,
+  file: File,
+  fileName: string,
+  onReportAccepted: () => void
+}) => {
+  const { t } = useTranslation()
+  const notify = useNotify()
+  const entity = useEntity()
+
+  const acceptAuditReport = useMutation(api.acceptAuditReport, {
+    onSuccess: () => {
+      onReportAccepted()
+    },
+    onError: () => {
+      notify(
+        t(
+          "L'envoi de votre rapport d'audita échoué. Merci de contacter l'équipe Carbure"
+        ),
+        {
+          variant: "danger",
+        }
+      )
+    }
+  })
+
+  const onAcceptFile = () => {
+    acceptAuditReport.execute(entity.id, applicationId, file)
+  }
+
+
+
+  return <>
+    <main>
+      {header}
+      <p>{t("Votre fichier d'audit {{fileName}} ne comporte aucune erreur.", { fileName })}</p>
+      <p>{t("Les informations peuvent être transmises à la DGEC.")}</p>
+    </main>
+    <footer>
+      <Button icon={Send} label={t("Transmettre le résultat d'audit")} variant="primary" action={onAcceptFile} />
+    </footer>
+  </>
+}
+
+
+export default ReportValidSection
