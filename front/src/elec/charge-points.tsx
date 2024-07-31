4f4b864dfe3e75f50abe056bb480d74d4652c9b3
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-07-31 14:49:41 +0200

    feat: add snapshot query charge points

--- front/src/elec/charge-points.tsx
+++ front/src/elec/charge-points.tsx
@@ -1,13 +1,29 @@
+import useEntity from "carbure/hooks/entity"
 import { Main } from "common/components/scaffold"
 import Select from "common/components/select"
+import { useQuery } from "common/hooks/async"
 import useYears from "common/hooks/years"
 import { useTranslation } from "react-i18next"
 import * as api from "./api-charge-points"
 import { ChargePointsTabs } from "./charge-points-tabs"
+import { ChargePointsSnapshot } from "./types-charge-points"
+
+const defaultSnapshot: ChargePointsSnapshot = {
+  charge_points: 0,
+  charge_points_pending: 0,
+  meter_reading: 0,
+}
 
 const ChargePoints = () => {
   const { t } = useTranslation()
+  const entity = useEntity()
   const years = useYears("charge-points", api.getYears)
+  const snapshotResponse = useQuery(api.getChargePointsSnapshot, {
+    key: "charge-points-snapshot",
+    params: [entity.id, years.selected],
+  })
+
+  const snapshot = snapshotResponse.result?.data.data ?? defaultSnapshot
 
   return (
     <Main>
@@ -27,7 +43,10 @@ const ChargePoints = () => {
         </section>
 
         <section>
-          <ChargePointsTabs />
+          <ChargePointsTabs
+            loading={snapshotResponse.loading}
+            snapshot={snapshot}
+          />
         </section>
       </header>
     </Main>
