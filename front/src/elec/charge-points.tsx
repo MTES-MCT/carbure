09d93eeee6f78ed3177e22c663985164b7f2506d
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-07-31 13:24:55 +0200

    feat: add years url charge points

--- front/src/elec/charge-points.tsx
+++ front/src/elec/charge-points.tsx
@@ -1,5 +1,37 @@
+import { Main } from "common/components/scaffold"
+import Select from "common/components/select"
+import useYears from "common/hooks/years"
+import { useTranslation } from "react-i18next"
+import * as api from "./api-charge-points"
+import { ChargePointsTabs } from "./charge-points-tabs"
+
 const ChargePoints = () => {
-  return <div>charge points page</div>
+  const { t } = useTranslation()
+  const years = useYears("charge-points", api.getYears)
+
+  return (
+    <Main>
+      <header>
+        <section>
+          <h1>{t("Points de recharge")}</h1>
+
+          <Select
+            loading={years.loading}
+            variant="inline"
+            placeholder={t("Choisir une année")}
+            value={years.selected}
+            onChange={years.setYear}
+            options={years.options}
+            sort={(year) => -year.value}
+          />
+        </section>
+
+        <section>
+          <ChargePointsTabs />
+        </section>
+      </header>
+    </Main>
+  )
 }
 
 export default ChargePoints
