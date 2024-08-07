ce0d8ccce992805eca1a27b50aa2e0602fd63969
Yohann <yohann.ulreich@gmail.com>
2024-08-07 11:03:31 +0200

    [back] New ElecMeter model and migrations

--- web/elec/models/elec_meter_reading.py
+++ web/elec/models/elec_meter_reading.py
@@ -3,6 +3,7 @@ from django.db import models
 from core.models import Entity
 from elec.models.elec_charge_point import ElecChargePoint
 from elec.models.elec_meter_reading_application import ElecMeterReadingApplication
+from elec.models.elec_meter import ElecMeter
 
 
 class ElecMeterReading(models.Model):
@@ -17,3 +18,4 @@ class ElecMeterReading(models.Model):
     charge_point = models.ForeignKey(ElecChargePoint, on_delete=models.deletion.CASCADE, related_name="elec_meter_readings")
     cpo = models.ForeignKey(Entity, on_delete=models.deletion.CASCADE, related_name="elec_meter_readings")
     application = models.ForeignKey(ElecMeterReadingApplication, on_delete=models.deletion.CASCADE, related_name="elec_meter_readings")  # fmt:skip
+    meter = models.ForeignKey(ElecMeter, on_delete=models.CASCADE, null=True, blank=False, related_name="elec_meter_readings")  # fmt:skip
