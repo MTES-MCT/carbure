31ea42178f84dade996d9c399444e26f9ecd6096
Yohann <yohann.ulreich@gmail.com>
2024-08-14 12:33:38 +0200

    [back] Add new post_compile to compile translations on deploy, and small files reorganization

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 # setup static files and database
-bash ./web/setup.sh
+bash ./bin/post_deploy.sh
 
 # start dev server and huey consumer
 python3 ./web/manage.py run_huey --simple --scheduler-interval 10 &
