9432834508089d8a9073f4f064642472b9844b88
Damien Romito <damien@maracuja.ac>
2024-08-27 19:17:17 +0200

    [front] accept audit without report + fix Alert multiline component

--- front/src/elec-audit-admin/components/charge-points/details-in-progress.tsx
+++ front/src/elec-audit-admin/components/charge-points/details-in-progress.tsx
@@ -1,13 +1,14 @@
 import { Button } from "common/components/button"
 import Checkbox from "common/components/checkbox"
 import { Divider } from "common/components/divider"
-import { Check, Cross, Download } from "common/components/icons"
+import { AlertCircle, Check, Cross, Download } from "common/components/icons"
 import { ElecChargePointsApplicationDetails } from "elec/types"
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
 import ApplicationSummary from "./details-application-summary"
 import SampleSummary from "../sample/details-sample-summary"
 import ChargePointsSampleMap from "../sample/sample-map"
+import Alert from "common/components/alert"
 
 interface ChargePointsApplicationDetailsInProgressProps {
   chargePointApplication: ElecChargePointsApplicationDetails | undefined
@@ -39,11 +40,14 @@ export const ChargePointsApplicationDetailsInProgress = ({
           </section>
         )}
         <section>
+          <Alert icon={AlertCircle} variant="warning" multiline label={t("L'échantillon a été reçu par l'auditeur. Vous serez informé par email lorsque ce dernier aura complété le rapport d'audit sur son espace sur Carbure.")} />
+        </section>
+        <section>
           <Checkbox
             value={confirmCheckbox}
             onChange={setConfirmCheckbox}
             label={t(
-              "Je confirme avoir reçu le résultat d'audit de la part de l'auditeur par e-mail afin de valider ou refuser l'inscription de ces points de charge."
+              "Je confirme avoir reçu le résultat d'audit ou souhaite valider sans audit."
             )}
           />
         </section>
