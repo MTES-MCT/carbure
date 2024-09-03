23c116d8106244a3ffc72537d538933e09255437
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-09-03 10:53:04 +0200

    feat: connect snapshot to pdc list page

--- front/src/elec-charge-points/__test__/data.ts
+++ front/src/elec-charge-points/__test__/data.ts
@@ -6,7 +6,7 @@ export const elecChargePointsSnapshot: ChargePointsSnapshot = {
   charge_points: 500,
   meter_reading_applications: 245,
   charge_point_applications: 221,
-  charge_points_pending: 221,
-  charge_points_accepted: 11,
-  charge_points_audit_in_progress: 10,
+  pending: 221,
+  accepted: 11,
+  audit_in_progress: 10,
 }
--- front/src/elec-charge-points/types.ts
+++ front/src/elec-charge-points/types.ts
@@ -4,7 +4,7 @@ export type ChargePointsSnapshot = {
   charge_points: number
 
   // Data used for status tab in "charge points" page
-  charge_points_pending: number
-  charge_points_audit_in_progress: number
-  charge_points_accepted: number
+  pending: number
+  audit_in_progress: number
+  accepted: number
 }
