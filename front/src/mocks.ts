48fe2171f2ad80c8ddb20fa69b202663aba7c40d
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-09-04 14:19:54 +0200

    refactor: renaming folders pages

--- front/src/mocks.ts
+++ front/src/mocks.ts
@@ -5,7 +5,7 @@ import * as elecAuditMocks from "elec-auditor/__test__/api"
 import * as elecMocks from "elec/__test__/api"
 import { okInviteUser } from "settings/__test__/api"
 import * as elecChargePointsMocks from "elec-charge-points/__test__/api"
-import * as elecChargePointsListPageMocks from "elec-charge-points/pages/charge-points-list/__test__/api"
+import * as elecChargePointsListPageMocks from "elec-charge-points/pages/list/__test__/api"
 export const worker = setupWorker(
   ...Object.values(elecAdminAuditMocks),
   ...Object.values(elecMocks),
