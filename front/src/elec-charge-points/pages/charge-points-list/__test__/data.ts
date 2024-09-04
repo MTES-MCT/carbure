ce7455d2c7370ddb25af93859dd8280d43fafbb0
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-09-04 10:14:37 +0200

    fix: fix errors after rebase

--- front/src/elec-charge-points/pages/charge-points-list/__test__/data.ts
+++ front/src/elec-charge-points/pages/charge-points-list/__test__/data.ts
@@ -1,7 +1,7 @@
 import { ChargePointsListData } from "../types"
 
 export const chargePointsList: ChargePointsListData = {
-  charge_points_list: [],
+  elec_charge_points: [],
   from: 0,
   ids: [],
   returned: 10,
