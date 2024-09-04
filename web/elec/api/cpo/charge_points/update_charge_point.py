b3dc6f5c5080b09599f5b412185178c942dbc290
Yohann <yohann.ulreich@gmail.com>
2024-09-04 11:27:20 +0200

    [back] Fixes for update_charge_point endpoint

--- web/elec/api/cpo/charge_points/update_charge_point.py
+++ web/elec/api/cpo/charge_points/update_charge_point.py
@@ -4,7 +4,7 @@ from core.carburetypes import CarbureError
 from core.common import ErrorResponse, SuccessResponse
 from core.decorators import check_user_rights
 from core.models import Entity
-from elec.models import ElecChargePoint, ElecChargePointApplication
+from elec.models import ElecChargePointApplication
 from elec.serializers.elec_charge_point import ElecChargePointUpdateSerializer
 
 
@@ -16,21 +16,26 @@ class ChargePointUpdateError:
 @require_POST
 @check_user_rights(entity_type=[Entity.CPO])
 def update_charge_point(request, entity, entity_id):
-    serializer = ElecChargePointUpdateSerializer(data=request.POST)
+    serializer = ElecChargePointUpdateSerializer(data=request.POST, partial=True)
     if not serializer.is_valid():
         return ErrorResponse(400, CarbureError.MALFORMED_PARAMS, serializer.errors)
 
-    cp = ElecChargePoint.objects.get(id=request.POST["id"])
+    cp = serializer.validated_data["id"]
 
     if cp.cpo != entity:
         return ErrorResponse(400, ChargePointUpdateError.CP_NOT_FOUND_ON_CPO)
 
-    if cp.application.status != ElecChargePointApplication.PENDING:
+    validated_data = serializer.validated_data
+
+    if "charge_point_id" in validated_data and cp.application.status != ElecChargePointApplication.PENDING:
         return ErrorResponse(400, ChargePointUpdateError.CP_CANNOT_BE_UPDATED)
 
-    validated_data = serializer.validated_data
-    cp.charge_point_id = validated_data["charge_point_id"]
-    cp.measure_reference_point_id = validated_data["measure_reference_point_id"]
+    if "charge_point_id" in validated_data:
+        cp.charge_point_id = validated_data["charge_point_id"]
+
+    if "measure_reference_point_id" in validated_data:
+        cp.measure_reference_point_id = validated_data["measure_reference_point_id"]
+
     cp.save()
 
     return SuccessResponse()
