f2e24d9c8d8fb4ac6f077c01118c4760485b8099
benjamin-grilleres <contact@benjamingrilleres.com>
2024-09-11 08:30:06 +0000

    Feature/move code elec to charge point page

---
+++ front/src/elec-charge-points/__test__/data.ts
@@ -0,0 +1,9 @@
+import { ChargePointsSnapshot } from "../types"
+
+export const elecChargePointsYears = [2023, 2024]
+
+export const elecChargePointsSnapshot: ChargePointsSnapshot = {
+  charge_points: 500,
+  meter_reading_applications: 245,
+  charge_point_applications: 221,
+}
