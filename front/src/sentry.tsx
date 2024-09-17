12a8ea199338238e338fcaed6220db68e9433c51
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-09-17 10:33:40 +0200

    feat(tech): terminate sentry integration with the frontend

--- front/src/sentry.tsx
+++ front/src/sentry.tsx
@@ -1,18 +1,46 @@
 import { PropsWithChildren } from "react"
 import * as Sentry from "@sentry/react"
+import Alert from "common/components/alert"
+import { Trans, useTranslation } from "react-i18next"
+import { MailTo } from "common/components/button"
 
-// Define sentry only if url is defined in .env AND environment is local
+// Define sentry only if url/env are defined in .env AND environment is not local
 if (
   process.env.REACT_APP_SENTRY_DSN &&
+  process.env.REACT_APP_SENTRY_ENV &&
   process.env.NODE_ENV !== "development"
 ) {
   Sentry.init({
     dsn: process.env.REACT_APP_SENTRY_DSN,
+    environment: process.env.REACT_APP_SENTRY_ENV,
   })
 }
 
 const FallbackComponent = () => {
-  return <div>An error has occured</div>
+  const { t } = useTranslation()
+  return (
+    <Alert variant="danger" style={{ margin: "auto", textAlign: "center" }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          rowGap: "10px",
+        }}
+      >
+        <p>
+          {t("Nous sommes désolé, une erreur technique est survenue.")}
+          <br />
+          {t(
+            "Merci de recharger la page ou nous contacter si le problème persiste."
+          )}
+        </p>
+        <MailTo user="carbure" host="beta.gouv.fr">
+          <Trans>Email</Trans>
+        </MailTo>
+      </div>
+    </Alert>
+  )
 }
 
 export const SentryProvider = ({ children }: PropsWithChildren) => {
