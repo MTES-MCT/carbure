e3fb29adfe785796c3960e9d436456979f648d99
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-09-17 11:58:34 +0200

    fix(auditor): remove ui bug from button

--- front/src/elec-audit-admin/components/sample/details-sample-audit-done-section.tsx
+++ front/src/elec-audit-admin/components/sample/details-sample-audit-done-section.tsx
@@ -8,34 +8,48 @@ import SampleSummary from "./details-sample-summary"
 
 const SampleDetailsAuditDoneSection = ({
   sample,
-  onDownloadSample
-}:
-  {
-    sample: ElecApplicationSample,
-    onDownloadSample: () => void
-  }) => {
-
+  onDownloadSample,
+}: {
+  sample: ElecApplicationSample
+  onDownloadSample: () => void
+}) => {
   const { t } = useTranslation()
 
   const commentCount = sample?.comment_count
   const auditorName = sample?.auditor_name
 
-  return <>
-    <section>
-      <SampleSummary sample={sample} />
-      <Divider />
-      <strong>{t("Résultat d'audit")}</strong>
-      <p>{t("Télécharger directement le fichier d'audit pour visualiser les informations entrées par l'auditeur.")}</p>
-      <Button icon={Download} label={t("Télécharger le rapport d'audit")} variant="secondary" action={onDownloadSample} />
-    </section>
-    {commentCount &&
+  return (
+    <>
       <section>
-        <Alert icon={Message} variant="info" >
-          <Trans defaults={"<b>{{ count }} points de charges </b>  ont été commentés par l'auditeur <b>{{auditorName}}</b> dans le fichier excel."} values={{ count: commentCount, auditorName }} />
-        </Alert>
+        <SampleSummary sample={sample} />
+        <Divider />
+        <strong>{t("Résultat d'audit")}</strong>
+        <p>
+          {t(
+            "Télécharger directement le fichier d'audit pour visualiser les informations entrées par l'auditeur."
+          )}
+        </p>
+        <Button
+          icon={Download}
+          label={t("Télécharger le rapport d'audit")}
+          variant="secondary"
+          action={onDownloadSample}
+        />
       </section>
-    }</>
-
+      {commentCount && commentCount > 0 && (
+        <section>
+          <Alert icon={Message} variant="info">
+            <Trans
+              defaults={
+                "<b>{{ count }} points de charges </b>  ont été commentés par l'auditeur <b>{{auditorName}}</b> dans le fichier excel."
+              }
+              values={{ count: commentCount, auditorName }}
+            />
+          </Alert>
+        </section>
+      )}
+    </>
+  )
 }
 
 export default SampleDetailsAuditDoneSection
