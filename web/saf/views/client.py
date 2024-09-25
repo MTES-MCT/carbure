093d2013ef0f6c81875de069febab337d49ba3b8
François Rossigneux <francois.rossigneux@beta.gouv.fr>
2024-09-26 00:10:33 +0200

    [back] SAF refactoring

---
+++ web/saf/views/client.py
@@ -0,0 +1,29 @@
+from django.db.models import Q
+from rest_framework.mixins import (
+    ListModelMixin,
+    RetrieveModelMixin,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
+
+from core.models import Entity
+from saf.filters import ClientFilter
+from saf.serializers.saf_ticket import SafClientSerializer
+
+
+class ClientViewSet(
+    ListModelMixin,
+    RetrieveModelMixin,
+    GenericViewSet,
+):
+    lookup_field = "id"
+    permission_classes = (IsAuthenticated,)
+    serializer_class = SafClientSerializer
+    filterset_class = ClientFilter
+    search_fields = ["name"]
+
+    def get_queryset(self):
+        is_airline = Q(entity_type=Entity.AIRLINE)
+        is_saf_operator = Q(entity_type=Entity.OPERATOR, has_saf=True)
+        queryset = Entity.objects.filter(is_airline | is_saf_operator).order_by("name")
+        return queryset
