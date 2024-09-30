7db6c988d136f9e3d9dc1a2d4ead5637d1b45762
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-09-30 11:15:53 +0200

    fix: specify custom hmr path

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -9,5 +9,12 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+    hmr: {
+      path: "/ws",
+    },
+  },
+  build: {
+    outDir: "./build",
+    emptyOutDir: true,
   },
 })
