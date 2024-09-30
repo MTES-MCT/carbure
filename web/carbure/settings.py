b00c7f464a2e11bcb4926008044c06c66d4b04c4
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-09-30 12:08:32 +0200

    fix: move assets to static

--- web/carbure/settings.py
+++ web/carbure/settings.py
@@ -199,7 +199,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = "/assets/"
-STATIC_ROOT = os.path.join(BASE_DIR, "assets")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 WHITENOISE_ALLOW_ALL_ORIGINS = False
