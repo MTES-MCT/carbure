a9a4c626f1f69d7a6f5bfe54cbd924eb0e37b41f
Julien Falxa <jfalxa.sta@gmail.com>
2024-10-02 13:41:12 +0200

    fix: more robust duplicate detection in meter reading upload

--- web/elec/services/import_meter_reading_excel.py
+++ web/elec/services/import_meter_reading_excel.py
@@ -1,10 +1,13 @@
+from collections import defaultdict
 import pandas as pd
 from typing import Iterable
 from django.core.files.uploadedfile import UploadedFile
 from django.utils.translation import gettext_lazy as _
 from django import forms
+from django.db.models import QuerySet
 from core.utils import Validator
 from elec.models.elec_charge_point import ElecChargePoint
+from elec.models.elec_meter_reading import ElecMeterReading
 from elec.models.elec_meter_reading_application import ElecMeterReadingApplication
 from elec.services.create_meter_reading_excel import (
     get_previous_readings_by_charge_point,
@@ -14,6 +17,7 @@ from elec.services.create_meter_reading_excel import (
 def import_meter_reading_excel(
     excel_file: UploadedFile,
     existing_charge_points: Iterable[ElecChargePoint],
+    previous_readings: QuerySet[ElecMeterReading],
     previous_application: ElecMeterReadingApplication = None,
     renewable_share: int = 1,
 ):
@@ -23,6 +27,7 @@ def import_meter_reading_excel(
     meter_readings_data = ExcelMeterReadings.validate_meter_readings(
         original_meter_readings_data,
         existing_charge_points,
+        previous_readings,
         previous_application,
         renewable_share,
     )
@@ -62,6 +67,7 @@ class ExcelMeterReadings:
     def validate_meter_readings(
         meter_readings: list[dict],
         registered_charge_points: Iterable[ElecChargePoint],
+        previous_readings: QuerySet[ElecMeterReading],
         previous_application: ElecMeterReadingApplication = None,
         renewable_share: int = 1,
     ):
@@ -70,17 +76,24 @@ class ExcelMeterReadings:
             registered_charge_points, previous_application
         )
 
+        previous_reading_dates_by_charge_point = defaultdict(list)
+        for reading in previous_readings:
+            previous_reading_dates_by_charge_point[reading.charge_point_id].append(
+                reading.reading_date
+            )
+
         context = {
             "renewable_share": renewable_share,
             "charge_point_by_id": charge_point_by_id,
             "previous_readings_by_charge_point": previous_readings_by_charge_point,
+            "previous_reading_dates_by_charge_point": previous_reading_dates_by_charge_point,
         }
 
         return ExcelMeterReadingValidator.bulk_validate(meter_readings, context)
 
 
 class ExcelMeterReadingValidator(Validator):
-    charge_point_id = forms.CharField()
+    charge_point_id = forms.IntegerField()
     extracted_energy = forms.FloatField(min_value=0)
     reading_date = forms.DateField(input_formats=Validator.DATE_FORMATS)
     renewable_energy = forms.FloatField()
@@ -110,6 +123,9 @@ class ExcelMeterReadingValidator(Validator):
     def validate(self, meter_reading):
         charge_point = self.context.get("charge_point")
         previous_extracted_energy = self.context.get("previous_extracted_energy")
+        previous_reading_dates_by_charge_point = self.context.get(
+            "previous_reading_dates_by_charge_point"
+        )
 
         if charge_point is None:
             self.add_error(
@@ -120,4 +136,13 @@ class ExcelMeterReadingValidator(Validator):
             self.add_error(
                 "extracted_energy",
                 _("La quantité d'énergie soutirée est inférieure au précédent relevé."),
+            )
+
+        charge_point_reading_dates = previous_reading_dates_by_charge_point.get(
+            meter_reading.get("charge_point_id"), []
+        )
+        if meter_reading.get("reading_date") in charge_point_reading_dates:
+            self.add_error(
+                "reading_date",
+                _(f"Le relevé du {meter_reading.get("reading_date")} existe déjà"),
             )
