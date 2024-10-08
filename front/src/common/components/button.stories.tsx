b03c5438e2ee562107edc1e077562075fdab8e35
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-10-08 16:58:45 +0200

    fix: try to throw CI with new screenshot

--- front/src/common/components/button.stories.tsx
+++ front/src/common/components/button.stories.tsx
@@ -37,6 +37,7 @@ export const AllVariants: Story = {
         <Button {...args} variant="warning" label="Warning" />
         <Button {...args} variant="link" label="With link" />
         <Button {...args} variant="icon" icon={Cross} />
+        <Button {...args} variant="icon" icon={Cross} />
       </div>
     )
   },
