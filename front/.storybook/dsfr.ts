9ad204e07a4bac94693e2bbd7cf9f330e121d581
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-10-08 15:36:56 +0200

    feat: add icon story

---
+++ front/.storybook/dsfr.ts
@@ -0,0 +1,4 @@
+// import dsfr files required to have the app running properly
+import "@codegouvfr/react-dsfr/dsfr/utility/icons/icons.min.css"
+import "@codegouvfr/react-dsfr/dsfr/core/core.min.css"
+import "@codegouvfr/react-dsfr/dsfr/component/button/button.min.css"
