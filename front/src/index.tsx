39f700b6aadd55f1ebd6ca91ee82984ff7accd94
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-10-14 14:44:34 +0200

    refactor: get entity type from backend

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,3 +1,5 @@
+/// <reference path="./api-schema.d.ts" />
+
 import React, { Suspense } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
