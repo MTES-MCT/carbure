5d1ce7bd85feb179046ec3197674bead884374b7
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-10-15 15:00:50 +0200

    feat: format number durability fields

--- front/src/saf/pages/operator/ticket-source-details/parent-lot/durability-fields.tsx
+++ front/src/saf/pages/operator/ticket-source-details/parent-lot/durability-fields.tsx
@@ -1,6 +1,10 @@
 import { Fieldset } from "common/components/form"
-import { NumberInput, TextInput } from "common/components/input"
-import { formatGHG, formatPercentage } from "common/utils/formatters"
+import { TextInput } from "common/components/input"
+import {
+  formatGHG,
+  formatNumber,
+  formatPercentage,
+} from "common/utils/formatters"
 import { useTranslation } from "react-i18next"
 import { SafDurability } from "../../../../types"
 
@@ -10,41 +14,41 @@ const DurabilityFields = ({ durability }: { durability: SafDurability }) => {
   return (
     <>
       <Fieldset small label={t("Émissions")}>
-        <NumberInput
+        <TextInput
           hasTooltip
           label="EEC"
           title={t("Émissions résultant de l'extraction ou de la culture des matières premières")} // prettier-ignore
-          value={durability.eec}
+          value={formatNumber(durability.eec)}
           readOnly
         />
-        <NumberInput
+        <TextInput
           hasTooltip
           label="EL"
           title={t("Émissions annualisées résultant de modifications des stocks de carbone dues à des changements dans l'affectation des sols")} // prettier-ignore
-          value={durability.el}
+          value={formatNumber(durability.el)}
           readOnly
         />
-        <NumberInput
+        <TextInput
           required
           hasTooltip
           label="EP"
           title={t("Émissions résultant dela transformation")}
-          value={durability.ep}
+          value={formatNumber(durability.ep)}
           readOnly
         />
-        <NumberInput
+        <TextInput
           required
           hasTooltip
           label="ETD"
           title={t("Émissions résultant du transport et de la distribution")}
-          value={durability.etd}
+          value={formatNumber(durability.etd)}
           readOnly
         />
-        <NumberInput
+        <TextInput
           label="EU"
           hasTooltip
           title={t("Émissions résultant du carburant à l'usage")}
-          value={durability.eu}
+          value={formatNumber(durability.eu)}
           readOnly
         />
 
@@ -56,32 +60,32 @@ const DurabilityFields = ({ durability }: { durability: SafDurability }) => {
         />
       </Fieldset>
       <Fieldset small label={t("Réductions")}>
-        <NumberInput
+        <TextInput
           label="ESCA"
           hasTooltip
           title={t("Réductions d'émissions dues à l'accumulation du carbone dans les sols grâce à une meilleure gestion agricole")} // prettier-ignore
-          value={durability.esca}
+          value={formatNumber(durability.esca)}
           readOnly
         />
-        <NumberInput
+        <TextInput
           label="ECCS"
           hasTooltip
           title={t("Réductions d'émissions dues au piégeage et au stockage géologique du carbone")} // prettier-ignore
-          value={durability.eccs}
+          value={formatNumber(durability.eccs)}
           readOnly
         />
-        <NumberInput
+        <TextInput
           label="ECCR"
           hasTooltip
           title={t("Réductions d'émissions dues au piégeage et à la substitution du carbone")} // prettier-ignore
-          value={durability.eccr}
+          value={formatNumber(durability.eccr)}
           readOnly
         />
-        <NumberInput
+        <TextInput
           label="EEE"
           hasTooltip
           title={t("Réductions d'émissions dues à la production excédentaire d'électricité dans le cadre de la cogénération")} // prettier-ignore
-          value={durability.eee}
+          value={formatNumber(durability.eee)}
           readOnly
         />
 
