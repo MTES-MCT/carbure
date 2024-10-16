68c7cee1a46ce58da12dbaf7c86607a27aa8c814
François Rossigneux <francois.rossigneux@beta.gouv.fr>
2024-10-16 16:33:19 +0200

    [back] fix serializer

--- web/saf/views/ticket_source/mixins/grouped_assign.py
+++ web/saf/views/ticket_source/mixins/grouped_assign.py
@@ -47,8 +47,8 @@ class GroupAssignActionMixin:
         ticket_sources_ids = serializer.validated_data["ticket_sources_ids"]
         client_id = serializer.validated_data["client_id"]
         volume = serializer.validated_data["volume"]
-        agreement_reference = serializer.validated_data["agreement_reference"]
-        agreement_date = serializer.validated_data["agreement_date"]
+        agreement_reference = serializer.validated_data.get("agreement_reference")
+        agreement_date = serializer.validated_data.get("agreement_date")
         free_field = serializer.validated_data.get("free_field")
         assignment_period = serializer.validated_data["assignment_period"]
 
