0eb64112ac320fa6398dbd394ccfcb4fc145c8f0
Yohann <yohann.ulreich@gmail.com>
2024-10-23 17:44:10 +0200

    feat(elec): script to delete duplicate charge points

---
+++ web/elec/management/commands/remove_duplicate_cp.py
@@ -0,0 +1,79 @@
+# Importer les modules
+from collections import defaultdict
+
+from django.core.management.base import BaseCommand
+from django.db.models import Count, Max, OuterRef, Subquery
+
+from elec.models import ElecChargePoint, ElecMeterReading
+
+
+class Command(BaseCommand):
+    help = "Delete duplicate charge points"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--dry-run", action="store_true", help="Dry run mode, no pdc deleted", default=True)
+
+    def handle(self, *args, **options):
+        dry_run = options["dry_run"]
+
+        print("-- Dry run mode : ", dry_run)
+
+        # Subquery to get charge points with at least 2 occurrences
+        duplicate_charge_points = (
+            ElecChargePoint.objects.values("charge_point_id")
+            .annotate(count=Count("charge_point_id"))
+            .filter(count__gt=1)
+            .values("charge_point_id")
+        )
+        # Get charge points objects
+        charge_points = ElecChargePoint.objects.filter(charge_point_id__in=Subquery(duplicate_charge_points))
+
+        print("Nombre de doublons : ", charge_points.count())
+        print("Nombre de pdc à conserver : ", len(duplicate_charge_points))
+
+        # Subquery to get the most recent reading date for each charge point
+        latest_readings = (
+            ElecMeterReading.objects.filter(meter__charge_point_id=OuterRef("charge_point_id"))
+            .values("meter__charge_point_id")
+            .annotate(latest_reading_date=Max("reading_date"))
+            .values("latest_reading_date")
+        )
+
+        # Annotate the main query with the latest reading date
+        queryset = (
+            charge_points.annotate(latest_reading_date=Subquery(latest_readings.values("reading_date")[:1]))
+            .values("id", "charge_point_id", "latest_reading_date")
+            .order_by("charge_point_id")
+        )
+
+        # Group charge points by charge point ID
+        grouped_charge_points = defaultdict(list)
+        for item in queryset:
+            grouped_charge_points[item["charge_point_id"]].append(item)
+
+        ids_to_keep = []
+
+        # For each group of charge points, apply rules to select the one to keep
+        for _, items in grouped_charge_points.items():
+            # Keep charge points wiht reading
+            with_reading = [item for item in items if item["latest_reading_date"] is not None]
+
+            if with_reading:
+                # If there are, we keep the one with the more recent reading
+                selected_point = max(with_reading, key=lambda x: x["latest_reading_date"])
+            else:
+                # Else we keep the one with the lowest ID (first created)
+                selected_point = min(items, key=lambda x: x["id"])
+
+            # Add charge point ID to the list of IDs to keep
+            ids_to_keep.append(selected_point["id"])
+
+        # Delete all charge points that are not in the list of IDs to keep
+        charge_points_to_delete = charge_points.exclude(id__in=ids_to_keep)
+        print("Points de charge à supprimer :", charge_points_to_delete.count())
+
+        if not dry_run:
+            charge_points_to_delete.delete()
+            print("Points de charge supprimés !")
+
+        print(f"Points de charge conservés : {len(ids_to_keep)}")
