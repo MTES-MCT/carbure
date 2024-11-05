e32a43e09fb0175a8d152b81490ac571de1bb15d
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-11-05 09:20:03 +0100

    feat: add search input

--- front/src/common/components/list/list.stories.tsx
+++ front/src/common/components/list/list.stories.tsx
@@ -1,15 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import { List } from "./list"
+import { useState } from "react"
 
 const meta: Meta<typeof List> = {
   component: List,
-}
-
-type Story = StoryObj<typeof List>
-
-export default meta
-
-export const DefaultList: Story = {
   args: {
     items: [
       { label: "Item 1", value: "1" },
@@ -17,7 +11,24 @@ export const DefaultList: Story = {
       { label: "Item 3", value: "3" },
     ],
   },
+  render: (args) => {
+    const [value, setValue] = useState<string | undefined>("")
+
+    return (
+      <List
+        {...args}
+        selectedValue={value}
+        onSelectValue={(item) => setValue(item as string)}
+      />
+    )
+  },
 }
+
+type Story = StoryObj<typeof List>
+
+export default meta
+
+export const DefaultList: Story = {}
 
 export const CustomStyleList: Story = {
   args: {
@@ -27,5 +38,40 @@ export const CustomStyleList: Story = {
         List of div paragraphs : {item.label}
       </div>
     ),
+  },
+}
+
+export const SelectedValue: Story = {
+  args: {
+    ...DefaultList.args,
+    selectedValue: "1",
+  },
+}
+
+export const SelectedValues: Story = {
+  args: {
+    ...DefaultList.args,
+    multiple: true,
+    selectedValues: ["1", "2"],
+  },
+  render: (args) => {
+    const [value, setValue] = useState<string[] | undefined>(
+      args.selectedValues as string[]
+    )
+
+    return (
+      <List
+        {...args}
+        selectedValues={value}
+        onSelectValues={(items) => setValue(items as string[])}
+      />
+    )
+  },
+}
+
+export const SearchList: Story = {
+  args: {
+    ...DefaultList.args,
+    search: true,
   },
 }
