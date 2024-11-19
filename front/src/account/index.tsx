d9323888e839e65247fae9913c17cbe5f9afa48c
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-11-19 16:48:03 +0100

    refactor: add all titles and remove tabs

--- front/src/account/index.tsx
+++ front/src/account/index.tsx
@@ -1,4 +1,4 @@
-import { Trans, useTranslation } from "react-i18next"
+import { useTranslation } from "react-i18next"
 import { Main } from "common/components/scaffold"
 import { AccountAccesRights, EntityDialog } from "./components/access-rights"
 import { AccountAuthentication } from "./components/authentication"
@@ -6,20 +6,17 @@ import useTitle from "common/hooks/title"
 import { CompanyRegistrationDialog } from "companies/components/registration-dialog"
 import { Route, Routes, useNavigate } from "react-router-dom"
 import { ROUTE_URLS } from "common/utils/routes"
+import { usePrivateNavigation } from "common/layouts/navigation"
 
 const Account = () => {
   const { t } = useTranslation()
   const navigate = useNavigate()
 
   useTitle(t("Mon compte"))
+  usePrivateNavigation(t("Mon compte"))
 
   return (
     <Main>
-      <header>
-        <h1>
-          <Trans>Mon compte</Trans>
-        </h1>
-      </header>
       <Routes>
         <Route
           path="add-company"
