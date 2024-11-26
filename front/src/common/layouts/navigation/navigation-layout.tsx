6370108175d5359450b10fb1cef1b42076133ff9
Benjamin Grilleres <bgrilleres@greenweez.com>
2024-11-26 16:06:44 +0100

    fix: fix dev banner style

--- front/src/common/layouts/navigation/navigation-layout.tsx
+++ front/src/common/layouts/navigation/navigation-layout.tsx
@@ -1,17 +1,23 @@
 import { useUser } from "carbure/hooks/user"
+import cl from "clsx"
 import { PropsWithChildren } from "react"
 import { PrivateNavigation } from "./private/private-navigation"
 import { PublicNavigation } from "./public/public-navigation"
 import { CarbureFooter } from "./footer"
 import { PrivateNavigationProvider } from "./private/private-navigation.context"
-// import { DevBanner } from "common/components/dev-banner"
+import { DevBanner } from "common/components/dev-banner"
+import css from "./navigation-layout.module.css"
 
 export const NavigationLayout = ({ children }: PropsWithChildren) => {
   const user = useUser()
   return (
-    <>
-      {/* Find a way to calculate the body height with dev banner */}
-      {/* <DevBanner /> */}
+    <div
+      className={cl(
+        css["navigation-layout"],
+        user.isAuthenticated() && css["authenticated"]
+      )}
+    >
+      <DevBanner />
 
       {user.isAuthenticated() ? (
         <PrivateNavigationProvider>
@@ -24,6 +30,6 @@ export const NavigationLayout = ({ children }: PropsWithChildren) => {
           <CarbureFooter />
         </>
       )}
-    </>
+    </div>
   )
 }
