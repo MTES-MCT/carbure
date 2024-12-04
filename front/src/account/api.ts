ef08e91feb2ae6e0a6c007b848593bc90d91f062
Julien Falxa <jfalxa.sta@gmail.com>
2024-12-04 19:42:37 +0100

    refactor: use typed endpoint for request access endpoint

--- front/src/account/api.ts
+++ front/src/account/api.ts
@@ -1,11 +1,10 @@
 import api from "common/services/api"
+import { api as apiFetch } from "common/services/api-fetch"
 import { UserRole } from "carbure/types"
 
 export function requestAccess(entity_id: number, role: UserRole, comment = "") {
-  return api.post("/user/request-access", {
-    entity_id,
-    comment,
-    role,
+  return apiFetch.POST("/user/request-access", {
+    body: { entity_id, role, comment },
   })
 }
 
