5fdfc795b6223947d8f481e7bdd392a8f356f596
Benjamin Grilleres <bgrilleres@greenweez.com>
2025-01-08 09:43:41 +0100

    feat(ds): add filearea

---
+++ front/src/common/components/inputs2/filearea/filearea.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react"
+import { FileArea } from "./filearea"
+import { DashboardLine } from "common/components/icon"
+
+const meta: Meta<typeof FileArea> = {
+  component: FileArea,
+  title: "common/components/inputs/FileArea",
+  render: (args) => {
+    return (
+      <FileArea {...args}>
+        <div style={{ height: "400px" }}>Drag and drop content anywhere</div>
+      </FileArea>
+    )
+  },
+  args: {
+    label: "Importer le fichier sur la plateforme",
+    icon: DashboardLine,
+  },
+}
+
+type Story = StoryObj<typeof FileArea>
+
+export default meta
+
+export const Default: Story = {}
