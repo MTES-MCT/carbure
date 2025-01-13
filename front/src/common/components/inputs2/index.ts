3b71ba04960b5ca1b1e4cddc2c5804d7517c68d5
Benjamin Grilleres <bgrilleres@greenweez.com>
2025-01-13 15:57:39 +0100

    feat(ds): add checkbox/checkboxgroup

--- front/src/common/components/inputs2/index.ts
+++ front/src/common/components/inputs2/index.ts
@@ -1,5 +1,7 @@
 export * from "./search/search-input"
 export * from "./number/number-input"
 export * from "./text/text-input"
+export * from "./radio/radio"
+export * from "./checkbox/checkbox"
 export * from "./file/file-input/file-input"
 export * from "./file/file-list-input/file-list-input"
