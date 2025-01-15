2a92a571b5d2e98c1e7cec7fe504464d38a31987
Benjamin Grilleres <bgrilleres@greenweez.com>
2025-01-15 11:08:01 +0100

    fix(ds): fix button with ts ignore

--- front/src/common/components/button2/button.tsx
+++ front/src/common/components/button2/button.tsx
@@ -2,7 +2,7 @@ import {
   Button as ButtonDSFR,
   ButtonProps as ButtonDSFRProps,
 } from "@codegouvfr/react-dsfr/Button"
-import { ForwardedRef, forwardRef } from "react"
+import { forwardRef } from "react"
 import cl from "clsx"
 import css from "./button.module.css"
 import { layout, Layout } from "../scaffold"
@@ -30,41 +30,23 @@ export const Button = forwardRef<
   HTMLButtonElement | HTMLAnchorElement,
   ButtonProps
 >(({ customPriority, ...props }, ref) => {
-  if (customPriority === "link") {
-    return (
-      <BaseButton
-        {...props}
-        ref={ref as ForwardedRef<HTMLButtonElement>}
-        className={cl(props.className, css["button-as-link-style"])}
-        {...layout(props)}
-      >
-        {props.children}
-      </BaseButton>
-    )
-  }
-
-  const baseProps = {
-    className: cl(props.className, {
-      [css["button-danger"]]: customPriority === "danger",
-      [css["button-success"]]: customPriority === "success",
-    }),
-  }
-
-  return props.linkProps ? (
+  return (
     <ButtonDSFR
-      {...baseProps}
-      {...props}
-      {...layout(props)}
-      ref={ref as ForwardedRef<HTMLAnchorElement>}
-    />
-  ) : // Couldn't find a better way to specify the iconId for the loading state
-  props.iconId ? (
-    <BaseButton
-      {...baseProps}
       {...props}
       iconId={props.loading ? "ri-loader-line" : props.iconId}
+      className={cl(props.className, {
+        [css["button-danger"] as string]: customPriority === "danger",
+        [css["button-success"] as string]: customPriority === "success",
+        [css["button-as-link-style"] as string]: customPriority === "link",
+      })}
+      priority={
+        customPriority && ["danger", "success"].includes(customPriority)
+          ? "tertiary"
+          : props.priority
+      }
+      {...layout(props)}
+      // @ts-ignore couldn't find a better way to manage different cases for button (anchor, button, icon only)
+      ref={ref}
     />
-  ) : (
-    <BaseButton {...baseProps} {...props} />
   )
 })
