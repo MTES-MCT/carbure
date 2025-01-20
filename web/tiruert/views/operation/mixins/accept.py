06a76fa9bf636f9d96b070b074fbd12bf2135d18
Yohann <yohann.ulreich@gmail.com>
2025-01-20 18:03:22 +0100

    feat(tiruert): add method to validate teneur

--- web/tiruert/views/operation/mixins/accept.py
+++ web/tiruert/views/operation/mixins/accept.py
@@ -5,6 +5,10 @@ from rest_framework.response import Response
 from tiruert.models import Operation
 
 
+class AcceptActionMixinErrors:
+    NOTHING_TO_VALIDATE = "NOTHING_TO_VALIDATE"
+
+
 class AcceptActionMixin:
     @action(detail=True, methods=["post"])
     def accept(self, request, pk=None):
@@ -12,3 +16,26 @@ class AcceptActionMixin:
         operation.status = Operation.ACCEPTED
         operation.save()
         return Response({"status": "accepted"}, status=status.HTTP_200_OK)
+
+    @action(
+        detail=False,
+        methods=["post"],
+        url_path="teneur/validate",
+    )
+    def validate_teneur(self, request):
+        queryset = (
+            self.filter_queryset(self.get_queryset())
+            .filter(type=Operation.TENEUR, status=Operation.PENDING)
+            .order_by("created_at")
+        )
+
+        if not queryset.exists():
+            return Response({"error": AcceptActionMixinErrors.NOTHING_TO_VALIDATE}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Get the first one to know which month to validate
+        month = queryset.first().created_at.month
+        queryset = queryset.filter(created_at__month=month)
+
+        queryset.update(status=Operation.ACCEPTED)
+
+        return Response({"status": "validated"}, status=status.HTTP_200_OK)
