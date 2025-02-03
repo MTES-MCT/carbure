e0a67a54ed352debe2c54548bdd6e1c549960658
Yohann <yohann.ulreich@gmail.com>
2025-02-03 17:42:08 +0100

    feat(tiruert): return and expect 'avoided tCO2' in /simulate endpoints

--- web/tiruert/models/operation_detail.py
+++ web/tiruert/models/operation_detail.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from tiruert.services.teneur import GHG_REFERENCE_RED_II
+
 
 class OperationDetail(models.Model):
     operation = models.ForeignKey("tiruert.Operation", on_delete=models.deletion.CASCADE, related_name="details")
@@ -9,14 +11,10 @@ class OperationDetail(models.Model):
 
     @property
     def avoided_emissions(self):
-        lot_energy = self.lot.biofuel.pci_litre * self.volume  # MJ - energie du lot utilisée pour la création du lot
-
-        ghg_reference = self.lot.ghg_reference_red_ii  # gCO2/MJ - valeur de référence pour la création du lot 100% fossile
-        emissions_reference = ghg_reference * lot_energy  # gCO2 - émissions pour la création du lot dans le cas 100% fossile
-
-        emissions_real = self.emission_rate_per_mj * lot_energy  # gCO2 - émissions réelles pour la création du lot
-
-        return (emissions_reference - emissions_real) / 1000000  # tCO2 - émissions évitées pour la création du lot
+        lot_energy = self.lot.biofuel.pci_litre * self.volume  # (MJ) energie du lot utilisée pour la création du lot
+        return (
+            (GHG_REFERENCE_RED_II - self.emission_rate_per_mj) * lot_energy / 1000000
+        )  # (tCO2) émissions évitées pour la création du lot
 
     class Meta:
         db_table = "tiruert_operation_details"
