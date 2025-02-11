c5478f10545d762f0523d0783891a4098a9fca48
Benjamin Grilleres <bgrilleres@greenweez.com>
2025-02-11 13:49:47 +0100

    feat(tiruert soldes): add generic stepper component

--- front/src/accounting/balances/components/debit-operation-dialog/cession-dialog/cession-dialog.tsx
+++ front/src/accounting/balances/components/debit-operation-dialog/cession-dialog/cession-dialog.tsx
@@ -1,20 +1,46 @@
 import Dialog from "common/components/dialog2/dialog"
-import { Notice } from "common/components/notice"
+// import { Notice } from "common/components/notice"
 import Portal from "common/components/portal"
 import { Grid, Main } from "common/components/scaffold"
-import { formatNumber } from "common/utils/formatters"
+// import { formatNumber } from "common/utils/formatters"
 import { Balance } from "accounting/balances/types"
 import { OperationText } from "accounting/components/operation-text"
 import { Trans, useTranslation } from "react-i18next"
 import { formatSector } from "accounting/utils/formatters"
+import { Stepper, useStepper } from "common/components/stepper"
 
 interface CessionDialogProps {
   onClose: () => void
   balance: Balance
 }
 
+enum CessionStepKey {
+  FromDepot = "from_depot",
+  Volume = "volume",
+  ToDepot = "to_depot",
+  Recap = "recap",
+}
+
 export const CessionDialog = ({ onClose, balance }: CessionDialogProps) => {
   const { t } = useTranslation()
+  const { currentStep, currentStepIndex, steps, nextStep } = useStepper([
+    {
+      key: CessionStepKey.FromDepot,
+      title: t("Dépôt d'expédition"),
+    },
+    {
+      key: CessionStepKey.Volume,
+      title: t("Volume de la cession en litres et tCO2 évitées"),
+    },
+    {
+      key: CessionStepKey.ToDepot,
+      title: t("Redevable et dépôt destinataire"),
+    },
+    {
+      key: CessionStepKey.Recap,
+      title: t("Récapitulatif"),
+    },
+  ])
 
   return (
     <Portal>
@@ -28,6 +54,12 @@ export const CessionDialog = ({ onClose, balance }: CessionDialogProps) => {
         }
       >
         <Main>
+          <Stepper
+            title={currentStep?.title}
+            stepCount={steps.length}
+            currentStep={currentStepIndex}
+            nextTitle={nextStep?.title}
+          />
           <Grid>
             <OperationText
               title={t("Filière")}
@@ -42,11 +74,11 @@ export const CessionDialog = ({ onClose, balance }: CessionDialogProps) => {
               description={balance.biofuel ?? ""}
             />
           </Grid>
-          <Notice variant="info" noColor>
+          {/* <Notice variant="info" noColor>
             <Trans>Solde disponible</Trans> {balance.biofuel}
             {" : "}
             <b>{formatNumber(Number(balance.available_balance))}l</b>
-          </Notice>
+          </Notice> */}
         </Main>
       </Dialog>
     </Portal>
---
+++ front/src/common/components/stepper/stepper.tsx
@@ -0,0 +1,62 @@
+import {
+  Stepper as DsfrStepper,
+  StepperProps as DsfrStepperProps,
+} from "@codegouvfr/react-dsfr/Stepper"
+import { useState } from "react"
+import styles from "./stepper.module.css"
+import cl from "clsx"
+
+type StepperProps = DsfrStepperProps & {
+  marginBottom?: boolean
+}
+export const Stepper = ({ marginBottom = false, ...props }: StepperProps) => {
+  return (
+    <DsfrStepper
+      {...props}
+      className={cl(!marginBottom && styles["stepper--no-margin-bottom"])}
+    />
+  )
+}
+
+type Step<Key extends string> = {
+  key: Key
+  title: StepperProps["title"]
+}
+
+export const useStepper = <Key extends string>(steps: Step<Key>[]) => {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+
+  const currentStep = steps[currentStepIndex]
+  const hasPreviousStep = currentStepIndex > 0
+  const hasNextStep = steps.length > currentStepIndex + 1
+  const nextStep = hasNextStep ? steps[currentStepIndex + 1] : null
+
+  const setStep = (stepKey: Step<Key>["key"]) => {
+    const stepIndex = steps.findIndex((step) => step.key === stepKey)
+    if (stepIndex !== -1) {
+      setCurrentStepIndex(stepIndex)
+    }
+  }
+
+  const goToNextStep = () => {
+    if (hasNextStep) {
+      setCurrentStepIndex(currentStepIndex + 1)
+    }
+  }
+
+  const goToPreviousStep = () => {
+    if (hasPreviousStep) {
+      setCurrentStepIndex(currentStepIndex - 1)
+    }
+  }
+
+  return {
+    currentStep,
+    currentStepIndex: currentStepIndex + 1, // Index displayed in the stepper, starts at 1
+    nextStep,
+    setStep,
+    goToNextStep,
+    goToPreviousStep,
+    steps,
+  }
+}
