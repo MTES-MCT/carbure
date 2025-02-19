501d76c58325e1409d208b9cdc76d0df28e6e9b3
Benjamin Grilleres <bgrilleres@greenweez.com>
2025-02-19 16:48:54 +0100

    refactor(saf ui): terminate refacto saf

--- front/src/saf/pages/airline/accept-assignment.tsx
+++ front/src/saf/pages/airline/accept-assignment.tsx
@@ -1,8 +1,7 @@
 import useEntity from "common/hooks/entity"
-import Button from "common/components/button"
-import Dialog from "common/components/dialog"
-import Form, { useForm } from "common/components/form"
-import { Check, Return } from "common/components/icons"
+import { Button } from "common/components/button2"
+import { Dialog } from "common/components/dialog2"
+import { Form, useForm } from "common/components/form2"
 import { useNotify } from "common/components/notifications"
 import Portal from "common/components/portal"
 import { useMutation } from "common/hooks/async"
@@ -10,17 +9,20 @@ import { useTranslation } from "react-i18next"
 import { SafTicket } from "saf/types"
 import TicketTag from "../../components/tickets/tag"
 import * as api from "./api"
-import { RadioGroup } from "common/components/radio"
 import { EtsStatusEnum } from "api-schema"
-import Tooltip from "@codegouvfr/react-dsfr/Tooltip"
+import { RadioGroup } from "common/components/inputs2"
 
 interface AcceptAssignmentProps {
   ticket: SafTicket
   onClose: () => void
 }
 
-const defaultAcceptFormValue = {
-  ets_status: undefined as EtsStatusEnum | undefined,
+type AcceptFormValue = {
+  ets_status: EtsStatusEnum | undefined
+}
+
+const defaultAcceptFormValue: AcceptFormValue = {
+  ets_status: undefined,
 }
 
 export const AcceptAssignment = ({
@@ -31,7 +33,7 @@ export const AcceptAssignment = ({
   const entity = useEntity()
   const notify = useNotify()
 
-  const form = useForm(defaultAcceptFormValue)
+  const form = useForm<AcceptFormValue>(defaultAcceptFormValue)
 
   const acceptSafTicket = useMutation(api.acceptSafTicket, {
     invalidates: [
@@ -54,75 +56,60 @@ export const AcceptAssignment = ({
 
   return (
     <Portal onClose={onClose}>
-      <Dialog onClose={onClose}>
-        <header>
-          <TicketTag status={ticket.status} />
-          <h1>
-            {t("Accepter le ticket n°")}
+      <Dialog
+        onClose={onClose}
+        header={
+          <Dialog.Title>
+            <TicketTag status={ticket?.status} />
+            {t("Ticket n°")}
             {ticket?.carbure_id ?? "..."}
-          </h1>
-        </header>
-
-        <main>
-          <section>
-            <Form id="accept-ticket" onSubmit={acceptTicket}>
-              <p>
-                {t(
-                  "Est-ce que ce ticket est concerné par une déclaration (ETS ou volontaire) ?"
-                )}
-              </p>
-
-              <RadioGroup
-                {...form.bind("ets_status")}
-                required
-                options={[
-                  {
-                    value: EtsStatusEnum.ETS_VALUATION,
-                    label: (
-                      <>
-                        {t("Valorisation ETS")}
-                        <Tooltip kind="hover" title={t("RED SAF")} />
-                      </>
-                    ),
-                  },
-                  {
-                    value: EtsStatusEnum.NOT_CONCERNED,
-                    label: (
-                      <>
-                        {t("Non concerné")}
-                        <Tooltip kind="hover" title={t("ICAO CEF")} />
-                      </>
-                    ),
-                  },
-                  {
-                    value: EtsStatusEnum.OUTSIDE_ETS,
-                    label: (
-                      <>
-                        {t("Volontaire")}
-                        <Tooltip
-                          kind="hover"
-                          title={t("Hors obligation ETS")}
-                        />
-                      </>
-                    ),
-                  },
-                ]}
-              />
-            </Form>
-          </section>
-        </main>
-
-        <footer>
+          </Dialog.Title>
+        }
+        footer={
           <Button
-            icon={Check}
+            iconId="ri-check-line"
+            customPriority="success"
             disabled={!form.value.ets_status}
-            label={t("Accepter le ticket")}
-            variant="success"
-            submit="accept-ticket"
-          />
+            onClick={acceptTicket}
+          >
+            {t("Accepter le ticket")}
+          </Button>
+        }
+      >
+        <section>
+          <Form id="accept-ticket" onSubmit={acceptTicket}>
+            <p>
+              {t(
+                "Est-ce que ce ticket est concerné par une déclaration (ETS ou volontaire) ?"
+              )}
+            </p>
 
-          <Button icon={Return} label={t("Retour")} action={onClose} />
-        </footer>
+            <RadioGroup
+              {...form.bind("ets_status")}
+              required
+              options={[
+                {
+                  value: EtsStatusEnum.ETS_VALUATION,
+                  label: t("Valorisation ETS"),
+                  hasTooltip: true,
+                  title: t("RED SAF"),
+                },
+                {
+                  value: EtsStatusEnum.NOT_CONCERNED,
+                  label: t("Non concerné"),
+                  hasTooltip: true,
+                  title: t("ICAO CEF"),
+                },
+                {
+                  value: EtsStatusEnum.OUTSIDE_ETS,
+                  label: t("Volontaire"),
+                  hasTooltip: true,
+                  title: t("Hors obligation ETS"),
+                },
+              ]}
+            />
+          </Form>
+        </section>
       </Dialog>
     </Portal>
   )
