b91f8fb7a4303f301a30bd9119814097ede8971b
François Rossigneux <francois.rossigneux@beta.gouv.fr>
2025-02-26 09:16:04 +0100

    feat: double count download link

---
+++ front/src/double-counting/components/files-table.tsx
@@ -0,0 +1,26 @@
+import { useTranslation } from "react-i18next"
+import { DoubleCountingApplicationDetails } from "../types"
+import { Link } from "react-router-dom"
+
+type FilesTableProps = {
+  application: DoubleCountingApplicationDetails
+}
+
+export const FilesTable = ({ application }: FilesTableProps) => {
+  const { t } = useTranslation()
+
+  return (
+    <section>
+      <h1>Dossier</h1>
+      <div style={{ marginLeft: 16 }}>
+        {application.download_link ? (
+          <Link to={application.download_link} target="_blank">
+            {t("Télécharger le dossier")}
+          </Link>
+        ) : (
+          <p>{t("Pas de lien disponible")}</p>
+        )}
+      </div>
+    </section>
+  )
+}
