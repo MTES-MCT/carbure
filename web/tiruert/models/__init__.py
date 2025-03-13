d0ebec50c79b843637da811b7ec6b15536affc59
Yohann <yohann.ulreich@gmail.com>
2025-03-13 10:57:46 +0100

    feat(tiruert): update on models fossil_fuel and mac_fossil_fuel

--- web/tiruert/models/__init__.py
+++ web/tiruert/models/__init__.py
@@ -3,3 +3,4 @@ from .operation_detail import OperationDetail
 from .fossil_fuel import FossilFuel
 from .fossil_fuel_category import FossilFuelCategory
 from .objectives import Objective
+from .mac_fossil_fuel import MacFossilFuel
