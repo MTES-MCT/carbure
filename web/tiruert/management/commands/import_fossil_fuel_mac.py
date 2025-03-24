cb67f49169148d7fac1b040f1d38cba1039218b3
Yohann <yohann.ulreich@gmail.com>
2025-03-24 14:47:31 +0100

    feat(tiruert): mac import, search and create company if siren not in bdd

--- web/tiruert/management/commands/import_fossil_fuel_mac.py
+++ web/tiruert/management/commands/import_fossil_fuel_mac.py
@@ -5,7 +5,8 @@ import pandas as pd
 from django.core.files.storage import default_storage
 from django.core.management.base import BaseCommand, CommandError
 
-from core.models import Entity
+from core.models import Entity, Pays
+from entity.views.registration.search_company import search_company_gouv_fr
 from tiruert.models import FossilFuel, MacFossilFuel
 from transactions.models import Depot
 
@@ -15,25 +16,189 @@ TMP_FOLDER = "/tmp/mac/"
 
 logger = logging.getLogger("mac_import")
 
+authorized_sirens = [
+    814959045,
+    453346009,
+    889082178,
+    528448079,
+    889212221,
+    864500418,
+    888185105,
+    388955130,
+    317007342,
+    902013309,
+    901972992,
+    779075241,
+    439730375,
+    493479489,
+    753251354,
+    722000965,
+    395003221,
+    334690435,
+    601251614,
+    401818174,
+    841236581,
+    389934423,
+    333306843,
+    542034327,
+    422510750,
+    720801349,
+    306094194,
+    830375390,
+    521502443,
+    324790096,
+    833442304,
+    390982635,
+    301862710,
+    789034915,
+    513024968,
+    342466174,
+    325366334,
+    340722750,
+    418245700,
+    305800997,
+    394579353,
+    879113363,
+    797280443,
+    490924271,
+    388301988,
+    887634525,
+    439793811,
+    809164239,
+    853498632,
+    969502004,
+    785950015,
+    385267950,
+    542010053,
+    450036967,
+    403427214,
+    912510005,
+    852602259,
+    309024867,
+    489638338,
+    910701796,
+    883330938,
+    412186066,
+    389838087,
+    401004353,
+    829708577,
+    302700901,
+    347873879,
+    579500455,
+    325079473,
+    393536941,
+    418541900,
+    978042547,
+    480050525,
+    389220872,
+    946050119,
+    489173713,
+    975420217,
+    347389371,
+    950722256,
+    797476199,
+    619802895,
+    947351680,
+    530054733,
+    877779355,
+    342706892,
+    350041984,
+    433533635,
+    384316709,
+    342925748,
+    490348141,
+    377850292,
+    493415095,
+    677280661,
+    332855923,
+    838911683,
+    329382600,
+    401517123,
+    343134805,
+    387924772,
+    777347386,
+    388678930,
+    15550601,
+    844763490,
+    327788675,
+    852506062,
+    444845218,
+    302180187,
+    432827608,
+    351766571,
+    529916793,
+    844395632,
+    501525851,
+    890849854,
+    978036937,
+    843715970,
+    913493607,
+    632820676,
+    953594769,
+    917974800,
+    353663750,
+    883750772,
+    353908593,
+    552048811,
+    388021156,
+    946851268,
+    353597677,
+    16250037,
+    151000031,
+    449398031,
+    414225052,
+    780130175,
+    315281113,
+    344059605,
+    481361228,
+    8820,
+    353261555,
+    912194354,
+    533247979,
+    352860639,
+    491968947,
+    956501100,
+    531680445,
+    303955876,
+    448546028,
+    562050401,
+    404075731,
+    337884559,
+    437673239,
+    383809605,
+    492203815,
+    492203215,
+    509584009,
+    979112018,
+    304425291,
+    586950313,
+    780094983,
+    819441411,
+    807857818,
+    702006297,
+    393194824,
+]
+
 
 class Command(BaseCommand):
     help = "Import 'mises à consommation' of fossil fuels from an excel file"
 
     def add_arguments(self, parser):
         pass
-        # parser.add_argument("--ids", type=str, help="Download specific certificates")
-        # parser.add_argument("--no-pdf", action="store_true", help="Download certificates without pdf")
 
     def handle(self, *args, **options):
         logger.info("--- Starting MAC import ---")
-        # filename = self.get_file_to_import()
-        filename = "mac.xlsx"
+        filename = self.get_file_to_import()
+        # filename = "mac.xlsx"
 
         mac_rows = self.parse_mac_excel(f"{TMP_FOLDER}{filename}")
         logger.info(f"Found {len(mac_rows)} aggregated MACs in the file")
 
-        mac_objects = self.data_validation(mac_rows)
+        mac_objects, entities = self.data_validation(mac_rows)
         logger.info(f"Validated {len(mac_objects)} MACs")
+
+        self.update_entities(entities)
+        logger.info(f"{len(entities)} entities updated")
 
         self.insert_mac_objects(mac_objects)
         logger.info("--- MAC import finished ---\n\n")
@@ -113,13 +278,20 @@ class Command(BaseCommand):
 
         for _, row in grouped_df.iterrows():
             siren = row["SIREN Redevable"]
-            depot_customs_id = str(row["N Accise Redevable"])[5:8]  # keep only 3 characters
+            depot_customs_id = str(row["N Accise Redevable"])[4:8]  # keep only 4 characters
+            depot_customs_id = (
+                int(depot_customs_id) if depot_customs_id.isdigit() else None
+            )  # convert to int to remove leading 0
             fuel_nomenclature = row["Nomenclature combinee"]
             period = f"{row['Année de MAC']}{int(row['Mois de MAC']):02d}"  # YYYYMM
             volume = row["Nombre d'unite"] * 100  # convert hL to L
             start_date = row["Date de debut de periode"]
             end_date = row["Fin de periode"]
             year = row["Année de MAC"]
+
+            if siren not in authorized_sirens:
+                logger.error(f"Unauthorized SIREN {siren} found in the file")
+                continue
 
             mac_rows.append(
                 {
@@ -151,7 +323,7 @@ class Command(BaseCommand):
 
         for row in mac_rows:
             if row["siren"] in not_found_sirens:
-                errors.add(f"Entity with SIREN {row['siren']} not found")
+                # errors.add(f"Entity with SIREN {row['siren']} not found")
                 continue
 
             # Consolidate siren
@@ -159,15 +331,20 @@ class Command(BaseCommand):
                 entity = entity_cache[row["siren"]]
             else:
                 try:
-                    entity = Entity.objects.get(registration_id=row["siren"], is_enabled=True, entity_type=Entity.OPERATOR)
+                    entity = Entity.objects.get(registration_id=row["siren"])
                     entity_cache[row["siren"]] = entity
                     logger.info(f"Entity found: {entity}")
                 except Entity.DoesNotExist:
-                    errors.add(f"Entity with SIREN {row['siren']} not found")
-                    not_found_sirens.add(row["siren"])
-                    continue
+                    # logger.info(f"Entity with SIREN {row['siren']} not found")
+                    entity, error = self.search_company_gouv_fr(row["siren"])
+                    if entity is None:
+                        not_found_sirens.add(row["siren"])
+                        errors.add(error)
+                        continue
+                    entity_cache[row["siren"]] = entity
                 except Entity.MultipleObjectsReturned:
                     errors.add(f"Multiple entities with SIREN {row['siren']} found")
+                    continue
 
             # Consolidate depot
             if row["depot_customs_id"] in not_found_depots:
@@ -182,6 +359,9 @@ class Command(BaseCommand):
                 except Depot.DoesNotExist:
                     errors.add(f"{row["siren"]} - Depot with accise number {row['depot_customs_id']} doesn't exist")
                     not_found_depots.add(row["depot_customs_id"])
+                    depot = None
+                except Depot.MultipleObjectsReturned:
+                    errors.add(f"{row["siren"]} - Multiple depots with accise number {row['depot_customs_id']} found")
                     depot = None
 
             # Consolidate fuel
@@ -216,12 +396,12 @@ class Command(BaseCommand):
             for error in errors:
                 logger.error(error)
 
-        return validate_macs
+        return validate_macs, entity_cache
 
     def insert_mac_objects(self, mac_objects):
         """
         Insert the validated MACs in bulk into the database
-        If a MAC already exists (same operator, fuel, period), update the volume
+        TODO: How to handle udpates?
         """
         batch_size = 100
         for i in range(0, len(mac_objects), batch_size):
@@ -232,3 +412,48 @@ class Command(BaseCommand):
             )
 
         logger.info(f"{len(mac_objects)} MACs inserted successfully")
+
+    def update_entities(self, entities):
+        """
+        Update 'is_tiruert_liable' to True for all entities in the list
+        """
+        Entity.objects.filter(id__in=[entity.id for entity in entities.values()]).update(is_tiruert_liable=True)
+
+    def search_company_gouv_fr(self, siren):
+        try:
+            logger.info(f"Searching company with SIREN {siren} via API")
+            company_found = search_company_gouv_fr(siren)
+        except Exception:
+            error = f"Company with SIREN {siren} not found"
+            return None, error
+
+        logger.info(f"Company found: {company_found['nom_complet']}")
+
+        company_siege = company_found["siege"]
+        company_city = company_siege["libelle_commune"]
+        company_zipcode = company_siege["code_postal"]
+        company_address = company_siege["adresse"]
+        string_to_remove = f"{company_zipcode} {company_city}"
+        company_address = company_address.replace(string_to_remove, "")
+
+        france = Pays.objects.get(code_pays="FR")
+
+        try:
+            entity = Entity.objects.create(
+                name=company_found["nom_complet"],
+                legal_name=company_found["nom_raison_sociale"],
+                registration_id=company_found["siren"],
+                registered_address=company_address,
+                registered_city=company_siege["libelle_commune"],
+                registered_zipcode=company_siege["code_postal"],
+                registered_country=france,
+                is_enabled=True,
+                entity_type=Entity.OPERATOR,
+                is_tiruert_liable=True,
+            )
+            logger.info(f"Entity with SIREN {siren} created successfully")
+        except Exception as e:
+            error = f"Error creating entity with SIREN {siren}, {e}"
+            return None, error
+
+        return entity, None
