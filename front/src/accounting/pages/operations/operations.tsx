7f6cc0efab4c1a8846ff67f39c390d6d7580748e
jfalxa <jfalxa.sta@gmail.com>
2025-04-09 17:10:37 +0200

    feat(tiruert-elec): give access to new elec operation page

--- front/src/accounting/pages/operations/operations.tsx
+++ front/src/accounting/pages/operations/operations.tsx
@@ -3,18 +3,22 @@ import { lazy, useEffect } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 
 const OperationsBiofuels = lazy(() => import("./biofuels"))
+const OperationsElec = lazy(() => import("./elec"))
 
 const Operations = () => {
   const { status } = useParams()
   const navigate = useNavigate()
 
   useEffect(() => {
-    if (status !== SectorTabs.BIOFUELS) {
-      navigate(`${SectorTabs.BIOFUELS}`)
-    }
+    if (!status) navigate(`${SectorTabs.BIOFUELS}`)
   }, [status, navigate])
 
-  return <>{status === "biofuels" && <OperationsBiofuels />}</>
+  return (
+    <>
+      {status === "biofuels" && <OperationsBiofuels />}
+      {status === "elec" && <OperationsElec />}
+    </>
+  )
 }
 
 export default Operations
