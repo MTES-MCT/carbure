88e6ae3746b03657477c3f9a465474eb566cd6b0
jfalxa <jfalxa.sta@gmail.com>
2025-04-09 17:50:00 +0200

    feat(tiruert-elec): add accept/reject endpoints for elec operations

--- web/tiruert/views/elec_operation/mixins/__init__.py
+++ web/tiruert/views/elec_operation/mixins/__init__.py
@@ -1,7 +1,10 @@
 from .filter import FilterActionMixin
-
+from .accept import AcceptActionMixin
+from .reject import RejectActionMixin
 
 class ActionMixin(
     FilterActionMixin,
+    AcceptActionMixin,
+    RejectActionMixin
 ):
     pass
---
+++ web/tiruert/views/elec_operation/mixins/accept.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+
+from drf_spectacular.utils import OpenApiResponse, extend_schema
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from tiruert.models import ElecOperation
+
+
+class AcceptActionMixinErrors:
+    NOTHING_TO_DECLARE = "NOTHING_TO_DECLARE"
+    OPERATION_NOT_FOUND = "OPERATION_NOT_FOUND"
+    OPERATION_ALREADY_ACCEPTED_VALIDATED = "OPERATION_ALREADY_ACCEPTED_VALIDATED"
+    OPERATION_TYPE_NOT_ALLOWED = "OPERATION_TYPE_NOT_ALLOWED"
+
+
+class AcceptActionMixin:
+    @extend_schema(
+        operation_id="accept_elec_operation",
+        description="Set status operation to ACCEPTED",
+        request=None,
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(response={"status": "accepted"}, description="Success message"),
+            status.HTTP_404_NOT_FOUND: OpenApiResponse(
+                response={"error": AcceptActionMixinErrors.OPERATION_NOT_FOUND}, description="Error message"
+            ),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                response={"error": AcceptActionMixinErrors.OPERATION_ALREADY_ACCEPTED_VALIDATED}, description="Error message"
+            ),
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                response={"error": AcceptActionMixinErrors.OPERATION_TYPE_NOT_ALLOWED}, description="Error message"
+            ),
+        },
+    )
+    @action(detail=True, methods=["post"])
+    def accept(self, request, pk=None):
+        operation = self.get_object()
+
+        if operation.status != ElecOperation.PENDING:
+            return Response(
+                {"error": AcceptActionMixinErrors.OPERATION_ALREADY_ACCEPTED_VALIDATED}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if operation.type == ElecOperation.CESSION:
+            operation.status = ElecOperation.ACCEPTED
+        else:
+            return Response(
+                {"error": AcceptActionMixinErrors.OPERATION_TYPE_NOT_ALLOWED}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        operation.validation_date = datetime.now()
+        operation.save()
+        return Response({"status": "accepted"}, status=status.HTTP_200_OK)
