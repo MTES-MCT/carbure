63132c70240f4ed7ab42534f9643e5b8397bb2f9
jfalxa <jfalxa.sta@gmail.com>
2025-04-11 18:17:42 +0200

    fix(tiruert-elec): right types for elec operation balance endpoint

--- web/tiruert/services/elec_balance.py
+++ web/tiruert/services/elec_balance.py
@@ -1,6 +1,6 @@
 from typing import TypedDict
 
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Sum
 
 from tiruert.models import ElecOperation
 
@@ -16,43 +16,8 @@ class ElecBalance(TypedDict):
 
 class ElecBalanceService:
     @staticmethod
-    def _init_balance_entry() -> ElecBalance:
-        """
-        Initializes a balance entry with default values
-        """
-        entry = {
-            "sector": ElecOperation.SECTOR,
-            "quantity": {"credit": 0, "debit": 0},
-            "emission_rate_per_mj": 0,
-            "pending_teneur": 0,
-            "pending_operations": 0,
-            "declared_teneur": 0,
-            "available_balance": 0,
-        }
-
-        return entry
-
-    def _update_quantity_and_teneur(balance: ElecBalance, operation: ElecOperation, entity_id):
-        """
-        Updates the balance entry with the operation
-        """
-        if operation.type == ElecOperation.TENEUR:
-            teneur_type = "pending_teneur" if operation.status == ElecOperation.PENDING else "declared_teneur"
-            balance[teneur_type] += operation.quantity
-
-        quantity_type = "credit" if operation.is_credit(entity_id) else "debit"
-        balance["quantity"][quantity_type] += operation.quantity
-
-        return balance
-
-    def _update_available_balance(balance: ElecBalance, operation: ElecOperation, entity_id):
-        """
-        Updates the balance entry with the details of the operation
-        """
-        volume_sign = 1 if operation.is_credit(entity_id) else -1
-        balance["available_balance"] += operation.quantity * volume_sign
-        balance["emission_rate_per_mj"] = ElecOperation.EMISSION_RATE_PER_MJ
-        return
+    def total(operations: QuerySet[ElecOperation]):
+        return operations.aggregate(Sum("quantity")).get("quantity__sum") or 0
 
     @staticmethod
     def calculate_balance(operations: QuerySet[ElecOperation], entity_id, date_from=None) -> ElecBalance:
@@ -60,21 +25,55 @@ class ElecBalanceService:
         Calculates balances based on the specified grouping
         'operations' is a queryset of already filtered operations
         """
-        balance = ElecBalanceService._init_balance_entry()
 
         operations = operations.filter(status__in=[ElecOperation.PENDING, ElecOperation.ACCEPTED, ElecOperation.DECLARED])
+        pending_operations = operations.filter(status=ElecOperation.PENDING)
 
-        for operation in operations:
-            if operation.is_credit(entity_id) and operation.status == ElecOperation.PENDING:
-                continue
+        credited_operations = operations.filter(
+            type__in=[ElecOperation.CESSION, ElecOperation.ACQUISITION_FROM_CPO],
+            status__in=[ElecOperation.ACCEPTED],
+            credited_entity_id=entity_id,
+        )
 
-            ElecBalanceService._update_available_balance(balance, operation, entity_id)
+        debited_operations = operations.filter(
+            type__in=[ElecOperation.CESSION, ElecOperation.TENEUR],
+            status__in=[ElecOperation.PENDING, ElecOperation.ACCEPTED, ElecOperation.DECLARED],
+            debited_entity_id=entity_id,
+        )
 
-            # Update quantity and teneur only if the operation date is after the date_from
-            if date_from is None or operation.created_at >= date_from:
-                ElecBalanceService._update_quantity_and_teneur(balance, operation, entity_id)
+        pending_teneur = operations.filter(
+            type=ElecOperation.TENEUR,
+            status=ElecOperation.PENDING,
+            debited_entity_id=entity_id,
+        )
 
-            if operation.status == ElecOperation.PENDING:
-                balance["pending_operations"] += 1
+        declared_teneur = operations.filter(
+            type=ElecOperation.TENEUR,
+            status=ElecOperation.DECLARED,
+            debited_entity_id=entity_id,
+        )
 
-        return balance
+        period_credited_operations = credited_operations
+        period_debited_operations = debited_operations
+
+        if date_from:
+            period_credited_operations = period_credited_operations.filter(created_at__gte=date_from)
+            period_debited_operations = period_debited_operations.filter(created_at__gte=date_from)
+            pending_teneur = pending_teneur.filter(created_at__gte=date_from)
+            declared_teneur = declared_teneur.filter(created_at__gte=date_from)
+
+        total_credit = ElecBalanceService.total(credited_operations)
+        total_debit = ElecBalanceService.total(debited_operations)
+
+        return {
+            "sector": ElecOperation.SECTOR,
+            "quantity": {
+                "credit": ElecBalanceService.total(period_credited_operations),
+                "debit": ElecBalanceService.total(period_debited_operations),
+            },
+            "emission_rate_per_mj": ElecOperation.EMISSION_RATE_PER_MJ,
+            "pending_operations": pending_operations.count(),
+            "pending_teneur": ElecBalanceService.total(pending_teneur),
+            "declared_teneur": ElecBalanceService.total(declared_teneur),
+            "available_balance": total_credit - total_debit,
+        }
