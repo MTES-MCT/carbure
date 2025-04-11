46b790bfe6518ae45f708b492380ae510edd674d
jfalxa <jfalxa.sta@gmail.com>
2025-04-11 17:10:50 +0200

    feat(tiruert-elec): add constant for elec sector

--- web/tiruert/serializers/elec_balance.py
+++ web/tiruert/serializers/elec_balance.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 
+from tiruert.models.elec_operation import ElecOperation
 from tiruert.serializers.balance import BalanceQuantitySerializer
 
 
 class ElecBalanceSerializer(serializers.Serializer):
+    sector = serializers.ChoiceField(choices=[(ElecOperation.SECTOR, ElecOperation.SECTOR)])
     initial_balance = serializers.SerializerMethodField()
     available_balance = serializers.FloatField()
     quantity = BalanceQuantitySerializer()
