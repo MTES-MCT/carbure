f0bb9e2273d46245174284728ae2bbd0fbf7ef12
Benjamin Grilleres <contact@benjamingrilleres.com>
2025-04-14 16:38:39 +0200

    feat: start refacto admin DC

--- front/src/double-counting/components/files-table.tsx
+++ front/src/double-counting/components/files-table.tsx
@@ -1,39 +1,51 @@
 import { useTranslation } from "react-i18next"
 import { DoubleCountingApplicationDetails } from "../types"
-import { Link } from "react-router-dom"
-import { useEffect } from "react"
 import { getDoubleCountingAgreementDownloadLink } from "double-counting/api"
-import { useState } from "react"
+
 import useEntity from "common/hooks/entity"
+import { useQuery } from "common/hooks/async"
+import { Column, Table } from "common/components/table2"
+import { NoResult } from "common/components/no-result2"
 type FilesTableProps = {
   application: DoubleCountingApplicationDetails
 }
 
 export const FilesTable = ({ application }: FilesTableProps) => {
   const { t } = useTranslation()
-  const [downloadLink, setDownloadLink] = useState<string | null>(null)
   const entity = useEntity()
+  const { loading, result } = useQuery(
+    () => getDoubleCountingAgreementDownloadLink(entity.id, application.id),
+    {
+      key: "double-counting-agreement-download-link",
+      params: [],
+    }
+  )
+  const file = result?.data
 
-  useEffect(() => {
-    getDoubleCountingAgreementDownloadLink(entity.id, application.id).then(
-      (res) => {
-        setDownloadLink(res.data?.download_link ?? null)
-      }
-    )
-  }, [application.id])
+  const rows = [
+    {
+      name: "Demande d'agrément",
+      url: file?.download_link,
+    },
+  ]
 
-  return (
-    <section>
-      <h1>Dossier</h1>
-      <div style={{ marginLeft: 16 }}>
-        {downloadLink ? (
-          <Link to={downloadLink} target="_blank">
-            {t("Télécharger le dossier")}
-          </Link>
-        ) : (
-          <p>{t("Pas de lien disponible")}</p>
-        )}
-      </div>
-    </section>
+  const columns: Column<{
+    name: string
+    url?: string
+  }>[] = [
+    {
+      header: t("Nom"),
+      cell: (row) => row.name,
+    },
+    {
+      header: t("Action"),
+      cell: (row) => row.url,
+    },
+  ]
+
+  return !loading && !file?.download_link ? (
+    <NoResult label={t("Aucun fichier disponible")} />
+  ) : (
+    <Table columns={columns} rows={rows} loading={loading} />
   )
 }
