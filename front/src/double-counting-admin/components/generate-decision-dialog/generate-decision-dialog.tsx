cf14faa1b83cbd2f814cd7ba72014afde7a98e2f
Benjamin Grilleres <contact@benjamingrilleres.com>
2025-04-23 14:14:29 +0200

    refactor: refacto admin DC

--- front/src/double-counting-admin/components/generate-decision-dialog/generate-decision-dialog.tsx
+++ front/src/double-counting-admin/components/generate-decision-dialog/generate-decision-dialog.tsx
@@ -1,8 +1,8 @@
 import useEntity from "common/hooks/entity"
-import Alert from "common/components/alert"
-import Dialog from "common/components/dialog"
-import { AlertTriangle, Download, Return } from "common/components/icons"
-import { Input } from "common/components/input"
+import { Notice } from "common/components/notice"
+import { Dialog } from "common/components/dialog2"
+import { Download } from "common/components/icons"
+import { TextInput } from "common/components/inputs2"
 import { useNotify } from "common/components/notifications"
 import { PortalInstance } from "common/components/portal"
 import { hasIndustrialWastes } from "double-counting-admin/utils"
@@ -37,62 +37,46 @@ const GenerateDecisionDialog = ({
     onClose()
   }
   return (
-    <Dialog onClose={onClose}>
-      <header>
-        <h1>{t("Générer la décision double comptage")}</h1>
-      </header>
-
-      <main>
-        <section>
-          <Form
-            id="generate-decision"
-            onSubmit={downloadDoubleCountingApplication}
-          >
-            {hasIndustrialWastes(application) && (
-              <div style={{ marginBottom: "8px" }}>
-                <Alert
-                  variant="info"
-                  icon={AlertTriangle}
-                  style={{ marginBottom: "8px" }}
-                >
-                  <p>
-                    {t(
-                      "La demande d’agrément du producteur comporte des déchets industriels. Afin de les incorporer dans la décision, veuillez les noter ci-dessous."
-                    )}
-                  </p>
-                </Alert>
-
-                <Input
-                  value={industrialWastes}
-                  onChange={(e) => setIndustrialWastes(e.target.value)}
-                  label={t(
-                    "Lister ici les déchets industriels - séparés par une virgule :"
-                  )}
-                  placeholder={t(
-                    "Ex: huile de blanchiment, huile acide contaminée par du souffre"
-                  )}
-                  required
-                />
-              </div>
-            )}
-            <p>
-              {t(
-                "Voulez-vous vraiment télécharger la décision double comptage ?"
-              )}
-            </p>
-          </Form>
-        </section>
-      </main>
-
-      <footer>
+    <Dialog
+      onClose={onClose}
+      header={
+        <Dialog.Title>{t("Générer la décision double comptage")}</Dialog.Title>
+      }
+      footer={
         <Button
           variant="primary"
           icon={Download}
           label={t("Générer la décision")}
           submit="generate-decision"
         />
-        <Button asideX icon={Return} action={onClose} label={t("Retour")} />
-      </footer>
+      }
+    >
+      <Form id="generate-decision" onSubmit={downloadDoubleCountingApplication}>
+        {hasIndustrialWastes(application) && (
+          <>
+            <Notice variant="info" icon="ri-alert-line">
+              {t(
+                "La demande d’agrément du producteur comporte des déchets industriels. Afin de les incorporer dans la décision, veuillez les noter ci-dessous."
+              )}
+            </Notice>
+
+            <TextInput
+              value={industrialWastes}
+              onChange={(v) => setIndustrialWastes(v ?? "")}
+              label={t(
+                "Lister ici les déchets industriels - séparés par une virgule :"
+              )}
+              placeholder={t(
+                "Ex: huile de blanchiment, huile acide contaminée par du souffre"
+              )}
+              required
+            />
+          </>
+        )}
+      </Form>
+      <p>
+        {t("Voulez-vous vraiment télécharger la décision double comptage ?")}
+      </p>
     </Dialog>
   )
 }
