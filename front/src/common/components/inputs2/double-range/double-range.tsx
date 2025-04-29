91c369620c568516af1fb5b642a096125331b191
jfalxa <jfalxa.sta@gmail.com>
2025-04-29 14:11:57 +0200

    feat(common): add DoubleRange input component

---
+++ front/src/common/components/inputs2/double-range/double-range.tsx
@@ -0,0 +1,56 @@
+import {
+  Range as BaseRange,
+  RangeProps as BaseRangeProps,
+} from "@codegouvfr/react-dsfr/Range"
+import { ComponentProps } from "react"
+
+type BoundaryProps = Omit<ComponentProps<"input">, "value" | "onChange"> & {
+  value?: number
+  onChange?: (value: number) => void
+}
+
+export type DoubleRangeProps = BaseRangeProps & {
+  minRange?: BoundaryProps
+  maxRange?: BoundaryProps
+}
+
+export const DoubleRange = ({
+  min,
+  max,
+  step,
+  minRange,
+  maxRange,
+  ...props
+}: DoubleRangeProps) => {
+  return (
+    <BaseRange
+      {...props}
+      double
+      min={min}
+      max={max}
+      step={step}
+      nativeInputProps={[
+        {
+          ...minRange,
+          onChange: (e) => {
+            const value = parseFloat(e.target.value)
+            minRange?.onChange?.(value)
+            if (value > (maxRange?.value ?? Infinity)) {
+              maxRange?.onChange?.(value)
+            }
+          },
+        },
+        {
+          ...maxRange,
+          onChange: (e) => {
+            const value = parseFloat(e.target.value)
+            maxRange?.onChange?.(value)
+            if (value < (minRange?.value ?? -Infinity)) {
+              minRange?.onChange?.(value)
+            }
+          },
+        },
+      ]}
+    />
+  )
+}
