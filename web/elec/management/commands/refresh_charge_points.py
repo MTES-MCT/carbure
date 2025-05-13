23e1e2ef4cb61f2c066e177e8716124292b2ade4
jfalxa <jfalxa.sta@gmail.com>
2025-05-13 11:16:39 +0200

    fix(elec): refresh charge point db update

--- web/elec/management/commands/refresh_charge_points.py
+++ web/elec/management/commands/refresh_charge_points.py
@@ -2,7 +2,6 @@ import pandas as pd
 from django.core.management.base import BaseCommand
 from django.forms import model_to_dict
 
-from core.utils import bulk_update_or_create
 from elec.models.elec_charge_point import ElecChargePoint
 from elec.services.transport_data_gouv import TransportDataGouv
 
@@ -29,33 +28,33 @@ class Command(BaseCommand):
 
         print("> Refresh charge point data from TDG")
 
-        charge_points = ElecChargePoint.objects.filter(is_deleted=False).order_by("charge_point_id")
+        pdcs = ElecChargePoint.objects.filter(is_deleted=False).order_by("charge_point_id")
         if cpo:
-            charge_points = charge_points.filter(cpo__name=cpo)
+            pdcs = pdcs.filter(cpo__name=cpo)
 
-        cp_df = pd.DataFrame([model_to_dict(cp) for cp in charge_points])
-        tdg_df = TransportDataGouv.get_transport_data(cp_df)
+        pdcs_df = pd.DataFrame([model_to_dict(cp) for cp in pdcs])
+        tdg_df = TransportDataGouv.get_transport_data(pdcs_df)
 
         diff_nps = 0
         not_found = 0
 
-        changed_cps = set()
+        changed_pdcs = set()
 
-        for cp in charge_points:
-            tdg_cp_df = tdg_df[tdg_df["charge_point_id"] == cp.charge_point_id]
+        for cp in pdcs:
+            pdc_df = tdg_df[tdg_df["charge_point_id"] == cp.charge_point_id]
             try:
-                tdg_cp = tdg_cp_df.iloc[0].to_dict()
-                nominal_power = tdg_cp.get("nominal_power")
+                tdg_pdc = pdc_df.iloc[0].to_dict()
+                nominal_power = tdg_pdc.get("nominal_power")
                 if nominal_power > 1000:
                     nominal_power /= 1000
                 if nominal_power != 0 and nominal_power != cp.nominal_power:
                     diff_nps += 1
                     cp.nominal_power = nominal_power
-                    changed_cps.add(cp)
+                    changed_pdcs.add(cp)
             except Exception:
                 not_found += 1
 
-        bulk_update_or_create(ElecChargePoint, "charge_point_id", list(charge_points), batch)
+        ElecChargePoint.objects.bulk_update(list(changed_pdcs), ["nominal_power"], batch)
 
         print(f"> {diff_nps} nominal power changed")
         print(f"> {not_found} charge point ids not found")
