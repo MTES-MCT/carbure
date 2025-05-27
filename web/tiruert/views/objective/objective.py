3f0db4d01a2dcf7098c5c0da1131817ffd4bd27d
Yohann <yohann.ulreich@gmail.com>
2025-05-27 10:49:32 +0200

    feat(tiruert): new consideration rate model

--- web/tiruert/views/objective/objective.py
+++ web/tiruert/views/objective/objective.py
@@ -273,7 +273,7 @@ class ObjectiveViewSet(GenericViewSet):
             return
 
         # 1. Calculate "assiette" used for objectives calculation
-        energy_basis = ObjectiveService.calculate_energy_basis(macs, objectives)
+        energy_basis = ObjectiveService.calculate_energy_basis(macs, year=query_params.get("year"))
 
         # 2. Calculate the balances per category and sector
         balance_per_category, balance_per_sector = ObjectiveService.get_balances_for_objectives_calculation(
