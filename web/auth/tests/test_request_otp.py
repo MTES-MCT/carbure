5fc31ea25e4b546aeb66c2882333ec8fcf1bd207
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2025-05-28 18:17:14 +0200

    cleanup: separate contexts in different test cases

--- web/auth/tests/test_request_otp.py
+++ web/auth/tests/test_request_otp.py
@@ -8,26 +8,28 @@ from rest_framework.test import APIClient
 
 User = get_user_model()
 
-
 class RequestOTPTest(TestCase):
     def setUp(self):
         self.client = APIClient()
+        self.request_otp_url = reverse("auth-request-otp")
         self.user = User.objects.create_user(
             name="testuser",
             email="testuser@example.com",
             password="testpassword123",
         )
-        self.request_otp_url = reverse("auth-request-otp")
+
+class RequestOTPWithAuthenticatedUserTest(RequestOTPTest):
+    def setUp(self):
+        super().setUp()
+        self.client.force_authenticate(user=self.user)
 
     def test_creates_email_device(self):
-        self.client.force_authenticate(user=self.user)
         assert not EmailDevice.objects.filter(user=self.user).exists()
 
         self.client.get(self.request_otp_url)
         assert EmailDevice.objects.filter(user=self.user).exists()
 
     def test_responds_with_token_validity(self):
-        self.client.force_authenticate(user=self.user)
         response = self.client.get(self.request_otp_url)
         assert response.status_code == status.HTTP_200_OK
         assert "valid_until" in response.data
@@ -36,35 +38,34 @@ class RequestOTPTest(TestCase):
     def test_sends_email(self):
         self.assertEqual(len(mail.outbox), 0)
 
-        self.client.force_authenticate(user=self.user)
         self.client.get(self.request_otp_url)
         self.assertEqual(len(mail.outbox), 1)
 
     def test_injects_server_base_url_in_sent_mail(self):
-        self.client.force_authenticate(user=self.user)
         self.client.get(self.request_otp_url)
         sent_mail = mail.outbox[0]
         self.assertRegex(sent_mail.body, "http://carbure.local:8090/")
 
     def test_does_not_precede_base_url_by_https(self):
-        self.client.force_authenticate(user=self.user)
         self.client.get(self.request_otp_url)
         sent_mail = mail.outbox[0]
         self.assertNotRegex(sent_mail.body, "https://http://")
 
     def test_sends_alternative_html_format(self):
-        self.client.force_authenticate(user=self.user)
         self.client.get(self.request_otp_url)
         sent_mail = mail.outbox[0]
         self.assertEqual(len(sent_mail.alternatives), 1)
         self.assertEqual(sent_mail.alternatives[0][1], 'text/html')
 
     def test_does_not_precede_base_url_by_https_in_alternative_html_format(self):
-        self.client.force_authenticate(user=self.user)
         self.client.get(self.request_otp_url)
         sent_mail = mail.outbox[0]
         html_content = sent_mail.alternatives[0][0]
         self.assertNotRegex(html_content, "https://http://")
+
+class RequestOTPWithNoAuthenticatedUserTest(RequestOTPTest):
+    def setUp(self):
+        super().setUp()
 
     def test_returns_http_403_if_user_not_authenticated(self):
         response = self.client.get(self.request_otp_url)
