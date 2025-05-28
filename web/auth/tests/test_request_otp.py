8eea7fd2782f3979fb2685f1b24bdce48071b3ae
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2025-05-28 17:14:25 +0200

    fix: inject correct server base URL
    We are currently using `get_current_site().domain` as the server base URL
    displayed in the mail to the user who requested an OTP. This doesn't
    properly display the correct port – which is problematic if different
    from standard port. Also, the mail templates are currently forcing
    `https` protocol and shouldn't.
    
    This present commit fixes these two problems.

--- web/auth/tests/test_request_otp.py
+++ web/auth/tests/test_request_otp.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core import mail
 from django.test import TestCase
 from django.urls import reverse
 from django_otp.plugins.otp_email.models import EmailDevice
@@ -30,6 +31,39 @@ class RequestOTPTest(TestCase):
 
         assert "valid_until" in response.data
         assert response.data["valid_until"] is not None
+
+    def test_sends_email(self):
+        self.assertEqual(len(mail.outbox), 0)
+
+        self.client.force_authenticate(user=self.user)
+        self.client.get(self.request_otp_url)
+        self.assertEqual(len(mail.outbox), 1)
+
+    def test_injects_server_base_url_in_sent_mail(self):
+        self.client.force_authenticate(user=self.user)
+        self.client.get(self.request_otp_url)
+        sent_mail = mail.outbox[0]
+        self.assertRegex(sent_mail.body, "http://carbure.local:8090/")
+
+    def test_does_not_precede_base_url_by_https(self):
+        self.client.force_authenticate(user=self.user)
+        self.client.get(self.request_otp_url)
+        sent_mail = mail.outbox[0]
+        self.assertNotRegex(sent_mail.body, "https://http://")
+
+    def test_sends_alternative_html_format(self):
+        self.client.force_authenticate(user=self.user)
+        self.client.get(self.request_otp_url)
+        sent_mail = mail.outbox[0]
+        self.assertEqual(len(sent_mail.alternatives), 1)
+        self.assertEqual(sent_mail.alternatives[0][1], 'text/html')
+
+    def test_does_not_precede_base_url_by_https_in_alternative_html_format(self):
+        self.client.force_authenticate(user=self.user)
+        self.client.get(self.request_otp_url)
+        sent_mail = mail.outbox[0]
+        html_content = sent_mail.alternatives[0][0]
+        self.assertNotRegex(html_content, "https://http://")
 
     def test_request_otp_creates_email_device_and_sends_token_not_auth(self):
         assert not EmailDevice.objects.filter(user=self.user).exists()
