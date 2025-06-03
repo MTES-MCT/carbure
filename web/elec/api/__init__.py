923a7662115b23c0f07ba29946bbb4d1ea63047d
Yohann <yohann.ulreich@gmail.com>
2025-06-03 17:13:38 +0200

    feat(elec): add endpoint to create provision certificates from qualicharge

--- web/elec/api/__init__.py
+++ web/elec/api/__init__.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("elec.urls")),
     path("cpo/", include("elec.api.cpo")),
     path("admin/", include("elec.api.admin")),
     path("auditor/", include("elec.api.auditor")),
