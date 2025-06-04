2fa40e8dedd3bc80cef30fe7f7d186806a58fa5d
Yohann <yohann.ulreich@gmail.com>
2025-06-04 12:49:59 +0200

    feat(elec): qualicharge - add simplejwt package

--- web/carbure/urls.py
+++ web/carbure/urls.py
@@ -6,6 +6,10 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from auth.views import admin_login_redirect
 from carbure.api.redirect_app import redirect_app
@@ -29,6 +33,8 @@ urlpatterns = [
     ),
     path("api/", include("carbure.api")),
     path("core/", include("core.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
 
 if settings.DEBUG:
