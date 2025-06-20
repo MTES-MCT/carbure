de364ca8cbbfd58d605d58ebac4665a5a763e43c
jfalxa <jfalxa.sta@gmail.com>
2025-06-20 16:05:33 +0200

    refactor(saf): relocate ticket pages

---
+++ front/src/saf/pages/ticket-details/accept-assignment.tsx
@@ -0,0 +1,121 @@
+import useEntity from "common/hooks/entity"
+import { Button } from "common/components/button2"
+import { Dialog } from "common/components/dialog2"
+import { Form, useForm } from "common/components/form2"
+import { useNotify } from "common/components/notifications"
+import Portal from "common/components/portal"
+import { useMutation } from "common/hooks/async"
+import { useTranslation } from "react-i18next"
+import { SafTicket } from "saf/types"
+import TicketTag from "saf/components/tickets/tag"
+import { EtsStatusEnum } from "api-schema"
+import { RadioGroup } from "common/components/inputs2"
+import { acceptTicket as acceptTicketApi } from "saf/api"
+
+interface AcceptAssignmentProps {
+  ticket: SafTicket
+  onClose: () => void
+}
+
+type AcceptFormValue = {
+  ets_status: EtsStatusEnum | undefined
+}
+
+const defaultAcceptFormValue: AcceptFormValue = {
+  ets_status: undefined,
+}
+
+export const AcceptAssignment = ({
+  ticket,
+  onClose,
+}: AcceptAssignmentProps) => {
+  const { t } = useTranslation()
+  const entity = useEntity()
+  const notify = useNotify()
+
+  const form = useForm<AcceptFormValue>(defaultAcceptFormValue)
+
+  const acceptSafTicket = useMutation(acceptTicketApi, {
+    invalidates: [
+      "ticket-details",
+      "airline-snapshot",
+      "tickets",
+      `nav-stats-${entity.id}`,
+    ],
+    onSuccess: () => ticketAccepted(),
+  })
+
+  const ticketAccepted = () => {
+    notify(t("Le ticket a été accepté."), { variant: "success" })
+    onClose()
+  }
+
+  const acceptTicket = async () => {
+    await acceptSafTicket.execute(entity.id, ticket.id, form.value.ets_status!)
+  }
+
+  return (
+    <Portal onClose={onClose}>
+      <Dialog
+        onClose={onClose}
+        header={
+          <Dialog.Title>
+            <TicketTag status={ticket?.status} />
+            {t("Ticket n°")}
+            {ticket?.carbure_id ?? "..."}
+          </Dialog.Title>
+        }
+        footer={
+          <Button
+            iconId="ri-check-line"
+            customPriority="success"
+            disabled={!form.value.ets_status}
+            type="submit"
+            nativeButtonProps={{
+              form: "accept-ticket",
+            }}
+          >
+            {t("Accepter le ticket")}
+          </Button>
+        }
+      >
+        <section>
+          <Form id="accept-ticket" onSubmit={acceptTicket}>
+            <p>
+              {t(
+                "Est-ce que ce ticket est concerné par une déclaration (ETS ou volontaire) ?"
+              )}
+            </p>
+
+            <RadioGroup
+              {...form.bind("ets_status")}
+              required
+              options={[
+                {
+                  value: EtsStatusEnum.ETS_VALUATION,
+                  label: t("Valorisation ETS"),
+                  hasTooltip: true,
+                  title: t("RED SAF"),
+                },
+                {
+                  value: EtsStatusEnum.NOT_CONCERNED,
+                  label: t("Non concerné"),
+                  hasTooltip: true,
+                  title: t("ICAO CEF"),
+                },
+                {
+                  value: EtsStatusEnum.OUTSIDE_ETS,
+                  label: t("Volontaire"),
+                  hasTooltip: true,
+                  title: t("Hors obligation ETS"),
+                },
+              ]}
+            />
+          </Form>
+        </section>
+      </Dialog>
+    </Portal>
+  )
+}
+
+export default AcceptAssignment
---
+++ front/src/saf/pages/tickets/status-switcher.tsx
@@ -0,0 +1,73 @@
+import { ChatDeleteLine, CheckLine, DraftFill } from "common/components/icon"
+import { Tabs } from "common/components/tabs2"
+import { compact } from "common/utils/collection"
+import { useTranslation } from "react-i18next"
+import { SafSnapshot, SafTicketType, SafTicketStatus } from "saf/types"
+
+interface StatusSwitcherProps {
+  status: SafTicketStatus
+  count?: SafSnapshot
+  type: SafTicketType
+  onSwitch: (status: SafTicketStatus) => void
+}
+export const SafStatusSwitcher = ({
+  status,
+  count,
+  type,
+  onSwitch,
+}: StatusSwitcherProps) => {
+  const { t } = useTranslation()
+
+  const displayedStatuses = compact([
+    SafTicketStatus.PENDING,
+    type === "assigned" && SafTicketStatus.REJECTED,
+    SafTicketStatus.ACCEPTED,
+  ])
+
+  const getStatusLabel = (status: SafTicketStatus) => {
+    if (type === "assigned") {
+      switch (status) {
+        case SafTicketStatus.PENDING:
+          return `${t("En attente")} (${count?.tickets_assigned_pending ?? 0})`
+        case SafTicketStatus.REJECTED:
+          return `${t("Refusés")} (${count?.tickets_assigned_rejected ?? 0})`
+        case SafTicketStatus.ACCEPTED:
+          return `${t("Acceptés")} (${count?.tickets_assigned_accepted ?? 0})`
+      }
+    } else if (type === "received") {
+      switch (status) {
+        case SafTicketStatus.PENDING:
+          return `${t("En attente")} (${count?.tickets_received_pending ?? 0})`
+        case SafTicketStatus.ACCEPTED:
+          return `${t("Acceptés")} (${count?.tickets_received_accepted ?? 0})`
+      }
+    }
+  }
+
+  const getStatusIcon = (status: SafTicketStatus) => {
+    switch (status) {
+      case SafTicketStatus.PENDING:
+        return DraftFill
+      case SafTicketStatus.REJECTED:
+        return ChatDeleteLine
+      case SafTicketStatus.ACCEPTED:
+        return CheckLine
+    }
+  }
+
+  return (
+    <Tabs
+      focus={status}
+      onFocus={(status) => onSwitch(status as SafTicketStatus)}
+      tabs={displayedStatuses.map((status) => {
+        return {
+          key: status,
+          label: getStatusLabel(status),
+          icon: getStatusIcon(status),
+          path: `../tickets-${type}/${status.toLowerCase()}`,
+        }
+      })}
+      keepSearch
+    />
+  )
+}
