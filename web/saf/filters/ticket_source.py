9c80668941910a1e3c34d213e574ba2579fa93c6
jfalxa <jfalxa.sta@gmail.com>
2025-06-20 15:57:01 +0200

    feat(saf): add admin access to ticket sources

--- web/saf/filters/ticket_source.py
+++ web/saf/filters/ticket_source.py
@@ -1,10 +1,9 @@
 from enum import Enum
 
 import django_filters
-from django.db.models import Q
-from django.db.models.expressions import F
+from django.db.models import F, Q
 from drf_spectacular.utils import extend_schema_field
-from rest_framework.serializers import CharField, ChoiceField, IntegerField, ListField
+from rest_framework.serializers import CharField, ChoiceField, ListField
 
 from saf.models import SafTicketSource
 
@@ -15,21 +14,20 @@ class TicketStatusEnum(Enum):
 
 
 class TicketSourceFilter(django_filters.FilterSet):
-    entity_id = django_filters.NumberFilter(field_name="added_by_id", required=True)
     year = django_filters.NumberFilter(field_name="year")
 
-    periods = django_filters.CharFilter(method="filter_periods")
-    feedstocks = django_filters.CharFilter(method="filter_feedstocks")
-    clients = django_filters.CharFilter(field_name="saf_tickets__client__name", method="filter_clients")
-    countries_of_origin = django_filters.CharFilter(method="filter_countries_of_origin")
-    production_sites = django_filters.CharFilter(method="filter_production_sites")
-    delivery_sites = django_filters.CharFilter(method="filter_delivery_sites")
+    period = django_filters.BaseInFilter(field_name="delivery_period", lookup_expr="in")
+    feedstock = django_filters.BaseInFilter(field_name="feedstock__code", lookup_expr="in")
+    client = django_filters.BaseInFilter(field_name="saf_tickets__client__name", lookup_expr="in")
+    supplier = django_filters.CharFilter(method="filter_supplier")
+    country_of_origin = django_filters.BaseInFilter(field_name="country_of_origin__code_pays", lookup_expr="in")
+    production_site = django_filters.BaseInFilter(field_name="carbure_production_site__name", lookup_expr="in")
+    delivery_site = django_filters.BaseInFilter(field_name="parent_lot__carbure_delivery_site__name", lookup_expr="in")
 
     status = django_filters.ChoiceFilter(
         choices=[(item.name, item.value) for item in TicketStatusEnum],
         method="filter_status",
     )
-    suppliers = django_filters.CharFilter(method="filter_suppliers")
 
     order = django_filters.OrderingFilter(
         fields=(
@@ -39,27 +37,6 @@ class TicketSourceFilter(django_filters.FilterSet):
             ("ghg_reduction", "ghg_reduction"),
         )
     )
-
-    class Meta:
-        model = SafTicketSource
-        fields = [
-            "entity_id",
-            "year",
-            "periods",
-            "feedstocks",
-            "clients",
-            "suppliers",
-            "countries_of_origin",
-            "production_sites",
-            "delivery_sites",
-            "status",
-        ]
-
-    def filter_multiple_values(self, queryset, field_name, param_name):
-        values = self.data.getlist(param_name)
-        if values:
-            return queryset.filter(Q(**{f"{field_name}__in": values}))
-        return queryset
 
     @extend_schema_field(ChoiceField(choices=["HISTORY", "AVAILABLE"]))
     def filter_status(self, queryset, name, value):
@@ -77,71 +54,25 @@ class TicketSourceFilter(django_filters.FilterSet):
             required=False,
         )
     )
-    def filter_suppliers(self, queryset, name, value):
+    def filter_supplier(self, queryset, name, value):
         request = self.request
-        suppliers_list = request.GET.getlist("suppliers")
+        suppliers_list = request.GET.getlist("supplier")
         return queryset.filter(
             Q(parent_lot__carbure_supplier__name__in=suppliers_list)
             | Q(parent_lot__unknown_supplier__in=suppliers_list)
             | Q(parent_ticket__supplier__name__in=suppliers_list)
         )
 
-    @extend_schema_field(
-        ListField(
-            child=IntegerField(),
-            help_text="List of periods provided via ?periods=period1&periods=period2&periods=period3",
-            required=False,
-        )
-    )
-    def filter_periods(self, queryset, name, value):
-        return self.filter_multiple_values(queryset, "delivery_period", "periods")
-
-    @extend_schema_field(
-        ListField(
-            child=CharField(),
-            help_text="List of feedstocks provided via ?feedstocks=feedstock1&feedstocks=feedstock2",
-            required=False,
-        )
-    )
-    def filter_feedstocks(self, queryset, name, value):
-        return self.filter_multiple_values(queryset, "feedstock__code", "feedstocks")
-
-    @extend_schema_field(
-        ListField(
-            child=CharField(),
-            help_text="List of clients provided via ?clients=client1&clients=client2&clients=client3",
-            required=False,
-        )
-    )
-    def filter_clients(self, queryset, name, value):
-        return self.filter_multiple_values(queryset, "saf_tickets__client__name", "clients")
-
-    @extend_schema_field(
-        ListField(
-            child=CharField(),
-            help_text="List of countries of origin provided via ?countries_of_origin=country1&countries_of_origin=country2",
-            required=False,
-        )
-    )
-    def filter_countries_of_origin(self, queryset, name, value):
-        return self.filter_multiple_values(queryset, "country_of_origin__code_pays", "countries_of_origin")
-
-    @extend_schema_field(
-        ListField(
-            child=CharField(),
-            help_text="List of production sites provided via ?production_sites=site1&production_sites=site2",
-            required=False,
-        )
-    )
-    def filter_production_sites(self, queryset, name, value):
-        return self.filter_multiple_values(queryset, "carbure_production_site__name", "production_sites")
-
-    @extend_schema_field(
-        ListField(
-            child=CharField(),
-            help_text="List of delivery sites provided via ?delivery_sites=site1&delivery_sites=site2",
-            required=False,
-        )
-    )
-    def filter_delivery_sites(self, queryset, name, value):
-        return self.filter_multiple_values(queryset, "parent_lot__carbure_delivery_site__name", "delivery_sites")
+    class Meta:
+        model = SafTicketSource
+        fields = [
+            "year",
+            "period",
+            "feedstock",
+            "client",
+            "supplier",
+            "country_of_origin",
+            "production_site",
+            "delivery_site",
+            "status",
+        ]
--- web/saf/views/ticket_source/mixins/filter.py
+++ web/saf/views/ticket_source/mixins/filter.py
@@ -54,21 +54,18 @@ class FilterActionMixin:
         filterset = self.filterset_class(query_params, queryset=self.get_queryset(), request=request)
         queryset = filterset.qs
 
-        if filter == "clients":
-            column = "saf_tickets__client__name"
-        elif filter == "periods":
-            column = "delivery_period"
-        elif filter == "feedstocks":
-            column = "feedstock__code"
-        elif filter == "countries_of_origin":
-            column = "country_of_origin__code_pays"
-        elif filter == "production_sites":
-            column = "carbure_production_site__name"
-        elif filter == "delivery_sites":
-            column = "parent_lot__carbure_delivery_site__name"
-        elif filter == "suppliers":
-            column = "parent_supplier"
-        else:  # raise an error for unknown filters
+        filters = {
+            "client": "saf_tickets__client__name",
+            "period": "delivery_period",
+            "feedstock": "feedstock__code",
+            "country_of_origin": "country_of_origin__code_pays",
+            "production_site": "carbure_production_site__name",
+            "delivery_site": "parent_lot__carbure_delivery_site__name",
+            "supplier": "parent_supplier",
+        }
+
+        column = filters.get(filter)
+        if not column:  # raise an error for unknown filters
             raise ValidationError({"message": "Filter '%s' does not exist for ticket sources" % filter})
 
         queryset = queryset.annotate(
