d6da2e8450d9db59de4a3a7308b708beb4b0ab68
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2025-07-15 10:48:02 +0200

    Introduce (eDelivery) Access Point domain object

---
+++ web/edelivery/ebms/access_points.py
@@ -0,0 +1,19 @@
+class AccessPoint:
+    def __init__(self, access_point_id, role):
+        self.id = access_point_id
+        self.role = role
+
+    def to_XML(self):
+        return f"""\
+<eb:PartyId type="urn:oasis:names:tc:ebcore:partyid-type:unregistered:UDB">{self.id}</eb:PartyId>
+<eb:Role>{self.role}</eb:Role>"""
+
+
+class Initiator(AccessPoint):
+    def __init__(self, access_point_id):
+        super().__init__(access_point_id, "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/initiator")
+
+
+class Responder(AccessPoint):
+    def __init__(self, access_point_id):
+        super().__init__(access_point_id, "http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/responder")
