805cdf92550d83af0b4c488217bb10377abc09fd
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2025-07-16 18:40:03 +0200

    Introduce SOAP envelope

--- web/edelivery/ebms/messages.py
+++ web/edelivery/ebms/messages.py
@@ -3,19 +3,33 @@ from os import environ
 from edelivery.adapters.base64_encoder import encode
 from edelivery.adapters.clock import timestamp
 from edelivery.adapters.uuid_generator import new_uuid
+from edelivery.ebms.access_points import Initiator, Responder
 
 
 class Message:
     def __init__(self, responder_id, body):
         self.id = new_uuid()
+        self.initiator = Initiator(environ["INITIATOR_ACCESS_POINT_ID"])
+        self.responder = Responder(responder_id)
         self.timestamp = timestamp()
         self.body = body
+
+    def encoded(self):
+        return encode(self.body)
 
     def identifier(self):
         return str(self.id)
 
     def initiator_id(self):
-        return environ["INITIATOR_ACCESS_POINT_ID"]
+        return self.initiator.id
 
-    def encoded(self):
-        return encode(self.body)
+    def initiator_to_XML(self):
+        return self.initiator.to_XML()
+
+    def responder_to_XML(self):
+        return self.responder.to_XML()
+
+
+class TestMessage(Message):
+    def __init__(self):
+        super().__init__(environ["INITIATOR_ACCESS_POINT_ID"], "<test>Hello World!</test>")
---
+++ web/edelivery/soap/envelope.py
@@ -0,0 +1,50 @@
+class Envelope:
+    def __init__(self, message):
+        self.message = message
+
+    def payload(self):
+        return f"""\
+<soap:Envelope
+  xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
+  xmlns:ns="http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/"
+  xmlns:_1="http://eu.domibus.wsplugin/"
+  xmlns:xm="http://www.w3.org/2005/05/xmlmime">
+
+  <soap:Header>
+    <eb:Messaging xmlns:eb="http://docs.oasis-open.org/ebxml-msg/ebms/v3.0/ns/core/200704/">
+      <eb:UserMessage>
+        <eb:MessageInfo>
+          <eb:Timestamp>{self.message.timestamp}</eb:Timestamp>
+          <eb:MessageId>{self.message.id}</eb:MessageId>
+        </eb:MessageInfo>
+        <eb:PartyInfo>
+          <eb:From>{self.message.initiator_to_XML()}</eb:From>
+          <eb:To>{self.message.responder_to_XML()}</eb:To>
+        </eb:PartyInfo>
+        <eb:CollaborationInfo>
+          <eb:Service>https://union-database.ec.europa.eu/e-delivery/services/send</eb:Service>
+          <eb:Action>https://union-database.ec.europa.eu/e-delivery/actions/sendRequest</eb:Action>
+        </eb:CollaborationInfo>
+        <eb:MessageProperties>
+          <eb:Property name="originalSender">{self.message.initiator_id()}</eb:Property>
+          <eb:Property name="finalRecipient">EC</eb:Property>
+        </eb:MessageProperties>
+        <eb:PayloadInfo>
+          <eb:PartInfo href="cid:message">
+            <eb:PartProperties>
+              <eb:Property name="MimeType">text/xml</eb:Property>
+            </eb:PartProperties>
+          </eb:PartInfo>
+        </eb:PayloadInfo>
+      </eb:UserMessage>
+    </eb:Messaging>
+  </soap:Header>
+
+  <soap:Body>
+    <_1:submitRequest>
+      <payload payloadId="cid:message" contentType="text/xml">
+        <value>{self.message.encoded()}</value>
+      </payload>
+    </_1:submitRequest>
+  </soap:Body>
+</soap:Envelope>"""
