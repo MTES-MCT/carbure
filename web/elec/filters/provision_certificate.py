0747db6bd2765100c63a4746e1037ef46156e933
Yohann <yohann.ulreich@gmail.com>
2025-07-17 17:17:48 +0200

    feat(qualicharge): add filters for front usage

--- web/elec/filters/provision_certificate.py
+++ web/elec/filters/provision_certificate.py
@@ -1,12 +1,25 @@
-from django_filters import ChoiceFilter, FilterSet, NumberFilter
+from django_filters import BooleanFilter, FilterSet, NumberFilter
 
-from elec.models import ElecProvisionCertificate
+from elec.models import ElecProvisionCertificateQualicharge
 
 
-class ProvisionCertificateFilter(FilterSet):
-    source = ChoiceFilter(
-        field_name="source",
-        choices=ElecProvisionCertificate.SOURCES,
-        lookup_expr="iexact",
-    )
+class ProvisionCertificateQualichargeFilter(FilterSet):
     entity_id = NumberFilter(field_name="cpo", lookup_expr="exact")
+    not_validated = BooleanFilter(method="filter_not_validated")
+
+    def filter_not_validated(self, queryset, name, value):
+        if value:
+            return queryset.exclude(validated_by=ElecProvisionCertificateQualicharge.BOTH)
+        return queryset
+
+    class Meta:
+        model = ElecProvisionCertificateQualicharge
+        fields = [
+            "entity_id",
+            "year",
+            "validated_by",
+            "not_validated",
+            "operating_unit",
+            "station_id",
+            "date_from",
+        ]
