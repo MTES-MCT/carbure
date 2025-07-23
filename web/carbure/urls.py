cbbb1cda454b09b0ffbaafbccd61c9df7a91f5f4
Yohann <yohann.ulreich@gmail.com>
2025-07-23 15:31:04 +0200

    feat(admin otp): add otp validation on admin login process

--- web/carbure/urls.py
+++ web/carbure/urls.py
@@ -1,19 +1,3 @@
-"""carbure URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
@@ -27,6 +11,7 @@ from carbure.api.redirect_app import redirect_app
 
 urlpatterns = [
     re_path("app/(.*)", redirect_app),
+    path("admin/otp/", include("admin_otp.urls")),
     path("admin/", admin.site.urls),
     # YOUR PATTERNS
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
