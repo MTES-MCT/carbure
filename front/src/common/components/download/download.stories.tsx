fd53f3a5b905899654ae7411a817e8f6c7548a89
Benjamin Grilleres <contact@benjamingrilleres.com>
2025-07-24 16:45:44 +0200

    Revert "feat: upgrade to storybook 9"
    This reverts commit f4d900e718c80ff2d3e64f05fb9f8d84cee4217f.

--- front/src/common/components/download/download.stories.tsx
+++ front/src/common/components/download/download.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react-vite"
+import type { Meta, StoryObj } from "@storybook/react"
 import { Download } from "./download"
 
 const meta: Meta<typeof Download> = {
