33f567af6701e86b18a1352b5b48789e10b135de
Yohann <yohann.ulreich@gmail.com>
2025-08-01 11:52:45 +0200

    feat(biomethane): 'agreement' rename to 'contract' and setup list, post, patch

--- web/biomethane/urls.py
+++ web/biomethane/urls.py
@@ -1,8 +1,15 @@
+from django.urls import path
 from rest_framework_nested.routers import SimpleRouter
 
-from .views.entity_config_agreement.entity_config_agreement import BiomethaneEntityConfigAgreementViewSet
+from .views.entity_config_contract.entity_config_contract import BiomethaneEntityConfigContractViewSet
 
 router = SimpleRouter()
-router.register("", BiomethaneEntityConfigAgreementViewSet, basename="entity-config-agreement")
+router.register("contract", BiomethaneEntityConfigContractViewSet, basename="entity-config-contract")
 
-urlpatterns = router.urls
+# URL for patching a contract without an ID
+contract_viewset = BiomethaneEntityConfigContractViewSet.as_view({"patch": "partial_update"})
+
+urlpatterns = [
+    path("contract/", contract_viewset, name="contract-patch"),
+    *router.urls,
+]
