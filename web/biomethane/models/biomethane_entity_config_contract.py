9cb369bd6603917533c0c59f795e176f5c81ab13
Yohann <yohann.ulreich@gmail.com>
2025-08-08 09:30:10 +0200

    feat(biomethane): split models and serializer in 2 for contract and amendment

--- web/biomethane/models/biomethane_entity_config_contract.py
+++ web/biomethane/models/biomethane_entity_config_contract.py
@@ -17,15 +17,6 @@ def rename_specific_conditions_file(instance, filename):
     return rename_file(instance, filename, base_filename)
 
 
-def rename_amendment_file(instance, filename):
-    base_filename = (
-        f"{instance.contract.pk}_amendment_"
-        f"{slugify(instance.contract.entity.name)}_"
-        f"{datetime.now().strftime('%Y%m%d_%H%M%S')}"
-    )
-    return rename_file(instance, filename, base_filename)
-
-
 def rename_file(instance, filename, base_filename):
     ext = filename.split(".")[-1]
     return f"biomethane/contracts/{base_filename}.{ext}"
@@ -76,45 +67,3 @@ class BiomethaneEntityConfigContract(models.Model):
 
     def does_contract_exist(self):
         return bool(self.signature_date)
-
-
-class BiomethaneEntityConfigAmendment(models.Model):
-    CMAX_PAP_UPDATE = "CMAX_PAP_UPDATE"
-    EFFECTIVE_DATE = "EFFECTIVE_DATE"
-    CMAX_ANNUALIZATION = "CMAX_ANNUALIZATION"
-    INPUT_BONUS_UPDATE = "INPUT_BONUS_UPDATE"
-    L_INDEXATION_UPDATE = "L_INDEXATION_UPDATE"
-    PRODUCER_BUYER_INFO_CHANGE = "PRODUCER_BUYER_INFO_CHANGE"
-    ENERGY_ENVIRONMENTAL_EFFICIENCY_UPDATE = "ENERGY_ENVIRONMENTAL_EFFICIENCY_UPDATE"
-    OTHER = "OTHER"
-
-    AMENDMENT_OBJECT_CHOICES = [
-        # Modification de la CMAX/PAP
-        (CMAX_PAP_UPDATE, CMAX_PAP_UPDATE),
-        # "Avenant fixant la date de prise d'effet"
-        (EFFECTIVE_DATE, EFFECTIVE_DATE),
-        # "Annualisation de la CMAX"
-        (CMAX_ANNUALIZATION, CMAX_ANNUALIZATION),
-        # "Modification des proportions de prime d'intrant"
-        (INPUT_BONUS_UPDATE, INPUT_BONUS_UPDATE),
-        # "Modification de l'indexation L"
-        (L_INDEXATION_UPDATE, L_INDEXATION_UPDATE),
-        # "Changement des informations relatives au producteur/acheteur de biométhane"
-        (PRODUCER_BUYER_INFO_CHANGE, PRODUCER_BUYER_INFO_CHANGE),
-        # "Modification des conditions d'efficacité énergétique et environnementale"
-        (ENERGY_ENVIRONMENTAL_EFFICIENCY_UPDATE, ENERGY_ENVIRONMENTAL_EFFICIENCY_UPDATE),
-        # "Autres"
-        (OTHER, OTHER),
-    ]
-
-    contract = models.ForeignKey(BiomethaneEntityConfigContract, on_delete=models.CASCADE, related_name="amendments")
-    signature_date = models.DateField()
-    effective_date = models.DateField()
-    amendment_object = models.JSONField(default=list)
-    amendment_file = models.FileField(storage=private_storage, upload_to=rename_amendment_file)
-    # Used only if amendment_object is OTHER
-    amendment_details = models.TextField(blank=True, null=True)
-
-    class Meta:
-        db_table = "biomethane_entity_config_amendment"
-        verbose_name = "Biométhane - Avenant"
