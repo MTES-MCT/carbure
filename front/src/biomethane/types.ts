eb04eac473cc53dad0a8fedbdb5bb1763b0c828b
jfalxa <jfalxa.sta@gmail.com>
2025-08-12 16:31:28 +0200

    feat(biomethane): add api client for production unit

--- front/src/biomethane/types.ts
+++ front/src/biomethane/types.ts
@@ -1,10 +1,29 @@
 import {
   TariffReferenceEnum as TariffReference,
   InstallationCategoryEnum as InstallationCategory,
+  UnitTypeEnum as UnitType,
+  HygienizationExemptionTypeEnum as HygienizationExemptionType,
+  IcpeRegimeEnum as IcpeRegime,
+  ProcessTypeEnum as ProcessType,
+  MethanizationProcessEnum as MethanizationProcess,
+  DigestateValorizationMethodEnum as DigestateValorizationMethod,
+  SpreadingManagementEnum as SpreadingManagement,
+  DigestateSaleTypeEnum as DigestateSaleType,
 } from "api-schema"
 import { apiTypes } from "common/services/api-fetch.types"
 
-export { TariffReference, InstallationCategory }
+export {
+  TariffReference,
+  InstallationCategory,
+  UnitType,
+  HygienizationExemptionType,
+  IcpeRegime,
+  ProcessType,
+  MethanizationProcess,
+  DigestateValorizationMethod,
+  SpreadingManagement,
+  DigestateSaleType,
+}
 
 // Contracts
 export type BiomethaneContract = apiTypes["BiomethaneContract"]
@@ -16,3 +35,17 @@ export type BiomethaneContractAmendment =
   apiTypes["BiomethaneContractAmendment"]
 export type BiomethaneAmendmentAddRequest =
   apiTypes["BiomethaneContractAmendmentAddRequest"]
+
+// Production Unit
+export type BiomethaneProductionUnit = apiTypes["BiomethaneProductionUnit"]
+export type BiomethaneProductionUnitAddRequest =
+  apiTypes["BiomethaneProductionUnitAddRequest"]
+export type BiomethaneProductionUnitPatchRequest =
+  apiTypes["PatchedBiomethaneProductionUnitPatchRequest"]
+
+// Digestate Storage
+export type BiomethaneDigestateStorage = apiTypes["BiomethaneDigestateStorage"]
+export type BiomethaneDigestateStorageAddRequest =
+  apiTypes["BiomethaneDigestateStorageAddRequest"]
+export type BiomethaneDigestateStoragePatchRequest =
+  apiTypes["PatchedBiomethaneDigestateStoragePatchRequest"]
