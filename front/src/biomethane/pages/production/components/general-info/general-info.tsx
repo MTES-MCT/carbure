29fbf3f7361d197f27f907b3f3e5a0aa06092ee4
jfalxa <jfalxa.sta@gmail.com>
2025-08-12 17:05:34 +0200

    feat(biomethane): connect production general info to backend

--- front/src/biomethane/pages/production/components/general-info/general-info.tsx
+++ front/src/biomethane/pages/production/components/general-info/general-info.tsx
@@ -3,44 +3,69 @@ import { RadioGroup, TextInput } from "common/components/inputs2"
 import { Grid } from "common/components/scaffold"
 import { EditableCard } from "common/molecules/editable-card"
 import { useTranslation } from "react-i18next"
+import { useForm } from "common/components/form2"
+import { DeepPartial } from "common/types"
+import {
+  BiomethaneProductionUnit,
+  BiomethaneProductionUnitAddRequest,
+} from "biomethane/types"
+import {
+  useMutateProductionUnit,
+  useUnitTypeOptions,
+} from "../../production.hooks"
 
-export function GeneralInfo() {
+type GeneralInfoForm = DeepPartial<BiomethaneProductionUnitAddRequest>
+
+export function GeneralInfo({
+  productionUnit,
+}: {
+  productionUnit?: BiomethaneProductionUnit
+}) {
   const { t } = useTranslation()
+  const { bind, value } = useForm<GeneralInfoForm>(productionUnit ?? {})
+  const unitTypeOptions = useUnitTypeOptions()
+  const { execute: updateProductionUnit, loading } = useMutateProductionUnit(
+    productionUnit !== undefined
+  )
 
   return (
     <EditableCard title={t("Informations générales du site de production")}>
       {({ isEditing }) => (
-        <EditableCard.Form onSubmit={() => {}}>
+        <EditableCard.Form onSubmit={() => updateProductionUnit(value!)}>
           <Grid cols={2} gap="lg">
             <TextInput
               readOnly={!isEditing}
-              label={t("Nom de l’unité")}
-              //
+              label={t("Nom de l'unité")}
+              required
+              {...bind("unit_name")}
             />
             <TextInput
               readOnly={!isEditing}
               label={t("SIRET")}
-              //
+              required
+              {...bind("siret_number")}
             />
             <RadioGroup
               readOnly={!isEditing}
-              label={t("Type d’installation")}
-              options={[
-                { value: "", label: t("Agricole autonome") },
-                { value: "", label: t("Agricole territorial") },
-                { value: "", label: t("Industriel territorial") },
-                { value: "", label: t("Déchets ménagers et biodéchets") },
-                { value: "", label: t("ISDND") },
-              ]}
+              label={t("Type d'installation")}
+              options={unitTypeOptions}
+              required
+              {...bind("unit_type")}
             />
             <TextInput
               readOnly={!isEditing}
               label={t("Adresse de la société (Numéro et rue)")}
-              //
+              required
+              {...bind("company_address")}
             />
           </Grid>
           {isEditing && (
-            <Button type="submit" iconId="ri-save-line" asideX>
+            <Button
+              type="submit"
+              iconId="ri-save-line"
+              asideX
+              loading={loading}
+            >
               {t("Sauvegarder")}
             </Button>
           )}
--- front/src/biomethane/pages/production/production.tsx
+++ front/src/biomethane/pages/production/production.tsx
@@ -4,11 +4,16 @@ import { GeneralInfo } from "./components/general-info/general-info"
 import { ICPE } from "./components/icpe/icpe"
 import { ProductionSite } from "./components/production-site/production-site"
 import { SanitaryAgreement } from "./components/sanitary-agreement/sanitary-agreement"
+import { useProductionUnit } from "./production.hooks"
 
 export const BiomethaneProductionPage = () => {
+  const { result: productionUnit } = useProductionUnit()
+
+  if (!productionUnit) return null
+
   return (
     <>
-      <GeneralInfo />
+      <GeneralInfo productionUnit={productionUnit} />
       <SanitaryAgreement />
       <ICPE />
       <ProductionSite />
