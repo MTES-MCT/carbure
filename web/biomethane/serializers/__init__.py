ff345eb5a174ab41064b5a985b5fa90d64c0a314
Yohann <yohann.ulreich@gmail.com>
2025-09-04 17:32:23 +0200

    feat(biomethane): move all serializers in folders for clarity

--- web/biomethane/serializers/__init__.py
+++ web/biomethane/serializers/__init__.py
@@ -1,15 +1,21 @@
-from .contract_amendment import BiomethaneContractAmendmentAddSerializer
-from .contract_amendment import BiomethaneContractAmendmentSerializer
-from .contract import BiomethaneContractSerializer
-from .contract import BiomethaneContractInputSerializer
-from .injection_site import BiomethaneInjectionSiteInputSerializer
-from .injection_site import BiomethaneInjectionSiteSerializer
+from .contract import (
+    BiomethaneContractAmendmentAddSerializer,
+    BiomethaneContractAmendmentSerializer,
+    BiomethaneContractSerializer,
+    BiomethaneContractInputSerializer,
+)
+from .injection_site import (
+    BiomethaneInjectionSiteInputSerializer,
+    BiomethaneInjectionSiteSerializer,
+)
 from .production_unit import (
     BiomethaneProductionUnitSerializer,
     BiomethaneProductionUnitUpsertSerializer,
-)
-from .digestate_storage import (
     BiomethaneDigestateStorageSerializer,
     BiomethaneDigestateStorageInputSerializer,
 )
-from .digestate import BiomethaneDigestateSerializer
+from .digestate import (
+    BiomethaneDigestateSerializer,
+    BiomethaneDigestateInputSerializer,
+    BiomethaneDigestateSpreadingSerializer,
+)
---
+++ web/biomethane/serializers/production_unit/__init__.py
@@ -0,0 +1,8 @@
+from .production_unit import (
+    BiomethaneProductionUnitSerializer,
+    BiomethaneProductionUnitUpsertSerializer,
+)
+from .digestate_storage import (
+    BiomethaneDigestateStorageSerializer,
+    BiomethaneDigestateStorageInputSerializer,
+)
