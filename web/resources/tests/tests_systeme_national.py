426ca660b541f9b213e53180531898049a93b1d9
jfalxa <jfalxa.sta@gmail.com>
2025-09-09 13:54:50 +0200

    feat(registry): return list of currently active system national certificates

---
+++ web/resources/tests/tests_systeme_national.py
@@ -0,0 +1,119 @@
+from datetime import date
+
+from django.test import TestCase
+from django.urls import reverse
+from matplotlib.dates import relativedelta
+
+from core.models import Entity, GenericCertificate
+from core.tests_utils import setup_current_user
+from resources.factories.generic_certificate import GenericCertificateFactory
+
+
+class SystemeNationalTest(TestCase):
+    active_valid_until = date.today() + relativedelta(years=10)
+
+    def setUp(self):
+        self.entity = Entity.objects.create(name="TestEntity", entity_type=Entity.OPERATOR)
+        self.user = setup_current_user(self, "tester@carbure.local", "Tester", "gogogo", [(self.entity, "RW")], True)
+
+    def get_sn_certificates(self, query=""):
+        url = reverse("resources-systeme-national")
+        return self.client.get(url, query_params={"query": query})
+
+    def test_list_sn_certificates(self):
+        sn_cert: GenericCertificate = GenericCertificateFactory.create(
+            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
+            valid_until=self.active_valid_until,
+        )
+
+        response = self.get_sn_certificates()
+
+        self.assertEqual(
+            response.json(),
+            [
+                {
+                    "certificate_id": sn_cert.certificate_id,
+                    "certificate_type": GenericCertificate.SYSTEME_NATIONAL,
+                    "certificate_holder": sn_cert.certificate_holder,
+                    "certificate_issuer": sn_cert.certificate_issuer,
+                    "address": sn_cert.address,
+                    "valid_from": f"{sn_cert.valid_from:%Y-%m-%d}",
+                    "valid_until": f"{sn_cert.valid_until:%Y-%m-%d}",
+                    "download_link": sn_cert.download_link,
+                    "scope": sn_cert.scope,
+                    "input": sn_cert.input,
+                    "output": sn_cert.output,
+                }
+            ],
+        )
+
+    def test_list_only_sn_certificates(self):
+        GenericCertificateFactory.create(certificate_type=GenericCertificate.ISCC)
+        response = self.get_sn_certificates()
+        self.assertEqual(response.json(), [])
+
+    def test_list_only_active_sn_certificates(self):
+        """
+        Active certificates are the ones where their valid_from/valid_until timespan
+        covers the current date.
+        """
+
+        today = date.today()
+
+        active_cert = GenericCertificateFactory.create(
+            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
+            valid_from=today - relativedelta(years=1),
+            valid_until=today + relativedelta(years=1),
+        )
+
+        _expired_cert = GenericCertificateFactory.create(
+            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
+            valid_from=today - relativedelta(years=2),
+            valid_until=today - relativedelta(years=1),
+        )
+
+        response = self.get_sn_certificates()
+        data = response.json()
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["certificate_id"], active_cert.certificate_id)
+
+    def test_filter_system_national_certificates_by_certificate_id(self):
+        _cert_a = GenericCertificateFactory.create(
+            certificate_id="ABCD",
+            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
+            valid_until=self.active_valid_until,
+        )
+
+        cert_b = GenericCertificateFactory.create(
+            certificate_id="EFGH",
+            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
+            valid_until=self.active_valid_until,
+        )
+
+        response = self.get_sn_certificates(query="FG")
+        data = response.json()
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["certificate_id"], cert_b.certificate_id)
+
+    def test_filter_system_national_certificates_by_certificate_holder(self):
+        _cert_a = GenericCertificateFactory.create(
+            certificate_id="ABCD",
+            certificate_holder="Alice",
+            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
+            valid_until=self.active_valid_until,
+        )
+
+        cert_b = GenericCertificateFactory.create(
+            certificate_id="EFGH",
+            certificate_holder="Bob",
+            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
+            valid_until=self.active_valid_until,
+        )
+
+        response = self.get_sn_certificates(query="ob")
+        data = response.json()
+
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["certificate_id"], cert_b.certificate_id)
