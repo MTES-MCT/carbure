56785cf177f7e7b2f5446344cc7885d00dd16e6d
jfalxa <jfalxa.sta@gmail.com>
2025-09-15 18:12:45 +0200

    test(resources): simplify systeme national tests

--- web/resources/tests/tests_systeme_national.py
+++ web/resources/tests/tests_systeme_national.py
@@ -23,9 +23,23 @@ class SystemeNationalTest(TestCase):
         return response.json()
 
     def test_list_sn_certificates(self):
-        sn_cert: GenericCertificate = GenericCertificateFactory.create(
+        today = date.today()
+
+        active_sn_cert: GenericCertificate = GenericCertificateFactory.create(
             certificate_type=GenericCertificate.SYSTEME_NATIONAL,
             valid_until=self.active_valid_until,
+        )
+
+        # the endpoint ignores any certificate that is not SYSTEME_NATIONAL
+        _non_sn_cert = GenericCertificateFactory.create(
+            certificate_type=GenericCertificate.ISCC,
+        )
+
+        # the endpoint ignores any certificate that isn't valid today
+        _expired_sn_cert = GenericCertificateFactory.create(
+            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
+            valid_from=today - relativedelta(years=2),
+            valid_until=today - relativedelta(years=1),
         )
 
         data = self.get_sn_certificates()
@@ -34,70 +48,22 @@ class SystemeNationalTest(TestCase):
             data,
             [
                 {
-                    "certificate_id": sn_cert.certificate_id,
+                    "certificate_id": active_sn_cert.certificate_id,
                     "certificate_type": GenericCertificate.SYSTEME_NATIONAL,
-                    "certificate_holder": sn_cert.certificate_holder,
-                    "certificate_issuer": sn_cert.certificate_issuer,
-                    "address": sn_cert.address,
-                    "valid_from": f"{sn_cert.valid_from:%Y-%m-%d}",
-                    "valid_until": f"{sn_cert.valid_until:%Y-%m-%d}",
-                    "download_link": sn_cert.download_link,
-                    "scope": sn_cert.scope,
-                    "input": sn_cert.input,
-                    "output": sn_cert.output,
+                    "certificate_holder": active_sn_cert.certificate_holder,
+                    "certificate_issuer": active_sn_cert.certificate_issuer,
+                    "address": active_sn_cert.address,
+                    "valid_from": f"{active_sn_cert.valid_from:%Y-%m-%d}",
+                    "valid_until": f"{active_sn_cert.valid_until:%Y-%m-%d}",
+                    "download_link": active_sn_cert.download_link,
+                    "scope": active_sn_cert.scope,
+                    "input": active_sn_cert.input,
+                    "output": active_sn_cert.output,
                 }
             ],
         )
 
-    def test_ignore_non_sn_certificates(self):
-        GenericCertificateFactory.create(certificate_type=GenericCertificate.ISCC)
-        data = self.get_sn_certificates()
-        self.assertEqual(data, [])
-
-    def test_list_only_active_sn_certificates(self):
-        """
-        Active certificates are the ones where their valid_from/valid_until timespan
-        covers today's date.
-        """
-
-        today = date.today()
-
-        active_cert = GenericCertificateFactory.create(
-            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
-            valid_from=today - relativedelta(years=1),
-            valid_until=today + relativedelta(years=1),
-        )
-
-        _expired_cert = GenericCertificateFactory.create(
-            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
-            valid_from=today - relativedelta(years=2),
-            valid_until=today - relativedelta(years=1),
-        )
-
-        data = self.get_sn_certificates()
-
-        self.assertEqual(len(data), 1)
-        self.assertEqual(data[0]["certificate_id"], active_cert.certificate_id)
-
     def test_search_sn_certificates_by_certificate_id(self):
-        _cert_a = GenericCertificateFactory.create(
-            certificate_id="ABCD",
-            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
-            valid_until=self.active_valid_until,
-        )
-
-        cert_b = GenericCertificateFactory.create(
-            certificate_id="EFGH",
-            certificate_type=GenericCertificate.SYSTEME_NATIONAL,
-            valid_until=self.active_valid_until,
-        )
-
-        data = self.get_sn_certificates(query="FG")
-
-        self.assertEqual(len(data), 1)
-        self.assertEqual(data[0]["certificate_id"], cert_b.certificate_id)
-
-    def test_search_sn_certificates_by_certificate_holder(self):
         _cert_a = GenericCertificateFactory.create(
             certificate_id="ABCD",
             certificate_holder="Alice",
@@ -112,7 +78,14 @@ class SystemeNationalTest(TestCase):
             valid_until=self.active_valid_until,
         )
 
-        data = self.get_sn_certificates(query="ob")
+        # match with certificate_id
+        data_from_id = self.get_sn_certificates(query="FG")
 
-        self.assertEqual(len(data), 1)
-        self.assertEqual(data[0]["certificate_id"], cert_b.certificate_id)
+        self.assertEqual(len(data_from_id), 1)
+        self.assertEqual(data_from_id[0]["certificate_id"], cert_b.certificate_id)
+
+        # match with certificate_holder
+        data_from_holder = self.get_sn_certificates(query="ob")
+
+        self.assertEqual(len(data_from_holder), 1)
+        self.assertEqual(data_from_holder[0]["certificate_id"], cert_b.certificate_id)
