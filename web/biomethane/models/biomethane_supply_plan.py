9b73296b74b881cca52f9e380bc24f4de8c4e31a
Yohann <yohann.ulreich@beta.gouv.fr>
2025-09-22 16:36:17 +0200

    feat(biomethane): add new models for supply page

---
+++ web/biomethane/models/biomethane_supply_plan.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from core.models import Entity
+
+
+class BiomethaneSupplyPlan(models.Model):
+    # Producteur associé au plan d'approvisionnement
+    producer = models.ForeignKey(Entity, on_delete=models.CASCADE, related_name="biomethane_supply_plans")
+
+    # Année du plan d'approvisionnement
+    year = models.IntegerField(verbose_name="Année")
+
+    class Meta:
+        db_table = "biomethane_supply_plan"
+        verbose_name = "Biométhane - Plan d'approvisionnement"
+        verbose_name_plural = "Biométhane - Plans d'approvisionnement"
+        unique_together = ["producer", "year"]
+
+    def __str__(self):
+        return f"Plan d'approvisionnement {self.year} - {self.producer.name}"
