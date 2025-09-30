44fb0b96e64fd3afaed95875a66ba6aca7d701c6
Yohann <yohann.ulreich@beta.gouv.fr>
2025-09-30 09:58:05 +0200

    feat(biomethane): excel import - refacto to split responsabilities

--- web/biomethane/views/supply_plan/mixins/excel_import.py
+++ web/biomethane/views/supply_plan/mixins/excel_import.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from biomethane.serializers import BiomethaneSupplyInputCreateSerializer, BiomethaneUploadExcelSerializer
-from core.excel_importer import ExcelImporter
+from core.excel_importer import ExcelImporter, ExcelValidationError
 
 
 class ExcelImportActionMixin:
@@ -20,47 +20,33 @@ class ExcelImportActionMixin:
 
         file = file_serializer.validated_data["file"]
 
+        config = {
+            "header_row": 1,
+            "sheet_name": "Plan d'approvisionnement",
+        }
+
         try:
-            config = {
-                "header_row": 1,
-                "sheet_name": "Plan d'approvisionnement",
-            }
             data = ExcelImporter.parse(file, **config)
 
-            # Collect all validation errors by row
-            validation_errors = []
-            valid_instances = []
-
-            for row_index, row in enumerate(data, start=1):
-                input_serializer = BiomethaneSupplyInputCreateSerializer(data=row)
-
-                if input_serializer.is_valid():
-                    valid_instances.append(input_serializer)
-                else:
-                    # Add row number to errors
-                    excel_row_number = row_index + config["header_row"] + 1
-                    validation_errors.append(
-                        {
-                            "row": excel_row_number,
-                            "errors": input_serializer.errors,
-                        }
-                    )
-
-            # If there are validation errors, return them all
-            if validation_errors:
-                return Response(
-                    {
-                        "validation_errors": validation_errors,
-                        "total_errors": len(validation_errors),
-                        "total_rows_processed": len(data),
-                    },
-                    status=400,
-                )
+            valid_instances = ExcelImporter.validate_retrieved_data(
+                data,
+                serializer_class=BiomethaneSupplyInputCreateSerializer,
+                config=config,
+            )
 
             # If all validations passed, save all instances
             for serializer in valid_instances:
                 serializer.save()
 
+        except ExcelValidationError as e:
+            return Response(
+                {
+                    "validation_errors": e.validation_errors,
+                    "total_errors": len(e.validation_errors),
+                    "total_rows_processed": e.total_rows_processed,
+                },
+                status=400,
+            )
         except Exception as e:
             return Response({"file": str(e)}, status=400)
 
--- web/core/excel_importer.py
+++ web/core/excel_importer.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
 
+class ExcelValidationError(Exception):
+    """Exception raised when Excel data validation fails"""
+
+    def __init__(self, validation_errors, total_rows_processed):
+        self.validation_errors = validation_errors
+        self.total_rows_processed = total_rows_processed
+        super().__init__("Excel validation failed")
+
+
 class ExcelImporter:
     @staticmethod
     def parse(file, header_row=0, sheet_name=None):
@@ -18,3 +27,45 @@ class ExcelImporter:
         df = pd.read_excel(file, header=header_row, sheet_name=sheet_name)
 
         return df.to_dict(orient="records")
+
+    @staticmethod
+    def validate_retrieved_data(data, serializer_class, config):
+        """
+        Validate data using the provided serializer class.
+
+        Args:
+            data: List of dictionaries to validate
+            serializer_class: DRF serializer class to use for validation
+            config: Configuration dict containing header_row info
+
+        Returns:
+            List of valid serializer instances
+
+        Raises:
+            ExcelValidationError: If validation errors are found
+        """
+        # Collect all validation errors by row
+        validation_errors = []
+        valid_instances = []
+
+        for row_index, row in enumerate(data, start=1):
+            input_serializer = serializer_class(data=row)
+
+            if input_serializer.is_valid():
+                valid_instances.append(input_serializer)
+            else:
+                # Add row number to errors
+                excel_row_number = row_index + config["header_row"] + 1
+                validation_errors.append(
+                    {
+                        "row": excel_row_number,
+                        "errors": input_serializer.errors,
+                    }
+                )
+
+        # If there are validation errors, raise exception
+        if validation_errors:
+            raise ExcelValidationError(validation_errors, len(data))
+
+        # If all validations passed
+        return valid_instances
