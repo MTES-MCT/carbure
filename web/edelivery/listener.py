3c9dc42a5a962bbcb4890df4ea302d19261cce18
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2025-10-06 22:49:08 +0200

    Make it work with Redis
    … And synchronous code

--- web/edelivery/listener.py
+++ web/edelivery/listener.py
@@ -1,58 +1,53 @@
 import asyncio
+import redis
+import time
+
+from os import environ
 
 from edelivery.adapters.edelivery_adapter import send_SOAP_request
 from edelivery.soap.actions import ListPendingMessages, RetrieveMessage, SubmitMessage
 
 
+REDIS_CHANNEL = "eDelivery messages"
+
 class EdeliveryListener():
     def __init__(self, send_callback=send_SOAP_request):
         self.send_callback = send_callback
         self.subscribers = {}
+        self.redis = redis.from_url(environ["REDIS_URL"])
 
-    def start(self):
-        asyncio.create_task(self.listen())
-
-    def subscribe(self, requester, request_id):
-        self.subscribers[request_id] = requester
-
-    def trigger_message_received(self, request_response):
-        request_id = request_response.request_id()
-        print(request_id)
-        if request_id in self.subscribers.keys():
-            requester = self.subscribers[request_id]
-            requester.response_received(request_response)
-
-    def unsubscribe(self, request_id):
-        if request_id in self.subscribers.keys():
-            self.subscribers.pop(request_id)
-
-    async def listen(self):
+    def listen(self):
         while True:
+            print("New attempt…")
             r = ListPendingMessages(self.send_callback).perform()
             if r.pending_message_present():
+                print("New message pending!")
                 i = r.next_pending_message_id()
-                rr = RetrieveMessage(i, self.send_callback).perform().request_response
-                self.trigger_message_received(rr)
-            await asyncio.sleep(1)
+                rr = RetrieveMessage(i, self.send_callback).perform().request_response.payload
+                self.redis.publish(REDIS_CHANNEL, rr)
+            time.sleep(1)
 
 
 class Requester:
-    def __init__(self, request, el):
+    def __init__(self, request):
         self.request = request
-        self.el = el
-        self.request_response = None
+        redis_client = redis.from_url(environ["REDIS_URL"])
+        self.redis_channel = redis_client.pubsub(ignore_subscribe_messages=True)
 
-    def response_received(self, request_response):
-        self.request_response = request_response
-
-    async def response(self):
-        self.el.subscribe(self, "ABC-1234567890")
+    def response(self):
+        self.redis_channel.subscribe(REDIS_CHANNEL)
         SubmitMessage(self.request).perform()
-        async with asyncio.timeout(10):
-            return await self.udb_response()
+        return self.udb_response()
 
-    async def udb_response(self):
-        while True:
-            if self.request_response != None:
-                return self.request_response
-            await asyncio.sleep(1)
+    def udb_response(self):
+        result = None
+        delay = 0.01
+        i = 0
+
+        while i < 15 / delay:
+            i += 1
+            message = self.redis_channel.get_message()
+            if message: result = message["data"]
+            time.sleep(0.01)
+
+        return result
