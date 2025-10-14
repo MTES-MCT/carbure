b638b9c2edec7cc35ab0b3b3b5670488646c7549
Benjamin Grilleres <contact@benjamingrilleres.com>
2025-10-14 14:47:06 +0200

    refactor: clean unused types

--- front/src/accounting/pages/balances/biofuels/debit-operation-dialog/transfert-dialog/transfert-dialog.types.ts
+++ front/src/accounting/pages/balances/biofuels/debit-operation-dialog/transfert-dialog/transfert-dialog.types.ts
@@ -1,7 +1,7 @@
 import { QuantityFormProps } from "accounting/components/quantity-form"
 import { RecipientFormProps } from "accounting/components/recipient-form"
-import { TransfertGHGRangeFormProps } from "./ghg-range-form"
+import { GHGRangeFormProps } from "accounting/components/ghg-range-form"
 
 export type TransfertDialogForm = RecipientFormProps &
   QuantityFormProps &
-  TransfertGHGRangeFormProps
+  GHGRangeFormProps
