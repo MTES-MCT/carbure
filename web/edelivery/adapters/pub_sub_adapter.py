9e39941e3f2ae3f569ab76353afbde52b2764bf1
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2025-10-21 11:22:34 +0200

    [FIX] Get Pub/Sub adapter to return values

--- web/edelivery/adapters/pub_sub_adapter.py
+++ web/edelivery/adapters/pub_sub_adapter.py
@@ -7,14 +7,17 @@ class PubSubAdapter:
     REDIS_CHANNEL = "eDelivery messages"
 
     def __init__(self):
-        redis_client = redis.from_url(environ["REDIS_URL"])
-        self.pubsub = redis_client.pubsub(ignore_subscribe_messages=True)
+        self.redis_client = redis.from_url(environ["REDIS_URL"])
+        self.pubsub = self.redis_client.pubsub(ignore_subscribe_messages=True)
 
     def next_message(self):
-        self.pubsub.get_message()
+        return self.pubsub.get_message()
+
+    def publish(self, message):
+        return self.redis_client.publish(self.REDIS_CHANNEL, message)
 
     def subscribe(self):
-        self.pubsub.subscribe(self.REDIS_CHANNEL)
+        return self.pubsub.subscribe(self.REDIS_CHANNEL)
 
     def unsubscribe(self):
-        self.pubsub.unsubscribe(self.REDIS_CHANNEL)
+        return self.pubsub.unsubscribe(self.REDIS_CHANNEL)
