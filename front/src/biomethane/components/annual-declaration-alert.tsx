081ff7d6e5efe671180852b405ea665a55b62b11
Benjamin Grilleres <contact@benjamingrilleres.com>
2025-10-21 15:38:33 +0200

    feat: add annual declaration alert

---
+++ front/src/biomethane/components/annual-declaration-alert.tsx
@@ -0,0 +1,28 @@
+import { useAnnualDeclaration } from "biomethane/providers/annual-declaration.provider"
+import { AnnualDeclarationStatus } from "biomethane/types"
+import { Notice } from "common/components/notice"
+import { useTranslation } from "react-i18next"
+
+export const AnnualDeclarationAlert = () => {
+  const { t } = useTranslation()
+  const { currentAnnualDeclaration, isInDeclarationPeriod } =
+    useAnnualDeclaration()
+  console.log(currentAnnualDeclaration)
+  if (
+    !isInDeclarationPeriod ||
+    currentAnnualDeclaration?.status === AnnualDeclarationStatus.IN_PROGRESS
+  )
+    return null
+
+  return (
+    <Notice
+      variant="warning"
+      icon="ri-alert-line"
+      title={t("Déclaration annuelle déjà transmise")}
+    >
+      {t(
+        "En cas de modification de champs sur cette page, votre déclaration annuelle (digestat et énergie) devra être soumise à nouveau."
+      )}
+    </Notice>
+  )
+}
