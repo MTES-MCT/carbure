5ab2f3ee625101de2816bd3803ac0be17e70b5b1
Yohann <yohann.ulreich@beta.gouv.fr>
2025-10-21 18:34:11 +0200

    feat(biomethane): reset declaration to IN_PROGRESS when update some fieds on contract or production unit

--- web/biomethane/services/annual_declaration.py
+++ web/biomethane/services/annual_declaration.py
@@ -1,4 +1,11 @@
-from biomethane.models import BiomethaneAnnualDeclaration, BiomethaneDigestate, BiomethaneEnergy
+from biomethane.models import (
+    BiomethaneContract,
+    BiomethaneDigestate,
+    BiomethaneEnergy,
+    BiomethaneProductionUnit,
+)
+from biomethane.models.biomethane_annual_declaration import BiomethaneAnnualDeclaration
+from biomethane.utils import get_declaration_period
 
 
 class BiomethaneAnnualDeclarationService:
@@ -69,3 +76,72 @@ class BiomethaneAnnualDeclarationService:
             return declaration.status != BiomethaneAnnualDeclaration.DECLARED
         except BiomethaneAnnualDeclaration.DoesNotExist:
             return True
+
+    @staticmethod
+    def get_watched_fields():
+        """
+        Return the list of fields from related models (production_unit, contract)
+        that impact the conditional rules of an energy or digestate instance.
+
+        Returns:
+            dict: {
+                'production_unit': list,  # Fields to watch on production_unit
+                'contract': list,         # Fields to watch on contract
+            }
+        """
+        PRODUCTION_UNIT_WATCHED_FIELDS = [
+            "installed_meters",  # Impacts FLARING_FIELDS
+            "has_digestate_phase_separation",  # Impacts RAW_DIGESTATE_FIELDS vs SEPARATED_DIGESTATE_FIELDS
+            "digestate_valorization_methods",  # Impacts SPREADING, INCINERATION, COMPOSTING fields
+            "spreading_management_methods",  # Impacts SALE_FIELDS
+        ]
+
+        CONTRACT_WATCHED_FIELDS = [
+            "tariff_reference",  # Impacts OLD_TARIFF_FIELDS and NEW_TARIFF_FIELDS
+            "installation_category",  # Impacts WWTP_FIELDS
+        ]
+
+        return {
+            "production_unit": PRODUCTION_UNIT_WATCHED_FIELDS,
+            "contract": CONTRACT_WATCHED_FIELDS,
+        }
+
+    @staticmethod
+    def has_watched_field_changed(instance, changed_fields):
+        """
+        Check if any watched field has changed on a production_unit or contract instance.
+
+        Args:
+            instance: A BiomethaneProductionUnit or BiomethaneContract instance
+            changed_fields: List of field names that have changed
+
+        Returns:
+            bool: True if any watched field has changed, False otherwise
+
+        """
+        watched_fields = BiomethaneAnnualDeclarationService.get_watched_fields()
+
+        if isinstance(instance, BiomethaneProductionUnit):
+            return any(field in changed_fields for field in watched_fields["production_unit"])
+        elif isinstance(instance, BiomethaneContract):
+            return any(field in changed_fields for field in watched_fields["contract"])
+
+        return False
+
+    @staticmethod
+    def reset_annual_declaration_status(producer):
+        """
+        Reset the annual declaration status to IN_PROGRESS for a given producer and year.
+
+        Args:
+            producer: The Entity instance representing the producer
+            year: The year of the declaration to reset
+
+        """
+        try:
+            declaration = BiomethaneAnnualDeclaration.objects.get(producer=producer, year=get_declaration_period())
+            if declaration.status != BiomethaneAnnualDeclaration.IN_PROGRESS:
+                declaration.status = BiomethaneAnnualDeclaration.IN_PROGRESS
+                declaration.save(update_fields=["status"])
+        except BiomethaneAnnualDeclaration.DoesNotExist:
+            pass
