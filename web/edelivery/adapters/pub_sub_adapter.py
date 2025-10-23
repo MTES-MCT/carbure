ccb19cb90aa11ee254207f8ee75f39c3936d108d
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2025-10-23 12:51:42 +0200

    Stop and restart polling through redis eDelivery service channel

--- web/edelivery/adapters/pub_sub_adapter.py
+++ web/edelivery/adapters/pub_sub_adapter.py
@@ -4,7 +4,8 @@ import redis
 
 
 class PubSubAdapter:
-    REDIS_CHANNEL = "eDelivery messages"
+    REDIS_RESPONSES_CHANNEL = "eDelivery responses"
+    REDIS_SERVICE_CHANNEL = "eDelivery service"
 
     def __init__(self):
         self.redis_client = redis.from_url(environ["REDIS_URL"])
@@ -15,10 +16,16 @@ class PubSubAdapter:
         return message and message["data"].decode("utf-8")
 
     def publish(self, message):
-        return self.redis_client.publish(self.REDIS_CHANNEL, message)
+        return self.redis_client.publish(self.REDIS_RESPONSES_CHANNEL, message)
+
+    def service(self, message):
+        return self.redis_client.publish(self.REDIS_SERVICE_CHANNEL, message)
 
     def subscribe(self):
-        return self.pubsub.subscribe(self.REDIS_CHANNEL)
+        return self.pubsub.subscribe(self.REDIS_RESPONSES_CHANNEL)
+
+    def subscribeToServiceChannel(self):
+        return self.pubsub.subscribe(self.REDIS_SERVICE_CHANNEL)
 
     def unsubscribe(self):
-        return self.pubsub.unsubscribe(self.REDIS_CHANNEL)
+        return self.pubsub.unsubscribe(self.REDIS_RESPONSES_CHANNEL)
--- web/edelivery/soap/listener.py
+++ web/edelivery/soap/listener.py
@@ -5,8 +5,23 @@ from edelivery.soap.actions import ListPendingMessages, RetrieveMessage
 
 
 class Listener:
+    START_SERVICE_COMMAND = "start_listener"
+    STOP_SERVICE_COMMAND = "stop_listener"
+
+    @staticmethod
+    def send_stop_signal():
+        p = PubSubAdapter()
+        p.service(Listener.STOP_SERVICE_COMMAND)
+
+    @staticmethod
+    def send_start_signal():
+        p = PubSubAdapter()
+        p.service(Listener.START_SERVICE_COMMAND)
+
     def __init__(self):
         self.pub_sub_adapter = PubSubAdapter()
+        self.pub_sub_adapter.subscribeToServiceChannel()
+        self.started = False
 
     def poll_once(self):
         list = ListPendingMessages().perform()
@@ -16,6 +31,17 @@ class Listener:
             self.pub_sub_adapter.publish(payload)
 
     def start(self):
+        self.started = True
+
         while True:
-            self.poll_once()
+            message = self.pub_sub_adapter.next_message()
+            if message == Listener.STOP_SERVICE_COMMAND:
+                self.started = False
+
+            if message == Listener.START_SERVICE_COMMAND:
+                self.started = True
+
+            if self.started:
+                self.poll_once()
+
             sleep(1)
