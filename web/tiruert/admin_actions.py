8302c1087cec36f840e089a36286b2820b85543a
Yohann <yohann.ulreich@beta.gouv.fr>
2025-10-30 16:08:07 +0100

    feat(tiruert): accept EXPORTATION and EXPEDITION operation types in django admin bulk validation

--- web/tiruert/admin_actions.py
+++ web/tiruert/admin_actions.py
@@ -5,15 +5,17 @@ from tiruert.models.operation import Operation
 
 def perform_bulk_operations_validation(modeladmin, request, queryset):
     """
-    Validates all selected 'INCORPORATION' or 'CESSION' operations in bulk.
+    Validates all selected 'INCORPORATION', 'CESSION', 'EXPORTATION' or 'EXPEDITION' operations in bulk.
     """
     with transaction.atomic():
         for obj in queryset:
             if obj.status == Operation.PENDING:
-                if obj.type == Operation.INCORPORATION:
+                if obj.type in [Operation.INCORPORATION, Operation.EXPORTATION, Operation.EXPEDITION]:
                     obj.status = Operation.VALIDATED
                 elif obj.type == Operation.CESSION:
                     obj.status = Operation.ACCEPTED
+                else:
+                    continue  # Skip all other operation types
                 obj.save()
 
     modeladmin.message_user(request, "Les opérations ont été validées avec succès.")
