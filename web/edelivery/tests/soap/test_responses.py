c9eebd9d220a57b3997c81ae8ca8c493afa011c0
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2025-11-03 13:44:21 +0100

    Set error flag on SubmitMessageResponse when appropriate

--- web/edelivery/tests/soap/test_responses.py
+++ web/edelivery/tests/soap/test_responses.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from unittest.mock import patch
 
 from edelivery.ebms.request_responses import BaseRequestResponse
-from edelivery.soap.responses import ListPendingMessagesResponse, RetrieveMessageResponse
+from edelivery.soap.responses import ListPendingMessagesResponse, RetrieveMessageResponse, SubmitMessageResponse
 
 
 class ListPendingMessagesResponseTest(TestCase):
@@ -66,3 +66,45 @@ class RetrieveMessageResponseTest(TestCase):
         response = RetrieveMessageResponse(self.response_payload("Base64EncodedZippedArchive"))
         self.patched_unzip.assert_called_with("Base64EncodedZippedArchive")
         self.assertEqual("<response/>", response.contents)
+
+
+class SubmitMessageResponseTest(TestCase):
+    @staticmethod
+    def submit_response_payload():
+        return """\
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+    <soap:Body>
+        <ns4:submitResponse xmlns:ns4="http://eu.domibus.wsplugin/">
+            <messageID>11111111-1111-1111-1111-111111111111</messageID>
+        </ns4:submitResponse>
+    </soap:Body>
+</soap:Envelope>"""
+
+    @staticmethod
+    def response_error_payload(code, message):
+        return f"""\
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+    <soap:Body>
+        <soap:Fault>
+            <!-- … -->
+            <soap:Detail>
+                <ns2:FaultDetail xmlns:ns2="http://eu.domibus.wsplugin/">
+                    <code>{code}</code>
+                    <message>{message}</message>
+                </ns2:FaultDetail>
+            </soap:Detail>
+        </soap:Fault>
+    </soap:Body>
+</soap:Envelope>"""
+
+    def test_sets_error_flag_when_error_in_payload(self):
+        payload = self.response_error_payload("EBMS:1234", "Some error message")
+        response = SubmitMessageResponse(payload)
+        self.assertTrue(response.error)
+        self.assertEqual("EBMS:1234 - Some error message", response.error_message)
+
+    def test_leaves_error_flag_and_error_message_unset_when_regular_response(self):
+        payload = self.submit_response_payload()
+        response = SubmitMessageResponse(payload)
+        self.assertFalse(response.error)
+        self.assertIsNone(response.error_message)
