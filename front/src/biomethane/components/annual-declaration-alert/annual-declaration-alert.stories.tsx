3dd04eb28e1554fe693164acf410c9068963ca80
Benjamin Grilleres <contact@benjamingrilleres.com>
2025-11-03 13:38:11 +0100

    refactor: simplify annual declaration provider mocks

--- front/src/biomethane/components/annual-declaration-alert/annual-declaration-alert.stories.tsx
+++ front/src/biomethane/components/annual-declaration-alert/annual-declaration-alert.stories.tsx
@@ -1,31 +1,16 @@
 import type { Meta, StoryObj } from "@storybook/react"
 import { AnnualDeclarationAlert } from "./annual-declaration-alert"
-import { AnnualDeclarationStatus } from "biomethane/types"
 import {
-  AnnualDeclarationContextProvider,
   createMockAnnualDeclaration,
+  generateAnnualDeclarationContextProvider,
 } from "./annual-declaration-alert.stories.utils"
 import { expect, within } from "@storybook/test"
+import { AnnualDeclarationStatus } from "biomethane/types"
 
 const meta: Meta<typeof AnnualDeclarationAlert> = {
   component: AnnualDeclarationAlert,
   title: "modules/biomethane/components/annual-declaration-alert",
-  decorators: [
-    (Story) => (
-      <AnnualDeclarationContextProvider
-        selectedYear={2024}
-        currentAnnualDeclaration={createMockAnnualDeclaration(
-          AnnualDeclarationStatus.IN_PROGRESS
-        )}
-        isInDeclarationPeriod={false}
-        isDeclarationValidated={false}
-        canEditDeclaration={false}
-        hasAnnualDeclarationMissingObjects={false}
-      >
-        <Story />
-      </AnnualDeclarationContextProvider>
-    ),
-  ],
+  decorators: [generateAnnualDeclarationContextProvider()],
 }
 
 export default meta
@@ -47,39 +32,21 @@ export const Default: Story = {
 export const DeclarationInProgress: Story = {
   ...Default,
   decorators: [
-    (Story) => (
-      <AnnualDeclarationContextProvider
-        currentAnnualDeclaration={createMockAnnualDeclaration(
-          AnnualDeclarationStatus.IN_PROGRESS
-        )}
-        selectedYear={2024}
-        isInDeclarationPeriod
-        isDeclarationValidated={false}
-        canEditDeclaration={false}
-        hasAnnualDeclarationMissingObjects={false}
-      >
-        <Story />
-      </AnnualDeclarationContextProvider>
-    ),
+    generateAnnualDeclarationContextProvider({
+      isInDeclarationPeriod: true,
+    }),
   ],
 }
 
 // Case 3: In declaration period and declaration already submitted - should display alert
 export const DeclarationAlreadySubmitted: Story = {
   decorators: [
-    (Story) => (
-      <AnnualDeclarationContextProvider
-        currentAnnualDeclaration={createMockAnnualDeclaration(
-          AnnualDeclarationStatus.DECLARED
-        )}
-        selectedYear={2024}
-        isInDeclarationPeriod
-        isDeclarationValidated={true}
-        canEditDeclaration={false}
-        hasAnnualDeclarationMissingObjects={false}
-      >
-        <Story />
-      </AnnualDeclarationContextProvider>
-    ),
+    generateAnnualDeclarationContextProvider({
+      isInDeclarationPeriod: true,
+      isDeclarationValidated: true,
+      currentAnnualDeclaration: createMockAnnualDeclaration(
+        AnnualDeclarationStatus.DECLARED
+      ),
+    }),
   ],
 }
