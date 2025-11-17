a27277bfd0239038740d56e8d637ce58b8a47de4
jfalxa <jfalxa.sta@gmail.com>
2025-11-17 11:40:27 +0100

    feat(saf): constrain airport lookup endpoint with new saf logistics model

--- web/core/utils.py
+++ web/core/utils.py
@@ -1,13 +1,14 @@
 import os
 import unicodedata
 from os import environ as env
+from typing import Type
 
 import pandas as pd
 import xlsxwriter
 from django import forms
 from django.conf import settings
 from django.core.paginator import Paginator
-from django.db import connection, transaction
+from django.db import connection, models, transaction
 from rest_framework.pagination import PageNumberPagination
 
 from core.models import Entity, UserRights
@@ -253,3 +254,7 @@ def is_bool_or_none(column):
     false_mask = (values == "false") | (values == "0") | (values == "non") | (values == "no")
     result[false_mask] = False
     return result
+
+
+def extract_enum(Model: Type[models.Model], field: str) -> list:
+    return [value for value, _ in Model._meta.get_field(field).choices]
