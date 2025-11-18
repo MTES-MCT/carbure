ac322729e08457d43d60765e601a5c87584f42ff
Benjamin Grilleres <contact@benjamingrilleres.com>
2025-11-18 08:25:31 +0100

    Add SiteAnonymizer for Site model anonymization
    - Create sites.py with SiteAnonymizer class following the same pattern as UserAnonymizer and EntityAnonymizer
    - Anonymize sensitive fields: name, site_siret, customs_id, address, postal_code, city, gps_coordinates, dc_number, dc_reference, manager_name, manager_phone, manager_email, icao_code
    - Fix GPS coordinates generation to use valid French coordinates (latitude: 42-51°N, longitude: -5-10°E)
    - Add conditional anonymization for dc_number and dc_reference based on eligible_dc flag

---
+++ web/core/services/data_anonymization/sites.py
@@ -0,0 +1,56 @@
+"""
+Anonymiseur spécialisé pour les sites (Site).
+"""
+
+from faker import Faker
+
+from transactions.models import Site
+
+from .base import Anonymizer
+from .utils import anonymize_fields_and_collect_modifications
+
+
+class SiteAnonymizer(Anonymizer):
+    def __init__(self, fake: Faker):
+        self.fake = fake
+
+    def get_model(self):
+        return Site
+
+    def get_queryset(self):
+        return Site.objects.filter(id=1)
+
+    def get_updated_fields(self):
+        return [
+            "name",
+            "site_siret",
+            "customs_id",
+            "address",
+            "postal_code",
+            "city",
+            "gps_coordinates",
+            "dc_number",
+            "manager_name",
+            "manager_phone",
+            "manager_email",
+            "icao_code",
+        ]
+
+    def process(self, site):
+        fields_to_anonymize = {
+            "name": f"Site {site.site_type} {site.id}",
+            "site_siret": self.fake.bothify(text="##############"),
+            "customs_id": self.fake.bothify(text="CUST-####"),
+            "address": self.fake.address(),
+            "postal_code": self.fake.postcode(),
+            "city": self.fake.city(),
+            "gps_coordinates": f"{42 + self.fake.random.uniform(0, 9):.6f}, {self.fake.random.uniform(-5, 10):.6f}",
+            "dc_number": self.fake.bothify(text="DC-####") if site.eligible_dc else None,
+            "dc_reference": self.fake.bothify(text="DC-REF-####") if site.eligible_dc else None,
+            "manager_name": self.fake.name(),
+            "manager_phone": self.fake.phone_number(),
+            "manager_email": f"manager{site.id}@anonymized.local",
+            "icao_code": self.fake.bothify(text="????").upper(),
+        }
+
+        return anonymize_fields_and_collect_modifications(site, fields_to_anonymize)
