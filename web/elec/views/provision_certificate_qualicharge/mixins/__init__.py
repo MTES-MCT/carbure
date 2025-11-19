3ca5ceea2abe41c188b204af5f7ff6fe6c085788
Yohann <yohann.ulreich@beta.gouv.fr>
2025-11-19 17:25:49 +0100

    feat(qualicharge): remove transfer mixin

--- web/elec/views/provision_certificate_qualicharge/mixins/__init__.py
+++ web/elec/views/provision_certificate_qualicharge/mixins/__init__.py
@@ -2,13 +2,11 @@
 from .bulk_create import BulkCreateMixin
 from .bulk_update import BulkUpdateMixin
 from .filter import FilterActionMixin
-from .transfer import TransferMixin
 
 
 class ActionMixin(
     BulkCreateMixin,
     BulkUpdateMixin,
-    TransferMixin,
     FilterActionMixin,
 ):
     pass
