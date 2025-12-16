18facf4c4112455f3623fbf13ce0dad1490a5092
Yohann <yohann.ulreich@beta.gouv.fr>
2025-12-16 11:10:00 +0100

    feat(entity): new model attribute and admin action to close entity

--- web/entity/admin.py
+++ web/entity/admin.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.contrib import admin
+from django.utils import timezone
 
-from core.models import Department, Entity
+from core.models import Department, Entity, UserRights, UserRightsRequest
 from entity.models import EntityScopeDepartment, EntityScopeDepot
 from entity.services.enable_entity import enable_entity
 from transactions.models import Depot
@@ -95,23 +96,38 @@ class EntityDepotInline(admin.TabularInline):
 
 class EntityAdmin(admin.ModelAdmin):
     list_display = (
-        "entity_type",
         "name",
+        "entity_type",
         "parent_entity",
         "is_enabled",
+        "closed_at",
     )
     search_fields = ("entity_type", "name")
     list_filter = ["entity_type"]
     readonly_fields = ["is_enabled"]
     inlines = [EntityDepartmentInline, EntityDepotInline]
 
-    actions = ["enable_entity"]
+    actions = ["enable_entity", "deactivate_entity"]
+
+    def get_queryset(self, request):
+        return Entity.all_objects.get_queryset()
 
     def enable_entity(self, request, queryset):
         for entity in queryset:
             enable_entity(entity, request)
 
-    enable_entity.short_description = "Activer les sociétés sélectionnées"
+    enable_entity.short_description = "Valider les sociétés sélectionnées"
+
+    def deactivate_entity(self, request, queryset):
+        for entity in queryset:
+            entity.closed_at = timezone.now()
+            entity.save()
+
+            # Remove all UserRightsRequests and UserRights for this entity
+            UserRightsRequest.objects.filter(entity=entity).delete()
+            UserRights.objects.filter(entity=entity).delete()
+
+    deactivate_entity.short_description = "Désactiver les sociétés (fermées) sélectionnées"
 
 
 admin.site.register(Entity, EntityAdmin)
