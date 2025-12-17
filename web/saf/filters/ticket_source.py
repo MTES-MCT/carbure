f4bffa5776685212bbee83d6fd9c600bc2fc5aaf
jfalxa <jfalxa.sta@gmail.com>
2025-12-17 19:01:51 +0100

    refactor(saf): use filter action mixin for ticket sources

--- web/saf/filters/ticket_source.py
+++ web/saf/filters/ticket_source.py
@@ -1,10 +1,13 @@
 from enum import Enum
 
 import django_filters
-from django.db.models import F, Q
+from django.db.models import F
+from django.db.models.functions import Coalesce
+from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
-from rest_framework.serializers import CharField, ChoiceField, ListField
+from rest_framework.serializers import ChoiceField
 
+from core.filters import AllAnnotatedValuesMultipleFilter
 from saf.models import SafTicketSource
 
 
@@ -14,26 +17,36 @@ class TicketStatusEnum(Enum):
 
 
 class TicketSourceFilter(django_filters.FilterSet):
-    year = django_filters.NumberFilter(field_name="year")
-
-    period = django_filters.AllValuesMultipleFilter(field_name="delivery_period")
-    feedstock = django_filters.AllValuesMultipleFilter(field_name="feedstock__code")
-    added_by = django_filters.AllValuesMultipleFilter(field_name="added_by__name")
-    client = django_filters.AllValuesMultipleFilter(field_name="saf_tickets__client__name")
-    supplier = django_filters.CharFilter(method="filter_supplier")
-    country_of_origin = django_filters.AllValuesMultipleFilter(field_name="country_of_origin__code_pays")
-    production_site = django_filters.AllValuesMultipleFilter(field_name="carbure_production_site__name")
-    delivery_site = django_filters.AllValuesMultipleFilter(field_name="parent_lot__carbure_delivery_site__name")
-
     status = django_filters.ChoiceFilter(
         choices=[(item.name, item.value) for item in TicketStatusEnum],
         method="filter_status",
     )
 
+    year = django_filters.NumberFilter(field_name="year")
+    added_by = django_filters.AllValuesMultipleFilter(field_name="added_by__name")
+
+    supplier = extend_schema_field(OpenApiTypes.STR)(
+        AllAnnotatedValuesMultipleFilter(
+            field_name="supplier",
+            annotation=Coalesce(
+                "parent_lot__carbure_supplier__name",
+                "parent_lot__unknown_supplier",
+                "parent_ticket__supplier__name",
+            ),
+        )
+    )
+
+    client = django_filters.AllValuesMultipleFilter(field_name="saf_tickets__client__name")
+    period = django_filters.AllValuesMultipleFilter(field_name="delivery_period")
+    feedstock = django_filters.AllValuesMultipleFilter(field_name="feedstock__code")
+    country_of_origin = django_filters.AllValuesMultipleFilter(field_name="country_of_origin__code_pays")
+    production_site = django_filters.AllValuesMultipleFilter(field_name="carbure_production_site__name")
+    origin_depot = django_filters.AllValuesMultipleFilter(field_name="origin_lot_site__name")
+
     order_by = django_filters.OrderingFilter(
         fields=(
             ("total_volume", "volume"),
-            ("delivery_period", "period"),
+            ("delivery_period", "delivery"),
             ("feedstock__code", "feedstock"),
             ("ghg_reduction", "ghg_reduction"),
             ("added_by__name", "added_by"),
@@ -49,33 +62,17 @@ class TicketSourceFilter(django_filters.FilterSet):
         else:
             raise Exception(f"Status '{value}' does not exist for ticket sources")
 
-    @extend_schema_field(
-        ListField(
-            child=CharField(),
-            help_text="List of suppliers provided via ?suppliers=supplier1&suppliers=supplier2&suppliers=supplier3",
-            required=False,
-        )
-    )
-    def filter_supplier(self, queryset, name, value):
-        request = self.request
-        suppliers_list = request.GET.getlist("supplier")
-        return queryset.filter(
-            Q(parent_lot__carbure_supplier__name__in=suppliers_list)
-            | Q(parent_lot__unknown_supplier__in=suppliers_list)
-            | Q(parent_ticket__supplier__name__in=suppliers_list)
-        )
-
     class Meta:
         model = SafTicketSource
         fields = [
+            "status",
             "year",
+            "added_by",
+            "supplier",
+            "client",
             "period",
             "feedstock",
-            "client",
-            "supplier",
             "country_of_origin",
             "production_site",
-            "delivery_site",
-            "status",
-            "added_by",
+            "origin_depot",
         ]
