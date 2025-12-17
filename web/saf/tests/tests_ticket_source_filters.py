0457b6121a42431f0d884e9c8d25c174befff499
jfalxa <jfalxa.sta@gmail.com>
2025-12-17 19:01:51 +0100

    refactor(saf): use filter action mixin for ticket sources

--- web/saf/tests/tests_ticket_source_filters.py
+++ web/saf/tests/tests_ticket_source_filters.py
@@ -1,12 +1,14 @@
-from django.urls import reverse
-
-from core.models import MatierePremiere
+from core.models import MatierePremiere, Pays
+from core.tests_utils import FiltersActionTestMixin
+from resources.factories.production_site import ProductionSiteFactory
 from saf.factories import SafTicketFactory, SafTicketSourceFactory
 from saf.models import SafTicket, SafTicketSource
 from saf.tests import TestCase
+from saf.views import SafTicketSourceViewSet
+from transactions.factories.depot import DepotFactory
 
 
-class SafTicketSourceFiltersTest(TestCase):
+class SafTicketSourceFiltersTest(TestCase, FiltersActionTestMixin):
     def setUp(self):
         super().setUp()
 
@@ -14,52 +16,25 @@ class SafTicketSourceFiltersTest(TestCase):
         self.colza = MatierePremiere.objects.get(code="COLZA")
         self.hau = MatierePremiere.objects.get(code="HUILE_ALIMENTAIRE_USAGEE")
         self.hga = MatierePremiere.objects.get(code="HUILES_OU_GRAISSES_ANIMALES_CAT1_CAT2")
+        self.fr = Pays.objects.create(code_pays="FR", name="France", name_en="France")
+        self.psite = ProductionSiteFactory.create(name="Production Site 1")
+        self.depot = DepotFactory.create(name="Depot 1")
 
         SafTicketSource.objects.all().delete()
         SafTicket.objects.all().delete()
         self.ticket_sources = [
-            SafTicketSourceFactory.create(
-                year=2022,
-                delivery_period=202201,
-                added_by_id=self.entity.id,
-                feedstock=self.hau,
-            ),
-            SafTicketSourceFactory.create(
-                year=2022,
-                delivery_period=202201,
-                added_by_id=self.entity.id,
-                feedstock=self.hga,
-            ),
-            SafTicketSourceFactory.create(
-                year=2022,
-                delivery_period=202201,
-                added_by_id=self.entity.id,
-                feedstock=self.ble,
-            ),
-            SafTicketSourceFactory.create(
-                year=2022,
-                delivery_period=202201,
-                added_by_id=self.entity.id,
-                feedstock=self.colza,
-            ),
-            SafTicketSourceFactory.create(
-                year=2022,
-                delivery_period=202202,
-                added_by_id=self.entity.id,
-                feedstock=self.hau,
-            ),
-            SafTicketSourceFactory.create(
-                year=2022,
-                delivery_period=202202,
-                added_by_id=self.entity.id,
-                feedstock=self.hga,
-            ),
+            self.create_ticket_source(),
+            self.create_ticket_source(feedstock=self.hga),
+            self.create_ticket_source(feedstock=self.ble),
+            self.create_ticket_source(feedstock=self.colza),
+            self.create_ticket_source(delivery_period=202202),
+            self.create_ticket_source(feedstock=self.hga, delivery_period=202202),
         ]
 
         first_id = self.ticket_sources[0].id
 
         SafTicket.objects.all().delete()
-        SafTicketFactory.create(
+        self.ticket = SafTicketFactory.create(
             year=2022,
             supplier_id=self.entity.id,
             client_id=self.client1.id,
@@ -74,64 +49,38 @@ class SafTicketSourceFiltersTest(TestCase):
             parent_ticket_source_id=first_id,
         )
 
-    def test_empty_ticket_source_filters(self):
-        query = {
-            "entity_id": self.entity.id,
-            "year": 2021,
-            "status": "AVAILABLE",
-            "filter": "feedstock",
-        }
-        response = self.client.get(reverse("saf-ticket-sources-filters"), query)
-        assert response.status_code == 200
-        assert response.json() == []
+        # create a ticket source with a parent ticket so we can check for the supplier
+        self.create_ticket_source(parent_ticket=self.ticket)
 
-    def test_ticket_source_filters_feedstock(self):
-        query = {
-            "entity_id": self.entity.id,
+    def create_ticket_source(self, **overrides):
+        props = {
             "year": 2022,
-            "status": "AVAILABLE",
-            "filter": "feedstock",
+            "delivery_period": 202201,
+            "added_by_id": self.entity.id,
+            "feedstock": self.hau,
+            "assigned_volume": 0,
+            "country_of_origin": self.fr,
+            "carbure_production_site": self.psite,
+            "origin_lot_site": self.depot,
+            **overrides,
         }
-        response = self.client.get(reverse("saf-ticket-sources-filters"), query)
 
-        assert response.status_code == 200
-        assert sorted(response.json()) == sorted(
-            ["HUILE_ALIMENTAIRE_USAGEE", "HUILES_OU_GRAISSES_ANIMALES_CAT1_CAT2", "BLE", "COLZA"]
+        return SafTicketSourceFactory.create(**props)
+
+    def test_ticket_source_filters(self):
+        self.assertFilters(
+            SafTicketSourceViewSet,
+            {
+                "added_by": [self.entity.name],
+                "supplier": [self.entity.name],
+                "client": [self.client1.name, self.client2.name],
+                "feedstock": [self.ble.code, self.colza.code, self.hau.code, self.hga.code],
+                "period": [202201, 202202],
+                "year": [2022],
+                "country_of_origin": [self.fr.code_pays],
+                "production_site": [self.psite.name],
+                "origin_depot": [self.depot.name],
+            },
+            entity=self.entity,
+            ignore=["order_by"],
         )
-
-    def test_ticket_source_filters_period_feedstock(self):
-        query = {
-            "entity_id": self.entity.id,
-            "year": 2022,
-            "status": "AVAILABLE",
-            "filter": "feedstock",
-            "period": 202202,
-        }
-        response = self.client.get(reverse("saf-ticket-sources-filters"), query)
-
-        assert response.status_code == 200
-        assert sorted(response.json()) == sorted(["HUILE_ALIMENTAIRE_USAGEE", "HUILES_OU_GRAISSES_ANIMALES_CAT1_CAT2"])
-
-    def test_ticket_source_filters_period(self):
-        query = {
-            "entity_id": self.entity.id,
-            "year": 2022,
-            "status": "AVAILABLE",
-            "filter": "period",
-        }
-        response = self.client.get(reverse("saf-ticket-sources-filters"), query)
-
-        assert response.status_code == 200
-        assert sorted(response.json()) == sorted([202201, 202202])
-
-    def test_ticket_source_filters_client(self):
-        query = {
-            "entity_id": self.entity.id,
-            "year": 2022,
-            "status": "AVAILABLE",
-            "filter": "client",
-        }
-        response = self.client.get(reverse("saf-ticket-sources-filters"), query)
-
-        assert response.status_code == 200
-        assert sorted(response.json()) == sorted([self.client1.name, self.client2.name])
