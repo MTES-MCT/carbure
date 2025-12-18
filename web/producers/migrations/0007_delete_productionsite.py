1856ccb7b8abadb68e5b3c14e4f818beb9ef9876
Yohann <yohann.ulreich@beta.gouv.fr>
2025-12-18 09:51:29 +0100

    fix(migration): fix migrations order

--- web/producers/migrations/0007_delete_productionsite.py
+++ web/producers/migrations/0007_delete_productionsite.py
@@ -6,6 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
     dependencies = [
         ("producers", "0006_alter_productionsiteinput_production_site_and_more"),
+        ("transactions", "0014_delete_contenttoupdate"),
     ]
 
     operations = [
