428db1f2c797007b767fcf4b41cdb4a7efc05805
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2026-02-04 15:34:16 +0100

    Raise `UDBConversionError` if UDB unit unknown

--- web/edelivery/ebms/converters.py
+++ web/edelivery/ebms/converters.py
@@ -1,6 +1,12 @@
 from core.models import CarbureLot
 
 
+class UDBConversionError(RuntimeError):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+
 class QuantityConverter:
     _udb_unit_to_conversion_function_mapping = {
         "MWh": (CarbureLot.lhv_amount.field.name, (lambda x: x * 3600)),
@@ -10,5 +16,8 @@ class QuantityConverter:
         self.conversion_mapping = conversion_mapping if conversion_mapping else self._udb_unit_to_conversion_function_mapping
 
     def from_udb(self, unit, quantity):
+        if unit not in self.conversion_mapping:
+            raise UDBConversionError(f"Unknown UDB Unit: {unit}")
+
         (attribute, conversion_function) = self.conversion_mapping.get(unit)
         return {attribute: conversion_function(quantity)}
--- web/edelivery/tests/ebms/test_converters.py
+++ web/edelivery/tests/ebms/test_converters.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from edelivery.ebms.converters import QuantityConverter
+from edelivery.ebms.converters import QuantityConverter, UDBConversionError
 
 
 class QuantityConverterTest(TestCase):
@@ -8,3 +8,11 @@ class QuantityConverterTest(TestCase):
         conversion_mapping = {"UDB_UNIT": ("some_model_attribute", (lambda x: x * 2))}
         converter = QuantityConverter(conversion_mapping)
         self.assertEqual({"some_model_attribute": 20}, converter.from_udb("UDB_UNIT", 10))
+
+    def test_raises_udb_conversion_error_if_unit_unknown(self):
+        conversion_mapping = {"UDB_UNIT": ("some_model_attribute", (lambda x: x * 2))}
+        converter = QuantityConverter(conversion_mapping)
+        with self.assertRaises(UDBConversionError) as context:
+            converter.from_udb("UNKNOWN_UNIT", 10)
+
+        self.assertEqual("Unknown UDB Unit: UNKNOWN_UNIT", context.exception.message)
