0a43af84df69ae9b8312d5ff1c6db0fd3e1b3b56
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2026-02-05 17:44:44 +0100

    Handle UDB responses containing more than one transaction

---
+++ web/edelivery/ebms/transaction.py
@@ -0,0 +1,75 @@
+from datetime import datetime
+from xml.etree import ElementTree as ET
+
+from edelivery.ebms.converters import MaterialConverter, QuantityConverter
+from edelivery.ebms.ntr import from_national_trade_register
+from transactions.helpers import compute_lot_quantity
+
+
+class Transaction:
+    @classmethod
+    def from_xml(cls, xml_data):
+        return cls(ET.fromstring(xml_data))
+
+    def __init__(self, xml_root_element):
+        self.xml_root_element = xml_root_element
+
+    def biofuel_code(self):
+        return self.xml_root_element.find("./MATERIAL_CODE").text
+
+    def client_id(self):
+        return self.xml_root_element.find("./BUYER_ECONOMIC_OPERATOR_NUMBER").text
+
+    def delivery_date(self):
+        delivery_date_text = self.xml_root_element.find("./DELIVERY_DATE").text
+        return datetime.fromisoformat(delivery_date_text)
+
+    def feedstock_code(self):
+        xpath = "./EO_TRANS_DETAIL_MATERIALS/POINT_OF_ORIGIN_MATERIAL_DATA/MATERIAL_CODE"
+        return self.xml_root_element.find(xpath).text
+
+    def iso_format_delivery_date(self):
+        return self.delivery_date().date().isoformat()
+
+    def period(self):
+        delivery_date = self.delivery_date()
+        return delivery_date.year * 100 + delivery_date.month
+
+    def status(self):
+        return self.xml_root_element.find("./STATUS").text
+
+    def supplier_id(self):
+        return self.xml_root_element.find("./SELLER_ECONOMIC_OPERATOR_NUMBER").text
+
+    def to_lot_attributes(self):
+        biofuel = MaterialConverter().from_udb_biofuel_code(self.biofuel_code())
+        client = from_national_trade_register(self.client_id())
+        feedstock = MaterialConverter().from_udb_feedstock_code(self.feedstock_code())
+        quantity_data = QuantityConverter().from_udb(self.unit(), self.quantity())
+        computed_quantity_data = compute_lot_quantity(biofuel, quantity_data)
+        supplier = from_national_trade_register(self.supplier_id())
+
+        return {
+            "biofuel": biofuel,
+            "carbure_client": client,
+            "carbure_supplier": supplier,
+            "delivery_date": self.iso_format_delivery_date(),
+            "feedstock": feedstock,
+            "period": self.period(),
+            "lot_status": self.status(),
+            "year": self.year(),
+            **computed_quantity_data,
+        }
+
+    def quantity(self):
+        quantity = self.xml_root_element.find("./EO_TRANS_DETAIL_MATERIALS/QUANTITY").text
+        return int(quantity)
+
+    def udb_transaction_id(self):
+        return self.xml_root_element.find("./TRANSACTION_ID").text
+
+    def unit(self):
+        return self.xml_root_element.find("./EO_TRANS_DETAIL_MATERIALS/MEASURE_UNIT").text
+
+    def year(self):
+        return self.delivery_date().year
---
+++ web/edelivery/tests/ebms/test_transaction.py
@@ -0,0 +1,134 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from core.models import Biocarburant, Entity, MatierePremiere
+from edelivery.ebms.transaction import Transaction
+from edelivery.tests.ebms.fixtures.udb_xml_data import transaction_data
+
+
+class TransactionTest(TestCase):
+    def setUp(self):
+        self.patched_MaterialConverter = patch("edelivery.ebms.transaction.MaterialConverter").start()
+        self.patched_MaterialConverter.return_value.from_udb_feedstock_code.return_value = MatierePremiere()
+        self.patched_MaterialConverter.return_value.from_udb_biofuel_code.return_value = Biocarburant()
+
+        self.patched_from_national_trade_register = patch(
+            "edelivery.ebms.transaction.from_national_trade_register",
+        ).start()
+        self.patched_from_national_trade_register.return_value = Entity()
+
+        self.patched_compute_lot_quantity = patch("edelivery.ebms.transaction.compute_lot_quantity").start()
+        self.patched_compute_lot_quantity.return_value = {}
+
+    def tearDown(self):
+        patch.stopall()
+
+    def test_knows_its_udb_transaction_id(self):
+        xml_data = transaction_data(udb_transaction_id="TRN-0000000000001-1234567890")
+        transaction = Transaction.from_xml(xml_data)
+        self.assertEqual("TRN-0000000000001-1234567890", transaction.udb_transaction_id())
+
+    def test_knows_its_delivery_date_in_ISO_format(self):
+        xml_data = transaction_data(delivery_date="2025-12-22T00:00:00.000Z")
+        transaction = Transaction.from_xml(xml_data)
+        self.assertEqual("2025-12-22", transaction.iso_format_delivery_date())
+
+        lot_attributes = transaction.to_lot_attributes()
+        self.assertEqual("2025-12-22", lot_attributes["delivery_date"])
+
+    def test_knows_its_delivery_month_period(self):
+        xml_data = transaction_data(delivery_date="2025-12-22T00:00:00.000Z")
+        transaction = Transaction.from_xml(xml_data)
+        self.assertEqual(202512, transaction.period())
+
+        lot_attributes = transaction.to_lot_attributes()
+        self.assertEqual(202512, lot_attributes["period"])
+
+    def test_knows_its_year(self):
+        xml_data = transaction_data(delivery_date="2025-12-22T00:00:00.000Z")
+        transaction = Transaction.from_xml(xml_data)
+        self.assertEqual(2025, transaction.year())
+
+        lot_attributes = transaction.to_lot_attributes()
+        self.assertEqual(2025, lot_attributes["year"])
+
+    def test_knows_its_supplier(self):
+        entity = Entity(name="Some Entity")
+        self.patched_from_national_trade_register.return_value = entity
+
+        xml_data = transaction_data(supplier_id="FR_SIREN_CD123456789")
+        transaction = Transaction.from_xml(xml_data)
+        self.assertEqual("FR_SIREN_CD123456789", transaction.supplier_id())
+        self.patched_from_national_trade_register.assert_not_called()
+
+        lot_attributes = transaction.to_lot_attributes()
+        self.patched_from_national_trade_register.assert_any_call("FR_SIREN_CD123456789")
+        self.assertEqual("Some Entity", lot_attributes["carbure_supplier"].name)
+
+    def test_knows_its_client(self):
+        entity = Entity(name="Some Entity")
+        self.patched_from_national_trade_register.return_value = entity
+
+        xml_data = transaction_data(client_id="FR_SIREN_CD123123123")
+        transaction = Transaction.from_xml(xml_data)
+        self.assertEqual("FR_SIREN_CD123123123", transaction.client_id())
+        self.patched_from_national_trade_register.assert_not_called()
+
+        lot_attributes = transaction.to_lot_attributes()
+        self.patched_from_national_trade_register.assert_any_call("FR_SIREN_CD123123123")
+        self.assertEqual("Some Entity", lot_attributes["carbure_client"].name)
+
+    def test_knows_its_status(self):
+        xml_data = transaction_data(status="PENDING")
+        transaction = Transaction.from_xml(xml_data)
+        self.assertEqual("PENDING", transaction.status())
+
+        lot_attributes = transaction.to_lot_attributes()
+        self.assertEqual("PENDING", lot_attributes["lot_status"])
+
+    def test_knows_its_feedstock_code(self):
+        patched_from_udb_feedstock_code = self.patched_MaterialConverter.return_value.from_udb_feedstock_code
+        patched_from_udb_feedstock_code.return_value = MatierePremiere(name="Betterave")
+
+        xml_data = transaction_data(feedstock={"code": "URWS023", "name": "Sugar beet"})
+        transaction = Transaction.from_xml(xml_data)
+        self.assertEqual("URWS023", transaction.feedstock_code())
+        patched_from_udb_feedstock_code.assert_not_called()
+
+        lot_attributes = transaction.to_lot_attributes()
+        patched_from_udb_feedstock_code.assert_called_with("URWS023")
+        self.assertEqual("Betterave", lot_attributes["feedstock"].name)
+
+    def test_knows_its_biofuel_code(self):
+        patched_from_udb_biofuel_code = self.patched_MaterialConverter.return_value.from_udb_biofuel_code
+        patched_from_udb_biofuel_code.return_value = Biocarburant(name="Biogaz")
+
+        xml_data = transaction_data(biofuel={"code": "SFC0015", "name": "Biogas"})
+        transaction = Transaction.from_xml(xml_data)
+        self.assertEqual("SFC0015", transaction.biofuel_code())
+        patched_from_udb_biofuel_code.assert_not_called()
+
+        lot_attributes = transaction.to_lot_attributes()
+        patched_from_udb_biofuel_code.assert_called_with("SFC0015")
+        self.assertEqual("Biogaz", lot_attributes["biofuel"].name)
+
+    def test_knows_its_biofuel_quantity_and_unit(self):
+        xml_data = transaction_data(quantity={"unit": "MWh", "value": 10})
+        transaction = Transaction.from_xml(xml_data)
+        self.assertEqual("MWh", transaction.unit())
+        self.assertEqual(10, transaction.quantity())
+
+    def test_computes_missing_quantity_data(self):
+        biofuel = Biocarburant(name="EMAG")
+        self.patched_MaterialConverter.return_value.from_udb_biofuel_code.return_value = biofuel
+        self.patched_compute_lot_quantity.return_value = {"weight": 10, "volume": 20, "lhv_amount": 3600 * 10}
+
+        xml_data = transaction_data(quantity={"unit": "MWh", "value": 10})
+        transaction = Transaction.from_xml(xml_data)
+        self.patched_compute_lot_quantity.assert_not_called()
+
+        lot_attributes = transaction.to_lot_attributes()
+        self.patched_compute_lot_quantity.assert_called_with(biofuel, {"lhv_amount": 3600 * 10})
+        self.assertEqual(3600 * 10, lot_attributes["lhv_amount"])
+        self.assertEqual(10, lot_attributes["weight"])
+        self.assertEqual(20, lot_attributes["volume"])
