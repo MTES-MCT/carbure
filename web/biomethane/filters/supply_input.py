bbed0c3a9ed5740ee977502152ac2c21ba610ea6
Yohann <yohann.ulreich@beta.gouv.fr>
2026-02-10 11:23:26 +0100

    feat(biomethane): return input name in filter

--- web/biomethane/filters/supply_input.py
+++ web/biomethane/filters/supply_input.py
@@ -6,7 +6,7 @@ from biomethane.models import BiomethaneSupplyInput
 class BaseBiomethaneSupplyInputFilter(FilterSet):
     entity_id = CharFilter(method="filter_by_entity")
     producer_id = CharFilter(field_name="supply_plan__producer__id", lookup_expr="exact")
-    input_name = CharFilter(field_name="input_name", lookup_expr="exact", required=False)
+    input_name = CharFilter(field_name="input_name__name", lookup_expr="exact", required=False)
     source = ChoiceFilter(field_name="source", choices=BiomethaneSupplyInput.SOURCE_CHOICES, required=False)
 
     def filter_by_entity(self, queryset, name, value):
