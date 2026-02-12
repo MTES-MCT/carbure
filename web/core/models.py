da9ad8f6772b74472620b4ef21965bb3105752ef
Yohann <yohann.ulreich@beta.gouv.fr>
2026-02-12 16:54:50 +0100

    feat(sites): deleting unused core.Depot and core.EntityDepot models

--- web/core/models.py
+++ web/core/models.py
@@ -4,7 +4,6 @@ from calendar import monthrange
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
@@ -420,126 +419,6 @@ class MatierePremiere(models.Model):
     objects = models.Manager()
     biofuel = MatierePremiereBiofuelManager()
     biomethane = MatierePremiereBiomethaneManager()
-
-
-class Depot(models.Model):
-    OTHER = "OTHER"
-    EFS = "EFS"
-    EFPE = "EFPE"
-    OILDEPOT = "OIL DEPOT"
-    BIOFUELDEPOT = "BIOFUEL DEPOT"
-    HEAT_PLANT = "HEAT PLANT"
-    POWER_PLANT = "POWER PLANT"
-    COGENERATION_PLANT = "COGENERATION PLANT"
-    EFCA = "EFCA"
-
-    TYPE_DEPOT = (
-        (OTHER, "Autre"),
-        (EFS, "EFS"),
-        (EFPE, "EFPE"),
-        (OILDEPOT, "OIL DEPOT"),
-        (BIOFUELDEPOT, "BIOFUEL DEPOT"),
-        (HEAT_PLANT, "HEAT PLANT"),
-        (POWER_PLANT, "POWER PLANT"),
-        (COGENERATION_PLANT, "COGENERATION PLANT"),
-        (EFCA, "EFCA"),
-    )
-
-    name = models.CharField(max_length=128, null=False, blank=False)
-    city = models.CharField(max_length=128, null=True, blank=True)
-    depot_id = models.CharField(max_length=32, null=False, blank=False)
-    country = models.ForeignKey(Pays, null=True, blank=False, on_delete=models.SET_NULL)
-    depot_type = models.CharField(max_length=32, choices=TYPE_DEPOT, default=OTHER)
-
-    address = models.CharField(max_length=128, blank=True)
-    postal_code = models.CharField(max_length=32, blank=True)
-
-    gps_coordinates = models.CharField(max_length=64, blank=True, null=True, default=None)
-    accise = models.CharField(max_length=32, blank=True, null=True, default=None)
-    private = models.BooleanField(default=False)
-
-    electrical_efficiency = models.FloatField(
-        blank=True,
-        null=True,
-        default=None,
-        help_text="Entre 0 et 1",
-        validators=[MinValueValidator(0), MaxValueValidator(1)],
-    )
-    thermal_efficiency = models.FloatField(
-        blank=True,
-        null=True,
-        default=None,
-        help_text="Entre 0 et 1",
-        validators=[MinValueValidator(0), MaxValueValidator(1)],
-    )
-    useful_temperature = models.FloatField(blank=True, null=True, default=None, help_text="En degrés Celsius")
-
-    is_enabled = models.BooleanField(default=True)
-    entity = models.ForeignKey(Entity, null=True, blank=True, on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return self.name
-
-    def natural_key(self):
-        return {
-            "depot_id": self.depot_id,
-            "name": self.name,
-            "city": self.city,
-            "country": self.country.natural_key(),
-            "depot_type": self.depot_type,
-            "address": self.address,
-            "postal_code": self.postal_code,
-            "electrical_efficiency": self.electrical_efficiency,
-            "thermal_efficiency": self.thermal_efficiency,
-            "useful_temperature": self.useful_temperature,
-        }
-
-    def clean(self):
-        fields_to_clear = {
-            Depot.POWER_PLANT: ["thermal_efficiency", "useful_temperature"],
-            Depot.HEAT_PLANT: ["electrical_efficiency", "useful_temperature"],
-        }
-
-        fields = fields_to_clear.get(self.depot_type, [])
-        for field in fields:
-            setattr(self, field, None)
-
-        super().clean()
-
-    class Meta:
-        db_table = "depots"
-        verbose_name = "Dépôt"
-        verbose_name_plural = "Dépôts"
-        ordering = ["name"]
-
-
-class EntityDepot(models.Model):
-    OWN = "OWN"
-    THIRD_PARTY = "THIRD_PARTY"
-    PROCESSING = "PROCESSING"
-    TYPE_OWNERSHIP = ((OWN, "Propre"), (THIRD_PARTY, "Tiers"), (PROCESSING, "Processing"))
-
-    entity = models.ForeignKey(Entity, null=False, blank=False, on_delete=models.CASCADE)
-    depot = models.ForeignKey(Depot, null=False, blank=False, on_delete=models.CASCADE)
-    ownership_type = models.CharField(max_length=32, choices=TYPE_OWNERSHIP, default=THIRD_PARTY)
-    blending_is_outsourced = models.BooleanField(default=False)
-    blender = models.ForeignKey(Entity, null=True, blank=True, on_delete=models.CASCADE, related_name="blender_old")
-
-    def __str__(self):
-        return str(self.id)
-
-    def natural_key(self):
-        return {
-            "depot": self.depot.natural_key(),
-            "ownership_type": self.ownership_type,
-            "blending_is_outsourced": self.blending_is_outsourced,
-            "blender": self.blender.natural_key() if self.blender else None,
-        }
-
-    class Meta:
-        db_table = "entity_depot"
-        verbose_name = "Dépôt Entité"
-        verbose_name_plural = "Dépôts Entité"
 
 
 class SustainabilityDeclaration(models.Model):
