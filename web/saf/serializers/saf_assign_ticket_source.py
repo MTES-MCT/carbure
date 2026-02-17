400806cbf2e7211651fc6b1c9627054a437a77af
jfalxa <jfalxa.sta@gmail.com>
2026-02-17 17:51:56 +0100

    feat(saf): add conditional required validation on some field during ticket assignement

--- web/saf/serializers/saf_assign_ticket_source.py
+++ web/saf/serializers/saf_assign_ticket_source.py
@@ -1,22 +1,42 @@
 from rest_framework import serializers
 
-from transactions.models.site import Site
+from core.models import Entity
+from saf.models.saf_logistics import SafLogistics
+from saf.models.saf_ticket import SafTicket
+from transactions.models.airport import Airport
+
+SAF_TYPES = [Entity.OPERATOR, Entity.AIRLINE, Entity.SAF_TRADER]
+CONSUMPTION_TYPES = [choice[0] for choice in SafTicket.CONSUMPTION_TYPES]
+SHIPPING_METHODS = [choice[0] for choice in SafLogistics.SHIPPING_METHODS]
 
 
 class SafTicketSourceAssignmentSerializer(serializers.Serializer):
-    client_id = serializers.IntegerField(required=True)
-    volume = serializers.FloatField(required=True)
-    agreement_reference = serializers.CharField(required=False)
+    client_id = serializers.PrimaryKeyRelatedField(queryset=Entity.objects.filter(entity_type__in=SAF_TYPES), required=True)
+    volume = serializers.FloatField(required=True, min_value=1)
+    agreement_reference = serializers.CharField(required=False, allow_blank=True)
     agreement_date = serializers.CharField(required=False)
     free_field = serializers.CharField(required=False, allow_null=True, allow_blank=True)
     assignment_period = serializers.IntegerField(required=True)
-    reception_airport = serializers.PrimaryKeyRelatedField(
-        queryset=Site.objects.filter(site_type=Site.AIRPORT), required=False, allow_null=True
-    )
-    consumption_type = serializers.CharField(required=False, allow_null=True, allow_blank=True)
-    shipping_method = serializers.CharField(required=False, allow_null=True, allow_blank=True)
+    reception_airport = serializers.PrimaryKeyRelatedField(queryset=Airport.objects.all(), required=False, allow_null=True)
+    consumption_type = serializers.ChoiceField(required=False, allow_null=True, allow_blank=True, choices=CONSUMPTION_TYPES)
+    shipping_method = serializers.ChoiceField(required=False, allow_null=True, allow_blank=True, choices=SHIPPING_METHODS)
     has_intermediary_depot = serializers.BooleanField(required=False, default=False)
-    pos_number = serializers.CharField(required=False)
+    pos_number = serializers.CharField(required=False, allow_blank=True)
+
+    def validate(self, attrs):
+        client = attrs["client_id"]
+        errors = {}
+
+        if client.entity_type in [Entity.AIRLINE, Entity.SAF_TRADER] and not attrs.get("reception_airport"):
+            errors["reception_airport"] = "This field is required for airline and SAF trader clients."
+
+        if client.entity_type == Entity.OPERATOR and not attrs.get("agreement_reference"):
+            errors["agreement_reference"] = "This field is required for operator clients."
+
+        if errors:
+            raise serializers.ValidationError(errors)
+
+        return attrs
 
 
 class SafTicketSourceGroupAssignmentSerializer(SafTicketSourceAssignmentSerializer):
