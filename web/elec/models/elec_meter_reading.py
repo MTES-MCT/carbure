e3f7818536a4e7ed19bb50fed9c79c79118918fd
jfalxa <jfalxa.sta@gmail.com>
2026-02-19 19:22:36 +0100

    feat(elec): add manager for meter readings that annotates extended info

--- web/elec/models/elec_meter_reading.py
+++ web/elec/models/elec_meter_reading.py
@@ -1,11 +1,36 @@
 from django.db import models
+from django.db.models import F, OuterRef, Subquery
+from django.db.models.functions import Coalesce
 
 from core.models import Entity
 from elec.models.elec_meter import ElecMeter
 from elec.models.elec_meter_reading_application import ElecMeterReadingApplication
 
 
+class ExtendedMeterReadingManager(models.Manager):
+    def get_queryset(self):
+        readings = ElecMeterReading.objects.all()
+        prev_readings = readings.filter(meter_id=OuterRef("meter_id"), pk__lt=OuterRef("pk")).order_by("-pk")[:1]
+        prev_index = Subquery(prev_readings.values("extracted_energy"))
+        prev_index_date = Subquery(prev_readings.values("reading_date"))
+
+        return (
+            super()
+            .get_queryset()
+            .select_related("meter", "meter__charge_point")
+            .annotate(reading_id=F("id"), charge_point_id=F("meter__charge_point__pk"))
+            .annotate(current_index=F("extracted_energy"), current_index_date=F("reading_date"))
+            .annotate(prev_index=Coalesce(prev_index, F("meter__initial_index")))
+            .annotate(prev_index_date=Coalesce(prev_index_date, F("meter__initial_index_date")))
+            .filter(meter__charge_point__is_deleted=False, meter__charge_point__is_article_2=False)
+            .order_by("meter", "reading_date")
+        )
+
+
 class ElecMeterReading(models.Model):
+    objects = models.Manager()
+    extended_objects = ExtendedMeterReadingManager()
+
     class Meta:
         db_table = "elec_meter_reading"
         verbose_name = "Relevé de point de recharge"
