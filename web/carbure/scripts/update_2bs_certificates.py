4ba90b9e653cebffd685138acbc8ec5d5f35762c
jfalxa <jfalxa.sta@gmail.com>
2026-04-02 15:03:47 +0200

    fix(2bs): don't use lower case status when creating dump file

--- web/carbure/scripts/update_2bs_certificates.py
+++ web/carbure/scripts/update_2bs_certificates.py
@@ -70,7 +70,7 @@ def download_certificates(url: str, status: str) -> None:
     html_io = StringIO(str(table))
     df = pd.read_html(html_io)[0]
     df = df[~df[DBS_NUMBER_KEY].isnull()]  # Pour une raison inconnue, la ligne de coordonnées est dupliquée
-    pd.DataFrame.to_csv(df, "%s/Certificates2BS_%s_%s.csv" % (DESTINATION_FOLDER, status.lower(), str(date.today())), index=False)
+    pd.DataFrame.to_csv(df, "%s/Certificates2BS_%s_%s.csv" % (DESTINATION_FOLDER, status, str(date.today())), index=False)
 
 
 def save_2bs_certificates(status: str) -> Tuple[int, list]:
