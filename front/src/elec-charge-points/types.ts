4b6ba2baeb0add42bd9e3ef92d0d65cdda8adfbf
Benjamin Grilleres <contact@benjamingrilleres.com>
2026-04-15 14:23:17 +0200

    fix: fix eslint errors

--- front/src/elec-charge-points/types.ts
+++ front/src/elec-charge-points/types.ts
@@ -21,8 +21,7 @@ export interface ElecChargePointsApplication {
   // audit_order_date?: string
 }
 
-export interface ElecChargePointsApplicationDetails
-  extends ElecChargePointsApplication {
+export interface ElecChargePointsApplicationDetails extends ElecChargePointsApplication {
   email_contacts: string[]
   sample?: ElecApplicationSample
 }
@@ -33,13 +32,11 @@ export interface ElecChargePointsSnapshot {
   power_total: number
 }
 
-export interface ElecChargePointsApplicationCheckInfo
-  extends UploadCheckReportInfo {
+export interface ElecChargePointsApplicationCheckInfo extends UploadCheckReportInfo {
   charge_point_count: number
 }
 
-export interface ElecMeterReadingsApplicationDetails
-  extends ElecMeterReadingsApplication {
+export interface ElecMeterReadingsApplicationDetails extends ElecMeterReadingsApplication {
   email_contacts: string[]
   power_total: number
   sample?: ElecApplicationSample
@@ -77,8 +74,7 @@ export interface ElecMeterReadingsApplication {
   status: ElecAuditApplicationStatus
 }
 
-export interface ElecMeterReadingsApplicationCheckInfo
-  extends UploadCheckReportInfo {
+export interface ElecMeterReadingsApplicationCheckInfo extends UploadCheckReportInfo {
   year: number
   quarter: number
   charge_point_count: number
