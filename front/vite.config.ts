ce4bb64f4191d5c213958515be714b6d497e7c61
Benjamin Grilleres <contact@benjamingrilleres.com>
2026-04-20 10:11:02 +0200

    fix: remove vitest from vite.config.ts

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,5 +1,6 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite"
 import { sentryVitePlugin } from "@sentry/vite-plugin"
-import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
 
