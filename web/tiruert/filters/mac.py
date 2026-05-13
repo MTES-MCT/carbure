dab7132cb4a82eda558f8fada40a365379054b0a
jfalxa <jfalxa.sta@gmail.com>
2026-05-13 15:42:45 +0200

    feat(iricc): make year field mandatory when filtering fossil fuels macs

--- web/tiruert/filters/mac.py
+++ web/tiruert/filters/mac.py
@@ -7,4 +7,4 @@ from django_filters import CharFilter, FilterSet, NumberFilter
 
 class MacFilter(FilterSet):
     entity_id = CharFilter(field_name="operator_id", lookup_expr="exact")
-    year = NumberFilter(field_name="year", lookup_expr="exact", initial=timezone.now().year)
+    year = NumberFilter(field_name="year", lookup_expr="exact", initial=timezone.now().year, required=True)
