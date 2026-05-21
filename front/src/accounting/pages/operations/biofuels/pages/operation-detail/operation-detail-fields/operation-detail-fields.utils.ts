4844f25ccf7d3db280fef116ae3fe02a1daaafb7
Benjamin Grilleres <contact@benjamingrilleres.com>
2026-05-21 11:37:14 +0200

    feat: add custom column avoided emissions

--- front/src/accounting/pages/operations/biofuels/pages/operation-detail/operation-detail-fields/operation-detail-fields.utils.ts
+++ front/src/accounting/pages/operations/biofuels/pages/operation-detail/operation-detail-fields/operation-detail-fields.utils.ts
@@ -1,16 +1,7 @@
-import { Operation, OperationType } from "accounting/types"
-import { getOperationQuantity } from "../../../operations.utils"
+import { Operation } from "accounting/types"
+import { formatValue, getOperationQuantity } from "../../../operations.utils"
 import { CONVERSIONS } from "common/utils/formatters"
 import { ExtendedUnit } from "common/types"
-
-// Format the value only for the incorporation operation
-export const formatValue = (operation: Operation, value: number) => {
-  if (!operation) return 0
-
-  return operation.type === OperationType.INCORPORATION
-    ? value * operation.renewable_energy_share
-    : value
-}
 
 export const formatQuantityDisplay = (
   operation: Operation,
