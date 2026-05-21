73bab4058aa00eb4e1d71ded08535d7fb6754bd4
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2026-05-21 17:38:14 +0200

    [PREFACTORING] Move `GetCertificateRequest` to its own module

--- web/edelivery/ebms/requests_temp.py
+++ web/edelivery/ebms/requests_temp.py
@@ -1,13 +1,4 @@
-from edelivery.ebms.request_responses.get_certificate_response import GetCertificateResponse
 from edelivery.ebms.requests.base_request import BaseRequest
-
-
-class GetCertificateRequest(BaseRequest):
-    def __init__(self):
-        body = """\
-<udb:GetCertificateRequest xmlns:udb="http://udb.ener.ec.europa.eu/services/udbModelService/udbService/v1">
-</udb:GetCertificateRequest>"""
-        super().__init__(body, GetCertificateResponse)
 
 
 class GetSourcingContactByIdRequest(BaseRequest):
