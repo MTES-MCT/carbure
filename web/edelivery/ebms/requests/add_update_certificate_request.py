4e1c86aca71cace927e3827bb36e7a7a16613e9b
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2026-05-27 16:03:31 +0200

    Inject certificate id

--- web/edelivery/ebms/requests/add_update_certificate_request.py
+++ web/edelivery/ebms/requests/add_update_certificate_request.py
@@ -2,12 +2,15 @@ from .base_request import BaseRequest
 
 
 class AddUpdateCertificateRequest(BaseRequest):
-    def __init__(self, entity):
+    def __init__(self, entity_certificate):
+        entity = entity_certificate.entity
+        certificate = entity_certificate.certificate
         payload = f"""\
 <udb:AddUpdateCertificateRequest xmlns:udb="http://udb.ener.ec.europa.eu/services/udbModelService/udbService/v1">
   <EO_CERTIFICATE_HEADER>
     <EO_CERTIFICATE>
       <ECONOMIC_OPERATOR_NUMBER>{entity.ntr_id()}</ECONOMIC_OPERATOR_NUMBER>
+      <CERTIFICATE_NUMBER>{certificate.certificate_id}</CERTIFICATE_NUMBER>
     </EO_CERTIFICATE>
   </EO_CERTIFICATE_HEADER>
 </udb:AddUpdateCertificateRequest>"""
--- web/edelivery/tests/ebms/requests/test_add_update_certificate_request.py
+++ web/edelivery/tests/ebms/requests/test_add_update_certificate_request.py
@@ -8,16 +8,18 @@ from .test_base_request import BaseRequestTest
 
 class AddUpdateCertificateRequestTest(BaseRequestTest):
     @staticmethod
-    def add_update_certificate_request_payload(entity):
-        request = AddUpdateCertificateRequest(entity)
+    def add_update_certificate_request_payload(entity_certificate):
+        request = AddUpdateCertificateRequest(entity_certificate)
         return ET.fromstring(request.body)
 
     def setUp(self):
         super().setUp()
         self.entity = MagicMock(**{"ntr_id.return_value": ""})
+        self.certificate = MagicMock(certificate_id="")
+        self.entity_certificate = MagicMock(entity=self.entity, certificate=self.certificate)
 
     def test_knowns_its_body(self):
-        root_xml_element = self.add_update_certificate_request_payload(self.entity)
+        root_xml_element = self.add_update_certificate_request_payload(self.entity_certificate)
 
         expected_tag = "{http://udb.ener.ec.europa.eu/services/udbModelService/udbService/v1}AddUpdateCertificateRequest"
         self.assertEqual(expected_tag, root_xml_element.tag)
@@ -25,6 +27,13 @@ class AddUpdateCertificateRequestTest(BaseRequestTest):
     def test_injects_entity_NTR_id(self):
         self.entity.ntr_id.return_value = "SOME_NTR_ID"
 
-        root_xml_element = self.add_update_certificate_request_payload(self.entity)
+        root_xml_element = self.add_update_certificate_request_payload(self.entity_certificate)
         eo_number_tag = root_xml_element.find("./EO_CERTIFICATE_HEADER/EO_CERTIFICATE/ECONOMIC_OPERATOR_NUMBER")
         self.assertEqual("SOME_NTR_ID", eo_number_tag.text)
+
+    def test_injects_certificate_id(self):
+        self.certificate.certificate_id = "SN_UN_2026_0123"
+
+        root_xml_element = self.add_update_certificate_request_payload(self.entity_certificate)
+        certificate_number_tag = root_xml_element.find("./EO_CERTIFICATE_HEADER/EO_CERTIFICATE/CERTIFICATE_NUMBER")
+        self.assertEqual("SN_UN_2026_0123", certificate_number_tag.text)
