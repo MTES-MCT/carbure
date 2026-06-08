6bfea9732d0895e74d0ff4a4bea44539f0549c49
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2026-06-08 12:29:56 +0200

    Add `ConversationId` tag to `SubmitMessage` SOAP action

--- web/edelivery/tests/soap/test_actions.py
+++ web/edelivery/tests/soap/test_actions.py
@@ -76,7 +76,7 @@ class SubmitMessageTest(TestCase):
         patched_new_uuid.return_value = "12345678-1234-1234-1234-1234567890ab"
         patched_timestamp.return_value = "2025-07-15T13:00:00+00:00"
 
-        request = MagicMock(**{"zipped_encoded.return_value": "abcdef"})
+        request = MagicMock(**{"zipped_encoded.return_value": "abcdef", "conversation_id": "12345"})
         action = SubmitMessage("responder_id", request)
 
         expected_payload = """\
@@ -102,6 +102,7 @@ class SubmitMessageTest(TestCase):
         <eb:CollaborationInfo>
           <eb:Service>https://union-database.ec.europa.eu/e-delivery/services/send</eb:Service>
           <eb:Action>https://union-database.ec.europa.eu/e-delivery/actions/sendRequest</eb:Action>
+          <eb:ConversationId>12345</eb:ConversationId>
         </eb:CollaborationInfo>
         <eb:MessageProperties>
           <eb:Property name="originalSender">CarbuRe_NTR</eb:Property>
