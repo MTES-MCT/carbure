d6cf1ec5327d85ee56aefacd4ea40b1b58f3a651
jfalxa <jfalxa.sta@gmail.com>
2026-06-08 18:29:16 +0200

    feat(dc): add mapping for huiles à haute teneur en soufre

--- web/doublecount/parser/excel_to_carbure_convertor.py
+++ web/doublecount/parser/excel_to_carbure_convertor.py
@@ -62,6 +62,7 @@ dc_feedstock_to_carbure_feedstock = {
     "alcool résiduel de synthèse pharmaceutique": "ALCOOL_DE_SYNTHESE_PHARMACEUTIQUE",
     "résidus de transestérification d'huiles": "RESIDUS_DE_TRANSESTERIFICATION_D_HUILES",
     "huiles acides neutralisées ayant une haute teneur en soufre": "HUILES_ACIDES_NEUTRALISEES_AYANT_UNE_HAUTE_TENEUR_EN_SOUFRE",  # noqa: E501
+    "huiles acides ayant une haute teneur en soufre": "HUILES_ACIDES_NEUTRALISEES_AYANT_UNE_HAUTE_TENEUR_EN_SOUFRE",  # noqa: E501
     "graisses brunes": "GRAISSES_BRUNES",
     "graisses d'insectes": "GRAISSES_D_INSECTES",
     "levures usagées": "LEVURES_USAGEES",
