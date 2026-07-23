b63ff5ee1adb5aa843842b6aa203af0bbdc0b5be
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2026-07-23 16:42:52 +0200

    Stub mandatory fields to get a successful `AddUpdateCertificateRequest` response

--- web/edelivery/ebms/requests/add_update_certificate_request.py
+++ web/edelivery/ebms/requests/add_update_certificate_request.py
@@ -20,14 +20,39 @@ class AddUpdateCertificateRequest(BaseRequest):
     <EO_CERTIFICATE>
       <ECONOMIC_OPERATOR_NUMBER>{entity.ntr_id()}</ECONOMIC_OPERATOR_NUMBER>
       <CERTIFICATE_NUMBER>{certificate.certificate_id}</CERTIFICATE_NUMBER>
+      {self.stubbed_certificate_body_fields()}
       <DATE_OF_ISSUE>{to_date_isoformat(certificate.valid_from)}</DATE_OF_ISSUE>
       <PLACE_OF_ISSUE>France</PLACE_OF_ISSUE>
       <CERT_DATE_FROM>{to_date_isoformat(certificate.valid_from)}</CERT_DATE_FROM>
       <CERT_DATE_TO>{to_date_isoformat(certificate.valid_until)}</CERT_DATE_TO>
       <VALIDITY_STATUS>{validity_status}</VALIDITY_STATUS>
       <GROUP_CERTIFICATION>NO</GROUP_CERTIFICATION>
+      {self.stubbed_additional_mandatory_fields()}
     </EO_CERTIFICATE>
   </EO_CERTIFICATE_HEADER>
 </udb:AddUpdateCertificateRequest>"""
 
         super().__init__(payload)
+
+    def stubbed_certificate_body_fields(self):
+        return """\
+<CERTIFICATE_BODY_NUMBER>CU343224531</CERTIFICATE_BODY_NUMBER>
+        """
+
+    def stubbed_additional_mandatory_fields(self):
+        return """\
+<EO_SCOPE>
+  <ORGANISATION_SCOPE>BG</ORGANISATION_SCOPE>
+</EO_SCOPE>
+<EO_CERTIFICATE_SITE>
+  <SITE_NAME>CarbureSite1</SITE_NAME>
+  <STREET_LINE>CarbureAddress1</STREET_LINE>
+  <POST_CODE>1230</POST_CODE>
+  <CITY>Paris</CITY>
+  <COUNTRY_CODE>FR</COUNTRY_CODE>
+  <MAIN_SITE>true</MAIN_SITE>
+</EO_CERTIFICATE_SITE>
+<CHAIN_OF_CUSTODIES>
+  <CHAIN_OF_CUSTODY>Segregation</CHAIN_OF_CUSTODY>
+</CHAIN_OF_CUSTODIES>
+        """
