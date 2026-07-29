ff619f9ef6450d012f9ee3c57b10bc157ea062ee
Yohann <yohann.ulreich@beta.gouv.fr>
2026-07-29 16:12:03 +0200

    refacto(iricc): continue to rename 'quantity' to 'energy' in calculate_balance_with_annotations()

--- web/tiruert/services/balance_annotations.py
+++ web/tiruert/services/balance_annotations.py
@@ -46,14 +46,15 @@ def _get_teneur_sector_expression(sector_expr):
 
 def _get_quantity_expression(unit):
     if unit == "mj":
-        factor_expr = F("operation__biofuel__pci_litre")
+        return ExpressionWrapper(
+            F("volume") * F("operation__renewable_energy_share") * F("operation__biofuel__pci_litre"),
+            output_field=FloatField(),
+        )
     else:
-        factor_expr = Value(1.0)
-
-    return ExpressionWrapper(
-        F("volume") * F("operation__renewable_energy_share") * factor_expr,
-        output_field=FloatField(),
-    )
+        return ExpressionWrapper(
+            F("volume"),
+            output_field=FloatField(),
+        )
 
 
 def _get_avoided_emissions_expression():
@@ -70,8 +71,8 @@ def _get_avoided_emissions_expression():
 
 
 def _build_common_context(entity_id, unit, date_from):
-    quantity_expr = _get_quantity_expression("l")
-    teneur_quantity_expr = _get_quantity_expression("mj")
+    quantity_expr = _get_quantity_expression(unit)
+    teneur_energy_expr = _get_quantity_expression("mj")
     avoided_emissions_expr = _get_avoided_emissions_expression()
 
     credit_cond = Q(operation__credited_entity_id=entity_id)
@@ -83,7 +84,7 @@ def _build_common_context(entity_id, unit, date_from):
     return {
         "unit": unit,
         "quantity_expr": quantity_expr,
-        "teneur_quantity_expr": teneur_quantity_expr,
+        "teneur_energy_expr": teneur_energy_expr,
         "avoided_emissions_expr": avoided_emissions_expr,
         "credit_cond": credit_cond,
         "debit_cond": debit_cond,
@@ -156,7 +157,7 @@ def _get_teneur_operation_contributions(details_qs, context, group_annotations, 
         )
         .values(*group_fields, "operation_id", "operation__status")
         .annotate(
-            operation_quantity=Sum(context["teneur_quantity_expr"]),
+            operation_energy=Sum(context["teneur_energy_expr"]),
             operation_saved_emissions=Sum(context["avoided_emissions_expr"]),
         )
     )
@@ -173,14 +174,14 @@ def _get_teneur_operation_contributions(details_qs, context, group_annotations, 
     for group in operation_groups:
         key = tuple(group[field] for field in group_fields)
         entry = grouped_contributions[key]
-        operation_quantity = int(group["operation_quantity"] or 0)
+        operation_energy = int(group["operation_energy"] or 0)
         operation_saved_emissions = group["operation_saved_emissions"] or 0.0
 
         if group["operation__status"] == Operation.PENDING:
-            entry["pending_teneur"] += operation_quantity
+            entry["pending_teneur"] += operation_energy
             entry["pending_saved_emissions"] += operation_saved_emissions
         elif group["operation__status"] == Operation.DECLARED:
-            entry["declared_teneur"] += operation_quantity
+            entry["declared_teneur"] += operation_energy
             entry["declared_saved_emissions"] += operation_saved_emissions
 
     return grouped_contributions
