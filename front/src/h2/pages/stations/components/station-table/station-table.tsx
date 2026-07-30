ad0696f9c49c25cd742b95e9465cf495542c708e
Julien Falxa <jfalxa.sta@gmail.com>
2026-07-30 17:48:52 +0200

    feat(h2): add basic table view for h2 stations

---
+++ front/src/h2/pages/stations/components/station-table/station-table.tsx
@@ -0,0 +1,51 @@
+import { Cell, Column, Table } from "common/components/table2"
+import { formatDate, formatNumber } from "common/utils/formatters"
+import { H2Station } from "h2/types"
+import { useTranslation } from "react-i18next"
+
+type StationTableProps = {
+  stations: H2Station[]
+}
+
+export const StationTable = ({ stations }: StationTableProps) => {
+  const { t } = useTranslation()
+
+  const columns: Column<H2Station>[] = [
+    {
+      header: t("Nom de la station"),
+      cell: (station) => <Cell text={station.name} />,
+    },
+    {
+      header: t("SIRET"),
+      cell: (station) => <Cell text={station.site_siret} />,
+    },
+    {
+      header: t("Nature du site"),
+      cell: (station) => <Cell text={station.access_type} />,
+    },
+    {
+      header: t("Pression (bar)"),
+      cell: (station) => (
+        <Cell text={station.distributed_pressure.join(" - ")} />
+      ),
+    },
+    {
+      header: t("Distribution (kg / jour)"),
+      cell: (station) => (
+        <Cell text={formatNumber(station.distribution_capacity)} />
+      ),
+    },
+    {
+      header: t("Compatible VP"),
+      cell: (station) => <Cell text={station.has_personal_vehicle_connector} />,
+    },
+    {
+      header: t("Mis en service le"),
+      cell: (station) => (
+        <Cell text={formatDate(station.commissioning_date ?? null)} />
+      ),
+    },
+  ]
+
+  return <Table columns={columns} rows={stations} />
+}
--- front/src/h2/pages/stations/index.tsx
+++ front/src/h2/pages/stations/index.tsx
@@ -1,15 +1,20 @@
-import { LoaderOverlay, Main } from "common/components/scaffold"
+import { Content, LoaderOverlay, Main } from "common/components/scaffold"
 import { usePrivateNavigation } from "common/layouts/navigation"
 import { useTranslation } from "react-i18next"
 import { EmptyStations } from "./components/empty-stations"
 import { getStations } from "./api"
 import useEntity from "common/hooks/entity"
 import { useQuery } from "common/hooks/async"
+import { StationTable } from "./components/station-table"
+import { Button } from "common/components/button2"
+import { useCreateStationDialog } from "./components/create-station-dialog"
 
 const StationsPage = () => {
   const { t } = useTranslation()
   const entity = useEntity()
   usePrivateNavigation(t("Mes stations"))
+
+  const openCreateStationDialog = useCreateStationDialog()
 
   const { result, loading } = useQuery(getStations, {
     key: "h2-stations",
@@ -19,11 +24,30 @@ const StationsPage = () => {
   const stations = result?.data?.results ?? []
   const hasStations = stations.length > 0
 
-  if (loading) {
-    return <LoaderOverlay />
+  if (!loading && !hasStations) {
+    return <EmptyStations />
   }
 
-  return <Main>{hasStations ? null : <EmptyStations />}</Main>
+  return (
+    <Main>
+      <header>
+        <Button
+          asideX
+          size="large"
+          iconId="ri-add-line"
+          onClick={openCreateStationDialog}
+        >
+          {t("Ajouter une station")}
+        </Button>
+      </header>
+
+      <Content marginTop>
+        <StationTable stations={stations} />
+      </Content>
+
+      {loading && <LoaderOverlay />}
+    </Main>
+  )
 }
 
 export default StationsPage
