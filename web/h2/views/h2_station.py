c7c39259f43269360b21e1aa453a8b321ebfd7d8
Julien Falxa <jfalxa.sta@gmail.com>
2026-08-03 17:28:08 +0200

    feat(h2): add filters for stations

--- web/h2/views/h2_station.py
+++ web/h2/views/h2_station.py
@@ -1,6 +1,8 @@
 from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework.viewsets import ModelViewSet
 
+from core.filters import FiltersActionFactory
+from h2.filters.h2_station import H2StationFilter
 from h2.models import H2Station
 from h2.permissions import HasHRSRights, HasHRSWriteRights
 from h2.serializers import H2StationInputSerializer, H2StationSerializer
@@ -17,10 +19,12 @@ from h2.serializers import H2StationInputSerializer, H2StationSerializer
         ),
     ]
 )
-class H2StationViewSet(ModelViewSet):
+class H2StationViewSet(FiltersActionFactory(), ModelViewSet):
     queryset = H2Station.objects.all()
     serializer_class = H2StationSerializer
     permission_classes = [HasHRSRights]
+    filterset_class = H2StationFilter
+    search_fields = ["name", "site_siret", "city"]
 
     def get_permissions(self):
         if self.action in ["create", "update", "partial_update", "destroy"]:
