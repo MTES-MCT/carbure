5ef4f3df6a654ebc6286fe6caf8487d2ee672325
Yohann <yohann.ulreich@beta.gouv.fr>
2026-08-04 18:12:51 +0200

    fix(core): adjust truncate function to handle float imprecision

--- web/core/utils.py
+++ web/core/utils.py
@@ -307,12 +307,12 @@ def truncate(value: float, decimal_places: int = 2) -> float:
 
     value = float(value)
 
+    if decimal_places == 0:
+        return int(value)
+
     rounded_value = round(value, decimal_places)
     if abs(value - rounded_value) <= 1e-6:
         return rounded_value
-
-    if decimal_places == 0:
-        return int(value)
 
     factor = 10**decimal_places
     epsilon = 1e-9 if value >= 0 else -1e-9
