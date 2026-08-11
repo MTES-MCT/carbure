3a18cb4275999c6719ed46a04b5a2389f98a7578
Thomas Gary <thomas.gary@i-carre.net>
2026-08-11 12:23:41 +0200

    feat(biomethane):check if ch4 rate is inconsistent

--- web/biomethane/serializers/energy/energy.py
+++ web/biomethane/serializers/energy/energy.py
@@ -1,7 +1,9 @@
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from biomethane.models.biomethane_energy import BiomethaneEnergy
 from biomethane.serializers.energy.monthly_report import BiomethaneEnergyMonthlyReportSerializer
+from biomethane.services.consistency_checks import BiomethaneEnergyConsistencyChecksService
 
 
 class BaseBiomethaneEnergySerializer(serializers.ModelSerializer):
@@ -21,6 +23,25 @@ class BaseBiomethaneEnergySerializer(serializers.ModelSerializer):
 
 class BiomethaneEnergySerializer(BaseBiomethaneEnergySerializer):
     monthly_reports = BiomethaneEnergyMonthlyReportSerializer(many=True, read_only=True)
+
+    consistency_warnings = serializers.SerializerMethodField()
+
+    @extend_schema_field(
+        {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "code": {"type": "string"},
+                    "level": {"type": "string"},
+                    "message": {"type": "string"},
+                },
+            },
+            "description": "List of consistency warnings for the energy data",
+        }
+    )
+    def get_consistency_warnings(self, instance):
+        return BiomethaneEnergyConsistencyChecksService.get_consistency_warnings(instance)
 
     class Meta(BaseBiomethaneEnergySerializer.Meta):
         exclude = []
