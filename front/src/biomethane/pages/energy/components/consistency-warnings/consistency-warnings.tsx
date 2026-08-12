7fb38e4d2c86f1bef1f4c4d455b32960cfd30df4
Thomas Gary <thomas.gary@i-carre.net>
2026-08-12 13:44:44 +0200

    feat(biomethane): add closing possibility

--- front/src/biomethane/pages/energy/components/consistency-warnings/consistency-warnings.tsx
+++ front/src/biomethane/pages/energy/components/consistency-warnings/consistency-warnings.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next"
 import { BiomethaneEnergy } from "../../types"
 import { Alert } from "common/components/alert2"
+import { useState } from "react"
 
 const getSeverity = (
   level?: string
@@ -15,6 +16,12 @@ const getSeverity = (
 }
 
 export function ConsistencyWarnings({ energy }: { energy?: BiomethaneEnergy }) {
+  const [hiddenIndexes, setHiddenIndexes] = useState<number[]>([])
+
+  const handleClose = (index: number) => {
+    setHiddenIndexes((prev) => [...prev, index])
+  }
+
   const { t } = useTranslation("errors")
 
   const warnings = energy?.consistency_warnings
@@ -23,16 +30,25 @@ export function ConsistencyWarnings({ energy }: { energy?: BiomethaneEnergy }) {
 
   return (
     <div>
-      {warnings.map((warning, index) => (
-        <Alert
-          key={`${warning.code}-${index}`}
-          severity={getSeverity(warning.level)}
-          description={t(warning.code ?? "", {
-            defaultValue: warning.message ?? "",
-          })}
-          small
-        />
-      ))}
+      {warnings.map((warning, index) => {
+        if (hiddenIndexes.includes(index)) {
+          return null
+        }
+        return (
+          <Alert
+            key={`${warning.code}-${index}`}
+            severity={getSeverity(warning.level)}
+            description={t(warning.code ?? "", {
+              defaultValue: warning.message ?? "",
+            })}
+            small
+            closable
+            onClose={() => {
+              handleClose(index)
+            }}
+          />
+        )
+      })}
     </div>
   )
 }
