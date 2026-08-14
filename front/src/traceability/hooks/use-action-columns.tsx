4bb1bc30757be3c29c0ee7bc99214602c1302082
Julien Falxa <jfalxa.sta@gmail.com>
2026-08-14 18:35:53 +0200

    feat(traceability): add hooks to generate config for general filters and columns

---
+++ front/src/traceability/hooks/use-action-columns.tsx
@@ -0,0 +1,46 @@
+import { useTranslation } from "react-i18next"
+
+import { Cell, Column } from "common/components/table2"
+import { EntityManager } from "common/hooks/entity"
+import { Action } from "traceability/types"
+
+export type ActionColumnDefinition = Column<Action> & {
+  condition?: (entity: EntityManager) => boolean
+}
+
+export function useActionColumns() {
+  const { t } = useTranslation()
+
+  return {
+    id: {
+      key: "id",
+      header: t("Id"),
+      cell: (action) => <Cell text={action.id} />,
+    },
+    holder: {
+      key: "holder",
+      header: t("Détenteur"),
+      cell: (action) => <Cell text={action.holder} />,
+    },
+    industry: {
+      key: "industry",
+      header: t("Filière"),
+      cell: (action) => <Cell text={action.industry} />,
+    },
+    material: {
+      key: "material",
+      header: t("Matière"),
+      cell: (action) => <Cell text={action.material} />,
+    },
+    quantity: {
+      key: "quantity",
+      header: t("Quantité de matière"),
+      cell: (action) => <Cell text={action.quantity} />,
+    },
+    site: {
+      key: "site",
+      header: t("Site"),
+      cell: (action) => <Cell text={action.site} />,
+    },
+  } satisfies Record<string, ActionColumnDefinition>
+}
---
+++ front/src/traceability/hooks/use-action-filters.tsx
@@ -0,0 +1,55 @@
+import { useTranslation } from "react-i18next"
+
+import { Normalizer } from "common/utils/normalize"
+import { EntityManager } from "common/hooks/entity"
+import { ActionFilter } from "traceability/types"
+
+export type ActionFilterDisplay = {
+  key: ActionFilter
+  label: string
+  normalizer?: Normalizer<unknown, string>
+  condition?: (entity: EntityManager) => boolean
+}
+
+export function useActionFilters() {
+  const { t } = useTranslation()
+
+  return {
+    holder: {
+      key: ActionFilter.holder,
+      label: t("Détenteur"),
+    },
+    industry: {
+      key: ActionFilter.industry,
+      label: t("Filière"),
+    },
+    material: {
+      key: ActionFilter.material,
+      label: t("Matière"),
+    },
+    order_by: {
+      key: ActionFilter.order_by,
+      label: t("Tri"),
+    },
+    shipping_method: {
+      key: ActionFilter.shipping_method,
+      label: t("Mode de transport"),
+    },
+    site: {
+      key: ActionFilter.site,
+      label: t("Site"),
+    },
+    status: {
+      key: ActionFilter.status,
+      label: t("Statut"),
+    },
+    type: {
+      key: ActionFilter.type,
+      label: t("Type"),
+    },
+    working_year: {
+      key: ActionFilter.working_year,
+      label: t("Année"),
+    },
+  } satisfies Record<ActionFilter, ActionFilterDisplay>
+}
