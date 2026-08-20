e5d8e12fb1a6aec61754e67738d05931b0408f49
Julien Falxa <jfalxa.sta@gmail.com>
2026-08-20 12:06:28 +0200

    feat(h2): show h2 action details in modal

--- front/src/h2/pages/lots/index.tsx
+++ front/src/h2/pages/lots/index.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from "react-i18next"
 import { ActionsPage } from "traceability/components/actions-page"
 import { useActionColumns } from "traceability/hooks/use-action-columns"
 import { useActionFilters } from "traceability/hooks/use-action-filters"
+import { useActionFields } from "traceability/hooks/use-action-fields"
 import { ActionIndustry, ActionQuery, ActionType } from "traceability/types"
 
 const H2_LOT_QUERY: Partial<ActionQuery> = {
@@ -13,8 +14,9 @@ const H2_LOT_QUERY: Partial<ActionQuery> = {
 const LotsPage = () => {
   const { t } = useTranslation()
 
-  const columns = useActionColumns()
   const filters = useActionFilters()
+  const columns = useActionColumns()
+  const fields = useActionFields()
 
   return (
     <ActionsPage
@@ -33,6 +35,27 @@ const LotsPage = () => {
         { ...columns.material, header: t("Nature d'H2") },
         columns.quantity,
         { ...columns.site, header: t("Station") },
+      ]}
+      fields={[
+        fields.id,
+        fields.holder,
+        fields.industry,
+        fields.material,
+        fields.quantity,
+        fields.site,
+        fields.status,
+        fields.pos_id,
+        fields.type,
+        fields.shipping_date,
+        fields.shipping_distance,
+        fields.shipping_method,
+        fields.working_date,
+        fields.parent,
+        fields.ei,
+        fields.ep,
+        fields.etd,
+        fields.eu,
+        fields.eccs,
       ]}
     />
   )
