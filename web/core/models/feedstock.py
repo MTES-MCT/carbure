f57aed07e768d32daba00fcd33b9d718bd3f197f
Julien Falxa <jfalxa.sta@gmail.com>
2026-08-24 19:32:45 +0200

    feat(biofuels): add compatible_gpl column to biofuel table

--- web/core/models/feedstock.py
+++ web/core/models/feedstock.py
@@ -19,6 +19,7 @@ class Biocarburant(models.Model):
     compatible_diesel = models.BooleanField(default=False)
     dgddi_category = models.CharField(max_length=8, blank=True, null=True, default=None)
     renewable_energy_share = models.FloatField(blank=True, null=True, help_text="saisir 0,50 pour 50%", default=1.0)
+    compatible_gpl = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
