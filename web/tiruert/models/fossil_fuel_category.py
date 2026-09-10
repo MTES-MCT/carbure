cb7c330d4eff6e0ad6d0dc34865a0f2e2dfa2b00
Yohann <yohann.ulreich@beta.gouv.fr>
2026-09-10 15:36:19 +0200

    feat(iric): new sector 'maritime' and new field 'pci_kg' in FossilFuelCategory

--- web/tiruert/models/fossil_fuel_category.py
+++ web/tiruert/models/fossil_fuel_category.py
@@ -4,6 +4,7 @@ from django.db import models
 class FossilFuelCategory(models.Model):
     name = models.CharField(max_length=255)
     pci_litre = models.FloatField()
+    pci_kg = models.FloatField(default=0)
 
     def __str__(self):
         return self.name
