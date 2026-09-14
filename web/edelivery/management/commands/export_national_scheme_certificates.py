7ea3761eb767e032e12ebdf8e1d24857cbe55025
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2026-09-14 20:11:42 +0200

    Introduce `GetOrganisationByIDRequest`

--- web/edelivery/management/commands/export_national_scheme_certificates.py
+++ web/edelivery/management/commands/export_national_scheme_certificates.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand
 
-from core.models import EntityCertificate
+from core.models.certificate import EntityCertificate
+from core.models.entity import Entity
 from edelivery.ebms.requests.add_update_certificate_request import AddUpdateCertificateRequest
+from edelivery.ebms.requests.get_organisation_by_id_request import GetOrganisationByIDRequest
 from edelivery.soap.requester import Requester
 
 
@@ -17,8 +19,20 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        ec = EntityCertificate.objects.get(entity__name=options["entity_name"])
-        r_add_certificate = AddUpdateCertificateRequest(ec)
-        rq = Requester(r_add_certificate, timeout=30)
-        result = rq.do_request()
+        def entity_not_in_udb(entity):
+            self.stdout.write(f"Checking if entity '{entity.name}' present in UDB…")
+            request = GetOrganisationByIDRequest(entity.ntr_id())
+            requester = Requester(request, timeout=30)
+            result = requester.do_request()
+            return "error" in result
+
+        entity = Entity.objects.get(name=options["entity_name"])
+        if entity_not_in_udb(entity):
+            self.stderr.write(f"Error: Entity '{entity.name}' needs to be present in UDB.")
+            exit(1)
+
+        ec = EntityCertificate.objects.get(entity=entity)
+        request = AddUpdateCertificateRequest(ec)
+        requester = Requester(request, timeout=30)
+        result = requester.do_request()
         self.stdout.write(str(result))
