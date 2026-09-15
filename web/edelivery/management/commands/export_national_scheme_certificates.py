93de876d2c5e885d863ba56e241a3b28bd8871fc
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2026-09-15 11:44:17 +0200

    Introduce `AddOrganisationRequest`

--- web/edelivery/management/commands/export_national_scheme_certificates.py
+++ web/edelivery/management/commands/export_national_scheme_certificates.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 
 from core.models.certificate import EntityCertificate
 from core.models.entity import Entity
+from edelivery.ebms.requests.add_organisation_request import AddOrganisationRequest
 from edelivery.ebms.requests.add_update_certificate_request import AddUpdateCertificateRequest
 from edelivery.ebms.requests.get_organisation_by_id_request import GetOrganisationByIDRequest
 from edelivery.soap.requester import Requester
@@ -26,13 +27,22 @@ class Command(BaseCommand):
             result = requester.do_request()
             return "error" in result
 
+        def export_entity(entity):
+            self.stdout.write("Creating entity in UDB…")
+            request = AddOrganisationRequest(entity)
+            requester = Requester(request, timeout=30)
+            requester.do_request()
+
+        def export_certificate(entity):
+            ec = EntityCertificate.objects.get(entity=entity)
+            self.stdout.write(f"Exporting certificate '{ec.certificate.certificate_id}'…")
+            request = AddUpdateCertificateRequest(ec)
+            requester = Requester(request, timeout=30)
+            return requester.do_request()
+
         entity = Entity.objects.get(name=options["entity_name"])
         if entity_not_in_udb(entity):
-            self.stderr.write(f"Error: Entity '{entity.name}' needs to be present in UDB.")
-            exit(1)
+            export_entity(entity)
 
-        ec = EntityCertificate.objects.get(entity=entity)
-        request = AddUpdateCertificateRequest(ec)
-        requester = Requester(request, timeout=30)
-        result = requester.do_request()
+        result = export_certificate(entity)
         self.stdout.write(str(result))
