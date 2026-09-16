c80aafb557e78e30bd227f8cab77c598c2e36224
Emmanuel Gaillot <emmanuel.gaillot@beta.gouv.fr>
2026-09-16 18:51:02 +0200

    Add --dry_run option to certificates export script

--- web/edelivery/management/commands/export_national_scheme_certificates.py
+++ web/edelivery/management/commands/export_national_scheme_certificates.py
@@ -19,34 +19,62 @@ class Command(BaseCommand):
             help="export certificate for given entity",
         )
 
+        parser.add_argument(
+            "--dry_run",
+            action="store_true",
+            help="Dry run the export: do checks and print what UDB request would be sent",
+        )
+
     def handle(self, *args, **options):
-        def entity_not_in_udb(entity):
+        def entity_not_in_udb(entity, dry_run):
+            if dry_run:
+                self.stdout.write(f"Would check if '{entity.name}' present in UDB… (assuming not)")
+                return True
+
             self.stdout.write(f"Checking if entity '{entity.name}' present in UDB…")
             request = GetOrganisationByIDRequest(entity.ntr_id())
             requester = Requester(request, timeout=30)
             result = requester.do_request()
             return "error" in result
 
-        def export_entity(entity):
+        def export_entity(entity, dry_run):
+            if dry_run:
+                self.stdout.write("Would create entity in UDB…")
+                return
+
             self.stdout.write("Creating entity in UDB…")
             request = AddOrganisationRequest(entity)
             requester = Requester(request, timeout=30)
             requester.do_request()
 
-        def export_certificates(entity):
+        def export_certificates(entity, dry_run):
             entity_certificates = EntityCertificate.objects.filter(entity=entity)
             entity_certificate_ids = ", ".join([f"'{ec.certificate.certificate_id}'" for ec in entity_certificates])
+
+            if dry_run:
+                self.stdout.write(
+                    f"Would export certificate{'s' if len(entity_certificates) > 1 else ''} {entity_certificate_ids}…"
+                )
+
+            request = AddUpdateCertificateRequest(*entity_certificates)
+            if dry_run:
+                self.stdout.write(f"""\
+Would send UDB request with body being:
+{request.body}
+…""")
+                return {"result": "Got to the end of the dry run"}
+
             self.stdout.write(
                 f"Exporting certificate{'s' if len(entity_certificates) > 1 else ''} {entity_certificate_ids}…"
             )
-            request = AddUpdateCertificateRequest(*entity_certificates)
             requester = Requester(request, timeout=30)
             result = requester.do_request()
             return result
 
+        dry_run = options["dry_run"]
         entity = Entity.objects.get(name=options["entity_name"])
-        if entity_not_in_udb(entity):
-            export_entity(entity)
+        if entity_not_in_udb(entity, dry_run):
+            export_entity(entity, dry_run)
 
-        result = export_certificates(entity)
+        result = export_certificates(entity, dry_run)
         self.stdout.write(str(result))
