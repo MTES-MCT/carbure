57594591f0eedcaa622ef6bfe7ad1948c39acdb8
Yohann <yohann.ulreich@beta.gouv.fr>
2026-09-23 10:27:11 +0200

    feat(maritime): typo in formatSector

--- front/src/accounting/utils/formatters.ts
+++ front/src/accounting/utils/formatters.ts
@@ -37,7 +37,7 @@ export const formatSector = (sector: string) => {
       return i18next.t("Carburéacteur")
     case OperationSector.GPL_C:
       return i18next.t("GPL-c")
-    case "MARITIME":
+    case OperationSector.MARITIME:
       return i18next.t("Maritime")
     case ElecOperationSector.ELEC:
       return i18next.t("Électricité")
