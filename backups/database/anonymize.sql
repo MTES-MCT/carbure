dfc2c0db5e57c66bbd890fa720eb0310c8e65974
Benjamin Grilleres <contact@benjamingrilleres.com>
2026-09-24 09:24:39 +0200

    feat(backups): anonymize user passwords and production site managers

--- backups/database/anonymize.sql
+++ backups/database/anonymize.sql
@@ -4,10 +4,17 @@
 UPDATE authtools_user
 SET
   email = CONCAT('user', id, '@anonymized.local'),
-  name = CONCAT('Utilisateur ', id);
+  name = CONCAT('Utilisateur ', id),
+  password = "anonymized-password";
 
 UPDATE entities
 SET
   sustainability_officer = CONCAT('Contact ', id),
   sustainability_officer_email = CONCAT('contact', id, '@anonymized.local'),
   sustainability_officer_phone_number = '';
+
+UPDATE sites_productionsites
+SET
+  manager_name = CONCAT('Manager ', site_ptr_id),
+  manager_email = CONCAT('manager', site_ptr_id, '@anonymized.local'),
+  manager_phone = '';
